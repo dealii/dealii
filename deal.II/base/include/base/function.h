32d98f36ee0ef6023a9da72f6ed962dd065b2057
Wolfgang Bangerth <bangerth@math.tamu.edu>
1998-03-19 10:33:06 +0000

    Offer general function object.
    git-svn-id: https://svn.dealii.org/trunk@86 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/base/include/base/function.h
@@ -0,0 +1,29 @@
+/*----------------------------   function.h     ---------------------------*/
+/*      <Id:>                 */
+#ifndef __function_H
+#define __function_H
+/*----------------------------   function.h     ---------------------------*/
+
+
+//forward declaration
+template <int dim> class Point;
+template <class T> class vector;
+
+
+
+
+template <int dim>
+class Function {
+  public:
+    virtual double operator () (const Point<dim> &p) const = 0;
+    virtual void value_list (const vector<Point<dim> > &points,
+			     vector<double>            &values) const;
+};
+
+
+
+
+/*----------------------------   function.h     ---------------------------*/
+/* end of #ifndef __function_H */
+#endif
+/*----------------------------   function.h     ---------------------------*/
