caae59cd82641a6d9a6d5f369ac45a76a1e64c5e
Wolfgang Bangerth <bangerth@math.tamu.edu>
1998-05-18 16:46:21 +0000

    Add time dependant functionality to the Function class.
    git-svn-id: https://svn.dealii.org/trunk@310 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/function.cc
+++ deal.II/base/source/function.cc
@@ -6,7 +6,14 @@
 
 
 template <int dim>
+Function<dim>::Function (const double initial_time) :
+		time(initial_time) {};
+
+
+
+template <int dim>
 Function<dim>::~Function () {};
+
 
 
 template <int dim>
@@ -49,6 +56,25 @@ void Function<dim>::gradient_list (const vector<Point<dim> > &points,
 };
 
 
+
+template <int dim>
+double Function<dim>::get_time () const {
+  return time;
+};
+
+
+
+template <int dim>
+void Function<dim>::set_time (const double new_time) {
+  time = new_time;
+};
+
+
+
+template <int dim>
+void Function<dim>::advance_time (const double delta_t) {
+  set_time (time+delta_t);
+};
 
 
 
