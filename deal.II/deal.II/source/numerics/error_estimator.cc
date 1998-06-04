430ce45547be42475f350ef662be468bdeb487a8
Wolfgang Bangerth <bangerth@math.tamu.edu>
1998-06-04 23:30:09 +0000

    Fix bug.
    git-svn-id: https://svn.dealii.org/trunk@380 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/numerics/error_estimator.cc
+++ deal.II/deal.II/source/numerics/error_estimator.cc
@@ -78,8 +78,10 @@ void KellyErrorEstimator<dim>::estimate_error (const DoFHandler<dim>    &dof,
 				   // neighbor cells, so using two objects
 				   // gives us a performance gain).
   FEFaceValues<dim> fe_face_values_cell (fe, quadrature,
-					 UpdateFlags(update_gradients  |
-						     update_JxW_values |
+					 UpdateFlags(update_gradients      |
+						     update_JxW_values     |
+						     (neumann_bc.empty() ?
+						      0 : update_q_points) |
 						     update_normal_vectors)); 
   FEFaceValues<dim> fe_face_values_neighbor (fe, quadrature, update_gradients);
   FESubfaceValues<dim> fe_subface_values (fe, quadrature, update_gradients);
