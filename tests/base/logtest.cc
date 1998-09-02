0aef911eec9596f99bd9bc27510a482ba36bbec8
Guido Kanschat <dr.guido.kanschat@gmail.com>
1998-09-02 16:45:26 +0000

    Initial revision
    git-svn-id: https://svn.dealii.org/trunk@543 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/logtest.cc
@@ -0,0 +1,20 @@
+#include "../newdeal/base/include/base/logstream.h"
+
+LogStream deallog;
+
+main()
+{
+  deallog << "Test" << endl;
+  deallog.push("l1");
+  deallog << "Test1" << endl;
+  deallog.push("l2");
+  deallog << "Test2" << "Test3" << endl;
+  deallog.push("l3");
+  deallog << "Test4";
+  deallog.pop();
+  deallog << "Test5" << endl;
+  deallog.pop();
+  deallog << "Test6" << endl;
+  deallog.pop();
+  deallog << "Test7" << endl;
+}
