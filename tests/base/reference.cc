7c2fe09bd262bd13cfc5bb4fa0b12402b1e89818
Wolfgang Bangerth <bangerth@math.tamu.edu>
1998-09-03 12:32:12 +0000

    Comment example.
    git-svn-id: https://svn.dealii.org/trunk@550 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/reference.cc
+++ tests/base/reference.cc
@@ -1,7 +1,8 @@
 
 #include <iostream>
-#include <base/exceptions.h>
 #include <base/subscriptor.h>
+
+
 
 class Test : public Subscriptor
 {
@@ -16,19 +17,23 @@ public:
   }
 };
 
+
+
 main()
 {
   Test a;
   const Test b;
-  SmartPointer<Test> r=&a;
-  const SmartPointer<Test> s=&a;
-  SmartPointer<Test> t=&b;
-  const SmartPointer<Test> u=&b;
+  SmartPointer<Test>       r=&a;
+  SmartPointer<const Test> s=&a;
+  SmartPointer<Test>       t=&b;    // this one should give a warning
+  SmartPointer<const Test> u=&b;
 
-  a.f();
-  b.f();
-  r->f();
-  s->f();
-  t->f();
-  u->f();
+  a.f();            // should print "mutable", since #a# is not const
+  b.f();            // should print "const", since #b# is const
+  r->f();           // should print "mutable", since it points to the non-const #a#
+  s->f();           // should print "const", since it points to the non-const #a#
+				   // but we made it const
+  t->f();           // should print "mutable", since #b# is const, but
+				   // we casted the constness away
+  u->f();           // should print "const" since #b# is const
 }
