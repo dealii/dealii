ba0d95028f99cf5edeb83856a99fd2d4224ee064
Guido Kanschat <dr.guido.kanschat@gmail.com>
1998-09-04 14:37:07 +0000

    Separate file for SmartReference
    git-svn-id: https://svn.dealii.org/trunk@567 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/base/include/base/smartreference.h
@@ -0,0 +1,50 @@
+/*----------------------------   smartreference.h     ---------------------------*/
+/*      $Id$                 */
+#ifndef __smartreference_H
+#define __smartreference_H
+/*----------------------------   smartreference.h     ---------------------------*/
+
+#include <base/subscriptor.h>
+
+/**
+ * Smart references avoid destruction of a referenced object.  This
+ * class has not been fully developed, since the compiler could not
+ * resolve the dot operator in a convenient manner. The use of
+ * #SmartPointer# is recommended, instead.
+ */
+template<class T>
+class SmartReference
+{
+    T& t;
+
+  public:
+				     /**
+				      * Constructor taking a normal reference.
+				      */
+    SmartReference(const T& tt)
+		    : t(tt) 
+      {
+	t.subscribe();
+      }
+    
+				     /**
+				      * Destructor, removing the subscription.
+				      */
+    ~SmartReference()
+      {
+	t.unsubscribe();
+      }
+  
+				     /**
+				      * Conversion to normal reference
+				      */
+    operator T& () const
+      {
+	return t;
+      }
+};
+
+/*----------------------------   smartreference.h     ---------------------------*/
+/* end of #ifndef __smartreference_H */
+#endif
+/*----------------------------   smartreference.h     ---------------------------*/
