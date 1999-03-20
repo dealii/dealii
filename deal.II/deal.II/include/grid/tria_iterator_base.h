527e48017f383f5ded5c464b0109bbe6cb4c26e0
Wolfgang Bangerth <bangerth@math.tamu.edu>
1999-03-20 15:00:38 +0000

    Add a file which I forgot yesterday.
    git-svn-id: https://svn.dealii.org/trunk@1032 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/deal.II/include/grid/tria_iterator_base.h
@@ -0,0 +1,19 @@
+/*----------------------------   tria_iterator_base.h     ---------------------------*/
+/*      $Id$                 */
+#ifndef __tria_iterator_base_H
+#define __tria_iterator_base_H
+/*----------------------------   tria_iterator_base.h     ---------------------------*/
+
+
+/**
+ *   The three states an iterator can be in: valid, past-the-end and
+ *   invalid.
+ */
+enum IteratorState { valid, past_the_end, invalid };
+
+
+
+/*----------------------------   tria_iterator_base.h     ---------------------------*/
+/* end of #ifndef __tria_iterator_base_H */
+#endif
+/*----------------------------   tria_iterator_base.h     ---------------------------*/
