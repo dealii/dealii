a3cee3d01a76bf1f3bd9984a44d451d3ab69835e
Guido Kanschat <dr.guido.kanschat@gmail.com>
1999-05-11 15:38:54 +0000

    Programs to show transformation and shape functions
    git-svn-id: https://svn.dealii.org/trunk@1312 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fe/show_shapes.cc
@@ -0,0 +1,61 @@
+// $Id$
+// (c) Guido Kanschat
+//
+// Show the shape functions implemented.
+
+#include <base/quadrature_lib.h>
+#include <lac/vector.h>
+#include <grid/tria.h>
+#include <grid/tria_iterator.h>
+#include <grid/dof_accessor.h>
+#include <grid/grid_generator.h>
+#include <fe/fe_lib.lagrange.h>
+#include <fe/fe_values.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+char fname[50];
+
+template<int dim>
+inline void
+plot_shape_functions(FiniteElement<dim>& finel, const char* name)
+{
+  const unsigned int div = 20;
+
+  QIteratedTrapez<2> q(div);
+  FEValues<dim> fe(finel, q, UpdateFlags(update_default));
+
+  sprintf(fname, "%s.dat", name);
+  ofstream gnuplot(fname);
+
+  unsigned int k=0;
+  for (unsigned int m=0;m<=div;++m)
+    {
+      for (unsigned int n=0;n<=div;++n)
+	{
+	  gnuplot << q.quad_point(k);
+	  
+	  for (unsigned int i=0;i<finel.total_dofs;++i)
+	    {
+	      gnuplot << " "<< fe.shape_value(i,k);
+	    }
+	  gnuplot << endl;
+	  k++;
+	}
+      gnuplot << endl;
+    }     
+}
+
+
+main()
+{
+  FEQ1<2> q1;
+  plot_shape_functions(q1,"Q1");
+  FEQ2<2> q2;
+  plot_shape_functions(q2,"Q2");
+  FEQ3<2> q3;
+  plot_shape_functions(q3,"Q3");
+  FEQ4<2> q4;
+  plot_shape_functions(q4,"Q4");
+}
---
+++ tests/fe/show_transform.cc
@@ -0,0 +1,69 @@
+// $Id$
+// (c) Guido Kanschat
+//
+// Show the shape functions implemented.
+
+#include <base/quadrature_lib.h>
+#include <lac/vector.h>
+#include <grid/tria.h>
+#include <grid/tria_iterator.h>
+#include <grid/dof_accessor.h>
+#include <grid/grid_generator.h>
+#include <fe/fe_lib.lagrange.h>
+#include <fe/fe_values.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+char fname[50];
+
+template<int dim>
+inline void
+plot_transformation(FiniteElement<dim>& finel,
+		    DoFHandler<dim>::cell_iterator& c,
+		    const char* name)
+{
+  const unsigned int div = 20;
+
+  QIteratedTrapez<dim> q(div);
+  FEValues<dim> fe(finel, q,
+		   UpdateFlags(update_q_points | update_JxW_values));
+
+  fe.reinit(c);
+  
+  sprintf(fname, "%s.dat", name);
+  ofstream gnuplot(fname);
+
+  unsigned int k=0;
+  for (unsigned int m=0;m<=div;++m)
+    {
+      for (unsigned int n=0;n<=div;++n)
+	{
+	  gnuplot << fe.quadrature_point(k);
+	  double J = fe.JxW(k) / q.weight(k);
+	  gnuplot << ' ' << J << endl;
+	  k++;
+	}
+      gnuplot << endl;
+    }     
+}
+
+
+main()
+{
+  Triangulation<2> tr;
+  FEQ1<2> q1;
+  DoFHandler<2> dof(&tr);
+
+  GridGenerator::hyper_cube(tr, -1., 1.);
+  dof.distribute_dofs(q1);
+  
+  DoFHandler<2>::cell_iterator c = dof.begin();
+
+  Point<2>& v = c->vertex(2);
+  
+  v(0) = 3.;
+  v(1) = 2.;
+  
+  plot_transformation(q1,c,"Transform-Q1");
+}
