be98da65a31e984a0f04fbc98035b84374ccd533
Ralf Hartmann <Ralf.Hartmann@dlr.de>
1999-06-28 14:45:06 +0000

    Revert Guido's changes on typename issues (why were they?)
    git-svn-id: https://svn.dealii.org/trunk@1484 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/solver_bicgstab.h
+++ deal.II/lac/include/lac/solver_bicgstab.h
@@ -49,11 +49,11 @@ class SolverBicgstab : public Solver<Matrix,Vector>
 				      * Solve primal problem only.
 				      */
     template<class Preconditioner>
-				     /*typename*/
-    Solver<Matrix,Vector>::ReturnState solve (const Matrix &A,
-						       Vector       &x,
-						       const Vector &b,
-						       const Preconditioner& precondition);
+    typename Solver<Matrix,Vector>::ReturnState
+    solve (const Matrix &A,
+	   Vector       &x,
+	   const Vector &b,
+	   const Preconditioner& precondition);
 
   protected:
 				     /**
@@ -148,7 +148,8 @@ class SolverBicgstab : public Solver<Matrix,Vector>
 				      * The iteration loop itself.
 				      */
     template<class Preconditioner>
-    typename Solver<Matrix,Vector>::ReturnState iterate(const Preconditioner& precondition);
+    typename Solver<Matrix,Vector>::ReturnState 
+    iterate(const Preconditioner& precondition);
   
 };
 
@@ -186,7 +187,7 @@ SolverBicgstab<Matrix, Vector>::start()
 
 template<class Matrix, class Vector>
 template<class Preconditioner>
-/*typename*/ Solver<Matrix,Vector>::ReturnState
+typename Solver<Matrix,Vector>::ReturnState
 SolverBicgstab<Matrix, Vector>::iterate(const Preconditioner& precondition)
 {
   SolverControl::State state = SolverControl::iterate;
