cec503f2ac4879c11c28c455e2dc762bbcc05aa3
Wolfgang Bangerth <bangerth@math.tamu.edu>
1999-07-15 12:16:36 +0000

    Fix really serious problem. (non-compilable)
    git-svn-id: https://svn.dealii.org/trunk@1580 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/dofs/dof_tools.cc
+++ deal.II/deal.II/source/dofs/dof_tools.cc
@@ -32,8 +32,7 @@ DoFTools::extract_dofs(const DoFHandler<dim> &dof,
       c->get_dof_indices(indices);
       for (unsigned int i=0;i<fe.total_dofs;++i)
 	{
-	  const pair<unsigned int, unsigned int> component
-	    = fe.system_to_component_index(i).first;
+	  const unsigned int component = fe.system_to_component_index(i).first;
 
 	  if (local_select[component] == true)
 	    selected_dofs[indices[i]] = true;
@@ -64,8 +63,7 @@ DoFTools::extract_level_dofs(const unsigned int       level,
       c->get_mg_dof_indices(indices);
       for (unsigned int i=0;i<fe.total_dofs;++i)
 	{
-	  const pair<unsigned int, unsigned int> component
-	    = fe.system_to_component_index(i).first;
+	  const unsigned int component = fe.system_to_component_index(i).first;
 	  if (local_select[component]  == true)
 	    selected_dofs[indices[i]] = true;
 	}
