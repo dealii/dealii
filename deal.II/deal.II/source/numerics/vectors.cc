2bd4df9d5df9d5e2c11705f1babb78fd39aee7f7
Wolfgang Bangerth <bangerth@math.tamu.edu>
1999-12-23 11:10:17 +0000

    Externalize this function since it does not depend on the dimension and therefore leads to problems when linking with libdeal_II_2d and libdeal_II_3d at the same time.
    git-svn-id: https://svn.dealii.org/trunk@2121 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/numerics/vectors.cc
+++ deal.II/deal.II/source/numerics/vectors.cc
@@ -951,31 +951,6 @@ VectorTools::integrate_difference (const DoFHandler<dim>    &dof,
 
 
 
-void
-VectorTools::subtract_mean_value(Vector<double>     &v,
-				 const vector<bool> &p_select)
-{
-  unsigned int n = v.size();
-  Assert(n == p_select.size(), ExcDimensionMismatch(n, p_select.size()));
-
-  double       s       = 0;
-  unsigned int counter = 0;
-  
-  for (unsigned int i=0; i<n; ++i)
-    if (p_select[i])
-      {
-	s += v(i);
-	++counter;
-      };
-
-  s /= counter;
-  
-  for (unsigned int i=0; i<n; ++i)
-    if (p_select[i])
-      v(i) -= s;  
-};
-
-
 
 // explicit instantiations
 template
