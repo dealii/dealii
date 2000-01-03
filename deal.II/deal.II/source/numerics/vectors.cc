ac68bb01f301506f0e41f23086e07eaa5cb7b733
Wolfgang Bangerth <bangerth@math.tamu.edu>
2000-01-03 13:13:37 +0000

    Make this file compilable in 1d as well.
    git-svn-id: https://svn.dealii.org/trunk@2150 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/numerics/vectors.cc
+++ deal.II/deal.II/source/numerics/vectors.cc
@@ -276,11 +276,11 @@ void VectorTools::interpolate (const DoFHandler<dim> &dof,
 
 
 template <int dim> void
-VectorTools::interpolate(const DoFHandler<dim>           &high_dof,
-			 const DoFHandler<dim>           &low_dof,
-			 const FullMatrix<double>        &transfer,
-			 const Vector<double>            &high,
-			 Vector<double>                  &low)
+VectorTools::interpolate (const DoFHandler<dim>           &high_dof,
+			  const DoFHandler<dim>           &low_dof,
+			  const FullMatrix<double>        &transfer,
+			  const Vector<double>            &high,
+			  Vector<double>                  &low)
 {
   Vector<double> cell_high(high_dof.get_fe().dofs_per_cell);
   Vector<double> cell_low(low_dof.get_fe().dofs_per_cell);
@@ -966,26 +966,12 @@ void VectorTools::project_boundary_values (const DoFHandler<deal_II_dimension>  
 					   const map<unsigned char,const Function<deal_II_dimension>*>&,
 					   const Quadrature<deal_II_dimension-1>&,
 					   map<int,double>        &);
-template
-void VectorTools::interpolate_boundary_values (const DoFHandler<deal_II_dimension> &,
-					       const unsigned char,
-					       const Function<deal_II_dimension>   &,
-					       map<int,double>       &,
-					       const vector<bool>    &);
+
 template
 void VectorTools::create_right_hand_side (const DoFHandler<deal_II_dimension> &,
 					const Quadrature<deal_II_dimension>   &,
 					const Function<deal_II_dimension>     &,
 					Vector<double>                        &);
-template
-void VectorTools::project (const DoFHandler<deal_II_dimension>   &,
-			   const ConstraintMatrix                &,
-			   const Quadrature<deal_II_dimension>   &,
-			   const Function<deal_II_dimension>     &,
-			   Vector<double>                        &,
-			   const bool,
-			   const Quadrature<deal_II_dimension-1> &,
-			   const bool);
 template
 void VectorTools::interpolate(const DoFHandler<deal_II_dimension> &,
 			      const DoFHandler<deal_II_dimension> &,
@@ -996,3 +982,23 @@ template
 void VectorTools::interpolate (const DoFHandler<deal_II_dimension> &,
 			       const Function<deal_II_dimension>   &,
 			       Vector<double>                      &);
+
+// the following two functions are not derived from a template in 1d
+// and thus need no explicit instantiation
+#if deal_II_dimension > 1
+template
+void VectorTools::interpolate_boundary_values (const DoFHandler<deal_II_dimension> &,
+					       const unsigned char,
+					       const Function<deal_II_dimension>   &,
+					       map<int,double>       &,
+					       const vector<bool>    &);
+template
+void VectorTools::project (const DoFHandler<deal_II_dimension>   &,
+			   const ConstraintMatrix                &,
+			   const Quadrature<deal_II_dimension>   &,
+			   const Function<deal_II_dimension>     &,
+			   Vector<double>                        &,
+			   const bool,
+			   const Quadrature<deal_II_dimension-1> &,
+			   const bool);
+#endif
