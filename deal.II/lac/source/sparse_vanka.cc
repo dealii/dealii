4c7a504b4dc8161f7d22c3639a07345fd5567066
Wolfgang Bangerth <bangerth@math.tamu.edu>
2000-02-01 15:21:49 +0000

    Implement the block version of the Vanka smoother.
    git-svn-id: https://svn.dealii.org/trunk@2306 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/source/sparse_vanka.cc
+++ deal.II/lac/source/sparse_vanka.cc
@@ -11,3 +11,12 @@ template void SparseVanka<double>::operator () (Vector<float>       &dst,
 						const Vector<float> &src) const;
 template void SparseVanka<double>::operator () (Vector<double>       &dst,
 						const Vector<double> &src) const;
+
+
+template class SparseBlockVanka<float>;
+template class SparseBlockVanka<double>;
+
+template void SparseBlockVanka<double>::operator () (Vector<float>       &dst,
+						     const Vector<float> &src) const;
+template void SparseBlockVanka<double>::operator () (Vector<double>       &dst,
+						     const Vector<double> &src) const;
