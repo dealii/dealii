cec5e66ae1722c789ca83d3c69f9090101ac88f5
Wolfgang Bangerth <bangerth@math.tamu.edu>
2000-02-16 12:44:12 +0000

    Add a script that validates whether cross-references in out documentation are valid or stale. It does not check external references, but it can check references within our directory tree.
    git-svn-id: https://svn.dealii.org/trunk@2413 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/auto/scripts/validate-xrefs.pl
@@ -0,0 +1,84 @@
+# $Id$
+# Check whether references in HTML files are valid or 
+# point to non-existing files/links/etc
+#
+# Author: Wolfgang Bangerth, 2000
+
+
+# set this to 1 if you want verbose output
+$debug = 0;
+
+foreach $filename (@ARGV) {
+    open IN, $filename
+        or die "---Can't open file `$filename'\n";
+
+    print "File: $filename\n" if $debug;
+    while (<IN>) {
+        # first find all hrefs
+        while ( /href=\"?(.*?)[\s\"]/g ) {
+	    # then decide whether they are relevant for 
+            # our purpose
+	    $link = $1;
+    
+	    if ( $link =~ /^mailto|http/ ) {
+	        # link is external. don't check it
+	        print "external link: $link\n" if $debug;
+	        next;
+	    }
+	    elsif ( $link =~ /^#(.*)/ )
+	    {
+		# this is a reference within this file. try to 
+	        # find its anchor
+	        $internal_ref = $1;
+	        print "internal reference: $link\n" if $debug;
+	    
+	        open IN2, $filename;
+	        $found = 0;
+	        while ( <IN2> ) {
+		    if ( /<a name=\"?(.*?)[\s\"]/ ) {
+		        if ( $1 eq $internal_ref) {
+			    print "                    found.\n" if $debug;
+			    $found = 1;
+			    last;
+			}
+		    }
+		}
+		
+		die "---Internal reference `$internal_ref' not found in file $filename\n"
+		    unless $found;
+		next;
+	    }
+	    elsif ( $link =~ /^(.*?)#(.*)/ )
+	    {
+		# this is a reference within another file. try to 
+		# find its anchor
+		$external_file = $1;
+		$external_ref = $2;
+		print "external reference: $link\n" if $debug;
+		
+		open IN2, $external_file;
+		$found = 0;
+		while ( <IN2> ) {
+		    if ( /<a name=\"?(.*?)[\s\"]/ ) {
+			if ( $1 eq $external_ref) {
+			    print "                    found.\n" if $debug;
+			    $found = 1;
+			    last;
+			}
+		    }
+		}
+		
+		die "---External reference `$internal_ref' not found in file $filename\n"
+		    unless $found;
+		next;
+	    }
+	    else {
+		# this must now be a regular file which is
+		# referenced. the file must be local
+		die "---Local file `$link' not found in file `$filename'\n"
+		    unless ((-r $link) && (-f $link));
+	    }
+		}
+		}
+}
+
