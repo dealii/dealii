00fe29604e0799a76090f3ce30afa269ea65a960
Wolfgang Bangerth <bangerth@math.tamu.edu>
2000-02-21 08:56:03 +0000

    Use actual definitions of static variables.
    git-svn-id: https://svn.dealii.org/trunk@2445 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/grid/geometry_info.cc
+++ deal.II/deal.II/source/grid/geometry_info.cc
@@ -3,13 +3,11 @@
 #include <grid/geometry_info.h>
 
 
-// enable these lines for gcc2.95
-//
-//const unsigned int GeometryInfo<deal_II_dimension>::vertices_per_cell;
-//const unsigned int GeometryInfo<deal_II_dimension>::lines_per_cell;
-//const unsigned int GeometryInfo<deal_II_dimension>::quads_per_cell;
-//const unsigned int GeometryInfo<deal_II_dimension>::hexes_per_cell;
-//const unsigned int GeometryInfo<deal_II_dimension>::children_per_cell;
+const unsigned int GeometryInfo<deal_II_dimension>::vertices_per_cell;
+const unsigned int GeometryInfo<deal_II_dimension>::lines_per_cell;
+const unsigned int GeometryInfo<deal_II_dimension>::quads_per_cell;
+const unsigned int GeometryInfo<deal_II_dimension>::hexes_per_cell;
+const unsigned int GeometryInfo<deal_II_dimension>::children_per_cell;
 
 
 template <>
