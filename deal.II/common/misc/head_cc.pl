b45e21e233fc65730bc68bdbce901dda3eaea4b5
Guido Kanschat <dr.guido.kanschat@gmail.com>
2000-02-22 19:50:02 +0000

    add new heads
    git-svn-id: https://svn.dealii.org/trunk@2473 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/common/misc/head_cc.pl
@@ -0,0 +1,28 @@
+$/='this should not appear in any of these stupid files';
+
+$fn = $ARGV;
+
+$head = << 'EOT'
+//----------------------------  XXX  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 1998, 1999, 2000 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  XXX  ---------------------------
+
+
+EOT
+    ;
+
+$head =~ s/XXX/$fn/g;
+
+s/^[^\#]+/$head\n\n\n/s;
+s/\/\*-+\s+$fn\s+-+\*\/\n//g;
+s/\/\*\s*end of.*H\s*\*\///g;
+s/\n[ \t]*\n[ \t]*\n\s*\n*/\n\n\n/g;
---
+++ deal.II/common/misc/head_h.pl
@@ -0,0 +1,34 @@
+$/='this should not appear in any of these stupid files';
+
+$fn = $ARGV;
+$guard = '__deal2__' . $fn;
+
+$guard =~ s/\./_/g;
+
+$head = << 'EOT'
+//----------------------------  XXX  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 1998, 1999, 2000 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  XXX  ---------------------------
+#ifndef GGG
+#define GGG
+
+
+EOT
+    ;
+
+$head =~ s/XXX/$fn/g;
+$head =~ s/GGG/$guard/g;
+
+s/^.+\#define __[^\n]*/$head/s;
+s/\/\*-+\s+$fn\s+-+\*\/\n//g;
+s/\/\*\s*end of.*H\s*\*\///g;
+s/\n[ \t]*\n[ \t]*\n\s*\n*/\n\n\n/g;
