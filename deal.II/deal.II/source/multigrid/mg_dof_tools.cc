3fa34090ebcec9e9b055d344083086d2debc19d8
Guido Kanschat <dr.guido.kanschat@gmail.com>
2000-02-29 14:40:54 +0000

    configure multigrid and tests
    git-svn-id: https://svn.dealii.org/trunk@2516 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/multigrid/mg_dof_tools.cc
+++ deal.II/deal.II/source/multigrid/mg_dof_tools.cc
@@ -63,11 +63,11 @@ MGDoFTools::make_flux_sparsity_pattern (const MGDoFHandler<dim> &dof,
   const unsigned int dofs_per_cell = dof.get_fe().dofs_per_cell;
   vector<unsigned int> dofs_on_this_cell(dofs_per_cell);
   vector<unsigned int> dofs_on_other_cell(dofs_per_cell);
-  DoFHandler<dim>::cell_iterator cell = dof.begin(level),
-					endc = dof.end(level);
+  MGDoFHandler<dim>::cell_iterator cell = dof.begin(level),
+				   endc = dof.end(level);
   for (; cell!=endc; ++cell)
     {
-      cell->get_dof_indices (dofs_on_this_cell);
+      cell->get_mg_dof_indices (dofs_on_this_cell);
 				       // make sparsity pattern for this cell
       for (unsigned int i=0; i<dofs_per_cell; ++i)
 	for (unsigned int j=0; j<dofs_per_cell; ++j)
@@ -82,8 +82,8 @@ MGDoFTools::make_flux_sparsity_pattern (const MGDoFHandler<dim> &dof,
 	  if ( (! cell->at_boundary(face)) &&
 	       (static_cast<unsigned int>(cell->neighbor_level(face)) == level) )
 	    {
-	      DoFHandler<dim>::cell_iterator neighbor = cell->neighbor(face);
-	      neighbor->get_dof_indices (dofs_on_other_cell);
+	      MGDoFHandler<dim>::cell_iterator neighbor = cell->neighbor(face);
+	      neighbor->get_mg_dof_indices (dofs_on_other_cell);
 					       // only add one direction
 					       // The other is taken care of
 					       // by neighbor.
