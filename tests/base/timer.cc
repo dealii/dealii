a583a7df09ce159d27464b1ad3a56620b83c65d6
Guido Kanschat <dr.guido.kanschat@gmail.com>
2000-03-14 22:42:17 +0000

    value s must be initialized
    git-svn-id: https://svn.dealii.org/trunk@2585 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/timer.cc
+++ tests/base/timer.cc
@@ -37,7 +37,7 @@ void compare (double t1, double t2, double ratio)
 
 void burn (unsigned int n)
 {
-  double s;
+  double s = 0.;
   for (unsigned int i=0 ; i<n ; ++i)
     {
       for (unsigned int j=1 ; j<100000 ; ++j)
