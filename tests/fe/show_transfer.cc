0eaebcfe9d62752139f2ca56808e6b64dbd9cceb
Guido Kanschat <dr.guido.kanschat@gmail.com>
2000-03-15 22:56:09 +0000

    check for grid transfer operators
    git-svn-id: https://svn.dealii.org/trunk@2588 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fe/show_transfer.cc
@@ -0,0 +1,90 @@
+//----------------------------  show_transfer.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 1998, 1999, 2000 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  show_transfer.cc  ---------------------------
+//
+// Print multigrid transfer matrices between one and four cells.
+//
+//----------------------------  show_transfer.cc  ---------------------------
+
+#include <base/quadrature_lib.h>
+#include <lac/vector.h>
+#include <grid/tria.h>
+#include <grid/tria_iterator.h>
+#include <dofs/dof_accessor.h>
+#include <grid/grid_generator.h>
+#include <fe/fe_lib.lagrange.h>
+#include <fe/fe_lib.dg.h>
+#include <fe/fe_values.h>
+#include <multigrid/multigrid.h>
+#include <multigrid/mg_dof_handler.h>
+
+#include <vector>
+#include <fstream>
+#include <string>
+
+char fname[50];
+
+#define TEST(el) { el fe; print_matrix(of, tr, fe, #el); }
+
+template<int dim>
+inline void
+print_matrix(ostream& of,
+	     Triangulation<dim>& tr,
+	     const FiniteElement<dim>& finel,
+	     const char* name)
+{
+  MGDoFHandler<dim> dof(tr);
+  dof.distribute_dofs(finel);
+
+  MGTransferPrebuilt transfer;
+  transfer.build_matrices(dof);
+
+  unsigned int n_coarse = dof.n_dofs(0);
+  unsigned int n_fine = dof.n_dofs(1);
+  Vector<double> in(n_fine);
+  Vector<double> out(n_coarse);
+
+  of << name << endl;
+  for (unsigned int i=0;i<n_fine;++i)
+    {
+      in = 0.;
+      out = 0.;
+      in(i) = 1.;
+      transfer.restrict_and_add(1,out,in);
+      out.print(of, 3, false);
+    }
+  of << endl;
+}
+
+
+int
+main()
+{
+  Triangulation<2> tr;
+
+  GridGenerator::hyper_cube(tr, -1., 1.);
+  tr.refine_global(1);
+
+  ofstream of("transfer.dat");
+  
+  TEST(FEQ1<2>);
+  TEST(FEQ2<2>);
+  TEST(FEQ3<2>);
+  TEST(FEQ4<2>);
+
+  TEST(FEDG_Q0<2>);
+  TEST(FEDG_Q1<2>);
+  TEST(FEDG_Q2<2>);
+  TEST(FEDG_Q3<2>);
+  TEST(FEDG_Q4<2>);
+  return 0;
+}
