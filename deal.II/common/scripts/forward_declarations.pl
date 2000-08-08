edb05f2d21c049a961d7de1ad6d7075452355b5f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2000-08-08 15:36:08 +0000

    Also catch specializations of classes.
    git-svn-id: https://svn.dealii.org/trunk@3241 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/common/scripts/forward_declarations.pl
+++ deal.II/common/scripts/forward_declarations.pl
@@ -70,7 +70,7 @@ sub parse_class_declarations {
 	if ( /^\s*((template\s*<
                      (
                         ([-\w,_=\s]   |
-                         <([-\w_,=\s])+>  )+ 
+                         <([-\w_,=\s])+>  )*
                      )>\s*)?
                    (class|struct))(.*)/x ) {
 	    # this is the declaration of a class, possibly a template
