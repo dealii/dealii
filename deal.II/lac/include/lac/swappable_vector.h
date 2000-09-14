16e401d3e9c8a3ec677ec5ff27a027117fcf14c7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2000-09-14 09:53:29 +0000

    Add doc about problems with parallel i/o
    git-svn-id: https://svn.dealii.org/trunk@3322 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/swappable_vector.h
+++ deal.II/lac/include/lac/swappable_vector.h
@@ -169,6 +169,25 @@ class SwappableVector : public Vector<number>
 				      * this function while the data
 				      * is still or already in memory
 				      * is allowed and has no effect.
+				      *
+				      * It is noted that versions of
+				      * gcc up to at least version
+				      * 2.95.2 are not thread-safe in
+				      * the C++ standard
+				      * library. Thus, parallel in-
+				      * and/or output may sometimes
+				      * lead to unreproducable
+				      * crashes, segmentation faults,
+				      * or bus errors of programs. You
+				      * should therefore only call
+				      * this function if either you
+				      * are sure that the I/O library
+				      * distributed with your compiler
+				      * supports parallel
+				      * writing/reading, or if you are
+				      * sure that during the run-time
+				      * of this function no other I/O
+				      * operations are active.
 				      */
     void alert ();
     
