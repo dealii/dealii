d4d7df42ddc6ed8d3b66df78d1ae775f2453bc18
Guido Kanschat <dr.guido.kanschat@gmail.com>
2001-02-27 12:52:48 +0000

    Extract gnuplot from performance results
    git-svn-id: https://svn.dealii.org/trunk@4043 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fe/performance.pl
@@ -0,0 +1,28 @@
+# $Id$
+#
+# Copyright (C) 2001 by the deal.II authors
+#
+#
+# Make gnuplot table from output of performance
+#
+# Usage:
+#  perl performance.pl performance.log
+#
+# order of values per line is
+#  1. dofs per cell
+#  2. Time for generating quadrature points
+#  3. Time for generating shape values
+#  4. Time for generating shape gradients
+#  5. Time for generating shape second derivatives
+#  6. Time for generating typical mix for matrix generation
+#  7. Time for generating all of the above
+
+while (<>)
+{
+    if (/(\d+\.\d+):DEAL:(\d+):([^:]+)::End/)
+    {
+	printf "%3d", $2 if ($3 eq "points");
+	printf "\t%f", $1;
+	print "\n" if ($3 eq "all---");
+    }
+}
