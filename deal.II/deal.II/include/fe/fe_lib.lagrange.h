f37d0eb85b2a3a76d99ea0b5e4ab1ff94364cd0c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2001-03-30 09:26:00 +0000

    Add doc.
    git-svn-id: https://svn.dealii.org/trunk@4349 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/fe/fe_lib.lagrange.h
+++ deal.II/deal.II/include/fe/fe_lib.lagrange.h
@@ -17,38 +17,93 @@
 #include<fe/fe_q.h>
 
 
-//TODO:[WB,RH] briefly document classes and mark them deprecated
 
 
+/**
+ * This class is an abbreviation for the @ref{FE_Q} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_Q}) is fixed to one, i.e. @p{d}-linear elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_Q} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
 template<int dim> class FEQ1: public FE_Q<dim>
 {
   public:
-    FEQ1(): FE_Q<dim>(1) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEQ1(): FE_Q<dim>(1)       {};
 };
 
 
+/**
+ * This class is an abbreviation for the @ref{FE_Q} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_Q}) is fixed to two, i.e. @p{d}-quadratic elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_Q} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
 template<int dim> class FEQ2: public FE_Q<dim>
 {
   public:
-    FEQ2(): FE_Q<dim>(2) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEQ2(): FE_Q<dim>(2)       {};
 };
 
 
+/**
+ * This class is an abbreviation for the @ref{FE_Q} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_Q}) is fixed to three, i.e. @p{d}-cubic elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_Q} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
 template<int dim> class FEQ3: public FE_Q<dim>
 {
   public:
-    FEQ3(): FE_Q<dim>(3) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEQ3(): FE_Q<dim>(3)       {};
 };
 
 
+/**
+ * This class is an abbreviation for the @ref{FE_Q} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_Q}) is fixed to four, i.e. @p{d}-quartic elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_Q} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
 template<int dim> class FEQ4: public FE_Q<dim>
 {
   public:
-    FEQ4(): FE_Q<dim>(4) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEQ4(): FE_Q<dim>(4)       {};
 };
 
 
