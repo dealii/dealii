ab3c72bc8c8e543851a8fe07df0cef76fef9400f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2001-03-30 09:29:06 +0000

    Add doc.
    git-svn-id: https://svn.dealii.org/trunk@4351 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/fe/fe_lib.dg.h
+++ deal.II/deal.II/include/fe/fe_lib.dg.h
@@ -17,46 +17,121 @@
 #include<fe/fe_dgq.h>
 
 
-//TODO:[WB,RH] briefly document classes and mark them deprecated
 
 
-template<int dim> class FEDG_Q0: public FE_DGQ<dim>
+
+/**
+ * This class is an abbreviation for the @ref{FE_DGQ} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_DGQ}) is fixed to zero, i.e. piecewise constant elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_DGQ} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
+template<int dim>
+class FEDG_Q0 : public FE_DGQ<dim>
 {
   public:
-    FEDG_Q0(): FE_DGQ<dim>(0) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEDG_Q0(): FE_DGQ<dim>(0)       {};
 };
 
 
-template<int dim> class FEDG_Q1: public FE_DGQ<dim>
+/**
+ * This class is an abbreviation for the @ref{FE_DGQ} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_DGQ}) is fixed to one, i.e. piecewise @p{d}-linear elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_DGQ} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
+template<int dim>
+class FEDG_Q1 : public FE_DGQ<dim>
 {
   public:
-    FEDG_Q1(): FE_DGQ<dim>(1) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEDG_Q1(): FE_DGQ<dim>(1)       {};
 };
 
 
-template<int dim> class FEDG_Q2: public FE_DGQ<dim>
+/**
+ * This class is an abbreviation for the @ref{FE_DGQ} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_DGQ}) is fixed to two, i.e. piecewise @p{d}-quadratic elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_DGQ} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
+template<int dim>
+class FEDG_Q2 : public FE_DGQ<dim>
 {
   public:
-    FEDG_Q2(): FE_DGQ<dim>(2) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEDG_Q2(): FE_DGQ<dim>(2)       {};
 };
 
 
-template<int dim> class FEDG_Q3: public FE_DGQ<dim>
+/**
+ * This class is an abbreviation for the @ref{FE_DGQ} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_DGQ}) is fixed to three, i.e. piecewise @p{d}-cubic elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_DGQ} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
+template<int dim>
+class FEDG_Q3 : public FE_DGQ<dim>
 {
   public:
-    FEDG_Q3(): FE_DGQ<dim>(3) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEDG_Q3(): FE_DGQ<dim>(3)       {};
 };
 
 
-template<int dim> class FEDG_Q4: public FE_DGQ<dim>
+/**
+ * This class is an abbreviation for the @ref{FE_DGQ} class, where the
+ * polynomial degree (which is passed through the constructor of
+ * @ref{FE_DGQ}) is fixed to four, i.e. piecewise @p{d}-quartic elements.
+ *
+ * This class is only here for backward compatibility and will go away
+ * someday. Use the @ref{FE_DGQ} class instead.
+ *
+ * @author Wolfgang Bangerth, Ralf Hartmann, 2001
+ */
+template<int dim>
+class FEDG_Q4 : public FE_DGQ<dim>
 {
   public:
-    FEDG_Q4(): FE_DGQ<dim>(4) 
-      {};
+				     /**
+				      * Constructor. Simply pass
+				      * correct polynomial degree to
+				      * constructor of base class.
+				      */
+    FEDG_Q4(): FE_DGQ<dim>(4)       {};
 };
 
 
