429d48697be98029640f4c26adeed13106d3a300
Wolfgang Bangerth <bangerth@math.tamu.edu>
2001-04-11 12:47:10 +0000

    Add this file to this directory as well.
    git-svn-id: https://svn.dealii.org/trunk@4429 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lac/abort.cc
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 1998, 1999, 2000, 2001 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------
+
+extern "C"
+void abort()
+{}
