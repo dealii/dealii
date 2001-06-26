9bbce92e6c50620046b8b33a477f9224facf9a03
Wolfgang Bangerth <bangerth@math.tamu.edu>
2001-06-26 13:41:52 +0000

    Don't try to assign to cerr.
    git-svn-id: https://svn.dealii.org/trunk@4757 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/reference.cc
+++ tests/base/reference.cc
@@ -47,7 +47,7 @@ int main()
   std::ofstream logfile("reference.output");
   deallog.attach(logfile);
   deallog.depth_console(0);
-  std::cerr = logfile;
+
   Test a("A");
   const Test b("B");
   SmartPointer<Test>       r=&a;
