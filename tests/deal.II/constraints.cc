37a7f8acf447a7bc15a9b1cade7ca9bdf676d5f4
Wolfgang Bangerth <bangerth@math.tamu.edu>
2001-08-09 13:44:09 +0000

    Just reindent.
    git-svn-id: https://svn.dealii.org/trunk@4874 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/constraints.cc
+++ tests/deal.II/constraints.cc
@@ -249,7 +249,7 @@ void merge_check ()
 				       // works correctly
       ConstraintMatrix c1, c2;
 
-				   // enter simple line
+				       // enter simple line
       c1.add_line (0);
       c1.add_entry (0, 11, 1.);
 				       // add more complex line
@@ -257,16 +257,19 @@ void merge_check ()
       c1.add_entry (1, 3, 0.5);
       c1.add_entry (1, 4, 0.5);
   
-				   // fill second constraints object
-				   // with one trivial line and one
-				   // which further constrains one of
-				   // the entries in the first object
+				       // fill second constraints
+				       // object with one trivial line
+				       // and one which further
+				       // constrains one of the
+				       // entries in the first object
       c2.add_line (10);
       c2.add_entry (10, 11, 1.);
       c2.add_line (3);
       c2.add_entry (3, 12, 0.25);
       c2.add_entry (3, 13, 0.75);
 
+				       // in one of the two runs,
+				       // close the objects
       if (run == 1)
 	{
 	  c1.close ();
