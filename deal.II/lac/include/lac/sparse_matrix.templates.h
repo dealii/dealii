17f99f8e7218fb22e860b20b63047b414b94c27b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2002-01-25 12:49:55 +0000

    Use <ostream> over <iostream> when that is available.
    git-svn-id: https://svn.dealii.org/trunk@5416 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/sparse_matrix.templates.h
+++ deal.II/lac/include/lac/sparse_matrix.templates.h
@@ -18,7 +18,14 @@
 #include <lac/vector.h>
 
 
-#include <iostream>
+// we only need output streams, but older compilers did not provide
+// them in a separate include file
+#ifdef HAVE_STD_OSTREAM_HEADER
+#  include <ostream>
+#else
+#  include <iostream>
+#endif
+
 #include <iomanip>
 #include <algorithm>
 #include <functional>
