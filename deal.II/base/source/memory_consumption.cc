05fdc243b2c19c7425c0d733a8a8e356d6ba3b95
Wolfgang Bangerth <bangerth@math.tamu.edu>
2002-03-18 12:26:38 +0000

    Move implementations to .cc file, since not used very often anyway and thus no point to mark inline. change triggered by Sun Forte 6.2's inability to first declare a function, then define it marked inline. testcase:
    /* ------------------------------------------ */
    /* Problem 4 -- "extern" and "inline" collide */
    namespace NS2
    {
      inline void f();
      inline void f() {};
    };
    
    
    git-svn-id: https://svn.dealii.org/trunk@5570 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/memory_consumption.cc
+++ deal.II/base/source/memory_consumption.cc
@@ -26,4 +26,189 @@ namespace MemoryConsumption
   };
   
 
+
+  unsigned int memory_consumption (const bool) 
+  {
+    return sizeof(bool);
+  };
+  
+  
+  
+  unsigned int memory_consumption (const char)
+  {
+    return sizeof(char);
+  };
+  
+
+
+  unsigned int memory_consumption (const short int) 
+  {
+    return sizeof(short int);
+  };
+  
+
+
+  unsigned int memory_consumption (const short unsigned int) 
+  {
+    return sizeof(short unsigned int);
+  };
+
+
+
+  unsigned int memory_consumption (const int) 
+  {
+    return sizeof(int);
+  };
+  
+
+
+  unsigned int memory_consumption (const unsigned int) 
+  {
+    return sizeof(unsigned int);
+  };
+
+
+
+  unsigned int memory_consumption (const float)
+  {
+    return sizeof(float);
+  };
+
+
+
+  unsigned int memory_consumption (const double)
+  {
+    return sizeof(double);
+  };
+
+
+  
+  unsigned int memory_consumption (const std::string &s)
+  {
+    return sizeof(s) + s.length();
+  };  
+
+
+
+  template <typename T>
+  unsigned int memory_consumption (const typename std::vector<T> &v)
+  {
+    unsigned int mem = sizeof(std::vector<T>);
+    const unsigned int n = v.size();
+    for (unsigned int i=0; i<n; ++i)
+      mem += memory_consumption(v[i]);
+    mem += (v.capacity() - n)*sizeof(T);
+    return mem;
+  };
+
+
+
+  template <typename T, int N>
+  unsigned int memory_consumption (const T (&v)[N])
+  {
+    unsigned int mem = 0;
+    for (unsigned int i=0; i<N; ++i)
+      mem += memory_consumption(v[i]);
+    return mem;
+  };
+
+
+
+  unsigned int memory_consumption (const std::vector<bool> &v)
+  {
+    return v.capacity() / 8 + sizeof(v);
+  };
+
+
+  
+  unsigned int memory_consumption (const std::vector<int> &v)
+  {
+    return (v.capacity() * sizeof(int) +
+	    sizeof(v));
+  };
+    
+    
+
+  unsigned int memory_consumption (const std::vector<double> &v)
+  {
+    return (v.capacity() * sizeof(double) +
+	    sizeof(v));
+  };
+    
+    
+
+  unsigned int memory_consumption (const std::vector<float> &v)
+  {
+    return (v.capacity() * sizeof(float) +
+	    sizeof(v));
+  };
+    
+    
+	
+  unsigned int memory_consumption (const std::vector<char> &v)
+  {
+    return (v.capacity() * sizeof(char) +
+	    sizeof(v));
+  };
+    
+
+    
+  unsigned int memory_consumption (const std::vector<unsigned char> &v)
+  {
+    return (v.capacity() * sizeof(unsigned char) +
+	    sizeof(v));
+  };
+
+
+    
+  template <typename T>
+  unsigned int memory_consumption (const typename std::vector<T *> &v)
+  {
+    return (v.capacity() * sizeof(T *) +
+	    sizeof(v));
+  };
+    
+
+				    
+  template <typename A, typename B>
+  unsigned int memory_consumption (const typename std::pair<A,B> &p)
+  {
+    return (memory_consumption(p.first) +
+	    memory_consumption(p.second));
+  };
+
+  
+		
+  template <typename T>
+  unsigned int
+  memory_consumption (const T * const)
+  {
+    return sizeof(T*);
+  };
+
+
+		
+  template <typename T>
+  unsigned int
+  memory_consumption (T * const)
+  {
+    return sizeof(T*);
+  };
+
+  
+	
+  unsigned int
+  memory_consumption (void * const)
+  {
+    return sizeof(void*);
+  };
+    
+    
+	
+  template <typename T>
+  unsigned int
+  memory_consumption (const T &t)
+  {
+    return t.memory_consumption();
+  };
 };
