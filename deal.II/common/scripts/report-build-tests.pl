181af50cf99701ae6ccfe51184ff8b2915e3f860
deal <deal@0785d39b-7218-0410-832d-ea1e28bc413d>
2002-05-16 15:26:11 +0000

    Add gcc 3.1 to list of known compilers.
    git-svn-id: https://svn.dealii.org/trunk@5838 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/common/scripts/report-build-tests.pl
+++ deal.II/common/scripts/report-build-tests.pl
@@ -1,6 +1,7 @@
 ############################################################
 $compiler_table{"gcc295"} = "gcc 2.95";
 $compiler_table{"gcc30"} = "gcc 3.0";
+$compiler_table{"gcc31"} = "gcc 3.1";
 $compiler_table{"icc50"} = "Intel ICC 5.0";
 $compiler_table{"icc60"} = "Intel ICC 6.0";
 
