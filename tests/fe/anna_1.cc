6f3960db4a03665e753768890fc997067c800bad
Wolfgang Bangerth <bangerth@math.tamu.edu>
2002-10-17 19:42:22 +0000

    Fix header.
    git-svn-id: https://svn.dealii.org/trunk@6682 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/fe/anna_1.cc
+++ tests/fe/anna_1.cc
@@ -1,4 +1,4 @@
-//----------------------------  non_primitive_1.cc  ---------------------------
+//----------------------------  anna_1.cc  ---------------------------
 //    $Id$
 //    Version: $Name$
 //
@@ -9,7 +9,7 @@
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  non_primitive_1.cc  ---------------------------
+//----------------------------  anna_1.cc  ---------------------------
 
 
 // check some things about Nedelec elements, basically also that the
