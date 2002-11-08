b5d3f6fd8177967b2013b7fc01a0e970a5b593b2
Wolfgang Bangerth <bangerth@math.tamu.edu>
2002-11-08 16:12:48 +0000

    anna_5
    git-svn-id: https://svn.dealii.org/trunk@6742 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/anna_5.cc
@@ -0,0 +1,24 @@
+//----------------------------  anna_5.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2002 by the deal.II authors and Anna Schneebeli
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  anna_5.cc  ---------------------------
+
+
+// this program used to fail at one point in time.
+
+#include <lac/block_sparsity_pattern.h>
+#include <fstream>
+
+int main ()
+{
+  std::ofstream o("anna_5.output");
+  BlockSparsityPattern      sparsity_pattern;
+};
