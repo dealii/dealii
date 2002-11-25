187b69340b770d82c7bcdcdd4f8fe1736ef155d9
Wolfgang Bangerth <bangerth@math.tamu.edu>
2002-11-25 19:32:06 +0000

    Add gerold_1. Keep it disabled.
    git-svn-id: https://svn.dealii.org/trunk@6785 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/gerold_1.cc
@@ -0,0 +1,136 @@
+
+/*    Copyright (C) 1999, 2000, 2001, 2002 by the deal.II authors */
+/*                                                                */
+/*    This file is subject to QPL and may not be  distributed     */
+/*    without copyright and license information. Please refer     */
+/*    to the file deal.II/doc/license.html for the  text  and     */
+/*    further information on this license.                        */
+
+
+// check whether we can read a 3d grid. this test is presenty
+// disabled, since we cannot read the file, and this will not be fixed
+// in the near future.
+
+#include <grid/tria.h>
+#include <dofs/dof_handler.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_accessor.h>
+#include <grid/tria_iterator.h>
+#include <dofs/dof_accessor.h>
+#include <fe/fe_q.h>
+#include <dofs/dof_tools.h>
+#include <fe/fe_values.h>
+#include <base/quadrature_lib.h>
+#include <base/function.h>
+#include <numerics/vectors.h>
+#include <numerics/matrices.h>
+
+
+#include <lac/vector.h>
+#include <lac/full_matrix.h>
+#include <lac/sparse_matrix.h>
+#include <lac/solver_cg.h>
+#include <lac/vector_memory.h>
+#include <lac/precondition.h>
+
+#include <numerics/data_out.h>
+#include <fstream>
+#include <iostream>
+
+#include <base/logstream.h>
+
+#include <grid/grid_in.h>
+#include <grid/tria_boundary_lib.h>
+
+#include <base/timer.h>
+
+
+template <int dim>
+class LaplaceProblem 
+{
+  public:
+    LaplaceProblem ();
+    void run ();
+    
+  private:
+
+    Triangulation<dim>   triangulation;
+    FE_Q<dim>            fe;
+    DoFHandler<dim>      dof_handler;
+
+    SparsityPattern      sparsity_pattern;
+    SparseMatrix<double> system_matrix;
+
+    Vector<double>       solution;
+    Vector<double>       system_rhs;
+};
+
+
+
+
+template <int dim>
+LaplaceProblem<dim>::LaplaceProblem () :
+                fe (1),
+		dof_handler (triangulation)
+{};
+
+
+template <int dim>
+void LaplaceProblem<dim>::run () 
+{
+  std::cout << "Solving problem in " << dim << " space dimensions." << std::endl;
+  
+ 
+  
+  GridIn<dim> grid_in;
+  grid_in.attach_triangulation (triangulation);
+
+
+ std::ifstream input_file("gerold_1.inp");
+ 
+
+  cout << "read ucd data file" << endl;
+
+
+  grid_in.read_ucd(input_file);
+  cout << "ucd data file readin exe" << endl;
+
+};
+			
+int main () 
+{
+  std::ofstream logfile("gerold_1.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+				
+  try
+    {
+      LaplaceProblem<3> laplace_problem_3d;
+      laplace_problem_3d.run ();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+		<< exc.what() << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    }
+  catch (...) 
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    };
+  
+  return 0;
+};
