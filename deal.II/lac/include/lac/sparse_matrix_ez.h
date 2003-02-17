f93302ee42de06f95e81603201c8e42d18a39b80
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-02-17 21:40:50 +0000

    Header was wrong.
    git-svn-id: https://svn.dealii.org/trunk@7132 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/sparse_matrix_ez.h
+++ deal.II/lac/include/lac/sparse_matrix_ez.h
@@ -1,15 +1,15 @@
-//----------------------------  sparse_matrix.h  ---------------------------
+//----------------------------  sparse_matrix_ez.h  ---------------------------
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 by the deal.II authors
+//    Copyright (C) 2002, 2003 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  sparse_matrix.h  ---------------------------
+//----------------------------  sparse_matrix_ez.h  ---------------------------
 #ifndef __deal2__sparse_matrix_ez_h
 #define __deal2__sparse_matrix_ez_h
 
--- deal.II/lac/source/sparse_matrix_ez.float.cc
+++ deal.II/lac/source/sparse_matrix_ez.float.cc
@@ -1,15 +1,15 @@
-//----------------------------  sparse_matrix.double.cc  ---------------------------
+//----------------------------  sparse_matrix_ez.float.cc  ---------------------------
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 by the deal.II authors
+//    Copyright (C) 2002, 2003 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  sparse_matrix.double.cc  ---------------------------
+//----------------------------  sparse_matrix_ez.float.cc  ---------------------------
 
 
 #include <lac/sparse_matrix_ez.templates.h>
