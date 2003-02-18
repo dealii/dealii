1ec4b6f3185777d8e2765207496bdc4521c9aebd
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-02-18 20:01:36 +0000

    Check for DoFTools::count_boundary_dofs, which presently on mainline still is DoFHandler::n_boundary_dofs, but is already renamed on the hp_branch.
    git-svn-id: https://svn.dealii.org/trunk@7168 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/dof_tools_14.cc
@@ -0,0 +1,42 @@
+//----------------------------  dof_tools_14.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2003 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  dof_tools_14.cc  ---------------------------
+
+#include "dof_tools_common.cc"
+#include <lac/vector.h>
+
+// check
+//   DoFTools::count_boundary_dofs
+
+
+
+std::string output_file_name = "dof_tools_14.output";
+
+
+template <int dim>
+void
+check_this (const DoFHandler<dim> &dof_handler)
+{
+                                   // no other args
+  deallog << dof_handler.n_boundary_dofs() << std::endl;
+
+                                   // with FunctionMap
+  typename FunctionMap<dim>::type fm;
+  fm[0] = 0;
+  deallog << dof_handler.n_boundary_dofs(fm) << std::endl;
+  
+                                   // with std::set
+  std::set<unsigned char> s;
+  s.insert (0);
+  deallog << dof_handler.n_boundary_dofs(s) << std::endl;
+  
+}
