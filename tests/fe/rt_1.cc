98e7645739369e5de8da1363c7f186489be22aee
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-04-21 16:23:52 +0000

    Tests for Raviart-Thomas elements.
    git-svn-id: https://svn.dealii.org/trunk@7427 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fe/rt_1.cc
@@ -0,0 +1,76 @@
+// $Id$
+// (c) Wolfgang Bangerth
+//
+// Show the shape functions of the Raviart-Thomas element on the unit cell
+
+#include <base/logstream.h>
+#include <fe/fe_raviart_thomas.h>
+
+#include <vector>
+#include <fstream>
+#include <string>
+
+#define PRECISION 2
+
+
+
+template<int dim>
+inline void
+plot_shape_functions(const unsigned int degree)
+{
+  FE_RaviartThomas<dim> fe_rt(degree);
+
+  const unsigned int div=2;
+  for (unsigned int mz=0;mz<=((dim>2) ? div : 0) ;++mz)
+    for (unsigned int my=0;my<=((dim>1) ? div : 0) ;++my)
+      for (unsigned int mx=0;mx<=div;++mx)
+        {
+          const Point<dim> p = (dim==2 ?
+                                Point<dim>(1.*mx/div, 1.*my/div) :
+                                Point<dim>(1.*mx/div, 1.*my/div, 1.*mz/div));
+          deallog << "q_point = " << p << std::endl;
+	      
+          for (unsigned int i=0;i<fe_rt.dofs_per_cell;++i)
+            {
+              deallog << "  phi(" << i << ") = [";
+              for (unsigned int c=0; c<dim; ++c)
+                deallog << " " << fe_rt.shape_value_component(i,p,c);
+              deallog << " ]" << std::endl;
+            };
+          for (unsigned int i=0;i<fe_rt.dofs_per_cell;++i)
+            {
+              deallog << "  grad phi(" << i << ") = ";
+              for (unsigned int c=0; c<dim; ++c)
+                {
+                  deallog << "[";
+                  for (unsigned int d=0; d<dim; ++d)
+                    deallog << " " << fe_rt.shape_grad_component(i,p,c)[d];
+                  deallog << " ]" << std::endl;
+                  if (c != dim-1)
+                    deallog << "                ";
+                };
+            };
+        }
+  
+  deallog << std::endl;
+}
+
+
+int
+main()
+{
+  std::ofstream logfile ("rt_1.output");
+  logfile.precision (PRECISION);
+  logfile.setf(std::ios::fixed);  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  for (unsigned int degree=0; degree<2; ++degree)
+    plot_shape_functions<2>(degree);
+//  plot_shape_functions<3>(degree);
+  
+  return 0;
+}
+
+
+
