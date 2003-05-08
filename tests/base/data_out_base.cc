d71527a2b4588f4a5ba37b47ac1a484fd2aaa06e
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-05-08 15:25:51 +0000

    Fix non-C++-isms
    git-svn-id: https://svn.dealii.org/trunk@7611 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/data_out_base.cc
+++ tests/base/data_out_base.cc
@@ -59,11 +59,13 @@ struct PatchInfo
 };
 
 
+template <>
 double PatchInfo<1>::vertices[2][3]
 = {{0, 0, 0}
   ,{3, 6, 9}
 };
 
+template <>
 double PatchInfo<2>::vertices[4][3]
 = {{0, 0, 0}
   ,{3, 0, 3}
@@ -72,6 +74,7 @@ double PatchInfo<2>::vertices[4][3]
 };
 
 
+template <>
 double PatchInfo<3>::vertices[8][3]
 = {{0, 0, 0}
   ,{3, 0, 0}
@@ -84,6 +87,7 @@ double PatchInfo<3>::vertices[8][3]
 };
 
 
+template <>
 int PatchInfo<1>::neighbors[2][2]
 = {{-1,  1}
   ,{ 0, -1}
@@ -91,6 +95,7 @@ int PatchInfo<1>::neighbors[2][2]
 
 
 
+template <>
 int PatchInfo<2>::neighbors[4][4]
 = {{-1,  1,  2, -1}
   ,{-1, -1,  3,  0}
@@ -100,6 +105,7 @@ int PatchInfo<2>::neighbors[4][4]
 
 
 
+template <>
 int PatchInfo<3>::neighbors[8][6]
 = {{-1, -1, -1, -1, -1, -1}
   ,{-1, -1, -1, -1, -1, -1}
@@ -112,12 +118,14 @@ int PatchInfo<3>::neighbors[8][6]
 };
 
 
+template <>
 double PatchInfo<1>::offsets[2][3]
 = {{ 0, 0, 0}
   ,{ 3, 0, 0}
 };
 
 
+template <>
 double PatchInfo<2>::offsets[4][3]
 = {{ 0, 0, 0}
   ,{ 3, 0, 0}
@@ -126,6 +134,7 @@ double PatchInfo<2>::offsets[4][3]
 };
 
 
+template <>
 double PatchInfo<3>::offsets[8][3]
 = {{ 0, 0, 0}
   ,{ 3, 0, 0}
