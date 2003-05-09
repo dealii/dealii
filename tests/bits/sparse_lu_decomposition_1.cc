0491640b4c120830aa658a12c09e67d3b00f2c80
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-05-09 21:13:30 +0000

    Add a test that presently doesn't compile, but which we should fix before the next release.
    git-svn-id: https://svn.dealii.org/trunk@7618 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/sparse_lu_decomposition_1.cc
@@ -0,0 +1,38 @@
+//----------------------------  sparse_lu_decomposition_1.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2003 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  sparse_lu_decomposition_1.cc  ---------------------------
+
+// this file didn't compile at one point in time due to the private
+// inheritance of SparseMatrix by SparseLUDecomposition, and the
+// associated lack of accessibility of the Subscriptor functions to
+// the SmartPointer
+
+#include <base/logstream.h>
+#include <base/smartpointer.h>
+#include <lac/sparse_ilu.h>
+#include <fstream>
+
+  
+
+int main () 
+{
+  std::ofstream logfile("sparse_lu_decomposition_1.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  SmartPointer<SparseLUDecomposition<double> > sparse_decomp;
+
+  deallog << "OK" << std::endl;
+  
+  return 0;
+}
+
