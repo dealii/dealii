ad51e4777437cbd3b6bdc35c5f42fa45db308635
Guido Kanschat <dr.guido.kanschat@gmail.com>
2003-06-01 16:26:56 +0000

    lines shortened
    git-svn-id: https://svn.dealii.org/trunk@7715 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/source/sparse_matrix_vector.in.h
+++ deal.II/lac/source/sparse_matrix_vector.in.h
@@ -1,4 +1,4 @@
-//----------------------------  sparse_matrix.2.templates  ---------------------------
+//---------------------------------------------------------------------------
 //    $Id$
 //    Version: $Name$
 //
@@ -9,55 +9,69 @@
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  sparse_matrix.2.templates  ---------------------------
+//---------------------------------------------------------------------------
 
 
 // Driver file for SparseMatrix functions with two types.
 
 // TYPEMAT and TYPEVEC are defined in sparsematrix?.cc
 
-template void SparseMatrix<TYPEMAT>::vmult<TYPEVEC> (Vector<TYPEVEC> &,
-						   const Vector<TYPEVEC> &) const;
-template void SparseMatrix<TYPEMAT>::Tvmult<TYPEVEC> (Vector<TYPEVEC> &,
-						    const Vector<TYPEVEC> &) const;
-template void SparseMatrix<TYPEMAT>::vmult_add<TYPEVEC> (Vector<TYPEVEC> &,
-						       const Vector<TYPEVEC> &) const;
-template void SparseMatrix<TYPEMAT>::Tvmult_add<TYPEVEC> (Vector<TYPEVEC> &,
-							const Vector<TYPEVEC> &) const;
+template void SparseMatrix<TYPEMAT>::vmult<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &) const;
+template void SparseMatrix<TYPEMAT>::Tvmult<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &) const;
+template void SparseMatrix<TYPEMAT>::vmult_add<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &) const;
+template void SparseMatrix<TYPEMAT>::Tvmult_add<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &) const;
 
 template TYPEVEC
-SparseMatrix<TYPEMAT>::matrix_norm_square<TYPEVEC> (const Vector<TYPEVEC> &) const;
+SparseMatrix<TYPEMAT>::matrix_norm_square<TYPEVEC> (
+  const Vector<TYPEVEC> &) const;
 
 template TYPEVEC
-SparseMatrix<TYPEMAT>::matrix_scalar_product<TYPEVEC> (const Vector<TYPEVEC> &,
-						     const Vector<TYPEVEC> &) const;
+SparseMatrix<TYPEMAT>::matrix_scalar_product<TYPEVEC> (
+  const Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &) const;
 
-template TYPEVEC SparseMatrix<TYPEMAT>::residual<TYPEVEC> (Vector<TYPEVEC> &,
-						       const Vector<TYPEVEC> &,
-						       const Vector<TYPEVEC> &) const;
+template TYPEVEC SparseMatrix<TYPEMAT>::residual<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &) const;
 
-template void SparseMatrix<TYPEMAT>::precondition_SSOR<TYPEVEC> (Vector<TYPEVEC> &,
-							       const Vector<TYPEVEC> &,
-							       const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::precondition_SSOR<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const TYPEMAT) const;
 
-template void SparseMatrix<TYPEMAT>::precondition_SOR<TYPEVEC> (Vector<TYPEVEC> &,
-							      const Vector<TYPEVEC> &,
-							      const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::precondition_SOR<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const TYPEMAT) const;
 
-template void SparseMatrix<TYPEMAT>::precondition_TSOR<TYPEVEC> (Vector<TYPEVEC> &,
-							  const Vector<TYPEVEC> &,
-							  const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::precondition_TSOR<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const TYPEMAT) const;
 
-template void SparseMatrix<TYPEMAT>::precondition_Jacobi<TYPEVEC> (Vector<TYPEVEC> &,
-							    const Vector<TYPEVEC> &,
-							    const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::precondition_Jacobi<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const TYPEMAT) const;
 
-template void SparseMatrix<TYPEMAT>::SOR<TYPEVEC> (Vector<TYPEVEC> &,
-						 const TYPEMAT) const;
-template void SparseMatrix<TYPEMAT>::TSOR<TYPEVEC> (Vector<TYPEVEC> &,
-						  const TYPEMAT) const;
-template void SparseMatrix<TYPEMAT>::SSOR<TYPEVEC> (Vector<TYPEVEC> &,
-						  const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::SOR<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::TSOR<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::SSOR<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const TYPEMAT) const;
 template void SparseMatrix<TYPEMAT>::PSOR<TYPEVEC> (
   Vector<TYPEVEC> &,
   const std::vector<unsigned int>&,
@@ -68,12 +82,15 @@ template void SparseMatrix<TYPEMAT>::TPSOR<TYPEVEC> (
   const std::vector<unsigned int>&,
   const std::vector<unsigned int>&,
   const TYPEMAT) const;
-template void SparseMatrix<TYPEMAT>::SOR_step<TYPEVEC> (Vector<TYPEVEC> &, const Vector<TYPEVEC> &,
-						      const TYPEMAT) const;
-template void SparseMatrix<TYPEMAT>::TSOR_step<TYPEVEC> (Vector<TYPEVEC> &,
-						       const Vector<TYPEVEC> &,
-						       const TYPEMAT) const;
-template void SparseMatrix<TYPEMAT>::SSOR_step<TYPEVEC> (Vector<TYPEVEC> &,
-						       const Vector<TYPEVEC> &, 
-						       const TYPEMAT) const;
-
+template void SparseMatrix<TYPEMAT>::SOR_step<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::TSOR_step<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &,
+  const TYPEMAT) const;
+template void SparseMatrix<TYPEMAT>::SSOR_step<TYPEVEC> (
+  Vector<TYPEVEC> &,
+  const Vector<TYPEVEC> &, 
+  const TYPEMAT) const;
