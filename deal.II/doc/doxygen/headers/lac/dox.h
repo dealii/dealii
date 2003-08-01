8da4f203c28e71023a0b5f0c8555aee6e47f39c6
Guido Kanschat <dr.guido.kanschat@gmail.com>
2003-08-01 23:42:55 +0000

    First setup for doxygen documentation
    git-svn-id: https://svn.dealii.org/trunk@7896 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/lac/dox.h
@@ -0,0 +1,55 @@
+//-------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2003 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-------------------------------------------------------------------------
+
+/**
+ * @defgroup LAC The Linear Algebra Classes
+ * @brief All the classes for linear algebra
+ * Bla bla bla
+ * @{
+ */
+
+/**
+ * @defgroup Solvers Linear Solver classes
+ *
+ * Here we collect iterative solvers and some control classes. All
+ * solvers inherit from the class template Solver, which provides some
+ * basic maintenance methods.
+ *
+ * The number of iteration steps of all solvers is controlled by
+ * objects of class SolverControl or its derived class
+ * ReductionControl.
+ */
+
+/**
+ * @defgroup Matrices Matrix classes
+ * @{
+ */
+
+/**
+ * @defgroup Matrix1 Basic matrices
+ */
+
+
+/**
+ * @defgroup Matrix2 Derived matrices
+ */
+
+/**
+ * @defgroup Preconditioners Preconditioners
+ * @}
+ */
+
+
+/**
+ * @}
+ */
