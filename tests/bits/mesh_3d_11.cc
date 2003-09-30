241c8a5d5893c9b73fe17a8efa8d2942802ea45e
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-09-30 14:45:51 +0000

    New test.
    git-svn-id: https://svn.dealii.org/trunk@8069 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/mesh_3d_11.cc
@@ -0,0 +1,117 @@
+//----------------------------  mesh_3d_11.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2003 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  mesh_3d_11.cc  ---------------------------
+
+
+// check that each cell with a neighbor is also that neighbor's
+// neighbor on exactly one side. this is tricky to get right for 3d
+// meshes with misoriented faces
+//
+// also check that not only the neighborship is correctly set, but
+// that they indeed share a common face
+
+#include "mesh_3d.h"
+
+#include <base/logstream.h>
+#include <base/quadrature_lib.h>
+#include <grid/tria.h>
+#include <grid/tria_accessor.h>
+#include <grid/tria_iterator.h>
+#include <grid/grid_reordering.h>
+#include <grid/grid_generator.h>
+#include <dofs/dof_handler.h>
+#include <fe/fe_q.h>
+#include <fe/fe_values.h>
+
+#include <fstream>
+#include <set>
+
+void check_this (Triangulation<3> &tria)
+{
+  for (Triangulation<3>::cell_iterator cell=tria.begin();
+       cell != tria.end(); ++cell)
+    for (unsigned int f=0; f<GeometryInfo<3>::faces_per_cell; ++f)
+      if (!cell->at_boundary())
+        {
+          const Triangulation<3>::cell_iterator neighbor = cell->neighbor(f);
+          const unsigned int nb_nb = cell->neighbor_of_neighbor(f);
+
+          bool found = false;
+          for (unsigned int ff=0; ff<GeometryInfo<3>::faces_per_cell; ++ff)
+            if (neighbor->neighbor(ff) == cell)
+              {
+                Assert (found == false, ExcInternalError());
+                Assert (ff == nb_nb, ExcInternalError());
+
+                Assert (cell->face(f) == neighbor->face(ff),
+                        ExcInternalError());
+
+                found = true;
+
+                break;
+              }
+          Assert (found == true, ExcInternalError());
+        }
+  deallog << "    ok." << std::endl;
+}
+
+
+void check (Triangulation<3> &tria)
+{
+  deallog << "Initial check" << std::endl;
+  check_this (tria);
+  
+  for (unsigned int r=0; r<3; ++r)
+    {
+      tria.refine_global (1);
+      deallog << "Check " << r << std::endl;
+      check_this (tria);
+    }
+
+  coarsen_global (tria);
+  deallog << "Check " << 1 << std::endl;
+  check_this (tria);
+  
+  tria.refine_global (1);
+  deallog << "Check " << 2 << std::endl;
+  check_this (tria);
+}
+
+
+int main () 
+{
+  std::ofstream logfile("mesh_3d_11.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  {  
+    Triangulation<3> coarse_grid;
+    create_two_cubes (coarse_grid);
+    check (coarse_grid);
+  }
+  
+  {  
+    Triangulation<3> coarse_grid;
+    create_L_shape (coarse_grid);
+    check (coarse_grid);
+  }
+  
+  {  
+    Triangulation<3> coarse_grid;
+    GridGenerator::hyper_ball (coarse_grid);
+    check (coarse_grid);
+  }
+  
+}
+
+  
+  
