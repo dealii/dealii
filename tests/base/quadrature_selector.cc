8dbd5515d6986d17e08d734b0bcade2208b0e1e1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2003-10-29 16:07:08 +0000

    New test.
    git-svn-id: https://svn.dealii.org/trunk@8172 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/quadrature_selector.cc
@@ -0,0 +1,57 @@
+//----------------------------  quadrature_selector.cc  ---------------------------
+//    quadrature_selector.cc,v 1.18 2003/01/08 17:58:18 wolf Exp
+//    Version: 
+//
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  quadrature_selector.cc  ---------------------------
+
+
+// make sure that the QuadratureSelector works for a selection of
+// arguments
+
+
+#include <fstream>
+
+#include <base/logstream.h>
+#include <base/quadrature_lib.h>
+#include <base/quadrature_selector.h>
+#include <string>
+#include <cmath>
+
+
+template <int dim>
+void check (const std::string     &name,
+	    const unsigned int     order,
+	    const Quadrature<dim> &q)
+{
+  Assert (QuadratureSelector<dim>(name, order).get_points() ==
+	  q.get_points(),
+	  ExcInternalError());
+  deallog << name << ' ' << order << " ok" << std::endl;
+}
+
+
+int main()
+{
+  std::ofstream logfile("quadrature_selector.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  check ("gauss", 2, QGauss2<1>());
+  check ("gauss", 2, QGauss2<2>());
+  check ("gauss", 2, QGauss2<3>());
+  
+  check ("gauss", 2, QGauss2<3>());
+  check ("gauss", 6, QGauss6<3>());
+  check ("gauss", 10, QGauss<3>(10));
+
+  check ("weddle", 0, QWeddle<2>());
+}
+
+
