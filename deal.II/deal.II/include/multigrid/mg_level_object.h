14933b2cdbf6fd2a4f3f3a5d48d8de4a4c0b546b
Guido Kanschat <dr.guido.kanschat@gmail.com>
2003-12-05 15:11:27 +0000

    memory_consumption
    git-svn-id: https://svn.dealii.org/trunk@8239 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/multigrid/mg_level_object.h
+++ deal.II/deal.II/include/multigrid/mg_level_object.h
@@ -197,7 +197,7 @@ MGLevelObject<Object>::memory_consumption () const
   typedef typename std::vector<boost::shared_ptr<Object> >::const_iterator Iter;
   const Iter end = objects.end();
   for (Iter o=objects.begin(); o!=end; ++o)
-    result += *o->memory_consumption();
+    result += (*o)->memory_consumption();
   
   return result;
 }
