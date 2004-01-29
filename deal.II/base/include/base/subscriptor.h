745be7dd8beb6380ea7cc06dfb04ac906f796446
Guido Kanschat <dr.guido.kanschat@gmail.com>
2004-01-29 10:39:18 +0000

    mark internal @internal
    git-svn-id: https://svn.dealii.org/trunk@8350 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/include/base/subscriptor.h
+++ deal.II/base/include/base/subscriptor.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -69,6 +69,10 @@ namespace internal
                                       */
     template <bool, typename T> struct PossiblyVolatile
     {
+					 /**
+					  * @internal
+					  * @brief The actual typedef.
+					  */
         typedef volatile T type;
     };
 
@@ -82,6 +86,10 @@ namespace internal
                                       */
     template <typename T> struct PossiblyVolatile<false,T>
     {
+					 /**
+					  * @internal
+					  * @brief The actual typedef.
+					  */
         typedef T type;
     };
   }
