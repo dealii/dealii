a117ff026deba7c2736aae71a1f1433428da2544
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-02-04 01:53:47 +0000

    Fix a warning about possible uninitialized use.
    git-svn-id: https://svn.dealii.org/trunk@8392 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/constraints.cc
+++ tests/deal.II/constraints.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2003 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2003, 2004 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -290,7 +290,7 @@ int main ()
   deallog.attach(logfile);
   deallog.depth_console(0);
 
-  FiniteElement<3> *fe;
+  FiniteElement<3> *fe = 0;
   
   for (unsigned int element=0; element<2; ++element)
     {
