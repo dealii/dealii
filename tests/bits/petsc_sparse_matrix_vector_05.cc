4056307af673ff133142323f267b1201ddbb65b5
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-02-09 23:02:51 +0000

    New tests.
    git-svn-id: https://svn.dealii.org/trunk@8445 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/petsc_sparse_matrix_vector_05.cc
@@ -0,0 +1,105 @@
+//----------------------------  petsc_sparse_matrix_vector_05.cc  ---------------------------
+//    vector_11.cc,v 1.4 2003/07/03 10:31:46 guido Exp
+//    Version: 
+//
+//    Copyright (C) 2004 by the deal.II authors and Anna Schneebeli
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  petsc_sparse_matrix_vector_05.cc  ---------------------------
+
+
+// check SparseMatrix::matrix_scalar_product
+
+#include "../tests.h"
+#include <lac/petsc_vector.h>
+#include <lac/petsc_sparse_matrix.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+void test (petsc_wrappers::Vector &v,
+           petsc_wrappers::Vector &w)
+{
+  petsc_wrappers::SparseMatrix m(v.size(),v.size(),v.size());
+  for (unsigned int i=0; i<m.m(); ++i)
+    for (unsigned int j=0; j<m.m(); ++j)
+        m.set (i,j, i+2*j);
+
+  for (unsigned int i=0; i<v.size(); ++i)
+    {
+      v(i) = i;
+      w(i) = i+1;
+    }
+  
+  m.compress ();
+  v.compress ();
+  w.compress ();
+
+                                   // <w,Mv>
+  const double s = m.matrix_scalar_product (w,v);
+
+                                   // make sure we get the expected result
+  for (unsigned int i=0; i<v.size(); ++i)
+    {
+      Assert (v(i) == i, ExcInternalError());
+      Assert (w(i) == i+1, ExcInternalError());
+    }
+
+  double result = 0;
+  for (unsigned int i=0; i<m.m(); ++i)
+    for (unsigned int j=0; j<m.m(); ++j)
+      result += (i+2*j)*j*(i+1);
+
+  Assert (s == result, ExcInternalError());
+
+  deallog << "OK" << std::endl;
+}
+
+
+
+int main (int argc, char **argv)
+{
+  std::ofstream logfile("petsc_sparse_matrix_vector_05.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  try
+    {
+      PetscInitialize(&argc,&argv,0,0);
+      {
+        petsc_wrappers::Vector v (100);
+        petsc_wrappers::Vector w (100);
+        test (v,w);
+      }
+      PetscFinalize ();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+		<< exc.what() << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      
+      return 1;
+    }
+  catch (...) 
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    };
+}
