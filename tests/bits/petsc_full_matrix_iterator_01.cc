56509ceaa85f80a08ea67b12915ed42aeafc30b1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-03-07 22:31:12 +0000

    New tests.
    git-svn-id: https://svn.dealii.org/trunk@8669 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/petsc_full_matrix_iterator_01.cc
@@ -0,0 +1,80 @@
+//----------------------------  petsc_full_matrix_iterator_01.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2004 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  petsc_full_matrix_iterator_01.cc  ---------------------------
+
+
+// test PETScWrappers::MatrixBase::const_iterator
+
+#include "../tests.h"
+#include <lac/petsc_full_matrix.h>    
+#include <fstream>
+#include <iostream>
+
+
+void test ()
+{
+  PETScWrappers::FullMatrix m(5,5);
+  m.set (0,0,1);
+  m.set (1,1,2);
+  m.set (1,2,3);  
+  m.compress ();
+  PETScWrappers::FullMatrix::const_iterator i = m.begin();
+  deallog << i->row() << ' ' << i->column() << ' ' << i->value() << std::endl;
+  ++i;
+  deallog << i->row() << ' ' << i->column() << ' ' << i->value() << std::endl;
+  i++;
+  deallog << i->row() << ' ' << i->column() << ' ' << i->value() << std::endl;
+  
+  deallog << "OK" << std::endl;
+}
+
+
+
+int main (int argc,char **argv)
+{
+  std::ofstream logfile("petsc_full_matrix_iterator_01.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  try
+    {
+      PetscInitialize(&argc,&argv,0,0);
+      {
+        test ();
+      }
+      PetscFinalize();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+		<< exc.what() << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      
+      return 1;
+    }
+  catch (...) 
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    };
+}
