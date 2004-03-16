ff19c53df7c5660c407126ae47ddb234cef229b9
Ralf Hartmann <Ralf.Hartmann@dlr.de>
2004-03-16 07:40:20 +0000

    Fix bug.
    git-svn-id: https://svn.dealii.org/trunk@8761 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/tests.h
+++ tests/tests.h
@@ -30,13 +30,13 @@ LogStream & operator << (LogStream &logstream,
 {
   if (std::fabs (d) < 1e-10)
     logstream.
-#ifndef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
+#ifdef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
       template
 #endif
       operator << <double> (0.);
   else
     logstream.
-#ifndef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
+#ifdef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
       template
 #endif
       operator << <double> (d);
@@ -50,13 +50,13 @@ LogStream & operator << (LogStream &logstream,
 {
   if (std::fabs (d) < 1e-8)
     logstream.
-#ifndef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
+#ifdef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
       template
 #endif
       operator << <float> (0.);
   else
     logstream.
-#ifndef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
+#ifdef DEAL_II_TEMPL_OP_DISAMBIGUATION_BUG
       template
 #endif
       operator << <float> (d);
