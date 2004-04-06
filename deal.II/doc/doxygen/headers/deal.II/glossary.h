2b69b01b40ddbe43c5ae151cecb8ccd350ce33bc
Guido Kanschat <dr.guido.kanschat@gmail.com>
2004-04-06 02:33:13 +0000

    glossary started; use liberally
    git-svn-id: https://svn.dealii.org/trunk@8977 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/deal.II/glossary.h
@@ -0,0 +1,10 @@
+/**
+ * @page DEALGlossary Glossary
+ *
+ * <dl>
+ *
+ * <dt>@anchor GlossActive active cells</dt>
+ * <dd>Mesh cells not refined any further in the hierarchy.</dd>
+ *
+ * </dl>
+ */
