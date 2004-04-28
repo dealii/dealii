b19ed1815fe8df7f27dac3a1323a5a32ac544b23
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-04-28 22:07:15 +0000

    Test for hp branch.
    git-svn-id: https://svn.dealii.org/trunk@9114 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/fe_collection_01.cc
@@ -0,0 +1,52 @@
+//----------------------------  fe_tools_01a.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2004 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  fe_tools_01a.cc  ---------------------------
+
+// FECollection would throw an exception when its destructor is called. Check
+// that this is fixed
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <grid/tria.h>
+#include <grid/tria_iterator.h>
+#include <grid/grid_generator.h>
+#include <dofs/dof_accessor.h>
+#include <dofs/dof_handler.h>
+#include <fe/fe_collection.h>
+#include <fe/fe_q.h>
+
+#include <fstream>
+#include <iostream>
+
+template <int dim>
+void
+check ()
+{
+  const FE_Q<dim> fe_1(1);
+  const FE_Q<dim> fe_2(2);
+
+  FECollection<dim> fc;
+  fc.add_fe (fe_1);
+  fc.add_fe (fe_2);
+}
+
+int main()
+{
+  std::ofstream logfile("deal_solver_01.output");
+  logfile.precision(4);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  
+  check<1> ();
+  check<2> ();
+  check<3> ();
+}
