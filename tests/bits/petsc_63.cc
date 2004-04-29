811ad842f3073db09154530cc9f93d607daaee2f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-04-29 18:02:07 +0000

    .
    git-svn-id: https://svn.dealii.org/trunk@9124 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/petsc_63.cc
@@ -0,0 +1,80 @@
+//----------------------------  petsc_63.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2004 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  petsc_63.cc  ---------------------------
+
+
+// PETScWrappers::SparseMatrix::reinit () was declared but not defined
+
+#include "../tests.h"
+#include <lac/petsc_sparse_matrix.h>
+#include <lac/vector.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+void test (PETScWrappers::MatrixBase &m)
+{
+  Assert (m.m() == 100, ExcInternalError());
+  Assert (m.n() == 100, ExcInternalError());
+
+  m.reinit ();
+  
+  Assert (m.m() == 100, ExcInternalError());
+  Assert (m.n() == 100, ExcInternalError());
+
+  deallog << "OK" << std::endl;
+}
+
+
+
+int main (int argc,char **argv) 
+{
+  std::ofstream logfile("petsc_63.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  try
+    {
+      PetscInitialize(&argc,&argv,0,0);
+      {
+        PETScWrappers::SparseMatrix v (100,100,5);
+        test (v);
+      }
+      PetscFinalize();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+		<< exc.what() << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      
+      return 1;
+    }
+  catch (...) 
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    };
+}
