1b027de1ed53138a0a8dcfe923f5dafe7cb447fd
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-05-03 14:17:57 +0000

    Fix.
    git-svn-id: https://svn.dealii.org/trunk@9142 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/block_sparse_matrix_iterator_04.cc
+++ tests/bits/block_sparse_matrix_iterator_04.cc
@@ -58,11 +58,11 @@ void test ()
                                    // this test, above assertion is ok, but an
                                    // elementwise one is not (we fail in the
                                    // first line)
-  Assert (it.row() == it2.row(), ExcInternalError());
-  Assert (it.block_row() == it2.block_row(), ExcInternalError());
-  Assert (it.column() == it2.column(), ExcInternalError());
-  Assert (it.block_column() == it2.block_column(), ExcInternalError());
-  Assert (it.index() == it2.index(), ExcInternalError());
+  Assert (it->row() == it2->row(), ExcInternalError());
+  Assert (it->block_row() == it2->block_row(), ExcInternalError());
+  Assert (it->column() == it2->column(), ExcInternalError());
+  Assert (it->block_column() == it2->block_column(), ExcInternalError());
+  Assert (it->index() == it2->index(), ExcInternalError());
    
   deallog << "OK" << std::endl;
 }
