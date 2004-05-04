54fd0bfe63839a4c530d200beb85fd7d5bf6fdec
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-05-04 13:51:14 +0000

    Mention correct namespace.
    git-svn-id: https://svn.dealii.org/trunk@9175 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/lac/dox.h
+++ deal.II/doc/doxygen/headers/lac/dox.h
@@ -97,11 +97,11 @@
  */
 
 /**
- * @defgroup PETSc PETSc
- * The classes in this module are wrappers around functionality provided by
- * the PETSc library. They provide a modern object-oriented interface that is
- * compatible with the interfaces of the other linear algebra classes in
- * deal.II.
+ * @defgroup PETScWrappers PETScWrappers The classes in this module are
+ * wrappers around functionality provided by the PETSc library. They provide a
+ * modern object-oriented interface that is compatible with the interfaces of
+ * the other linear algebra classes in deal.II. All classes and functions in
+ * this group reside in a namespace @p PETScWrappers.
  *
  * These classes are only available if a PETSc installation was detected
  * during configuration of deal.II. Refer to the README file for more details
