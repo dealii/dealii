2317b6d256f5d0aefa16d35be39bdd5ca88f043f
Ralf Hartmann <Ralf.Hartmann@dlr.de>
2004-10-29 09:20:26 +0000

    DataOutBase<2,3>::write_tecplot did not write the variable name z. This is now fixed.
    git-svn-id: https://svn.dealii.org/trunk@9732 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/data_out_base.cc
+++ deal.II/base/source/data_out_base.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -2811,7 +2811,7 @@ void DataOutBase::write_tecplot (const std::vector<Patch<dim,spacedim> > &patche
 
     out << "Variables=";
 
-    switch (dim)
+    switch (spacedim)
       {
         case 1:
 	      out << "\"x\"";
