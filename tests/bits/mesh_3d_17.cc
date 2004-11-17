23f98f04ea38689ff09f6a6a6ce6552b2c426091
Wolfgang Bangerth <bangerth@math.tamu.edu>
2004-11-17 03:26:11 +0000

    Add comment.
    git-svn-id: https://svn.dealii.org/trunk@9778 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/mesh_3d_17.cc
+++ tests/bits/mesh_3d_17.cc
@@ -1,4 +1,20 @@
-// check TriaCellAccessor::neighbor_child_on_subface
+//----------------------------  mesh_3d_17.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2003, 2004 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  mesh_3d_17.cc  ---------------------------
+
+
+// we used to have an abort in an assertion in
+// TriaCellAccessor::neighbor_child_on_subface. it turned out that the
+// assertion was wrong. make sure it works now.
 
 #include "../tests.h"
 #include <base/quadrature_lib.h>
