b6b52a7e014c3f761c5e41f1e30fbd54a980b3ea
Wolfgang Bangerth <bangerth@math.tamu.edu>
2005-02-22 07:31:48 +0000

    Add virtual destructor.
    git-svn-id: https://svn.dealii.org/trunk@9905 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/data_out_base.cc
+++ deal.II/base/source/data_out_base.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -3964,6 +3964,13 @@ DataOutBase::write_gmv_reorder_data_vectors (const std::vector<Patch<dim,spacedi
 
 
 /* --------------------------- class DataOutInterface ---------------------- */
+
+
+template <int dim, int spacedim>
+DataOutInterface<dim,spacedim>::~DataOutInterface ()
+{}
+
+
 
 
 template <int dim, int spacedim>
