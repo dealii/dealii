a6c525c464dba3d763c83c6ae07cd77b5bcb92b2
Luca Heltai <luca.heltai@sissa.it>
2005-03-08 14:28:15 +0000

    iterator to const_iterator.
    git-svn-id: https://svn.dealii.org/trunk@10041 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/function_parser.cc
+++ deal.II/base/source/function_parser.cc
@@ -55,7 +55,8 @@ void FunctionParser<dim>::initialize(const std::string variables,
   for (unsigned int i=0; i<this->n_components; ++i) {
     // Add the variuous constants to the parser.
     std::map< std::string, double >::iterator
-      constant = constants.begin(),
+      constant = constants.begin();
+    std::map< std::string, double >::const_iterator
       endc  = constants.end();
     for(;constant != endc; ++constant) {
       AssertThrow( fp[i].AddConstant(constant->first, constant->second),
