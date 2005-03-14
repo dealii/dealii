c5a17c9ab4622460dcb375efb7b16c082eaa0362
Luca Heltai <luca.heltai@sissa.it>
2005-03-14 14:58:30 +0000

    Added get_fe_from_name.cc test. NOT WORKING.
    git-svn-id: https://svn.dealii.org/trunk@10123 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/get_fe_from_name.cc
@@ -0,0 +1,49 @@
+//----------------------------  get_fe_from_name.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005 by the deal.II authors 
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  get_fe_from_name.cc  ---------------------------
+// Test the get_fe_from_name function of the fe_tools. 
+
+#include "../tests.h"
+#include <fstream>
+#include <base/logstream.h>
+#include <fe/fe.h>
+#include <fe/fe_tools.h>
+
+int main () 
+{ 
+  std::ofstream logfile("get_fe_from_name.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  
+  FiniteElement<1> * fe1;
+  FiniteElement<2> * fe2;
+  FiniteElement<3> * fe3;
+  
+  std::string name;
+  name = "FE_Q(1)";
+  fe1 = FETools::get_fe_from_name(name);
+  fe2 = FETools::get_fe_from_name(name);
+  fe3 = FETools::get_fe_from_name(name);
+  
+  deallog << "Read " << name << std::endl;
+  deallog << "Generated :" << std::endl;
+  deallog << fe1->get_name()  << std::endl;
+  deallog << fe2->get_name()  << std::endl;
+  deallog << fe3->get_name()  << std::endl;
+  
+  delete fe1;
+  delete fe2;
+  delete fe3;
+  
+  return 0;
+}
+    
