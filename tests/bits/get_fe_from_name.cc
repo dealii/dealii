0418774318270f7f549fcd3e1c366d03fdc8de23
Luca Heltai <luca.heltai@sissa.it>
2005-03-14 17:29:40 +0000

    Almost extensive testing of get_fe_from_name.
    git-svn-id: https://svn.dealii.org/trunk@10131 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/get_fe_from_name.cc
+++ tests/bits/get_fe_from_name.cc
@@ -18,32 +18,74 @@
 #include <fe/fe.h>
 #include <fe/fe_tools.h>
 
-int main () 
-{ 
-  std::ofstream logfile("get_fe_from_name.output");
-  deallog.attach(logfile);
-  deallog.depth_console(0);
+class Test {
+public:
+  
+  void generate(const char * myname) {
+    std::string name = myname;
+    
+    fe1 = FETools::get_fe_from_name<1>(name);
+    fe2 = FETools::get_fe_from_name<2>(name);
+    fe3 = FETools::get_fe_from_name<3>(name);
+    
+    deallog << "Read " << name << std::endl;
+    deallog << "Generated :" << std::endl;
+    deallog << fe1->get_name()  << std::endl;
+    deallog << fe2->get_name()  << std::endl;
+    deallog << fe3->get_name()  << std::endl;
+    
+    delete fe1;
+    delete fe2;
+    delete fe3;
+  }
   
   FiniteElement<1> * fe1;
   FiniteElement<2> * fe2;
   FiniteElement<3> * fe3;
+ 
+};
+
+int main () 
+{  
+  std::ofstream logfile("get_fe_from_name.output");
+  deallog.attach(logfile);
   
-  std::string name;
-  name = "FE_Q(1)";
-  fe1 = FETools::get_fe_from_name<1>(name);
-  fe2 = FETools::get_fe_from_name<2>(name);
-  fe3 = FETools::get_fe_from_name<3>(name);
+  deallog.depth_console(0);
   
-  deallog << "Read " << name << std::endl;
-  deallog << "Generated :" << std::endl;
-  deallog << fe1->get_name()  << std::endl;
-  deallog << fe2->get_name()  << std::endl;
-  deallog << fe3->get_name()  << std::endl;
+  Test gen;
   
-  delete fe1;
-  delete fe2;
-  delete fe3;
+  gen.generate("FE_Q_Hierarchical(1)");
+  gen.generate("FE_DGPNonparametric(1)");
+  gen.generate("FE_DGQ(1)");
+  gen.generate("FE_Q(1)");
   
+  gen.generate("FE_Q_Hierarchical(2)");
+  gen.generate("FE_DGPNonparametric(2)");
+  gen.generate("FE_DGQ(2)");
+  gen.generate("FE_Q(2)");
+  
+  gen.generate("FESystem[FE_Q_Hierarchical(1)^2-FE_Q_Hierarchical(1)]");
+  gen.generate("FESystem[FE_DGPNonparametric(1)^2-FE_Q_Hierarchical(1)]");
+  gen.generate("FESystem[FE_DGQ(1)^2-FE_Q_Hierarchical(1)]");
+  gen.generate("FESystem[FE_Q(1)^2-FE_Q_Hierarchical(1)]");
+  
+  gen.generate("FESystem[FE_Q_Hierarchical(1)^2-FE_DGPNonparametric(1)]");
+  gen.generate("FESystem[FE_DGPNonparametric(1)^2-FE_DGPNonparametric(1)]");
+  gen.generate("FESystem[FE_DGQ(1)^2-FE_DGPNonparametric(1)]");
+  gen.generate("FESystem[FE_Q(1)^2-FE_DGPNonparametric(1)]");
+  
+  gen.generate("FESystem[FE_Q_Hierarchical(1)^2-FE_DGQ(1)]");
+  gen.generate("FESystem[FE_DGPNonparametric(1)^2-FE_DGQ(1)]");
+  gen.generate("FESystem[FE_DGQ(1)^2-FE_DGQ(1)]");
+  gen.generate("FESystem[FE_Q(1)^2-FE_DGQ(1)]");
+  
+  
+  gen.generate("FESystem[FE_Q_Hierarchical(1)^2-FE_Q(1)]");
+  gen.generate("FESystem[FE_DGPNonparametric(1)^2-FE_Q(1)]");
+  gen.generate("FESystem[FE_DGQ(1)^2-FE_Q(1)]");
+  gen.generate("FESystem[FE_Q(1)^2-FE_Q(1)]");
+  
+ 
   return 0;
 }
     
