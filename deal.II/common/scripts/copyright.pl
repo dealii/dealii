8ac35019a376f3d50d33506e2443480fad4a1b86
Guido Kanschat <dr.guido.kanschat@gmail.com>
2005-03-28 23:05:37 +0000

    Check copyright lines against log
    git-svn-id: https://svn.dealii.org/trunk@10268 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/common/scripts/copyright.pl
@@ -0,0 +1,62 @@
+######################################################################
+# $Id$
+#
+# Copyright
+#
+######################################################################
+# Check the copyright line of a file against cvs log
+#
+# Call: perl copyright.pl <filename>
+######################################################################
+
+use strict;
+
+my $file = $ARGV[0];
+
+print "# $file\n";
+
+my @log = `cvs log $file`;
+my %years;
+
+foreach (@log)
+{
+    next unless (m/^date: (\d\d\d\d)/);
+    my $year = $1;
+    $years{$year} = 1;
+}
+
+my $copystring;
+
+foreach (sort keys %years)
+{
+    if ($copystring)
+    {
+	$copystring .= ", $_";
+    } else {
+	$copystring = "$_";
+    }
+}
+
+my $copyreg = "Copyright \\(C\\) $copystring by the deal.II authors";
+$copystring = "Copyright (C) $copystring by the deal.II authors";
+
+my $found = 0;
+my $ok = 0;
+
+while(<>)
+{
+    next unless (m/(Copyright.*)/);
+    my $copyfile = $1;
+    $found = 1;
+    if (m/$copyreg/)
+    {
+	$ok = 1;
+    } else { 
+	print "perl -pi~ -e 's/$copyfile/$copystring/;' $file\n";
+    }
+}
+
+print "# OK: $copystring\n" if ($ok);
+
+print "# no copyright found\n" unless ($found);
+
