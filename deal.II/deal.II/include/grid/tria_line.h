8fbc210d49bdd17f5a6dd9e49586990f64b1c32c
Guido Kanschat <dr.guido.kanschat@gmail.com>
2005-03-29 00:32:19 +0000

    adjust copyright after last changes
    git-svn-id: https://svn.dealii.org/trunk@10279 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/grid/tria_line.h
+++ deal.II/deal.II/include/grid/tria_line.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 2000, 2001, 2002, 2003, 2004 by the deal.II authors
+//    Copyright (C) 1998, 2000, 2001, 2002, 2003, 2004, 2005 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
