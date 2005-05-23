2f2c6637ff3e49ae684c6e1e94de3468a9c7c278
Guido Kanschat <dr.guido.kanschat@gmail.com>
2005-05-23 22:17:05 +0000

    new module Quadrature
    git-svn-id: https://svn.dealii.org/trunk@10705 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/base/dox.h
+++ deal.II/doc/doxygen/headers/base/dox.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2003 by the deal.II authors
+//    Copyright (C) 2005 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -11,3 +11,25 @@
 //
 //-------------------------------------------------------------------------
 
+/**
+ * @defgroup Quadrature Quadrature formulæ
+ *
+ * This module contains the base class Quadrature as well as the
+ * quadrature formulæ provided by deal.II.
+ *
+ * The class QIterated is used, to construct an iterated quadrature
+ * formula out of an existing one, thereby increasing the accuracy of
+ * the formula without increasing the order.
+ *
+ * While the usual quadrature formulæ of higher dimensions
+ * generate tensor products which are equal in each direction, the
+ * class QAnisotropic generates tensor products of possibly different
+ * formulæ in each direction.
+ *
+ * The class QProjector is not actually a quadrature rule by itself,
+ * but it provides functions for computing the quadrature on the
+ * surfaces of higher dimensional cells.
+ *
+ * All other classes in this module actually implement quadrature
+ * rules of different order and other characteristics.
+ */
