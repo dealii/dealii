fa3fec3d56cfff8f4de6fa9749094618a184bcc7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2005-06-27 21:05:25 +0000

    New tests.
    git-svn-id: https://svn.dealii.org/trunk@10954 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/petsc_slowness_01.cc
@@ -0,0 +1,145 @@
+//----------------------------  petsc_slowness_01.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  petsc_slowness_01.cc  ---------------------------
+
+
+// this is part of a whole suite of tests that checks the relative speed of
+// using PETSc for sparse matrices as compared to the speed of our own
+// library. the tests therefore may not all actually use PETSc, but they are
+// meant to compare it
+//
+// the tests build the 5-point stencil matrix for a uniform grid of size N*N
+
+#include "../tests.h"
+#include <lac/sparse_matrix.h>
+#include <lac/vector.h>
+#include <lac/petsc_sparse_matrix.h>
+#include <fstream>
+#include <iostream>
+
+
+void test ()
+{
+  const unsigned int N = 200;
+
+                                   // first build the sparsity pattern
+  SparsityPattern sparsity (N*N, N*N, 5);
+  for(unsigned int i=0; i<N; i++)
+    for(unsigned int j=0; j<N; j++)
+      {
+        const unsigned int global = i*N+j;
+        sparsity.add(global, global);
+        if (j>0)
+          {
+            sparsity.add(global-1, global);
+            sparsity.add(global, global-1);
+          }
+        if (j<N-1)
+          {
+            sparsity.add(global+1, global);
+            sparsity.add(global, global+1);
+          }
+        if (i>0)
+          {
+            sparsity.add(global-N, global);
+            sparsity.add(global, global-N);
+          }
+        if (i<N-1)
+          {
+            sparsity.add(global+N, global);
+            sparsity.add(global, global+N);
+          }
+      }
+  sparsity.compress();
+  
+
+                                   // next build the sparse matrix itself
+  SparseMatrix<double> matrix (sparsity);
+  for(unsigned int i=0; i<N; i++)
+    for(unsigned int j=0; j<N; j++)
+      {
+        const unsigned int global = i*N+j;
+        matrix.add(global, global, 4);
+        if (j>0)
+          {
+            matrix.add(global-1, global, -1);
+            matrix.add(global, global-1, -1);
+          }
+        if (j<N-1)
+          {
+            matrix.add(global+1, global, -1);
+            matrix.add(global, global+1, -1);
+          }
+        if (i>0)
+          {
+            matrix.add(global-N, global, -1);
+            matrix.add(global, global-N, -1);
+          }
+        if (i<N-1)
+          {
+            matrix.add(global+N, global, -1);
+            matrix.add(global, global+N, -1);
+          }
+      }
+  
+                                   // then do a single matrix-vector
+                                   // multiplication with subsequent formation
+                                   // of the matrix norm
+  Vector<double> v1(N*N), v2(N*N);
+  for (unsigned int i=0; i<N*N; ++i)
+    v1(i) = i;
+  matrix.vmult (v2, v1);
+  
+  deallog << v1*v2 << std::endl;
+}
+
+
+
+int main (int argc,char **argv) 
+{
+  std::ofstream logfile("petsc_slowness_01.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  try
+    {
+      PetscInitialize(&argc,&argv,0,0);
+      {
+        test ();
+      }
+      PetscFinalize();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+		<< exc.what() << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      
+      return 1;
+    }
+  catch (...) 
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    };
+}
---
+++ tests/bits/petsc_slowness_02.cc
@@ -0,0 +1,116 @@
+//----------------------------  petsc_slowness_02.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  petsc_slowness_02.cc  ---------------------------
+
+
+// this is part of a whole suite of tests that checks the relative speed of
+// using PETSc for sparse matrices as compared to the speed of our own
+// library. the tests therefore may not all actually use PETSc, but they are
+// meant to compare it
+//
+// the tests build the 5-point stencil matrix for a uniform grid of size N*N
+
+#include "../tests.h"
+#include <lac/sparse_matrix.h>
+#include <lac/petsc_sparse_matrix.h>
+#include <lac/petsc_vector.h>
+#include <fstream>
+#include <iostream>
+
+
+void test ()
+{
+  const unsigned int N = 200;
+
+                                   // build the sparse matrix 
+  PETScWrappers::SparseMatrix matrix (N*N, N*N, 5);
+  for(unsigned int i=0; i<N; i++)
+    for(unsigned int j=0; j<N; j++)
+      {
+        const unsigned int global = i*N+j;
+        matrix.add(global, global, 4);
+        if (j>0)
+          {
+            matrix.add(global-1, global, -1);
+            matrix.add(global, global-1, -1);
+          }
+        if (j<N-1)
+          {
+            matrix.add(global+1, global, -1);
+            matrix.add(global, global+1, -1);
+          }
+        if (i>0)
+          {
+            matrix.add(global-N, global, -1);
+            matrix.add(global, global-N, -1);
+          }
+        if (i<N-1)
+          {
+            matrix.add(global+N, global, -1);
+            matrix.add(global, global+N, -1);
+          }
+      }
+  matrix.compress ();
+  
+  
+                                   // then do a single matrix-vector
+                                   // multiplication with subsequent formation
+                                   // of the matrix norm
+  PETScWrappers::Vector v1(N*N), v2(N*N);
+  for (unsigned int i=0; i<N*N; ++i)
+    v1(i) = i;
+  matrix.vmult (v2, v1);
+  
+  deallog << v1*v2 << std::endl;
+}
+
+
+
+int main (int argc,char **argv) 
+{
+  std::ofstream logfile("petsc_slowness_02.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  try
+    {
+      PetscInitialize(&argc,&argv,0,0);
+      {
+        test ();
+      }
+      PetscFinalize();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+		<< exc.what() << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      
+      return 1;
+    }
+  catch (...) 
+    {
+      std::cerr << std::endl << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+		<< "Aborting!" << std::endl
+		<< "----------------------------------------------------"
+		<< std::endl;
+      return 1;
+    };
+}
