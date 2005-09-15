f452dbf7c0bc55d994c877febd4ad9bc568a6783
Guido Kanschat <dr.guido.kanschat@gmail.com>
2005-09-15 13:52:54 +0000

    Tar-file generator
    git-svn-id: https://svn.dealii.org/trunk@11451 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/common/scripts/make_tarfile.sh
@@ -0,0 +1,19 @@
+######################################################################
+# We assume that the most recent checked out version of the branch
+# is in subdir checkout and configured.
+#
+# Will create both tar files (source and documentation)
+#
+######################################################################
+
+MAJOR=`perl -n -e "m/(\\d+)\\.(\\d+)\\.(\\d+)/; print \\$1" checkout/Version`
+MINOR=`perl -n -e "m/(\\d+)\\.(\\d+)\\.(\\d+)/; print \\$2" checkout/Version`
+PATCH=`perl -n -e "m/(\\d+)\\.(\\d+)\\.(\\d+)/; print \\$3" checkout/Version`
+
+cvs -d `cat checkout/CVS/Root` export -kv -r Version-$MAJOR-$MINOR-$PATCH deal.II
+tar czf deal.II-$MAJOR.$MINOR.$PATCH.tar.gz deal.II
+cd deal.II
+./configure --with-umfpack
+make online-doc
+cd ..
+tar czf deal.doc-$MAJOR.$MINOR.$PATCH.tar.gz deal.II/doc/doxygen
