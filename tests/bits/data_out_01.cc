4edabb6b05c0fcab9e39f2375cc2f03db75431c4
Guido Kanschat <dr.guido.kanschat@gmail.com>
2005-09-15 14:32:29 +0000

    remove redundancy
    git-svn-id: https://svn.dealii.org/trunk@11453 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/data_out_01.cc
+++ tests/bits/data_out_01.cc
@@ -1,15 +1,15 @@
-//----------------------------  data_out_01.cc  ---------------------------
+//----------------------------------------------------------------------
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2003, 2004 by the deal.II authors
+//    Copyright (C) 2003, 2004, 2005 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  data_out_01.cc  ---------------------------
+//----------------------------------------------------------------------
 
 #include "../tests.h"
 #include "data_out_common.cc"
