62e1a14deded225363d4475d571c7d87fd30e37c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2005-10-12 02:04:04 +0000

    Mark one parameter as const.
    git-svn-id: https://svn.dealii.org/trunk@11589 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/fe/fe_dgp.cc
+++ deal.II/deal.II/source/fe/fe_dgp.cc
@@ -87,7 +87,7 @@ FE_DGP<dim>::clone() const
 
 template <int dim>
 std::vector<unsigned int>
-FE_DGP<dim>::get_dpo_vector(unsigned int deg)
+FE_DGP<dim>::get_dpo_vector(const unsigned int deg)
 {
   std::vector<unsigned int> dpo(dim+1, 0U);
   dpo[dim] = ++deg;
