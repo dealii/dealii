240e0f6ceb302215573b9c89b7f780fcaa54eda3
Wolfgang Bangerth <bangerth@math.tamu.edu>
2005-10-17 13:37:21 +0000

    New test.
    git-svn-id: https://svn.dealii.org/trunk@11608 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/oliver_01.cc
@@ -0,0 +1,44 @@
+//----------------------------  oliver_01.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005 by the deal.II authors and Oliver Kayser-Herold
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  oliver_01.cc  ---------------------------
+
+
+// Oliver found an example, where sparse_matrix_iterator->value=0 didn't work,
+// because the iterator->value expects a double on the right hand side, not an
+// integer. If the right hand side is zero, it can also be converted to a
+// pointer, which leads to an ambiguity. Fix this by having an additional
+// operator= in the iterator/reference class
+
+#include "../tests.h"
+#include <lac/sparse_matrix.h>
+#include <iostream>
+#include <fstream>
+
+
+int main () 
+{
+  std::ofstream logfile("oliver_01.output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+                                   // this test only needs to compile, not run
+  if (false)
+    {
+      SparseMatrix<double>::iterator *i;
+      (*i)->value () = (int)0;
+    }
+
+  deallog << "OK" << std::endl;
+  
+  return 0;
+}
