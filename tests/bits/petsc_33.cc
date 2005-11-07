563b59025806e5b6ff96e3cebb1fd495936f3102
Ralf Hartmann <Ralf.Hartmann@dlr.de>
2005-11-07 11:56:30 +0000

    Make it compile also for PetscScalar=float.
    git-svn-id: https://svn.dealii.org/trunk@11724 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/petsc_33.cc
+++ tests/bits/petsc_33.cc
@@ -34,8 +34,8 @@ void test (PETScWrappers::Vector &v)
 
                                    // then check the norm
   const double eps=typeid(PetscScalar)==typeid(double) ? 1e-14 : 1e-5;
-  Assert (std::fabs(v.lp_norm(3) - std::pow(sum, 1./3.)) <
-          eps*std::pow(sum, 1./3.),
+  const double true_value=std::pow(sum, static_cast<PetscScalar> (1./3.));
+  Assert (std::fabs(v.lp_norm(3) - true_value) < eps*true_value,
           ExcInternalError());
 
   deallog << "OK" << std::endl;
