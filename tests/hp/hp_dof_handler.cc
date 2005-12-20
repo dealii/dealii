e9e0f8e7be5476494af772c238b68a4aee51c4ed
oliver <oliver@0785d39b-7218-0410-832d-ea1e28bc413d>
2005-12-20 22:08:16 +0000

    Added a simple test for the hpDoFHandler, which tests the behaviour when coarsening and refining is applied to the triangulation.
    git-svn-id: https://svn.dealii.org/trunk@11900 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/hp_dof_handler.cc
@@ -0,0 +1,60 @@
+//----------------------------  mg_dof_handler.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  mg_dof_handler.cc  ---------------------------
+
+
+/* Author: Ralf Hartmann, 2005, O. Kayser-Herold, simply modified
+  the mg_dof_handler.cc test for the hpDoFHandler. */
+
+#include <base/logstream.h>
+#include <grid/tria.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_accessor.h>
+#include <grid/grid_out.h>
+#include <fe/fe_dgq.h>
+#include <dofs/hp_dof_handler.h>
+
+#include <fstream>
+
+
+int main ()
+{
+  std::ofstream logfile("hp_dof_handler/output");
+  logfile.precision(2);
+  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  const unsigned int dim=2;
+  Triangulation<dim> tria;
+  GridGenerator::hyper_cube(tria);
+
+  FECollection<dim> fe_collection;
+  fe_collection.add_fe (FE_DGQ<dim> (1));
+
+  hpDoFHandler<dim> dof_handler(tria);
+  
+  tria.begin_active()->set_refine_flag();
+  tria.execute_coarsening_and_refinement();
+
+  Triangulation<dim>::active_cell_iterator
+    cell=tria.begin_active(),
+    endc=tria.end();
+  for (; cell!=endc; ++cell)
+    cell->set_coarsen_flag();
+  tria.execute_coarsening_and_refinement();
+
+  dof_handler.distribute_dofs(fe_collection);
+
+  deallog << "OK" << std::endl;
+}
