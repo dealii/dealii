4ba8be135acdca4d42e64399310013594db838d7
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-01-09 21:29:05 +0000

    visualization tools
    git-svn-id: https://svn.dealii.org/trunk@11973 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/base/software.h
@@ -0,0 +1,41 @@
+/**
+ * @page DEALSoftware Third party software deal.II collaborates with.
+ *
+ * @note Names of software products are trademarks of their respective owners.
+ *
+ * @section SoftwareVis Visualization tools
+ *
+ * @subsection SoftwareAVS AVS Express
+ *
+ * deal.II reads and writes the UCD format specified in the AVS
+ * Express user manual. See http://www.avs.com for more details on
+ * this software.
+ *
+ * @subsection SoftwareGnuplot gnuplot
+ *
+ * Two-dimensional data can be written in a format suitable for
+ * gnuplot, even on locally refined and unstructured meshes, See
+ * http://www.gnuplot.info/
+ *
+ * @subsection SoftwareOpenDX
+ *
+ * The former IBM Visual Data Explorer, now an OpenSource project at
+ * http://www.opendx.org.
+ *
+ * @subsection SoftwarePovray
+ *
+ * While it is not actually taylored to scientific visualization, you
+ * may be able to produce impressive pictures of three-dimensional
+ * deformed bodies with http://www.povray.org
+ *
+ * @subsection SoftwareTecplot Tecplot
+ *
+ * deal.II writes textual and binary files for Tecplot. @see
+ * http://www.tecplot.com for more details on this software.
+ *
+ * @subsection @SoftwareXFig XFig
+ *
+ * Not a visualization tool at all, you can write grids in XFig format
+ * 3.2. Those can be postprocessed manually within XFig and written in
+ * many different graphics formats. See http://www.xfig.org
+ */
