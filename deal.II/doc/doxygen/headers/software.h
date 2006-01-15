f6828d02ce99e8063bca0f6f0c56fb7a03a568ba
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-01-15 01:08:41 +0000

    modify structure
    git-svn-id: https://svn.dealii.org/trunk@12033 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/software.h
+++ deal.II/doc/doxygen/headers/software.h
@@ -3,57 +3,60 @@
  *
  * @note Names of software products are trademarks of their respective owners.
  *
- * 
- * @section SoftwareVis Visualization tools
+ * @section SodtwarePrePost Pre- and Postprocessing
+ *
+ * @subsection SoftwareVis Visualization tools
  *
  * This is a list of visualization formats and software that deal.II
  * supports. Data in these formats is written by the DataOutBase and in parts
  * by the GridOut classes (see @ref output).
  * 
  *
- * @subsection SoftwareAVS AVS Express
+ * @subsubsection SoftwareAVS AVS Express
  *
  * deal.II reads and writes the UCD format specified in the AVS
  * Express user manual. See http://www.avs.com/ for more details on
  * this software.
  *
  * 
- * @subsection SoftwareGnuplot gnuplot
+ * @subsubsection SoftwareGnuplot gnuplot
  *
  * Two-dimensional data can be written in a format suitable for
  * gnuplot, even on locally refined and unstructured meshes, See
  * http://www.gnuplot.info/
  *
  * 
- * @subsection SoftwareOpenDX OpenDX
+ * @subsubsection SoftwareOpenDX OpenDX
  *
  * The former IBM Visual Data Explorer, now an OpenSource project at
  * http://www.opendx.org/.
  *
  * 
- * @subsection SoftwarePovray Povray
+ * @subsubsection SoftwarePovray Povray
  *
  * While it is not actually taylored to scientific visualization, you
  * may be able to produce impressive pictures of three-dimensional
  * deformed bodies with http://www.povray.org/
  *
  * 
- * @subsection SoftwareTecplot Tecplot
+ * @subsubsection SoftwareTecplot Tecplot
  *
  * deal.II writes textual and binary files for Tecplot. See
  * http://www.tecplot.com for more details on this software.
  *
  * 
- * @subsection SoftwareXFig XFig
+ * @subsubsection SoftwareXFig XFig
  *
  * Though not a visualization tool at all, you can nevertheless write grids in
  * XFig format 3.2. Those can be postprocessed manually within XFig and
  * written in many different graphics formats. See http://www.xfig.org/
  *
  *
- * @subsection SoftwareVTK Visualization Toolkit (VTK)
+ * @subsubsection SoftwareVTK Visualization Toolkit (VTK)
  *
- * VTK is an increasingly popular open format supported by a number of newer
- * visualization projects such as ParaView, VisIt, or MayaVi. See
+ * VTK is an open format supported by a number of visualization
+ * projects such as ParaView, VisIt, or MayaVi. See
  * http://www.kitware.com/vtk.html
+ *
+ * @section SoftwareLibs Libraries used inside deal.II
  */
