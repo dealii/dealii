011556e7d7e7f06fd0ce6b4079a943b612d76c1d
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-01-16 13:24:01 +0000

    test different dx formats; no evaluation yet
    git-svn-id: https://svn.dealii.org/trunk@12048 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/data_out_base_dx.cc
@@ -0,0 +1,79 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+#include "../tests.h"
+#include <base/data_out_base.h>
+#include <base/logstream.h>
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "patches.h"
+
+// Output data on repetitions of the unit hypercube
+
+template <int dim, int spacedim>
+void check(DataOutBase::DXFlags flags,
+	   std::ostream& out)
+{
+  const unsigned int np = 4;
+  
+  std::vector<DataOutBase::Patch<dim, spacedim> > patches(np);
+  
+  create_patches(patches);
+  
+  std::vector<std::string> names(5);
+  names[0] = "x1";
+  names[1] = "x2";
+  names[2] = "x3";
+  names[3] = "x4";
+  names[4] = "i";
+  DataOutBase::write_dx(patches, names, flags, out);
+}
+
+
+template<int dim, int spacedim>
+void check_all()
+{
+  DataOutBase::DXFlags flags(false, false, false, false);
+  if (true) {
+    std::ofstream out("data_out_base_dx/fff3ff22.dx");
+    check<dim,spacedim>(flags, out);
+  }
+  flags.int_binary = true;
+  if (true) {
+    std::ofstream out("data_out_base_dx/tff3ff22.dx");
+    check<dim,spacedim>(flags, out);
+  }
+  flags.coordinates_binary = true;
+  if (true) {
+    std::ofstream out("data_out_base_dx/ttf3ff22.dx");
+    check<dim,spacedim>(flags, out);
+  }
+  flags.data_binary = true;
+  if (true) {
+    std::ofstream out("data_out_base_dx/ttt3ff22.dx");
+    check<dim,spacedim>(flags, out);
+  }
+}
+
+int main()
+{
+  check_all<1,1>();
+  check_all<1,2>();
+  check_all<2,2>();
+  check_all<2,3>();
+  check_all<3,3>();  
+}
