0d1cd2abc91dbe3e25bced25868114c4bfc73276
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-10 21:56:17 +0000

    Check that left bound < right bound for hyper_cube.
    git-svn-id: https://svn.dealii.org/trunk@12306 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/grid/grid_generator.cc
+++ deal.II/deal.II/source/grid/grid_generator.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -131,6 +131,9 @@ void GridGenerator::hyper_cube (Triangulation<dim> &tria,
 			        const double        left,
 			        const double        right)
 {
+  Assert (left < right,
+	  ExcMessage ("Invalid left and right bounds of hypercube"));
+  
   Point<dim> p1;
   Point<dim> p2;
   for (unsigned int i=0;i<dim;++i)
