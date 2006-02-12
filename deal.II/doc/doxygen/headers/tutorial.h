c051e4a388d0dedfb3aeed8121244445fd0a0031
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-12 20:02:32 +0000

    Nicer format.
    git-svn-id: https://svn.dealii.org/trunk@12337 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/tutorial.h
+++ deal.II/doc/doxygen/headers/tutorial.h
@@ -27,91 +27,61 @@
  * jump to any of these programs directly, simply follow one of the
  * links below:
  * 
- * <ul>
- *
- * <li>
+ * <table align="center">
+ * <tr>
+ * <td>
+ * 
  * @anchor step_1 <a href="../../tutorial/chapter-2.step-by-step/step-1.html">step-1</a>
- * </li>
  * 
- * <li>
  * @anchor step_2 <a href="../../tutorial/chapter-2.step-by-step/step-2.html">step-2</a>
- * </li>
  * 
- * <li>
  * @anchor step_3 <a href="../../tutorial/chapter-2.step-by-step/step-3.html">step-3</a>
- * </li>
  * 
- * <li>
  * @anchor step_4 <a href="../../tutorial/chapter-2.step-by-step/step-4.html">step-4</a>
- * </li>
  * 
- * <li>
  * @anchor step_5 <a href="../../tutorial/chapter-2.step-by-step/step-5.html">step-5</a>
- * </li>
  * 
- * <li>
  * @anchor step_6 <a href="../../tutorial/chapter-2.step-by-step/step-6.html">step-6</a>
- * </li>
  * 
- * <li>
+ * </td>
+ * <td>
+ * 
  * @anchor step_7 <a href="../../tutorial/chapter-2.step-by-step/step-7.html">step-7</a>
- * </li>
  * 
- * <li>
  * @anchor step_8 <a href="../../tutorial/chapter-2.step-by-step/step-8.html">step-8</a>
- * </li>
  * 
- * <li>
  * @anchor step_9 <a href="../../tutorial/chapter-2.step-by-step/step-9.html">step-9</a>
- * </li>
  * 
- * <li>
  * @anchor step_10 <a href="../../tutorial/chapter-2.step-by-step/step-10.html">step-10</a>
- * </li>
  * 
- * <li>
  * @anchor step_11 <a href="../../tutorial/chapter-2.step-by-step/step-11.html">step-11</a>
- * </li>
  * 
- * <li>
  * @anchor step_12 <a href="../../tutorial/chapter-2.step-by-step/step-12.html">step-12</a>
- * </li>
  * 
- * <li>
+ * </td>
+ * <td>
+ * 
  * @anchor step_13 <a href="../../tutorial/chapter-2.step-by-step/step-13.html">step-13</a>
- * </li>
  * 
- * <li>
  * @anchor step_14 <a href="../../tutorial/chapter-2.step-by-step/step-14.html">step-14</a>
- * </li>
  * 
- * <li>
  * @anchor step_15 <a href="../../tutorial/chapter-2.step-by-step/step-15.html">step-15</a>
- * </li>
  * 
- * <li>
  * @anchor step_16 <a href="../../tutorial/chapter-2.step-by-step/step-16.html">step-16</a>
- * </li>
  * 
- * <li>
  * @anchor step_17 <a href="../../tutorial/chapter-2.step-by-step/step-17.html">step-17</a>
- * </li>
  * 
- * <li>
  * @anchor step_18 <a href="../../tutorial/chapter-2.step-by-step/step-18.html">step-18</a>
- * </li>
  * 
- * <li>
+ * </td>
+ * <td>
+ * 
  * @anchor step_19 <a href="../../tutorial/chapter-2.step-by-step/step-19.html">step-19</a>
- * </li>
  * 
- * <li>
  * @anchor step_20 <a href="../../tutorial/chapter-2.step-by-step/step-20.html">step-20</a>
- * </li>
  * 
- * <li>
  * @anchor step_21 <a href="../../tutorial/chapter-2.step-by-step/step-21.html">step-21</a>
- * </li>
  * 
- * </ul>
+ * </td>
+ * </tr>
  */
