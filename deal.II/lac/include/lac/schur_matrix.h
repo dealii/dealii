b195900c4c0fe8fc1a7059dd802d93ff0d116738
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-12 04:56:15 +0000

    Use standard indentation.
    git-svn-id: https://svn.dealii.org/trunk@12329 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/schur_matrix.h
+++ deal.II/lac/include/lac/schur_matrix.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2001, 2002, 2003, 2004, 2005 by the deal.II authors
+//    Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -89,8 +89,7 @@
  * @author Guido Kanschat, 2000, 2001, 2002
  */
 template <class MA_inverse, class MB, class MDt, class MC>
-class SchurMatrix :
-  public Subscriptor
+class SchurMatrix : public Subscriptor
 {
   public:
 
