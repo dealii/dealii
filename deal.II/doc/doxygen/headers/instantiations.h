c1ad3c4ddbc043ead0c30b2cb18efab8da1cbed0
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-12 18:26:15 +0000

    Add copyright notice.
    git-svn-id: https://svn.dealii.org/trunk@12333 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/instantiations.h
+++ deal.II/doc/doxygen/headers/instantiations.h
@@ -1,3 +1,16 @@
+//-------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-------------------------------------------------------------------------
+
 /**
  * @page Instantiations Template instantiations
  *
