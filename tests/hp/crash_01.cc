0ab982c9a1901d2286b3424ef2a43a1cf3086f18
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-13 22:52:16 +0000

    Add a crash test that shouldn't crash. Gotta investigate.
    git-svn-id: https://svn.dealii.org/trunk@12371 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/crash_01.cc
@@ -0,0 +1,63 @@
+//----------------------------  crash_01.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  crash_01.cc  ---------------------------
+
+
+// check a crash in hp::DoFHandler<2>::reserve_space found 2/13/2006
+
+
+#include <base/logstream.h>
+#include <grid/tria.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_accessor.h>
+#include <grid/grid_out.h>
+#include <fe/fe_dgq.h>
+#include <dofs/hp_dof_handler.h>
+
+#include <fstream>
+
+
+int main ()
+{
+  std::ofstream logfile("crash_01/output");
+  logfile.precision(2);
+  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  {
+    const unsigned int dim=2;
+    Triangulation<dim> tria;
+    GridGenerator::hyper_cube(tria);
+
+    hp::FECollection<dim> fe_collection;
+    fe_collection.add_fe (FE_DGQ<dim> (1));
+
+    hp::DoFHandler<dim> dof_handler(tria);
+    dof_handler.distribute_dofs(fe_collection);
+  }
+
+  {
+    const unsigned int dim=3;
+    Triangulation<dim> tria;
+    GridGenerator::hyper_cube(tria);
+
+    hp::FECollection<dim> fe_collection;
+    fe_collection.add_fe (FE_DGQ<dim> (1));
+
+    hp::DoFHandler<dim> dof_handler(tria);
+    dof_handler.distribute_dofs(fe_collection);
+  }
+  
+  deallog << "OK" << std::endl;
+}
