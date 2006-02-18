e3f0ed14eab0765a46bcc899644250a3a61c50da
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-18 00:45:49 +0000

    Add new crashing test
    git-svn-id: https://svn.dealii.org/trunk@12405 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/crash_02.cc
@@ -0,0 +1,55 @@
+//----------------------------  crash_02.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  crash_02.cc  ---------------------------
+
+
+// check a crash in hp::DoFHandler because the
+// DoFHandler::active_fe_indices array isn't initialized when
+// attaching to a triangulation
+
+
+#include <base/logstream.h>
+#include <grid/tria.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_accessor.h>
+#include <grid/grid_out.h>
+#include <grid/tria_iterator.h>
+#include <dofs/hp_dof_handler.h>
+#include <dofs/dof_accessor.h>
+#include <fe/fe_dgq.h>
+
+#include <fstream>
+
+
+int main ()
+{
+  std::ofstream logfile("crash_02/output");
+  logfile.precision(2);
+  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  {
+    const unsigned int dim=2;
+    Triangulation<dim> tria;
+    GridGenerator::hyper_cube(tria);
+
+    hp::FECollection<dim> fe_collection;
+    fe_collection.push_back (FE_DGQ<dim> (1));
+
+    hp::DoFHandler<dim> dof_handler(tria);
+    dof_handler.begin_active()->set_active_fe_index(0);
+  }
+  
+  deallog << "OK" << std::endl;
+}
