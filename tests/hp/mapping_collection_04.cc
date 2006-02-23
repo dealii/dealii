7a4d483c44e3140107a7cb244436b1d194fdaccb
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-23 02:58:05 +0000

    Even simpler testcase.
    git-svn-id: https://svn.dealii.org/trunk@12472 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/mapping_collection_04.cc
@@ -0,0 +1,54 @@
+//----------------------------  mapping_collection_04.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  mapping_collection_04.cc  ---------------------------
+
+
+// a test that shows that mapping_collection_0[1-3] really is due to
+// the fact that MappingQ has a dysfunctional copy constructor...
+
+
+#include <base/logstream.h>
+#include <fe/mapping_collection.h>
+#include <fe/mapping_q.h>
+
+#include <fstream>
+
+
+template <int dim>
+void test ()
+{
+  MappingQ<dim> mapping(2);
+  {
+    deallog << "Copying..." << std::endl;
+    MappingQ<dim> copy(mapping);
+    deallog << "Deleting clone..." << std::endl;
+  }
+  deallog << "Destroying original..." << std::endl;  
+}
+
+
+
+int main ()
+{
+  std::ofstream logfile("mapping_collection_04/output");
+  logfile.precision(2);
+  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  test<1> ();
+  test<2> ();
+  test<3> ();
+  
+  deallog << "OK" << std::endl;
+}
