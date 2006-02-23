1b9cb8752318a481f13c835272742ff62ba65021
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-02-23 02:38:33 +0000

    An even simpler testcase that also fails.
    git-svn-id: https://svn.dealii.org/trunk@12471 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/mapping_collection_03.cc
@@ -0,0 +1,54 @@
+//----------------------------  mapping_collection_03.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  mapping_collection_03.cc  ---------------------------
+
+
+// a test that triggers really hard to track down failures in
+// mapping_collection_01 in a really simple way; an even further
+// simplified form of mapping_collection_02
+
+
+#include <base/logstream.h>
+#include <fe/mapping_collection.h>
+#include <fe/mapping_q.h>
+
+#include <fstream>
+
+
+template <int dim>
+void test ()
+{
+  MappingQ<dim> mapping(2);
+  deallog << "Cloning..." << std::endl;
+  Mapping<dim> *copy = mapping.clone();
+  deallog << "Deleting clone..." << std::endl;
+  delete copy;
+  deallog << "Destroying original..." << std::endl;  
+}
+
+
+
+int main ()
+{
+  std::ofstream logfile("mapping_collection_03/output");
+  logfile.precision(2);
+  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  test<1> ();
+  test<2> ();
+  test<3> ();
+  
+  deallog << "OK" << std::endl;
+}
