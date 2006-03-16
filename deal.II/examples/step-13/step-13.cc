7f6dc880ed6409a5dac547a8a65a877237ac2fbb
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-03-16 17:00:11 +0000

    Use proper formulas
    git-svn-id: https://svn.dealii.org/trunk@12635 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/step-13/step-13.cc
+++ deal.II/examples/step-13/step-13.cc
@@ -1768,7 +1768,7 @@ namespace LaplaceSolver
 				 //
 				 // For this example, let us choose as
 				 // exact solution the function
-				 // u(x,y)=exp(x+sin(10y+5x^2)). In more
+				 // $u(x,y)=exp(x+sin(10y+5x^2))$. In more
 				 // than two dimensions, simply repeat
 				 // the sine-factor with ``y''
 				 // replaced by ``z'' and so on. Given
