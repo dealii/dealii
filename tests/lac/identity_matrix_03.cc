b104ad965c6ca25082df11d0184a5c69ab165f13
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-04-24 23:06:30 +0000

    New tests
    git-svn-id: https://svn.dealii.org/trunk@12869 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lac/identity_matrix_03.cc
@@ -0,0 +1,78 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+// check assignment from IdentityMatrix to FullMatrix
+
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <lac/identity_matrix.h>
+#include <lac/full_matrix.h>
+#include <lac/vector.h>
+
+#include <fstream>
+#include <iostream>
+#include <cmath>
+
+template<typename number>
+void
+check_vmult()
+{
+  FullMatrix<number> M;
+  M = IdentityMatrix(4);
+  Vector<number> u(4);
+  Vector<number> v(4);
+
+  for (unsigned int i=0;i<4;++i)
+    u(i) = i+1;
+  
+  M.vmult(v,u);
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+
+  M.vmult_add(v,u);
+  v /= 2;
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+
+  M.Tvmult(v,u);
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+
+  M.Tvmult_add(v,u);
+  v /= 2;
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+}
+
+
+int main()
+{
+  std::ofstream logfile("identity_matrix_03/output");
+  logfile.setf(std::ios::fixed);
+  logfile.precision(0);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  check_vmult<double>();
+  check_vmult<float>();
+}
---
+++ tests/lac/identity_matrix_04.cc
@@ -0,0 +1,82 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+// check conversion constructor from IdentityMatrix to SparseMatrix
+
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <lac/identity_matrix.h>
+#include <lac/sparse_matrix.h>
+#include <lac/vector.h>
+
+#include <fstream>
+#include <iostream>
+#include <cmath>
+
+template<typename number>
+void
+check_vmult()
+{
+  SparsityPattern sp(4,4,1);
+  for (unsigned int i=0; i<4; ++i)
+    sp.add (i,i);
+  sp.compress ();
+  
+  SparseMatrix<number> M(sp, IdentityMatrix(4));
+  Vector<number> u(4);
+  Vector<number> v(4);
+
+  for (unsigned int i=0;i<4;++i)
+    u(i) = i+1;
+  
+  M.vmult(v,u);
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+
+  M.vmult_add(v,u);
+  v /= 2;
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+
+  M.Tvmult(v,u);
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+
+  M.Tvmult_add(v,u);
+  v /= 2;
+  Assert (v == u, ExcInternalError());
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << v(i);
+  deallog << std::endl;
+}
+
+
+int main()
+{
+  std::ofstream logfile("identity_matrix_04/output");
+  logfile.setf(std::ios::fixed);
+  logfile.precision(0);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  check_vmult<double>();
+  check_vmult<float>();
+}
