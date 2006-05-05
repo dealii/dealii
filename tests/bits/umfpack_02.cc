41e1df4b25d933448bd3895ba57b8cc292befbda
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-05-05 10:33:16 +0000

    fixed output and copyright
    git-svn-id: https://svn.dealii.org/trunk@13062 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/umfpack_02.cc
+++ tests/bits/umfpack_02.cc
@@ -1,15 +1,15 @@
-//----------------------------  umfpack_02.cc  ---------------------------
+//----------------------------------------------------------------------
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2002, 2003, 2004, 2005 by the deal.II authors and Brian Carnes
+//    Copyright (C) 2002, 2003, 2004, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  umfpack_02.cc  ---------------------------
+//----------------------------------------------------------------------
 
 // test the umfpack sparse direct solver on a mass matrix; test that the
 // functions that allow for repeated solves yield the correct result even
--- tests/bits/umfpack_03.cc
+++ tests/bits/umfpack_03.cc
@@ -1,15 +1,15 @@
-//----------------------------  umfpack_03.cc  ---------------------------
+//----------------------------------------------------------------------
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2002, 2003, 2004, 2005 by the deal.II authors and Brian Carnes
+//    Copyright (C) 2002, 2003, 2004, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
 //    to the file deal.II/doc/license.html for the  text  and
 //    further information on this license.
 //
-//----------------------------  umfpack_03.cc  ---------------------------
+//----------------------------------------------------------------------
 
 // test the umfpack sparse direct solver on a mass matrix that is slightly modified to make it nonsymmetric
 
