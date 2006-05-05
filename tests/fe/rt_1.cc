56480a352e501dc607aae60d60c008e27955a989
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-05-05 09:40:35 +0000

    new output verified
    git-svn-id: https://svn.dealii.org/trunk@13059 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/fe/rt_1.cc
+++ tests/fe/rt_1.cc
@@ -2,7 +2,7 @@
 //    rt_1.cc,v 1.3 2003/06/09 16:00:38 wolf Exp
 //    Version: 
 //
-//    Copyright (C) 2003, 2005 by the deal.II authors
+//    Copyright (C) 2003, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -13,6 +13,7 @@
 
 
 // Show the shape functions of the Raviart-Thomas element on the unit cell
+// Plots are gnuplot compatible if lines with desired prefix are selected.
 
 #include "../tests.h"
 #include <base/logstream.h>
@@ -20,6 +21,7 @@
 
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 #define PRECISION 2
@@ -30,44 +32,51 @@ template<int dim>
 inline void
 plot_shape_functions(const unsigned int degree)
 {
-  deallog << "FE_RaviartThomas<" << dim << "> (" << degree << ")"
-	  << std::endl;
-
+  std::ostringstream prefix;
+  prefix << "RT<" << dim << ">(" << degree << ')';
+  
+  deallog.push(prefix.str());
   FE_RaviartThomas<dim> fe_rt(degree);
 
   const unsigned int div=2;
   for (unsigned int mz=0;mz<=((dim>2) ? div : 0) ;++mz)
     for (unsigned int my=0;my<=((dim>1) ? div : 0) ;++my)
-      for (unsigned int mx=0;mx<=div;++mx)
-        {
-          const Point<dim> p = (dim==2 ?
-                                Point<dim>(1.*mx/div, 1.*my/div) :
-                                Point<dim>(1.*mx/div, 1.*my/div, 1.*mz/div));
-          deallog << "q_point = " << p << std::endl;
-	      
-          for (unsigned int i=0;i<fe_rt.dofs_per_cell;++i)
-            {
-              deallog << "  phi(" << i << ") = [";
-              for (unsigned int c=0; c<dim; ++c)
-                deallog << " " << fe_rt.shape_value_component(i,p,c);
-              deallog << " ]" << std::endl;
-            };
-          for (unsigned int i=0;i<fe_rt.dofs_per_cell;++i)
-            {
-              deallog << "  grad phi(" << i << ") = ";
-              for (unsigned int c=0; c<dim; ++c)
-                {
-                  deallog << "[";
-                  for (unsigned int d=0; d<dim; ++d)
-                    deallog << " " << fe_rt.shape_grad_component(i,p,c)[d];
-                  deallog << " ]" << std::endl;
-                  if (c != dim-1)
-                    deallog << "                ";
-                };
-            };
-        }
+      {
+	for (unsigned int mx=0;mx<=div;++mx)
+	  {
+	    const Point<dim> p = (dim==2 ?
+				  Point<dim>(1.*mx/div, 1.*my/div) :
+				  Point<dim>(1.*mx/div, 1.*my/div, 1.*mz/div));
+	    
+					     // Lines with function
+					     // values contain
+					     // quadrature point and one
+					     // vector of dim entries
+					     // for each chape function
+	    deallog << "value " << p;
+	    for (unsigned int i=0;i<fe_rt.dofs_per_cell;++i)
+	      {
+		for (unsigned int c=0; c<dim; ++c)
+		  deallog << " " << fe_rt.shape_value_component(i,p,c);
+		deallog << "  ";
+	      }
+	    deallog << std::endl << "grad "  << p;
+	    for (unsigned int i=0;i<fe_rt.dofs_per_cell;++i)
+	      {
+		for (unsigned int c=0; c<dim; ++c)
+		  {
+		    deallog << ' ';
+		    for (unsigned int d=0; d<dim; ++d)
+		      deallog << ' ' << fe_rt.shape_grad_component(i,p,c)[d];
+		  }
+	      }
+	    deallog << std::endl;
+	  }
+      deallog << "value " << std::endl;
+      deallog << "grad " << std::endl;
+    }
   
-  deallog << std::endl;
+  deallog.pop();  
 }
 
 
