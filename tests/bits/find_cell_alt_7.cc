40930d121ebd3130df2e6d1475449e2f2f256e95
rschulz <rschulz@0785d39b-7218-0410-832d-ea1e28bc413d>
2006-05-10 15:09:12 +0000

    7th test for new cell finding routine
    git-svn-id: https://svn.dealii.org/trunk@13091 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/find_cell_alt_7.cc
@@ -0,0 +1,80 @@
+//----------------------------  find_cell_alt_7.cc  ------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2006 by the deal.II authors and Ralf B. Schulz
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  find_cell_alt_7.cc  ------------------------
+
+
+// Use a circular domain with boundary mappings, and determine cell for points
+// close to the boundary
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <grid/tria.h>
+#include <grid/tria_accessor.h>
+#include <grid/tria_iterator.h>
+#include <grid/grid_tools.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_boundary_lib.h>
+
+#include <fe/mapping_q.h>
+
+#include <fstream>
+
+
+
+
+void check (Triangulation<2> &tria)
+{
+   MappingQ<2> map(5);
+
+   // Test for a number of points, every ten degrees
+   for(unsigned int i=0; i<200; i++)
+      {
+         Point<2> p(std::sin((double)i/100.*M_PI), std::cos((double)i/100.*M_PI));
+         p *= 1.-1e-8;
+
+         std::pair<Triangulation<2>::active_cell_iterator, Point<2> > cell
+            = GridTools::find_active_cell_around_point (map, tria, p);
+
+         deallog << cell.first << std::endl;
+         for (unsigned int v=0; v<GeometryInfo<2>::vertices_per_cell; ++v)
+            deallog << "<" << cell.first->vertex(v) << "> ";
+         deallog << "[ " << cell.second << "] ";
+
+         deallog << std::endl;
+
+         Assert (GeometryInfo<2>::distance_to_unit_cell(cell.second) < 1e-10,
+                 ExcInternalError());
+      }
+  
+
+}
+
+
+int main () 
+{
+  std::ofstream logfile("find_cell_alt_7/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  {  
+    Triangulation<2> coarse_grid;
+    GridGenerator::hyper_ball (coarse_grid);
+    static const HyperBallBoundary<2> boundary;
+    coarse_grid.set_boundary (0, boundary);
+    coarse_grid.refine_global (2);
+    check (coarse_grid);
+  }
+}
+
+  
+  
