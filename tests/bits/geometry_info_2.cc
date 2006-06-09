c822d9b1f21cfbb0b53363f45637115ea9b3cb1a
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-06-09 07:01:40 +0000

    test all integrer functions of GeometryInfo
    git-svn-id: https://svn.dealii.org/trunk@13209 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/bits/geometry_info_2.cc
+++ tests/bits/geometry_info_2.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2003, 2004, 2005 by the deal.II authors
+//    Copyright (C) 2003, 2004, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -12,7 +12,7 @@
 //----------------------------  geometry_info_2.cc  ---------------------------
 
 
-// output most all static members of GeometryInfo
+// output all integer values and functions of GeometryInfo
 
 #include "../tests.h"
 #include <base/logstream.h>
@@ -24,18 +24,85 @@
 template <int dim>
 void test ()
 {
-  deallog << GeometryInfo<dim>::children_per_cell << std::endl;
-  deallog << GeometryInfo<dim>::faces_per_cell << std::endl;
-  deallog << GeometryInfo<dim>::subfaces_per_face << std::endl;
-  deallog << GeometryInfo<dim>::vertices_per_cell << std::endl;
+  deallog << "children_per_cell "
+	  << GeometryInfo<dim>::children_per_cell << std::endl;
+  deallog << "faces_per_cell    "
+	  << GeometryInfo<dim>::faces_per_cell << std::endl;
+  deallog << "subfaces_per_cell "
+	  << GeometryInfo<dim>::subfaces_per_face << std::endl;
+  deallog << "vertices_per_cell "
+	  <<GeometryInfo<dim>::vertices_per_cell << std::endl;
+  deallog << "lines_per_cell    "
+	  << GeometryInfo<dim>::lines_per_cell << std::endl;
+  deallog << "quads_per_cell    "
+	  << GeometryInfo<dim>::quads_per_cell << std::endl;
+  deallog << "hexes_per_cell    "
+	  << GeometryInfo<dim>::hexes_per_cell << std::endl;
 
-  deallog << GeometryInfo<dim>::vertices_per_face << std::endl;
-  deallog << GeometryInfo<dim>::lines_per_face << std::endl;
-  deallog << GeometryInfo<dim>::quads_per_face << std::endl;
+  deallog << "vertices_per_face "
+	  << GeometryInfo<dim>::vertices_per_face << std::endl;
+  deallog << "lines_per_face    "
+	  << GeometryInfo<dim>::lines_per_face << std::endl;
+  deallog << "quads_per_face    "
+	  << GeometryInfo<dim>::quads_per_face << std::endl;
+
+  for (unsigned int f=0;f<GeometryInfo<dim>::faces_per_cell;++f)
+    deallog << "face normal" << f << ' '
+	    << (GeometryInfo<dim>::unit_normal_orientation[f] > 0.
+		? '+' : '-')
+	    << "x" << GeometryInfo<dim>::unit_normal_direction[f]
+	    << std::endl;
+
+  for (unsigned int f=0;f<GeometryInfo<dim>::faces_per_cell;++f)
+    {
+      deallog << "face_children" << f << "[true ]";
+      for (unsigned int v=0;v < GeometryInfo<dim>::subfaces_per_face;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::child_cell_on_face(f, v, true);
+      deallog << std::endl;
+      deallog << "face_children" << f << "[false]";
+      for (unsigned int v=0;v < GeometryInfo<dim>::subfaces_per_face;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::child_cell_on_face(f, v, false);
+      deallog << std::endl;
+    }
   
-  deallog << GeometryInfo<dim>::lines_per_cell << std::endl;
-  deallog << GeometryInfo<dim>::quads_per_cell << std::endl;
-  deallog << GeometryInfo<dim>::hexes_per_cell << std::endl;
+  for (unsigned int f=0;f<GeometryInfo<dim>::faces_per_cell;++f)
+    {
+      deallog << "face_vertices" << f << "[true ]";
+      for (unsigned int v=0;v < GeometryInfo<dim>::vertices_per_face;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::face_to_cell_vertices(f, v, true);
+      deallog << std::endl;
+      deallog << "face_vertices" << f << "[false]";
+      for (unsigned int v=0;v < GeometryInfo<dim>::vertices_per_face;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::face_to_cell_vertices(f, v, false);
+      deallog << std::endl;
+    }
+  
+  for (unsigned int f=0;f<GeometryInfo<dim>::faces_per_cell;++f)
+    {
+      deallog << "face_lines" << f << "[true ]";
+      for (unsigned int v=1;v <= GeometryInfo<dim>::lines_per_face;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::face_to_cell_lines(f, v-1, true);
+      deallog << std::endl;
+      deallog << "face_lines" << f << "[false]";
+      for (unsigned int v=1;v <= GeometryInfo<dim>::lines_per_face;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::face_to_cell_lines(f, v-1, false);
+      deallog << std::endl;
+    }
+  
+  for (unsigned int f=0;f<GeometryInfo<dim>::lines_per_cell;++f)
+    {
+      deallog << "line_vertices" << f;
+      for (unsigned int v=0;v < GeometryInfo<1>::vertices_per_cell;++v)
+	deallog << ' '
+		<< GeometryInfo<dim>::line_to_cell_vertices(f, v);
+      deallog << std::endl;
+    }  
 }
 
 
@@ -46,9 +113,14 @@ int main ()
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
 
+  deallog.push("1d");
   test<1> ();
+  deallog.pop();
+  deallog.push("2d");
   test<2> ();
+  deallog.pop();
+  deallog.push("3d");
   test<3> ();
-
+  deallog.pop();
   return 0;
 }
