9eb47bb9ebf9418153e663be22abe205d329e290
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-06-14 01:57:30 +0000

    Move functions into the right file
    git-svn-id: https://svn.dealii.org/trunk@13237 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/dofs/dof_levels.all_dimensions.cc
+++ deal.II/deal.II/source/dofs/dof_levels.all_dimensions.cc
@@ -28,38 +28,6 @@ namespace internal
       return MemoryConsumption::memory_consumption (cell_dof_indices_cache);
     }
 
-#if deal_II_dimension == 1
-
-    unsigned int
-    DoFLevel<1>::memory_consumption () const
-    {
-      return (DoFLevel<0>::memory_consumption () +
-              MemoryConsumption::memory_consumption (lines));
-    }
-
-#endif
-
-#if deal_II_dimension == 2
-    
-    unsigned int
-    DoFLevel<2>::memory_consumption () const
-    {
-      return (DoFLevel<0>::memory_consumption () +
-              MemoryConsumption::memory_consumption (quads));
-    }
-
-#endif
-
-#if deal_II_dimension == 3
-
-    unsigned int
-    DoFLevel<3>::memory_consumption () const
-    {
-      return (DoFLevel<0>::memory_consumption () +
-              MemoryConsumption::memory_consumption (hexes));
-    }
-
-#endif
   }
   
 }
--- deal.II/deal.II/source/dofs/dof_levels.cc
+++ deal.II/deal.II/source/dofs/dof_levels.cc
@@ -2,15 +2,46 @@
 
 #include <base/exceptions.h>
 #include <base/memory_consumption.h>
-#include <dofs/dof_objects.h>
-#include <dofs/dof_handler.h>
-#include <fe/fe.h>
+#include <dofs/dof_levels.h>
 
 
 namespace internal
 {
   namespace DoFHandler
   {
+
+#if deal_II_dimension == 1
+
+    unsigned int
+    DoFLevel<1>::memory_consumption () const
+    {
+      return (DoFLevel<0>::memory_consumption () +
+              MemoryConsumption::memory_consumption (lines));
+    }
+
+#endif
+
+#if deal_II_dimension == 2
+    
+    unsigned int
+    DoFLevel<2>::memory_consumption () const
+    {
+      return (DoFLevel<0>::memory_consumption () +
+              MemoryConsumption::memory_consumption (quads));
+    }
+
+#endif
+
+#if deal_II_dimension == 3
+
+    unsigned int
+    DoFLevel<3>::memory_consumption () const
+    {
+      return (DoFLevel<0>::memory_consumption () +
+              MemoryConsumption::memory_consumption (hexes));
+    }
+
+#endif
     
   }
   
