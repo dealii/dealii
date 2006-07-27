4dcf0b3d48b4721886d2943542d75ffbb23f8d8e
kayser-herold <kayser-herold@0785d39b-7218-0410-832d-ea1e28bc413d>
2006-07-27 19:10:33 +0000

    Added another testcase for the FE::face_to_equivalent_cell_index method. The test verifies that the method gives the same results as the FE::face_to_cell_index method.
    git-svn-id: https://svn.dealii.org/trunk@13451 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/fe_tools_14.cc
@@ -0,0 +1,47 @@
+//    $Id: fe_tools_13.cc 12738 2006-03-28 23:25:03Z wolf $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  fe_tools_13.cc  ---------------------------
+
+#include "../tests.h"
+#include "fe_tools_common.cc"
+
+// comparison of the results of
+//   FE::face_to_equivalent_cell_index (i)
+// and
+// FE::face_to_cell_index (i, 0)
+// According to the documentation they should be the same!
+
+
+std::string output_file_name = "fe_tools_14/output";
+
+
+template <int dim>
+void
+check_this (const FiniteElement<dim> &fe1,
+            const FiniteElement<dim> &fe2)
+{
+  bool ok = true;
+  
+  for (unsigned int i = 0; i < fe1.dofs_per_face; ++i)
+    {
+      if (fe1.face_to_equivalent_cell_index (i) !=
+	  fe1.face_to_cell_index (i, 0))
+	ok = false;
+
+      if (fe2.face_to_equivalent_cell_index (i) !=
+	  fe2.face_to_cell_index (i, 0))
+	ok = false;
+    }
+
+  ok ? deallog << "OK" << std::endl :
+    deallog << "Error!" << std::endl;
+}
+
