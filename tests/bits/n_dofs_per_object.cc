e5a7bb2585ffca47f6c4844b5cf6df5217c7c236
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-07-28 15:49:23 +0000

    Add new test
    git-svn-id: https://svn.dealii.org/trunk@13468 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/n_dofs_per_object.cc
@@ -0,0 +1,39 @@
+//----------------------------  n_dofs_per_object.cc  ---------------------------
+//    $Id: n_dofs_per_object.cc 11749 2005-11-09 19:11:20Z wolf $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2003, 2004, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  n_dofs_per_object.cc  ---------------------------
+
+#include "../tests.h"
+#include "dof_tools_common.cc"
+#include <lac/vector.h>
+
+// check
+//   FiniteElement::n_dofs_per_object
+
+
+
+std::string output_file_name = "n_dofs_per_object/output";
+
+
+template <int dim>
+void
+check_this (const DoFHandler<dim> &dof_handler)
+{
+  const FiniteElement<dim> &fe = dof_handler.get_fe();
+  deallog << fe.dofs_per_vertex << ' '
+	  << fe.dofs_per_line << ' '
+	  << fe.dofs_per_quad << ' '
+	  << fe.dofs_per_hex << std::endl;
+  deallog << fe.template n_dofs_per_object<0>() << ' '
+	  << fe.template n_dofs_per_object<1>() << ' '
+	  << fe.template n_dofs_per_object<2>() << ' '
+	  << fe.template n_dofs_per_object<3>() << std::endl;  
+}
