f298ffab3e81133dc685fa005353d6f63a547f80
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-08-02 17:43:08 +0000

    make sure test is correct
    git-svn-id: https://svn.dealii.org/trunk@13576 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/lac/tridiagonal_matrix.cc
+++ tests/lac/tridiagonal_matrix.cc
@@ -19,46 +19,148 @@
 
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <cmath>
+
+
+// Symmetric matrix with constant diagonals [-1,2,-1]
+template<typename number>
+void
+matrix1(TridiagonalMatrix<number>& M)
+{
+  const unsigned int n = M.n();
+  for (unsigned int i=0;i<n;++i)
+    {
+      M(i,i) = 2.;
+      if (i>0) M(i,i-1) = -1.;
+      if (i<n-1) M(i,i+1) = -1.;
+    }
+}
+
+
+// Nonsymmetric matrix with constant diagonals [-1,2,-3]
+template<typename number>
+void
+matrix2(TridiagonalMatrix<number>& M)
+{
+  const unsigned int n = M.n();
+  for (unsigned int i=0;i<n;++i)
+    {
+      M(i,i) = 2.;
+      if (i>0) M(i,i-1) = -1.;
+      if (i<n-1) M(i,i+1) = -3.;
+    }
+}
+
+
+// Matrix with increasing diagonals and symmetric rows
+template<typename number>
+void
+matrix3(TridiagonalMatrix<number>& M)
+{
+  const unsigned int n = M.n();
+  for (unsigned int i=0;i<n;++i)
+    {
+      M(i,i) = 2.+i;
+      if (i>0) M(i,i-1) = -1.-i;
+      if (i<n-1) M(i,i+1) = -1.-i;
+    }
+}
+
+
+// Symmetric matrix with increasing diagonals
+template<typename number>
+void
+matrix4(TridiagonalMatrix<number>& M)
+{
+  const unsigned int n = M.n();
+  for (unsigned int i=0;i<n;++i)
+    {
+      M(i,i) = 2.+i;
+      if (i>0) M(i,i-1) = 0.-i;
+      if (i<n-1) M(i,i+1) = -1.-i;
+    }
+}
+
+
+// Nonsymmetric matrix with increasing diagonals
+template<typename number>
+void
+matrix5(TridiagonalMatrix<number>& M)
+{
+  const unsigned int n = M.n();
+  for (unsigned int i=0;i<n;++i)
+    {
+      M(i,i) = 2.+i;
+      if (i>0) M(i,i-1) = -1.-i;
+      if (i<n-1) M(i,i+1) = 5.-2.*i;
+    }
+}
+
 
 template<typename number>
 void
-check_vmult()
+check_vmult(TridiagonalMatrix<number>& M)
 {
-  TridiagonalMatrix<number> M(4);
-  Vector<number> u(4);
-  Vector<number> v(4);
+  const unsigned int n = M.n();
+  Vector<number> u(n);
+  Vector<number> v(n);
 
-  for (unsigned int i=0;i<4;++i)
-    {
-      u(i) = i+1;
-      M(i,i) = i+1;
-      if (i>0)
-	M(i,i-1) = 0.-i;
-      if (i<3)
-	M(i,i+1) = 4.-i;
-    }
+  for (unsigned int i=0;i<n;++i)
+    u(i) = 1 << i;
+
+  for (unsigned int i=0;i<u.size();++i)
+    deallog << ' ' << std::setw(6) << u(i);
+  deallog << std::endl;
+
   M.vmult(v,u);
   for (unsigned int i=0;i<v.size();++i)
-    deallog << ' ' << v(i);
+    deallog << ' ' << std::setw(6) << v(i);
   deallog << std::endl;
-
+  
   M.vmult_add(v,u);
   for (unsigned int i=0;i<v.size();++i)
-    deallog << ' ' << v(i);
+    deallog << ' ' << std::setw(6) << v(i);
   deallog << std::endl;
-
+  
   M.Tvmult(v,u);
   for (unsigned int i=0;i<v.size();++i)
-    deallog << ' ' << v(i);
+    deallog << ' ' << std::setw(6) << v(i);
   deallog << std::endl;
-
+  
   M.Tvmult_add(v,u);
   for (unsigned int i=0;i<v.size();++i)
-    deallog << ' ' << v(i);
+    deallog << ' ' << std::setw(6) << v(i);
   deallog << std::endl;
 }
 
+
+template<typename number>
+void
+check(unsigned int size)
+{
+  TridiagonalMatrix<number>M(size);
+  matrix1(M);
+  deallog << "Matrix [-1,2,-1]" << std::endl;
+  M.print(deallog, 7, 0);
+  check_vmult(M);
+  matrix2(M);
+  deallog << "Matrix [-1,2,-3]" << std::endl;
+  M.print(deallog, 7, 0);
+  check_vmult(M);
+  matrix3(M);
+  deallog << "Matrix increasing symmetric rows" << std::endl;
+  M.print(deallog, 7, 0);
+  check_vmult(M);
+  matrix4(M);
+  deallog << "Matrix increasing symmetric" << std::endl;
+  M.print(deallog, 7, 0);
+  check_vmult(M);
+  matrix5(M);
+  deallog << "Matrix increasing nonsymmetric" << std::endl;
+  M.print(deallog, 7, 0);
+  check_vmult(M);
+}
 
 int main()
 {
@@ -69,6 +171,6 @@ int main()
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
   
-  check_vmult<double>();
-  check_vmult<float>();
+  check<double>(5);
+  check<float>(4);
 }
