c69a7b209440874579b403b921fb1a01aa3f8384
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-08-09 14:39:03 +0000

    Remove now unused variable...
    git-svn-id: https://svn.dealii.org/trunk@13624 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/numerics/vectors.all_dimensions.cc
+++ deal.II/deal.II/source/numerics/vectors.all_dimensions.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2005 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -14,11 +14,6 @@
 
 #include <lac/vector.h>
 #include <numerics/vectors.h>
-
-
-
-// allocate storage and initialize static variables
-const Quadrature<0> * const VectorTools::invalid_face_quadrature = 0;
 
 
 
