f54dcbccdef4131859483dbefe3ca4ce1621a4a3
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-08-10 20:53:10 +0000

    Add a new test
    git-svn-id: https://svn.dealii.org/trunk@13644 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/fe_tools_14.cc
@@ -0,0 +1,35 @@
+//----------------------------  fe_tools_14.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  fe_tools_14.cc  ---------------------------
+
+#include "../tests.h"
+#include "fe_tools_common.cc"
+
+// check
+//   FE::hp_constraints_are_implemented ()
+
+
+std::string output_file_name = "fe_tools_14/output";
+
+
+
+template <int dim>
+void
+check_this (const FiniteElement<dim> &fe1,
+            const FiniteElement<dim> &fe2)
+{
+  deallog << (fe1.hp_constraints_are_implemented () ? "true" : "false")
+	  << std::endl;
+  deallog << (fe2.hp_constraints_are_implemented () ? "true" : "false")
+	  << std::endl;
+}
+
