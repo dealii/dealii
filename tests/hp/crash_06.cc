8c91d6314dd4a378afd690bfc9f332021cac2f47
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-09-03 05:02:41 +0000

    Add another crash test
    git-svn-id: https://svn.dealii.org/trunk@13816 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/crash_06.cc
@@ -0,0 +1,110 @@
+//----------------------------  crash_06.cc  ---------------------------
+//    $Id: crash_06.cc 12732 2006-03-28 23:15:45Z wolf $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  crash_06.cc  ---------------------------
+
+
+// like all the hp_constraint_*_03 tests that produced a crash at one point,
+// except that we don't refine the mesh that much
+
+char logname[] = "crash_06/output";
+
+
+#include "../tests.h"
+#include <base/function.h>
+#include <base/logstream.h>
+#include <base/quadrature_lib.h>
+#include <lac/vector.h>
+
+#include <grid/tria.h>
+#include <dofs/hp_dof_handler.h>
+#include <dofs/dof_constraints.h>
+#include <grid/grid_generator.h>
+#include <grid/grid_refinement.h>
+#include <grid/tria_accessor.h>
+#include <grid/tria_iterator.h>
+#include <grid/tria_boundary_lib.h>
+#include <dofs/dof_accessor.h>
+#include <dofs/dof_tools.h>
+#include <numerics/vectors.h>
+#include <fe/fe_abf.h>
+#include <fe/fe_dgp.h>
+#include <fe/fe_dgp_monomial.h>
+#include <fe/fe_dgp_nonparametric.h>
+#include <fe/fe_dgq.h>
+#include <fe/fe_nedelec.h>
+#include <fe/fe_q.h>
+#include <fe/fe_q_hierarchical.h>
+#include <fe/fe_raviart_thomas.h>
+#include <fe/fe_system.h>
+
+#include <fstream>
+#include <vector>
+
+
+template <int dim>
+void test ();
+
+
+
+
+template <int dim>
+void do_check (const Triangulation<dim> &triangulation,
+	       const hp::FECollection<dim> &fe)
+{  
+  hp::DoFHandler<dim>        dof_handler(triangulation);
+
+				   // distribute fe_indices randomly
+  for (typename hp::DoFHandler<dim>::active_cell_iterator
+	 cell = dof_handler.begin_active();
+       cell != dof_handler.end(); ++cell)
+    cell->set_active_fe_index (rand() % fe.size());
+  dof_handler.distribute_dofs (fe);
+
+  deallog << "n_dofs=" << dof_handler.n_dofs() << std::endl;
+
+  ConstraintMatrix constraints;
+  DoFTools::make_hanging_node_constraints (dof_handler,
+					   constraints);
+  constraints.close ();
+
+  constraints.print (deallog.get_file_stream());
+}
+
+
+
+void test_with_wrong_face_orientation (const hp::FECollection<3> &fe)
+{
+  Triangulation<3>     triangulation;
+  GridGenerator::hyper_ball (triangulation);
+  triangulation.begin_active()->set_refine_flag ();
+  triangulation.execute_coarsening_and_refinement ();
+  
+  do_check (triangulation, fe);
+}
+
+
+
+int main ()
+{
+  std::ofstream logfile(logname);
+  logfile.precision (3);
+  
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  hp::FECollection<3> fe;
+  for (unsigned int i=0; i<4; ++i)
+    fe.push_back (FE_DGQ<3>(i));
+  test_with_wrong_face_orientation (fe);
+}
+
