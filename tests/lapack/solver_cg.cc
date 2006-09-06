75f4d6e09e434e039397675630d658998f10b1f5
Guido Kanschat <dr.guido.kanschat@gmail.com>
2006-09-06 21:29:34 +0000

    test eigenvalue capabilities of SolverCG
    git-svn-id: https://svn.dealii.org/trunk@13843 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lapack/solver_cg.cc
@@ -0,0 +1,136 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+// Check eigenvalue capabilities of SolverCG
+
+#include "../tests.h"
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include "../lac/testmatrix.h"
+#include <base/logstream.h>
+#include <lac/sparse_matrix.h>
+#include <lac/sparse_matrix.templates.h>
+#include <lac/vector.h>
+#include <lac/vector_memory.h>
+#include <lac/solver_control.h>
+#include <lac/solver_cg.h>
+#include <lac/precondition.h>
+
+template<class SOLVER, class MATRIX, class VECTOR, class PRECONDITION>
+void
+check_solve( SOLVER& solver, const MATRIX& A,
+	     VECTOR& u, VECTOR& f, const PRECONDITION& P)
+{
+  u = 0.;
+  f = 1.;
+  try 
+    {
+      solver.solve(A,u,f,P);
+    }
+  catch (std::exception& e)
+    {
+      deallog << e.what() << std::endl;
+    }  
+}
+
+template<class SOLVER, class MATRIX, class VECTOR, class PRECONDITION>
+void
+check_Tsolve(SOLVER& solver, const MATRIX& A,
+	     VECTOR& u, VECTOR& f, const PRECONDITION& P)
+{
+  u = 0.;
+  f = 1.;
+  try 
+    {
+      solver.Tsolve(A,u,f,P);
+    }
+  catch (std::exception& e)
+    {
+      deallog << e.what() << std::endl;
+    }  
+}
+
+int main()
+{
+  std::ofstream logfile("solver_cg/output");
+//  logfile.setf(std::ios::fixed);
+  logfile.precision(4);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  GrowingVectorMemory<> mem;
+  SolverControl control(100, 1.e-3);
+  SolverControl verbose_control(100, 1.e-3, true);
+  SolverCG<>::AdditionalData data(false, true, true, true);
+  SolverCG<> cg(control, mem, data);
+
+  for (unsigned int size=4; size <= 30; size *= 3)
+    {
+      unsigned int dim = (size-1)*(size-1);
+
+      deallog << "Size " << size << " Unknowns " << dim << std::endl;
+      
+				       // Make matrix
+      FDMatrix testproblem(size, size);
+      SparsityPattern structure(dim, dim, 5);
+      testproblem.five_point_structure(structure);
+      structure.compress();
+      SparseMatrix<double>  A(structure);
+      testproblem.five_point(A);
+
+      PreconditionIdentity prec_no;
+      PreconditionRichardson prec_richardson;
+      prec_richardson.initialize(0.6);
+      PreconditionSSOR<> prec_ssor;
+      prec_ssor.initialize(A, 1.2);
+
+      std::vector<unsigned int> permutation(dim);
+      std::vector<unsigned int> inverse_permutation(dim);
+
+      Vector<double>  f(dim);
+      Vector<double>  u(dim);
+      Vector<double> res(dim);
+
+      f = 1.;
+      u = 1.;
+      
+      try
+	{
+	  deallog.push("no-fail");
+	  control.set_max_steps(10);
+	  check_solve(cg,A,u,f,prec_no);
+	  control.set_max_steps(100);
+	  deallog.pop();
+	  
+	  deallog.push("no");
+	  check_solve(cg,A,u,f,prec_no);
+	  deallog.pop();
+	  
+	  deallog.push("rich");
+	  check_solve(cg,A,u,f,prec_richardson);
+	  deallog.pop();
+	  
+	  deallog.push("ssor");
+	  check_solve(cg,A,u,f,prec_ssor);
+	  deallog.pop();
+	}
+      catch (std::exception& e)
+	{
+	  std::cerr << "Exception: " << e.what() << std::endl;
+	}
+    }
+}
+
