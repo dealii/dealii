68623fed77f86d09688390403c9c0cdec65a5e91
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-10-18 23:41:29 +0000

    All tests should include ../tests.h
    git-svn-id: https://svn.dealii.org/trunk@14019 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/mg_dof_handler.cc
+++ tests/deal.II/mg_dof_handler.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2005 by the deal.II authors
+//    Copyright (C) 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -16,6 +16,7 @@
    when the triangulation had unused vertices before, make sure that
    this is now fixed. */
 
+#include "../tests.h"
 #include <base/logstream.h>
 #include <grid/tria.h>
 #include <grid/grid_generator.h>
