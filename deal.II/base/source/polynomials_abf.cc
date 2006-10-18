8800192d9421e159a5a51aff1d68346c3a4dc18c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2006-10-18 00:28:53 +0000

    Remove unnecessary using namespace declarations.
    git-svn-id: https://svn.dealii.org/trunk@14013 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/polynomials_abf.cc
+++ deal.II/base/source/polynomials_abf.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2004, 2005 by the deal.II authors
+//    Copyright (C) 2004, 2005, 2006 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -15,8 +15,7 @@
 #include <base/quadrature_lib.h>
 #include <iostream>
 #include <iomanip>
-using namespace std;
-using namespace Polynomials;
+
 
 
 template <int dim>
