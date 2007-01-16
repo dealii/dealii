383c138b3641a337dc92bef648801bf7e8aedf90
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-01-16 22:21:26 +0000

    On Mac OSX, the linker complains about files with no symbols as well.
    git-svn-id: https://svn.dealii.org/trunk@14319 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/source/petsc_block_sparse_matrix.cc
+++ deal.II/lac/source/petsc_block_sparse_matrix.cc
@@ -98,4 +98,8 @@ namespace PETScWrappers
   
 DEAL_II_NAMESPACE_CLOSE
 
+#else // #if DEAL_II_USE_PETSC
+// On gcc2.95 on Alpha OSF1, the native assembler does not like empty
+// files, so provide some dummy code
+namespace { void dummy () {} }
 #endif
