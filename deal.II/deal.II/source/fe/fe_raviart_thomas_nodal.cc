3aba83c7e8e43b75c36650bf2842b6afe9ddab95
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-01-19 20:36:11 +0000

    Add TODOs about the adjust_face_orientation field.
    git-svn-id: https://svn.dealii.org/trunk@14352 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/fe/fe_raviart_thomas_nodal.cc
+++ deal.II/deal.II/source/fe/fe_raviart_thomas_nodal.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 2003, 2004, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -24,6 +24,10 @@
 #include <fe/fe_tools.h>
 
 #include <sstream>
+
+//TODO: implement the adjust_quad_dof_index_for_face_orientation_table field,
+//and write a test similar to bits/face_orientation_and_fe_q_01
+
 
 DEAL_II_NAMESPACE_OPEN
 
