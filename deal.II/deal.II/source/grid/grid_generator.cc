a460666b61f818cee84a8fb0588487d701131171
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-01-22 05:17:56 +0000

    Add an apparently needed include file.
    git-svn-id: https://svn.dealii.org/trunk@14358 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/grid/grid_generator.cc
+++ deal.II/deal.II/source/grid/grid_generator.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -34,6 +34,7 @@
 #include <numerics/matrices.h>
 
 #include <cmath>
+#include <limits>
 
 DEAL_II_NAMESPACE_OPEN
 
