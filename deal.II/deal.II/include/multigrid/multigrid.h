27346a173b2b6ed0768aee98ee4095589691bc11
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-01-22 02:23:14 +0000

    Remove unnecessary qualification that would also require a presently missing 'typename'
    git-svn-id: https://svn.dealii.org/trunk@14357 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/multigrid/multigrid.h
+++ deal.II/deal.II/include/multigrid/multigrid.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -521,7 +521,7 @@ Multigrid<VECTOR>::Multigrid (const MGDoFHandler<dim>& mg_dof_handler,
 			      const MGTransferBase<VECTOR>& transfer,
 			      const MGSmootherBase<VECTOR>& pre_smooth,
 			      const MGSmootherBase<VECTOR>& post_smooth,
-			      Multigrid<VECTOR>::Cycle cycle)
+			      Cycle                         cycle)
 		:
 		cycle_type(cycle),
 		minlevel(0),
