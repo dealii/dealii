ca0c262ca8608753a60ff7675b18c7366862b57b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-02-09 02:45:35 +0000

    Add a test for SparseMatrixEZ::print_formatted().
    git-svn-id: https://svn.dealii.org/trunk@14429 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lac/print_formatted_ez_01.cc
@@ -0,0 +1,49 @@
+//----------------------------  print_formatted_ez_01.cc,v  ---------------------------
+//    $Id: matrices.cc 11749 2005-11-09 19:11:20Z wolf $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2007 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  print_formatted_ez_01.cc,v  ---------------------------
+
+// Check SparseMatrixEZ::print_formatted
+
+#include "../tests.h"
+
+#include <base/logstream.h>
+
+#include <lac/sparse_matrix_ez.h>
+
+#include <fstream>
+
+int main()
+{
+  std::ofstream logfile("print_formatted_ez_01/output");
+  logfile.setf(std::ios::fixed);
+  logfile.precision(3);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+  SparseMatrixEZ<double> ez(5,4);
+  ez.set(0,0,2.);
+  ez.set(0,2,3.);
+  ez.set(0,3,4.);
+  ez.set(1,0,5.);
+  ez.set(1,1,6.);
+  ez.set(1,3,7.);
+  ez.set(2,0,8.);
+  ez.set(2,1,9.);
+  ez.set(2,2,10.);
+  ez.set(2,3,11.);
+  ez.set(4,0,12.);
+  ez.set(4,2,13.);
+  ez.set(4,3,14.);
+
+  ez.print_formatted(logfile, 0, false, 5, "~");
+}
