d459b47c4c6df4030db923992a7d3c56809706f9
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-05-22 06:06:41 +0000

    More changes for style
    git-svn-id: https://svn.dealii.org/trunk@14685 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/step-21/step-21.cc
+++ deal.II/examples/step-21/step-21.cc
@@ -952,7 +952,6 @@ void TwoPhaseFlowProblem<dim>::assemble_system ()
                                        // right hand side vector:
       cell->get_dof_indices (local_dof_indices);
       for (unsigned int i=0; i<dofs_per_cell; ++i)
-      
         for (unsigned int j=0; j<dofs_per_cell; ++j)
 	  system_matrix.add (local_dof_indices[i],
 			     local_dof_indices[j],
