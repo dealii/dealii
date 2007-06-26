acf6f2d981613144c5f134a9bb4b2316a2c62ed9
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-06-26 05:14:49 +0000

    Add new test
    git-svn-id: https://svn.dealii.org/trunk@14815 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/utilities_02.cc
@@ -0,0 +1,49 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2005, 2006, 2007 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+// test Utilities::fixed_power
+
+#include "../tests.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+#include <base/utilities.h>
+
+
+template <int dim>
+void test () 
+{
+  deallog << Utilities::fixed_power<dim> (2) << std::endl;
+  deallog << Utilities::fixed_power<dim> (-2) << std::endl;
+  deallog << Utilities::fixed_power<dim> (2.5) << std::endl;
+  deallog << Utilities::fixed_power<dim> (-2.5) << std::endl;
+  deallog << std::endl;
+}
+
+  
+  
+
+int main()
+{
+  std::ofstream logfile("utilities_02/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test<1> ();
+  test<2> ();
+  test<3> ();
+  test<4> ();
+}
