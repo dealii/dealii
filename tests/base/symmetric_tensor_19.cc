96b3d31cb9e64100c6d42410547a28acd7bf6622
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-08-02 22:08:44 +0000

    Add missing include file
    git-svn-id: https://svn.dealii.org/trunk@14889 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/symmetric_tensor_19.cc
+++ tests/base/symmetric_tensor_19.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2005 by the deal.II authors
+//    Copyright (C) 2005, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -18,6 +18,7 @@
 #include <base/logstream.h>
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
 
 
 
