9f6b8d3e1fc235e804ab451af684ea7b4de774d3
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-08-02 22:13:14 +0000

    Avoid unnecessary semicolons that trip up current gcc mainline versions
    git-svn-id: https://svn.dealii.org/trunk@14892 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/solver_qmrs.h
+++ deal.II/lac/include/lac/solver_qmrs.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -96,7 +96,7 @@ class SolverQMRS : public Solver<VECTOR>
 		       double breakdown=1.e-16) :
 			exact_residual(exact_residual),
 			breakdown(breakdown)
-	  {};
+	  {}
 	
 					 /**
 					  * Flag for exact computation of residual.
