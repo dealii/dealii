29526cdce5e276590aae20bfb0c7020d49908aca
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-08-09 21:51:49 +0000

    Instantiate the correct variable
    git-svn-id: https://svn.dealii.org/trunk@14931 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/grid/persistent_tria.cc
+++ deal.II/deal.II/source/grid/persistent_tria.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2005, 2006 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -22,7 +22,7 @@ DEAL_II_NAMESPACE_OPEN
 
 template <int dim>
 const unsigned int
-Triangulation<dim>::dimension;
+PersistentTriangulation<dim>::dimension;
 
 
 template <int dim>
