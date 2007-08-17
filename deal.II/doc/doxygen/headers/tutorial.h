26749dcf68f2fd35a77caa8b3dd1a3cf9cd0d9fc
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-08-17 16:22:11 +0000

    Revisit the listing of tutorial programs. Sort not only by number but also by topic. Remove the duplicate listing in the doxygen page.
    git-svn-id: https://svn.dealii.org/trunk@14966 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/tutorial.h
+++ deal.II/doc/doxygen/headers/tutorial.h
@@ -14,85 +14,11 @@
 /**
  * @page Tutorial Tutorial programs
  *
- * The tutorial is a separate part of the documentation for deal.II
- * (in fact, the second part, next to the manual you are presently
- * in). It consists of a collection of small programs, each more or
- * less built atop of the previous ones, which demonstrate various
- * aspects of the library. A more detailed overview can be found <a
- * href="../tutorial/index.html">on this
- * page</a> that also includes short summaries of what each of the
- * tutorial programs are supposed to demonstrate.
- *
- * The tutorial programs are named step-1, step-2, etc. If you want to
- * jump to any of these programs directly, simply follow one of the
- * links below:
- * 
- * <table align="center">
- * <tr>
- * <td>
- * 
- * @ref step_1 "step-1"
- * 
- * @ref step_2 "step-2"
- * 
- * @ref step_3 "step-3"
- * 
- * @ref step_4 "step-4"
- * 
- * @ref step_5 "step-5"
- * 
- * @ref step_6 "step-6"
- * 
- * </td>
- * <td>
- * 
- * @ref step_7 "step-7"
- * 
- * @ref step_8 "step-8"
- * 
- * @ref step_9 "step-9"
- * 
- * @ref step_10 "step-10"
- * 
- * @ref step_11 "step-11"
- * 
- * @ref step_12 "step-12"
- * 
- * </td>
- * <td>
- * 
- * @ref step_13 "step-13"
- * 
- * @ref step_14 "step-14"
- * 
- * @ref step_15 "step-15"
- * 
- * @ref step_16 "step-16"
- * 
- * @ref step_17 "step-17"
- * 
- * @ref step_18 "step-18"
- * 
- * </td>
- * <td>
- * 
- * @ref step_19 "step-19"
- * 
- * @ref step_20 "step-20"
- * 
- * @ref step_21 "step-21"
- * 
- * @ref step_23 "step-23"
- * 
- * @ref step_24 "step-24"
- * 
- * @ref step_25 "step-25"
- * 
- * </td>
- * <td>
- * 
- * @ref step_19 "step-27"
- * 
- * </td>
- * </tr>
+ * The tutorial is a separate part of the documentation for deal.II. It
+ * consists of a collection of small programs, each more or less built atop
+ * previous ones. They demonstrate various aspects of the library. A more
+ * detailed overview can be found <a href="../tutorial/index.html">on this
+ * page</a> that also includes short summaries of what each of the tutorial
+ * programs are supposed to demonstrate, as well as a list of the programs
+ * sorted by topic.
  */
