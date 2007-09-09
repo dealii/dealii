3e06553f153fa8ead46134d39642dde10ca188a5
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-09-09 00:34:38 +0000

    Remove now outdated comment.
    git-svn-id: https://svn.dealii.org/trunk@15174 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/swappable_vector.h
+++ deal.II/lac/include/lac/swappable_vector.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -166,25 +166,6 @@ class SwappableVector : public Vector<number>
 				      * this function while the data
 				      * is still or already in memory
 				      * is allowed and has no effect.
-				      *
-				      * It is noted that versions of
-				      * gcc up to at least version
-				      * 2.95.2 are not thread-safe in
-				      * the C++ standard
-				      * library. Thus, parallel in-
-				      * and/or output may sometimes
-				      * lead to unreproducable
-				      * crashes, segmentation faults,
-				      * or bus errors of programs. You
-				      * should therefore only call
-				      * this function if either you
-				      * are sure that the I/O library
-				      * distributed with your compiler
-				      * supports parallel
-				      * writing/reading, or if you are
-				      * sure that during the run-time
-				      * of this function no other I/O
-				      * operations are active.
 				      */
     void alert ();
     
