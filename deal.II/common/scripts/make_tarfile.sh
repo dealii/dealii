e2b57b014f6e3b16192fc860b181e8e30e4c17f2
Guido Kanschat <dr.guido.kanschat@gmail.com>
2007-09-11 15:12:17 +0000

    stripped tarfile
    git-svn-id: https://svn.dealii.org/trunk@15192 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/common/scripts/make_tarfile.sh
+++ deal.II/common/scripts/make_tarfile.sh
@@ -14,9 +14,29 @@ PATCH=$3
 
 svn export http://wolfgang.math.tamu.edu/svn/public/deal.II/tags/Version-$MAJOR-$MINOR-$PATCH/deal.II
 
+# Generate distribution tarfile
+
 tar czf deal.II-$MAJOR.$MINOR.$PATCH.tar.gz deal.II
+
+# Generate stripped tarfile
+
+mv deal.II/doc deal.II/examples .
+cd deal.II
+mkdir doc mkdir doxygen
+cd doc
+touch Makefile.in
+cd doxygen
+touch Makefile.in options.dox.in options.136.in header.html.in header.136.in header.tex.in
+cd ../../..
+tar czf deal.nodoc-$MAJOR.$MINOR.$PATCH.tar.gz deal.II
+rm -rf deal.II/doc
+mv doc examples deal.II
+
+# Generate documentation
+
 cd deal.II
 ./configure --with-umfpack
 make online-doc
 cd ..
 tar czf deal.doc-$MAJOR.$MINOR.$PATCH.tar.gz deal.II/doc
+
