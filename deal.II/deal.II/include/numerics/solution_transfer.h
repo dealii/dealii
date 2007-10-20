9ff47e4d87e7977d621a674adf44daf6dd2bd79d
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-10-20 00:46:48 +0000

    Fix a typo.
    git-svn-id: https://svn.dealii.org/trunk@15355 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/numerics/solution_transfer.h
+++ deal.II/deal.II/include/numerics/solution_transfer.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -51,7 +51,7 @@ DEAL_II_NAMESPACE_OPEN
  *                                     // for refinement,
  * tria->prepare_coarsening_and_refinement();
  *                                     // tell the SolutionTransfer object
- *                                     // that we intent to do pure refinement,
+ *                                     // that we intend to do pure refinement,
  * soltrans.prepare_for_pure_refinement();
  *                                     // actually execute the refinement,
  * tria->execute_coarsening_and_refinement();
