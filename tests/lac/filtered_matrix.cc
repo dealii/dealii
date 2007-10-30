67a777360326a86953fc771e4bd173b01d8bd4c4
Guido Kanschat <dr.guido.kanschat@gmail.com>
2007-10-30 20:23:41 +0000

    test FilteredMatrix
    git-svn-id: https://svn.dealii.org/trunk@15398 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lac/filtered_matrix.cc
@@ -0,0 +1,102 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2007 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+// Test properties of FilteredMatrix and iterators
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <lac/filtered_matrix.h>
+#include <lac/matrix_lib.h>
+#include <lac/precondition.h>
+#include <lac/vector.h>
+
+#include <fstream>
+
+
+template<class VECTOR>
+void test (const FilteredMatrix<VECTOR>& M)
+{
+  deallog << "Iterator";
+  
+  unsigned int max = 0;
+  for (typename FilteredMatrix<VECTOR>::const_iterator i= M.begin();
+       i != M.end();++i)
+    {
+      Assert(i->row() == i->column(), ExcInternalError());
+      deallog << ' ' << i->row() << ':' << i->value();
+      max = i->row();
+    }
+  VECTOR v(max+1);
+  VECTOR w(max+1);
+  
+  for (unsigned int i=0;i<v.size();++i)
+    v(i) = 31+i;
+
+  deallog << std::endl << "vmult ";
+
+  w = 0.;
+  M.vmult(w,v);
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << w(i);
+  
+  deallog << std::endl << "Tvmult";
+  
+  w = 0.;
+  M.Tvmult(w,v);
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << w(i);
+  
+  deallog << std::endl << "vmult_add";
+  
+  M.vmult_add(w,v);
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << w(i);
+
+  deallog << std::endl << "boundary";
+  
+  M.apply_constraints(w, true);
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << w(i);
+
+  deallog << std::endl << "boundary";
+  
+  M.apply_constraints(w, false);
+  for (unsigned int i=0;i<v.size();++i)
+    deallog << ' ' << w(i);
+  
+  deallog << std::endl;
+}
+
+
+
+int main()
+{
+  std::ofstream logfile("filtered_matrix/output");
+  deallog.attach(logfile);
+  deallog.depth_console(10);
+  
+  PreconditionIdentity identity;
+  ScaledMatrix<Vector<double> > s(identity, 3.);
+  
+  FilteredMatrix<Vector<double> > f;
+  f.initialize(s, false);
+  test(f);
+
+  for (unsigned int i=0;i<5;++i)
+    f.add_constraint(i*i,i/2.);
+  test(f);
+  
+  f.initialize(s, true);
+  test(f);
+  
+}
