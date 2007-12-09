1d05bbb954e6b4295ad7c5f27905c599e1933a48
Wolfgang Bangerth <bangerth@math.tamu.edu>
2007-12-09 18:29:00 +0000

    Generate instantiations for SparseMatrixEZ automatically.
    git-svn-id: https://svn.dealii.org/trunk@15578 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/lac/source/sparse_matrix_ez.cc
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+//    $Id: sparse_matrix_ez.double.cc 14038 2006-10-23 02:46:34Z bangerth $
+//    Version: $Name$
+//
+//    Copyright (C) 2007 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+
+#include <lac/sparse_matrix_ez.templates.h>
+#include <lac/block_vector.h>
+
+DEAL_II_NAMESPACE_OPEN
+#include "sparse_matrix_ez.inst"
+DEAL_II_NAMESPACE_CLOSE
