6867ce8ed385e6908668cf2c4cb9d92c3b709429
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-01-16 20:41:26 +0000

    Generate instantiations using our own preprocessor, not through include files.
    git-svn-id: https://svn.dealii.org/trunk@15619 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/numerics/vectors.cc
+++ deal.II/deal.II/source/numerics/vectors.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -18,24 +18,10 @@
 
 // explicit instantiations
 
-#define VEC Vector<double>
-#include "vectors.instance.h"
-#undef VEC
-
-#define VEC Vector<float>
-#include "vectors.instance.h"
-#undef VEC
-
-#define VEC BlockVector<double>
-#include "vectors.instance.h"
-#undef VEC
-
-#define VEC BlockVector<float>
-#include "vectors.instance.h"
-#undef VEC
-
 
 DEAL_II_NAMESPACE_OPEN
+
+#include "vectors.inst"
 
 template
 void VectorTools::create_right_hand_side<deal_II_dimension>
