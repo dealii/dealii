06ce98d7e7f5505b92d76baf04da071b2a687f1c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-02-26 14:50:13 +0000

    Also do what I just did in step-20
    git-svn-id: https://svn.dealii.org/trunk@15783 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/step-21/step-21.cc
+++ deal.II/examples/step-21/step-21.cc
@@ -527,9 +527,8 @@ template <class Matrix>
 void InverseMatrix<Matrix>::vmult (Vector<double>       &dst,
                                    const Vector<double> &src) const
 {
-  SolverControl         solver_control (src.size(), 1e-8*src.l2_norm());
-  GrowingVectorMemory<> vector_memory;    
-  SolverCG<>            cg (solver_control, vector_memory);
+  SolverControl solver_control (src.size(), 1e-8*src.l2_norm());
+  SolverCG<>    cg (solver_control, vector_memory);
 
   dst = 0;
   
