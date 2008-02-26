d75ee58d412a11adb9d526d881bf749a5a7112c9
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-02-26 14:47:20 +0000

    Remove outdated comment
    git-svn-id: https://svn.dealii.org/trunk@15781 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/vector_memory.h
+++ deal.II/doc/doxygen/headers/vector_memory.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2003, 2004, 2006 by the deal.II authors
+//    Copyright (C) 2003, 2004, 2006, 2008 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -19,8 +19,6 @@
  * deallocating vectors over and over in iterative procedures. These methods
  * all use an object of the base class VectorMemory to get their auxiliary
  * vectors.
- *
- * Presently, only step-20 illustrates the use of these classes.
  *
  * @ingroup LAC
  */
