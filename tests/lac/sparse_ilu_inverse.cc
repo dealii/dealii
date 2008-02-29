211c6989765b0237b6795c53fcf4744bd5ab8179
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-02-29 22:04:35 +0000

    Add a test where we use the ILU to compute actual inverses that we can check.
    git-svn-id: https://svn.dealii.org/trunk@15818 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lac/sparse_ilu_inverse.cc
@@ -0,0 +1,87 @@
+//----------------------------  sparse_ilu_inverse.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2008 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  sparse_ilu_inverse.cc  ---------------------------
+
+
+// compute the inverse of a small matrix using the SparseILU with
+// infinite fill-in
+
+#include "../tests.h"
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iomanip>
+#include <cstdlib>
+#include "testmatrix.h"
+#include <base/logstream.h>
+#include <lac/sparse_matrix.h>
+#include <lac/full_matrix.h>
+#include <lac/sparse_ilu.h>
+#include <lac/vector.h>
+
+
+int main()
+{
+  std::ofstream logfile("sparse_ilu_inverse/output");
+  deallog << std::fixed;
+  deallog << std::setprecision(3);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+  
+
+  for (unsigned int N=1; N<5; ++N)
+    {
+      deallog << "N=" << N << std::endl;
+      
+      SparsityPattern structure(N, N, N);
+      for (unsigned int i=0; i<N; ++i)
+	for (unsigned int j=0; j<N; ++j)
+	  structure.add(i,j);
+      structure.compress();
+      SparseMatrix<double>  A(structure);
+      for (unsigned int i=0; i<N; ++i)
+	{
+	  A.set(i,i,2);
+	  if (i>=1)
+	    A.set(i,i-1, -1);
+	  if (i<N-1)
+	    A.set(i,i+1, -1);
+	}
+
+      SparseILU<double> ilu;
+      ilu.initialize (A, SparseILU<double>::AdditionalData());
+
+				       // now get an explicit
+				       // representation of the
+				       // inverse
+      FullMatrix<double> inverse (N,N);
+      Vector<double> tmp1(N), tmp2(N);
+      for (unsigned int i=0; i<N; ++i)
+	{
+	  tmp1 = 0;
+	  tmp1(i) = 1;
+	  ilu.vmult (tmp2, tmp1);
+	  for (unsigned int j=0; j<N; ++j)
+	    inverse(i,j) = tmp2(j);
+	}
+
+      deallog << "Matrix A:" << std::endl;
+      A.print_formatted (deallog.get_file_stream());
+  
+      deallog << "Matrix A^{-1}:" << std::endl;
+      inverse.print_formatted (deallog.get_file_stream());
+
+      deallog << std::endl;
+    }
+}
+
