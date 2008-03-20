208bfc72de4227b8906e6959d4aff86209d603fa
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-03-20 13:26:33 +0000

    Initialize a few variables to silence warnings that they may be used uninitialized (which they aren't).
    git-svn-id: https://svn.dealii.org/trunk@15918 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/solver_minres.h
+++ deal.II/lac/include/lac/solver_minres.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
+//    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -236,9 +236,9 @@ SolverMinRes<VECTOR>::solve (const MATRIX         &A,
   v.reinit(b,true);
 
 				   // some values needed
-  double delta[3];
-  double f[2];
-  double e[2]; 
+  double delta[3] = { 0, 0, 0 };
+  double f[2] = { 0, 0 };
+  double e[2] = { 0, 0 }; 
 
   double r_l2 = 0;
   double r0   = 0;
