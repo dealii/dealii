3a8d4afad4b5db68545a00d97406ba83c2566a3f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-03-20 00:25:53 +0000

    Document namespace dealii.
    git-svn-id: https://svn.dealii.org/trunk@15911 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/namespace_dealii.h
@@ -0,0 +1,11 @@
+/**
+ * This is the namespace in which everything in deal.II is in. To avoid
+ * prefixing every class and function call with dealii::, the @ref Tutorial
+ * therefore have a <code>using namespace dealii;</code> at the top of the
+ * code.
+ *
+ * In this documentation, the namespace prefix is suppressed for brevity.
+ */
+namespace dealii
+{
+}
