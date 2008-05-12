22178f0cda5298ebc671269dfdbd4328164dc934
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-05-12 20:54:59 +0000

    Reference step-33.
    git-svn-id: https://svn.dealii.org/trunk@16082 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/include/base/function_parser.h
+++ deal.II/base/include/base/function_parser.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2005, 2006, 2007 by the deal.II authors
+//    Copyright (C) 2005, 2006, 2007, 2008 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -44,6 +44,7 @@ template <typename> class Vector;
  * from the fparser.txt file that comes with the fparser library. Please refer
  * also to that file both for clarifications on how this wrapper works, as
  * well as for any issue regarding the licence that applies to this class.
+ * This class is used in the @ref step_33 "step-33" tutorial program.
  *
  * By using this class you indicate that you accept the terms of the licence
  * that comes with the fparser library. If you do not agree with them, you
