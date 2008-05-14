537eb8e5729c12b5a5488045ecf51493308c2168
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-05-14 19:18:25 +0000

    Fix tense.
    git-svn-id: https://svn.dealii.org/trunk@16094 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/include/base/parameter_handler.h
+++ deal.II/base/include/base/parameter_handler.h
@@ -772,7 +772,7 @@ namespace Patterns
  *
  *   The ParameterHandler class is discussed in detail in the @ref
  *   step_19 "step-19" example program, and is used in more realistic
- *   situation in @ref step_29 "step-29" and @ref step_33 "step-33".
+ *   situations in @ref step_29 "step-29" and @ref step_33 "step-33".
  *   
  *   <h3>Declaring entries</h3>
  *   
