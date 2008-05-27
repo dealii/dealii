a8805f4716fea6454f8a4d5ba6b60c4eff23c33a
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-05-27 22:35:40 +0000

    Update filenames of moved files.
    git-svn-id: https://svn.dealii.org/trunk@16249 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/face_orientations_3d.cc
+++ tests/deal.II/face_orientations_3d.cc
@@ -78,12 +78,12 @@ int main ()
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
 
-  test ("grid_in_3d_1.in");
-  test ("grid_in_3d_2.in");
-  test ("grid_in_3d_3.in");
-  test ("grid_in_3d_4.in");
+  test ("grid_in_3d/1.in");
+  test ("grid_in_3d/2.in");
+  test ("grid_in_3d/3.in");
+  test ("grid_in_3d/4.in");
 
-  test ("grid_in_3d_evil_0.in");
-  test ("grid_in_3d_evil_4.in");
+  test ("grid_in_3d/evil_0.in");
+  test ("grid_in_3d/evil_4.in");
 }
 
