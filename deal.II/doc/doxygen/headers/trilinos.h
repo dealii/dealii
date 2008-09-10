2b9c387f9ef24aabc1620aefb3200328821718f7
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2008-09-10 16:10:34 +0000

    Added a reference to TrilinosWrappers in the LAC documentation.
    git-svn-id: https://svn.dealii.org/trunk@16789 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/trilinos.h
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------------------
+//    $Id: trilinos.h 12036 2006-01-15 21:24:49Z wolf $
+//    Version: $Name$
+//
+//    Copyright (C) 2008 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-------------------------------------------------------------------------
+
+
+/**
+ * @defgroup TrilinosWrappers TrilinosWrappers
+ * 
+ * The classes in this module are wrappers around functionality
+ * provided by the Trilinos library. They provide a modern
+ * object-oriented interface that is compatible with the interfaces of
+ * the other linear algebra classes in deal.II. All classes and
+ * functions in this group reside in a namespace @p TrilinosWrappers.
+ *
+ * These classes are only available if a Trilinos installation was
+ * detected during configuration of deal.II. Refer to the README file
+ * for more details about this.
+ *
+ * @author Martin Kronbichler, Wolfgang Bangerth, 2008
+ * @ref SoftwareTrilinos
+ *
+ * @ingroup LAC
+ */
