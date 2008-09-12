7800e05c8c618b577e3633a55cc09db6c122c570
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-09-12 18:41:23 +0000

    Avoid confusing the compiler by explicitly casting the zero to its intended data type.
    git-svn-id: https://svn.dealii.org/trunk@16822 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/vector.h
+++ deal.II/lac/include/lac/vector.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
+//    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -935,7 +935,7 @@ void Vector<Number>::reinit (const unsigned int n, const bool fast)
     };
   vec_size = n;
   if (fast == false)
-    *this = 0;
+    *this = static_cast<Number>(0);
 }
 
 
