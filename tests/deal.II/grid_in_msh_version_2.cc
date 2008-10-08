3acf291ef21890e0a5a263d2a3366447e5773a4c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2008-10-08 01:48:56 +0000

    Add proper output file.
    git-svn-id: https://svn.dealii.org/trunk@17144 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/grid_in_msh_version_2.cc
+++ tests/deal.II/grid_in_msh_version_2.cc
@@ -59,8 +59,8 @@ void filename_resolution()
 
 int main ()
 {
-  deallog << std::setprecision (2);
-  logfile << std::setprecision (2);
+  deallog << std::setprecision (5);
+  logfile << std::setprecision (5);
   deallog.attach(logfile);
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
