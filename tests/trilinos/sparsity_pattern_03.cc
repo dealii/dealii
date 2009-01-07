e421faee38587c7e556e99ef917e5da2a58ab057
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2009-01-07 15:03:36 +0000

    Tests for Trilinos sparsity pattern.
    git-svn-id: https://svn.dealii.org/trunk@18119 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/trilinos/sparsity_pattern_03.cc
@@ -0,0 +1,35 @@
+//----------------------------  compressed_sparsity_pattern_03.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2008 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  sparsity_pattern_03.cc  ---------------------------
+
+
+// check TrilinosWrappers::SparsityPattern::print
+
+#include "sparsity_pattern_common.h"
+#include <base/utilities.h>
+
+int main (int argc, char** argv)
+{
+  Utilities::System::MPI_InitFinalize mpi_init (argc, argv);
+
+  std::ofstream logfile("sparsity_pattern_03/output");
+  logfile.setf(std::ios::fixed);
+  deallog << std::setprecision(3);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  print ();
+}
+
+  
+  
---
+++ tests/trilinos/sparsity_pattern_common.h
@@ -0,0 +1,85 @@
+//---------------------------  sparsity_pattern_common.h  -------------------------
+//    $Id: sparsity_pattern_01.cc 15674 2008-01-24 17:40:56Z kanschat $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2008 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------  sparsity_pattern_common.h  -------------------------
+
+
+
+// check
+
+#include "../tests.h"
+#include <lac/trilinos_sparsity_pattern.h>
+#include "../lac/testmatrix.h"
+
+const unsigned int N = 15;
+
+
+// reinitialize sparsity patterns for 5-point star
+void do_reinit (TrilinosWrappers::SparsityPattern &sp)
+{
+  sp.reinit((N-1)*(N-1), (N-1)*(N-1));
+}
+
+
+
+void build_sparsity (TrilinosWrappers::SparsityPattern &sparsity_pattern)
+{
+				   // generate usual 5-point sparsity pattern
+  do_reinit (sparsity_pattern);
+  FDMatrix(N,N).five_point_structure (sparsity_pattern);
+  sparsity_pattern.compress ();
+
+  deallog << sparsity_pattern.n_rows() << " "
+	  << sparsity_pattern.n_cols() << " "
+	  << sparsity_pattern.bandwidth() << " "
+	  << sparsity_pattern.n_nonzero_elements()
+	  << std::endl;
+}
+
+
+
+void row_length ()
+{
+  TrilinosWrappers::SparsityPattern sparsity_pattern;
+  build_sparsity (sparsity_pattern);
+  
+  for (unsigned int i=0; i<sparsity_pattern.n_rows(); ++i)
+    deallog << sparsity_pattern.row_length(i) << std::endl;
+
+  deallog << "OK" << std::endl;
+}
+
+
+
+void print_gnuplot ()
+{
+  TrilinosWrappers::SparsityPattern sparsity_pattern;
+  build_sparsity (sparsity_pattern);
+
+  sparsity_pattern.print_gnuplot(deallog.get_file_stream());
+
+  deallog << "OK" << std::endl;
+}
+
+
+
+void print ()
+{
+  TrilinosWrappers::SparsityPattern sparsity_pattern;
+  build_sparsity (sparsity_pattern);
+
+  sparsity_pattern.print(deallog.get_file_stream());
+
+  deallog << "OK" << std::endl;
+}
+
+
+  
