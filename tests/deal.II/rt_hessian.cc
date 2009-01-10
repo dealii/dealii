8640070ec6e815bfae76dfc4f736287122da3f7e
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-01-10 00:42:05 +0000

    Suppress miniscule numbers.
    git-svn-id: https://svn.dealii.org/trunk@18180 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/rt_hessian.cc
+++ tests/deal.II/rt_hessian.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2008 by the deal.II authors
+//    Copyright (C) 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -81,7 +81,7 @@ int main()
 
   deallog.attach(logfile);
   deallog.depth_console (0);
-  deallog.threshold_double(1.e-10);
+  deallog.threshold_double(1.e-8);
 
   test_hyper_sphere<2>();
   test_hyper_sphere<3>();
