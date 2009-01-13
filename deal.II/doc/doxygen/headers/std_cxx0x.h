3acb26d35cdc5574328a5fe183c7a6f28b483487
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-01-13 04:06:59 +0000

    add
    git-svn-id: https://svn.dealii.org/trunk@18199 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/std_cxx0x.h
@@ -0,0 +1,25 @@
+//-------------------------------------------------------------------------
+//    $Id: multithreading.h 12072 2006-01-17 23:42:23Z wolf $
+//    Version: $Name$
+//
+//    Copyright (C) 2009 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-------------------------------------------------------------------------
+
+/**
+ * A namespace that contains classes and functions that will be part
+ * of the next C++ standard (tentatively called C++0x) and that are
+ * currently available via the <a
+ * href="http://www.boost.org/">BOOST</a> library. The elements that
+ * are available through the current namespace are either imported
+ * from namespace std (if a compiler's library provides them) or from
+ * namespace boost.
+ */
+namespace std_cxx0x
+{
+}
