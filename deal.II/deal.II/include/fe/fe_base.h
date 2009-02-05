631b9cfc2879fdbb3c5111f81ed9b5df0b3d5d0b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-02-05 22:49:45 +0000

    Avoid a problem with doxygen not finding the matching declaration of a function.
    git-svn-id: https://svn.dealii.org/trunk@18337 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/fe/fe_base.h
+++ deal.II/deal.II/include/fe/fe_base.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 by the deal.II authors
+//    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -772,7 +772,7 @@ FiniteElementData<dim>::tensor_degree () const
 template <int dim>
 inline
 bool
-FiniteElementData<dim>::conforms (Conformity space) const
+FiniteElementData<dim>::conforms (const Conformity space) const
 {
   return ((space & conforming_space) == space);
 }
