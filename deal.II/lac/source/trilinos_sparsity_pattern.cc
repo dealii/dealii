0e30547af3335e2682b5774d76c86ae863a53fa0
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-02-12 19:44:16 +0000

    Refer to member function as   const_iterator::Accessor::visit_present_row () not   SparsityPattern::const_iterator::Accessor::visit_present_row ()
    (This worked because we had
      struct SparsityPattern
      {
          typedef SparsityPatternIterators::const_iterator const_iterator;
      };
    ).
    
    
    git-svn-id: https://svn.dealii.org/trunk@18378 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/source/trilinos_sparsity_pattern.cc
+++ deal.II/lac/source/trilinos_sparsity_pattern.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2008 by the deal.II authors
+//    Copyright (C) 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -27,7 +27,7 @@ namespace TrilinosWrappers
   namespace SparsityPatternIterators
   {
     void
-    SparsityPattern::const_iterator::Accessor::
+    const_iterator::Accessor::
     visit_present_row ()
     {
 				  // if we are asked to visit the
