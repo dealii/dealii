3f2ae8f49974c7a33203996090d75078ed434f55
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-04-08 12:40:11 +0000

    Remove unused variable.
    git-svn-id: https://svn.dealii.org/trunk@18567 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/source/compressed_sparsity_pattern.cc
+++ deal.II/lac/source/compressed_sparsity_pattern.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2008 by the deal.II authors
+//    Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -211,7 +211,6 @@ CompressedSparsityPattern::Line::add_entries (ForwardIterator begin,
     return;
 
   const unsigned int n_cols = static_cast<unsigned int>(n_elements);
-  const unsigned int stop_size = entries.size() + n_cols;
 
   if (indices_are_sorted == true)
     {
