1c7a400c46d9ca878c4f9cf3c921646ada79119a
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-04-27 16:45:02 +0000

    Fix an error where we didn't size an array correctly.
    git-svn-id: https://svn.dealii.org/trunk@18750 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/umfpack/umfpack_04.cc
+++ tests/umfpack/umfpack_04.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 2002, 2003, 2004, 2005, 2006, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -122,7 +122,7 @@ void test ()
   
   deallog << "Number of dofs = " << dof_handler.n_dofs() << std::endl;
   
-  std::vector<unsigned int> size;
+  std::vector<unsigned int> size (dim);
   DoFTools::count_dofs_per_component(dof_handler, size);
   
   BlockSparsityPattern b_sparsity_pattern;
