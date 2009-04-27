3af29397c98b7875ffafee9b1deaf7a90d9b5891
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-04-27 16:47:45 +0000

    Reduce precision to avoid spurious differences.
    git-svn-id: https://svn.dealii.org/trunk@18751 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/umfpack/umfpack_03.cc
+++ tests/umfpack/umfpack_03.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 2002, 2003, 2004, 2005, 2006, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -123,7 +123,7 @@ int main ()
   std::ofstream logfile("umfpack_03/output");
   deallog.attach(logfile);
   deallog.depth_console(0);
-  deallog.threshold_double(1.e-10);
+  deallog.threshold_double(1.e-9);
 
   test<1> ();
   test<2> ();
