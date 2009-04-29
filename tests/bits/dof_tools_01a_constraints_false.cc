e35c64fb5c3f48e8ac56cf959d281dcf81b5f4c6
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-04-29 15:50:11 +0000

    New tests.
    git-svn-id: https://svn.dealii.org/trunk@18789 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/dof_tools_01a_constraints_false.cc
@@ -0,0 +1,68 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2003, 2004, 2007, 2009 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+#include "../tests.h"
+#include "dof_tools_common.cc"
+#include <lac/sparsity_pattern.h>
+
+// check
+//   DoFTools::
+//   make_sparsity_pattern (const DoFHandler<dim> &,
+//	                    SparsityPattern       &,
+//                          ConstraintMatrix,
+//                          true);
+
+
+std::string output_file_name = "dof_tools_01a_constraints_false/output";
+
+
+template <int dim>
+void
+check_this (const DoFHandler<dim> &dof_handler)
+{
+  ConstraintMatrix cm;
+  DoFTools::make_hanging_node_constraints (dof_handler, cm);
+  cm.close ();
+  
+                                   // create sparsity pattern
+  SparsityPattern sp (dof_handler.n_dofs(),
+                      dof_handler.max_couplings_between_dofs());
+  DoFTools::make_sparsity_pattern (dof_handler, sp, cm, false);
+  sp.compress ();
+  
+                                   // write out 10 lines of this
+                                   // pattern (if we write out the
+                                   // whole pattern, the output file
+                                   // would be in the range of 40 MB)
+  for (unsigned int l=0; l<10; ++l)
+    {
+      const unsigned int line = l*(sp.n_rows()/10);
+      for (unsigned int c=0; c<sp.row_length(line); ++c)
+        deallog << sp.column_number(line,c) << " ";
+      deallog << std::endl;
+    }
+
+                                   // write out some other indicators
+  deallog << sp.bandwidth () << std::endl
+          << sp.max_entries_per_row () << std::endl
+          << sp.n_nonzero_elements () << std::endl;
+
+  unsigned int hash = 0;
+  for (unsigned int l=0; l<sp.n_rows(); ++l)
+    hash += l*(sp.row_length(l) +
+               sp.get_rowstart_indices()[l] +
+               sp.get_column_numbers()[sp.get_rowstart_indices()[l]
+                                       +
+                                       (sp.row_length(l)>1 ? 1 : 0)]);
+  deallog << hash << std::endl;
+}
