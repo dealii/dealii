ceb2b459bb326227d8ec3e1872da0dbbcae177ab
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2009-06-08 04:00:37 +0000

    Limit number of digits that are written to file.
    git-svn-id: https://svn.dealii.org/trunk@18916 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/aniso/solution_transfer.cc
+++ tests/aniso/solution_transfer.cc
@@ -121,6 +121,8 @@ void transfer(std::ostream &out)
 int main()
 {
   std::ofstream logfile("solution_transfer/output");
+  deallog << std::setprecision (4);
+  logfile << std::setprecision (4);
   deallog.attach(logfile);
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
