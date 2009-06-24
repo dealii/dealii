2b77e9bbcb0a95bba042b6d18f1ae497a28ac656
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-06-24 16:24:44 +0000

    Fix missing word.
    git-svn-id: https://svn.dealii.org/trunk@18961 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/trilinos/50.cc
+++ tests/trilinos/50.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2004, 2005, 2008 by the deal.II authors
+//    Copyright (C) 2004, 2005, 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -12,7 +12,7 @@
 //----------------------------  trilinos_50.cc  ---------------------------
 
 
-// check TrilinosWrappers::operator = (Vector<T>) with T!=TrilinosScalar
+// check TrilinosWrappers::Vector::operator = (Vector<T>) with T!=TrilinosScalar
 
 #include "../tests.h" 
 #include <base/utilities.h>
