0943717cecea23e3876376d65bf1a1f0a6f83e7e
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-06-29 09:33:48 +0000

    Fix a compile problem in a template function that had apparently never been executed.
    git-svn-id: https://svn.dealii.org/trunk@18992 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/trilinos_vector.h
+++ deal.II/lac/include/lac/trilinos_vector.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2008 by the deal.II authors
+//    Copyright (C) 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -438,7 +438,7 @@ namespace TrilinosWrappers
       const int ierr = vector->ReplaceGlobalValues (size, &indices[0], 
 						    &values[0]);
 
-      AssertThrow (ierr == 0, VectorBase::ExcTrilinosError());
+      AssertThrow (ierr == 0, VectorBase::ExcTrilinosError(ierr));
     }
 
   
