dc1de7b4dbfceacedb136e57f6aff96665e6231b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-08-17 23:18:05 +0000

    Wording change, set props.
    git-svn-id: https://svn.dealii.org/trunk@19294 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/std_cxx1x.h
+++ deal.II/doc/doxygen/headers/std_cxx1x.h
@@ -1,5 +1,5 @@
 //-------------------------------------------------------------------------
-//    $Id: multithreading.h 12072 2006-01-17 23:42:23Z wolf $
+//    $Id$
 //    Version: $Name$
 //
 //    Copyright (C) 2009 by the deal.II authors
@@ -12,13 +12,12 @@
 //-------------------------------------------------------------------------
 
 /**
- * A namespace that contains classes and functions that will be part
- * of the next C++ standard (tentatively called C++1x) and that are
- * currently available via the <a
- * href="http://www.boost.org/">BOOST</a> library. The elements that
- * are available through the current namespace are either imported
- * from namespace std (if a compiler's library provides them) or from
- * namespace boost.
+ * A namespace that contains classes and functions that will be part of the
+ * next C++ standard (tentatively called C++1x) and that are currently
+ * available via the <a href="http://www.boost.org/">BOOST</a> library. The
+ * elements that are available through the current namespace are either
+ * imported from namespace <code>std</code> (if a compiler's library provides
+ * them) or from namespace boost.
  */
 namespace std_cxx1x
 {
