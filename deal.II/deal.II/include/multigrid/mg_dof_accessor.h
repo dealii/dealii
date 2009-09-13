df5424a33cb1f1f819dbb29ba803820b8671ebda
Guido Kanschat <dr.guido.kanschat@gmail.com>
2009-09-13 16:59:35 +0000

    add container type to MGDoFAccessor
    git-svn-id: https://svn.dealii.org/trunk@19448 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/multigrid/mg_dof_accessor.h
+++ deal.II/deal.II/include/multigrid/mg_dof_accessor.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -161,6 +161,12 @@ class MGDoFAccessor : public internal::MGDoFAccessor::Inheritance<structdim,dim,
 				      * iterator classes.
 				      */
     typedef MGDoFHandler<dim,spacedim> AccessorData;
+
+				     /**
+				      * The dof handler type used by
+				      * this accessor.
+				      */
+    typedef MGDoFHandler<dim, spacedim> Container;
     
 				     /**
 				      * @name Constructors
