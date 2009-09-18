a693e02543e7244cd8b77ab2243bca0a3fd6b283
Guido Kanschat <dr.guido.kanschat@gmail.com>
2009-09-18 23:29:12 +0000

    Assert pointer nonzero in dereferencing operators
    git-svn-id: https://svn.dealii.org/trunk@19477 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/include/base/smartpointer.h
+++ deal.II/base/include/base/smartpointer.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -50,7 +50,7 @@ DEAL_II_NAMESPACE_OPEN
  * <tt>SmartPointer<ABC></tt> is a mutable pointer.
  *
  * @ingroup memory
- * @author Guido Kanschat, Wolfgang Bangerth, 1998 - 2005
+ * @author Guido Kanschat, Wolfgang Bangerth, 1998 - 2009
  */
 template<typename T>
 class SmartPointer
@@ -299,6 +299,7 @@ template <typename T>
 inline
 T & SmartPointer<T>::operator * () const
 {
+  Assert(t != 0, ExcNotInitialized());
   return *t;
 }
 
@@ -308,6 +309,7 @@ template <typename T>
 inline
 T * SmartPointer<T>::operator -> () const
 {
+  Assert(t != 0, ExcNotInitialized());
   return t;
 }
 
