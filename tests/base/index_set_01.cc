0f0a753de87ea567bf2cfeb6523770592d97ff9e
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-10-09 21:56:14 +0000

    New class IndexSet.
    git-svn-id: https://svn.dealii.org/trunk@19788 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/index_set_01.cc
@@ -0,0 +1,51 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2009 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+// test functions in namespace Utilities
+
+#include "../tests.h"
+#include <iomanip>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+#include <base/index_set.h>
+
+
+void test ()
+{
+  IndexSet index_set (20);
+  index_set.add_range (2,4);
+  index_set.add_range (12,18);
+  index_set.add_index (6);
+  index_set.add_index (8);
+  index_set.add_index (14);
+  index_set.add_index (16);
+
+  for (unsigned int i=0; i<index_set.size(); ++i)
+    deallog << i << ' ' << (index_set.is_element(i) ? "true" : "false")
+	    << std::endl;
+}
+
+
+
+
+int main()
+{
+  std::ofstream logfile("index_set_01/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test ();
+}
