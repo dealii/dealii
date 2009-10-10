0d088c173bf5e2e07584c5d1649eb88d0117bd86
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-10-10 01:55:18 +0000

    More tests.
    git-svn-id: https://svn.dealii.org/trunk@19797 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/index_set_03.cc
@@ -0,0 +1,55 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2009 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+// test IndexSet::is_contiguous and compress()
+
+#include "../tests.h"
+#include <iomanip>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+#include <base/index_set.h>
+
+
+void test ()
+{
+  IndexSet index_set (10);
+  index_set.add_index (2);
+  index_set.add_index (3);
+  index_set.add_index (4);
+
+  index_set.add_index (6);
+  index_set.add_index (7);
+
+  deallog << (index_set.is_contiguous() ? "true" : "false")
+	  << std::endl;
+  Assert (index_set.is_contiguous() == false, ExcInternalError());
+
+  for (unsigned int i=0; i<index_set.size(); ++i)
+    deallog << i << ' ' << (index_set.is_element(i) ? "true" : "false")
+	    << std::endl;
+}
+
+
+
+
+int main()
+{
+  std::ofstream logfile("index_set_03/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test ();
+}
