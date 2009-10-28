52b7168e6e85cdd8f5d14fbab9f0ef3fb823bd4a
Wolfgang Bangerth <bangerth@math.tamu.edu>
2009-10-28 04:38:11 +0000

    Work around a problem with complex numbers.
    git-svn-id: https://svn.dealii.org/trunk@19999 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/dofs/dof_accessor.cc
+++ deal.II/deal.II/source/dofs/dof_accessor.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -179,7 +179,7 @@ get_interpolated_dof_values (const InputVector &values,
             if (restriction_is_additive[i]) 
               interpolated_values(i) += tmp2(i);
             else
-              if (tmp2(i) != 0)
+              if (tmp2(i) != number())
                 interpolated_values(i) = tmp2(i);
 	}
     }
