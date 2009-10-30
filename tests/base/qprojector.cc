428c12de9f9701d0465018c0467be4c48eb1f9d6
Guido Kanschat <dr.guido.kanschat@gmail.com>
2009-10-30 03:59:45 +0000

    test QProjector for Quadrature of dimension zero
    git-svn-id: https://svn.dealii.org/trunk@20008 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/qprojector.cc
+++ tests/base/qprojector.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2009 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -161,6 +161,7 @@ void check(Quadrature<1>& q)
   check_line<3> (q);
   deallog.pop();
   deallog.push("face");
+  check_face<1>(q);
   check_face<2>(q);
   check_face<3>(q);
   deallog.pop();
