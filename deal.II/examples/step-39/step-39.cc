1efb5f5b9caf5390c9713ce5f9646f1c729b2496
Guido Kanschat <dr.guido.kanschat@gmail.com>
2009-11-10 02:27:08 +0000

    create directory and file for step 39
    git-svn-id: https://svn.dealii.org/trunk@20080 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/examples/step-39/step-39.cc
@@ -0,0 +1,11 @@
+/* $Id$ */
+/* Author: Guido Kanschat, Texas A&M University, 2009 */
+
+/*    $Id$       */
+/*                                                                */
+/*    Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2009 by the deal.II authors */
+/*                                                                */
+/*    This file is subject to QPL and may not be  distributed     */
+/*    without copyright and license information. Please refer     */
+/*    to the file deal.II/doc/license.html for the  text  and     */
+/*    further information on this license.                        */
