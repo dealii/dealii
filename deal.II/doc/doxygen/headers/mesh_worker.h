70edb0bd05f6268cec3141b8bf6bd0639a4849ab
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-01-10 10:23:25 +0000

    add MeshWorker module
    git-svn-id: https://svn.dealii.org/trunk@20346 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/mesh_worker.h
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------------------
+//    $Id: lac.h 12034 2006-01-15 21:16:14Z wolf $
+//    Version: $Name$
+//
+//    Copyright (C) 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-------------------------------------------------------------------------
+
+
+/**
+ * @defgroup MeshWorker The MeshWorker interface
+ *
+ * A collection of classes and functions simplifying the coding of
+ * loops over all cells and faces.
+ * All classes and functions of this module are in the MeshWorker
+ * namespace, which also contains documentation on the usage.
+ */
