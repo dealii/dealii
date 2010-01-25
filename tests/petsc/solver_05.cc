8385b725ff40b127a20f833d06834f3bee4124ca
Timo Heister <timo.heister@gmail.com>
2010-01-25 09:33:23 +0000

    tests/petsc: more cleanup
    git-svn-id: https://svn.dealii.org/trunk@20440 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/petsc/solver_05.cc
+++ tests/petsc/solver_05.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2004, 2005 by the deal.II authors
+//    Copyright (C) 2004, 2005, 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -55,8 +55,8 @@ check_solve( SOLVER& solver, const MATRIX& A,
 int main(int argc, char **argv)
 {
   std::ofstream logfile("solver_05/output");
-  logfile.precision(4);
   deallog.attach(logfile);
+  deallog << std::setprecision(4);
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
 
