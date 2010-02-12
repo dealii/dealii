eedafc395c10d8fa1e52b427bd9efe14304d082e
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-02-12 23:12:23 +0000

    superfluous exception removed
    git-svn-id: https://svn.dealii.org/trunk@20579 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/lac/include/lac/block_sparse_matrix.templates.h
+++ deal.II/lac/include/lac/block_sparse_matrix.templates.h
@@ -52,11 +52,9 @@ BlockSparseMatrix<number> &
 BlockSparseMatrix<number>::
 operator = (const BlockSparseMatrix<number> &m) 
 {
-  Assert (this->row_block_indices == m.row_block_indices,
-          ExcBlockDimensionMismatch());
-  Assert (this->column_block_indices == m.column_block_indices,
-          ExcBlockDimensionMismatch());
-
+  AssertDimension (this->row_block_indices, m.row_block_indices);
+  AssertDimension (this->column_block_indices, m.column_block_indices);
+  
                                    // this operator does not do
 				   // anything except than checking
 				   // whether the base objects want to
