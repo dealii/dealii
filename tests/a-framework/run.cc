356ddee4dcfcc8874e32288633d52c07040e0098
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-02-26 05:52:47 +0000

    Add a test that is guaranteed to run successfully.
    git-svn-id: https://svn.dealii.org/trunk@20699 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/a-framework/run.cc
@@ -0,0 +1,30 @@
+//----------------------------  timer.cc  ---------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2010by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  timer.cc  ---------------------------
+
+
+// test the testsuite framework. this test is supposed to run successfully
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <fstream>
+
+
+int main ()
+{
+  std::ofstream logfile("run/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  deallog << "OK" << std::endl;
+}
+
