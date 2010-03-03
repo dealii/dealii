531b22cb63c60e6401b5553c059ba63c989b74be
Baerbel Jannsen <baerbel.janssen@gmail.com>
2010-03-03 19:55:30 +0000

    copy_to_and_from_indices hold the global dof number and not a shifted global number
    git-svn-id: https://svn.dealii.org/trunk@20730 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/multigrid/mg_transfer_component.cc
+++ deal.II/deal.II/source/multigrid/mg_transfer_component.cc
@@ -651,7 +651,7 @@ void MGTransferSelect<number>::build_matrices (
 		  const unsigned int global_start
 		    = component_start[target_component[component]];
 		  temp_copy_indices[level_dof_indices[i]-level_start] = 
-		    global_dof_indices[i]-global_start;
+		    global_dof_indices[i];//-global_start;
 		}
 	    }
 	}
