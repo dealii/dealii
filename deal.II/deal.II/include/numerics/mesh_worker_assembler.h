a0134ebef1395e10cace4c7f1dd4414e66151726
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-03-15 16:26:16 +0000

    do MeshWorker::loop() sequentially for the time being
    git-svn-id: https://svn.dealii.org/trunk@20828 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/include/numerics/mesh_worker_assembler.h
+++ deal.II/deal.II/include/numerics/mesh_worker_assembler.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 //    $Id$
 //
-//    Copyright (C) 2006, 2007, 2008, 2009, 2010 by the deal.II authors
+//    Copyright (C) 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
