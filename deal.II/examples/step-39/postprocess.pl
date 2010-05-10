f483e3563f408d99d4bc342d9f1d92e4532815ed
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-05-10 03:17:55 +0000

    postprocess iteration steps
    git-svn-id: https://svn.dealii.org/trunk@21109 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/step-39/postprocess.pl
+++ deal.II/examples/step-39/postprocess.pl
@@ -10,16 +10,17 @@ my $step;     # The iteration step in the adaptive loop
 my @dofs;     # The number of degrees of freedom in each step
 my @error;    # The error of the solution
 my @estimate; # The a posteriori error estimate
-
+my @steps;    # The number of multigrid iteration steps
 while(<>)
 {
     $step = $1 if m/DEAL::Step\s*(\d+)/;
     $dofs[$step] = $1 if m/DEAL::DoFHandler\s*(\d+)/;
     $error[$step] = $1 if m/DEAL::Error\s*(\S+)/;
     $estimate[$step] = $1 if m/DEAL::Estimate\s*(\S+)/;
+    $steps[$step] = $1 if m/DEAL:\w+::Convergence step\s*(\S+)/;
 }
 
 for (my $i=0;$i<=$step;++$i)
 {
-    printf "%-3d\t%-7d\t%g\t%g\n", $i, $dofs[$i], $error[$i], $estimate[$i];
+    printf "%-3d\t%-7d\t%g\t%g\t%d\n", $i, $dofs[$i], $error[$i], $estimate[$i], $steps[$i];
 }
