313920a767d2addb77a1969580a8cf8088091a8d
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-06-07 21:14:00 +0000

    Fix a couple links.
    git-svn-id: https://svn.dealii.org/trunk@21168 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/parallel.h
+++ deal.II/doc/doxygen/headers/parallel.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2009 by the deal.II authors
+//    Copyright (C) 2009, 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -17,7 +17,9 @@
  * @brief A module discussing the use of multiple processor.
  *
  * This module contains information on %parallel computing. It is
- * subdivided into parts on @ref threads and on @ref distributed.
+ * subdivided into parts on @ref threads and on distributed
+ * computing. The latter part will become available in a future
+ * public release.
  */
 
 
@@ -25,8 +27,7 @@
  * A namespace in which we define classes and algorithms that deal
  * with running in %parallel on shared memory machines when deal.II is
  * configured to use multiple threads (see @ref threads), as well as
- * running things in %parallel on %distributed memory machines (see @ref
- * distributed).
+ * running things in %parallel on %distributed memory machines.
  *
  * @ingroup threads
  * @author Wolfgang Bangerth, 2008, 2009
