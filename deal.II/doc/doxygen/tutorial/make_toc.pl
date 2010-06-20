a62dacf047b2df18cade603819f8c9a38b109008
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-06-20 17:12:12 +0000

    add tooltips
    git-svn-id: https://svn.dealii.org/trunk@21232 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/tutorial/make_toc.pl
+++ deal.II/doc/doxygen/tutorial/make_toc.pl
@@ -4,14 +4,34 @@
 
 $/ = undef;
 
+# Read source for web page
 open TOC, "<toc.html.in";
 $toc = <TOC>;
 close TOC;
 
+# Read generated map file
 open MAP, "<steps.cmapx";
 $map = <MAP>;
 close MAP;
 
+# Insert contents of map file for @@MAP@@
 $toc =~ s/\@\@MAP\@\@/$map/;
+
+# Find tooltip files
+$list = `ls ../../../examples/step-*/doc/tooltip`;
+@list = split "\n", $list;
+
+foreach (@list)
+{
+    # Only the first line of the tooltip file is used
+    open TF, "<$_";
+    $tooltip = <TF>;
+    close TF;
+    chop $tooltip;
+    
+    m/step-(\d+)/;
+    $n = $1;
+    $toc =~ s/\@step$n\@/$tooltip/;
+}
 
 print $toc;
