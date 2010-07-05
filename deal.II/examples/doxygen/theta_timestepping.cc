9c8ac43c0dd25daa60804514998ae6c04c8deeab
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-07-05 18:14:15 +0000

    working on new example
    git-svn-id: https://svn.dealii.org/trunk@21454 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/doxygen/theta_timestepping.cc
+++ deal.II/examples/doxygen/theta_timestepping.cc
@@ -22,43 +22,106 @@
 using namespace dealii;
 using namespace Algorithms;
 
+
 class Explicit
-  : Operator<Vector<double> >
+  : public Operator<Vector<double> >
 {
   public:
+    Explicit(const FullMatrix<double>& matrix);
     void operator() (NamedData<Vector<double>*>& out,
 		     const NamedData<Vector<double>*>& in);
 
-    void initialize_timestep_data(const TimeStepData&);
+    void initialize_timestep_data(const TimestepData&);
   private:
-    const TimeStepData* timestep_data;
+    const TimestepData* timestep_data;
+    SmartPointer<const FullMatrix<double>, Explicit> matrix;
+    FullMatrix<double> m;
 };
 
   
 class Implicit
-  : Operator<Vector<double> >
+  : public Operator<Vector<double> >
 {
   public:
+    Implicit(const FullMatrix<double>& matrix);
     void operator() (NamedData<Vector<double>*>& out,
 		     const NamedData<Vector<double>*>& in);
 
-    void initialize_timestep_data(const TimeStepData&);
+    void initialize_timestep_data(const TimestepData&);
   private:
-    const TimeStepData* timestep_data;
+    const TimestepData* timestep_data;
+    SmartPointer<const FullMatrix<double>, Implicit> matrix;
+    FullMatrix<double> m;
 };
 
   
 int main()
 {
-  Explicit op_explicit;
-  Implicit op_implicit;
+  FullMatrix<double> matrix(2);
+  matrix(0,0) = -.1;
+  matrix(1,1) = -.1;
+  matrix(0,1) = 31.4;
+  matrix(1,0) = -31.4;
+
+  Explicit op_explicit(matrix);
+  Implicit op_implicit(matrix);
   ThetaTimestepping<Vector<double> > solver(op_explicit, op_implicit);
   op_explicit.initialize_timestep_data(solver.explicit_data());
   op_implicit.initialize_timestep_data(solver.implicit_data());
-
+  solver.notify(Events::initial);
+  
   Vector<double> value(2);
   NamedData<Vector<double>*> indata;
   NamedData<Vector<double>*> outdata;
-  outdata.add(&value, "value");
+  Vector<double>* p = &value;
+  outdata.add(p, "value");
   solver(outdata, indata);
 }
+
+
+Explicit::Explicit(const FullMatrix<double>& M)
+		:
+		matrix(&M)
+{}
+
+
+void
+Explicit::initialize_timestep_data(const TimestepData& t)
+{
+  timestep_data = &t;
+}
+
+
+void
+Explicit::operator() (NamedData<Vector<double>*>& out, const NamedData<Vector<double>*>& in)
+{
+  this->notifications.print(deallog);
+  deallog << std::endl;
+  unsigned int i = in.find("Previous time");
+  m.vmult(*out(0), *in(i));
+}
+
+
+Implicit::Implicit(const FullMatrix<double>& M)
+		:
+		matrix(&M)
+{}
+
+
+void
+Implicit::initialize_timestep_data(const TimestepData& t)
+{
+  timestep_data = &t;
+}
+
+
+void
+Implicit::operator() (NamedData<Vector<double>*>& out, const NamedData<Vector<double>*>& in)
+{
+  this->notifications.print(deallog);
+  deallog << std::endl;
+  unsigned int i = in.find("Previous time");
+  m.vmult(*out(0), *in(i));
+}
+
+
