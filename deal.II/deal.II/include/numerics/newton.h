a4ebb4348540688288b43bcaf9c8e24088fbd292
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-07-18 04:08:12 +0000

    re-add deprecated header files
    git-svn-id: https://svn.dealii.org/trunk@21514 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/deal.II/include/numerics/newton.h
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//
+//    Copyright (C) 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+/**
+ * @file
+ * @deprecated This file has moved to <tt>lac/algorithms</tt>
+ */
+
+#include <algorithms/newton.h>
