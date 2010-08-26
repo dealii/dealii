0abf0ca3f3d876adca8bf128f8b583cb02e52aad
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-08-26 09:52:15 +0000

    Add comment.
    git-svn-id: https://svn.dealii.org/trunk@21720 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/point_01.cc
+++ tests/base/point_01.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 by the deal.II authors
+//    Copyright (C) 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -11,6 +11,7 @@
 //
 //----------------------------  point_01.cc  ---------------------------
 
+// test for Point::operator()
 
 #include "../tests.h"
 #include <base/point.h>
