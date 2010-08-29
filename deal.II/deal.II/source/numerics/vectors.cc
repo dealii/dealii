31ad889036c7e00a6a12763604c3f2405f26e983
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-08-29 22:12:52 +0000

    Instantiate a missing function.
    git-svn-id: https://svn.dealii.org/trunk@21784 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/numerics/vectors.cc
+++ deal.II/deal.II/source/numerics/vectors.cc
@@ -186,6 +186,27 @@ void VectorTools::interpolate_boundary_values (
   const std::vector<bool>    &);
 
 template
+void VectorTools::interpolate_boundary_values (
+  const DoFHandler<deal_II_dimension> &,
+  const FunctionMap<deal_II_dimension>::type   &,
+  ConstraintMatrix                    &,
+  const std::vector<bool>    &);
+
+template
+void VectorTools::interpolate_boundary_values (
+  const hp::DoFHandler<deal_II_dimension> &,
+  const FunctionMap<deal_II_dimension>::type   &,
+  ConstraintMatrix                    &,
+  const std::vector<bool>    &);
+
+template
+void VectorTools::interpolate_boundary_values (
+  const MGDoFHandler<deal_II_dimension> &,
+  const FunctionMap<deal_II_dimension>::type   &,
+  ConstraintMatrix                    &,
+  const std::vector<bool>    &);
+
+template
 void VectorTools::project_boundary_values<deal_II_dimension>
 (const Mapping<deal_II_dimension>     &,
  const DoFHandler<deal_II_dimension>  &,
