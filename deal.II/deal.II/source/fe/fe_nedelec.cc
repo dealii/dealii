b2b79055895bd1a25b489e66807038c44da34466
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-09-13 11:18:12 +0000

    Re-add copyright notice.
    git-svn-id: https://svn.dealii.org/trunk@21937 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/deal.II/source/fe/fe_nedelec.cc
+++ deal.II/deal.II/source/fe/fe_nedelec.cc
@@ -1,3 +1,16 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
 #include <base/logstream.h>
 #include <base/utilities.h>
 #include <base/quadrature.h>
