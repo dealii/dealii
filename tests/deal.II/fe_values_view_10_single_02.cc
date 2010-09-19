2a5ef2af6234535b2a6b4ab3533a336d9619a55c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-09-19 14:57:53 +0000

    New test.
    git-svn-id: https://svn.dealii.org/trunk@22054 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/deal.II/fe_values_view_10_single_02.cc
@@ -0,0 +1,111 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$ 
+//
+//    Copyright (C) 2007, 2008, 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+
+// like _10, but with only a single non-primitive element. this test exists in
+// order to find out why at the time of writing the test the branch
+// implementing distributed meshes produced different output for the _10 test
+//
+// this test uses the FE_Nedelec as the single non-primitive element
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <base/function.h>
+#include <base/quadrature_lib.h>
+#include <lac/vector.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_boundary_lib.h>
+#include <dofs/dof_handler.h>
+#include <fe/fe_q.h>
+#include <fe/fe_dgq.h>
+#include <fe/fe_nedelec.h>
+#include <fe/fe_raviart_thomas.h>
+#include <fe/fe_system.h>
+#include <fe/fe_values.h>
+#include <fe/mapping_q1.h>
+
+#include <fstream>
+
+
+
+template<int dim>
+void test (const Triangulation<dim>& tr,
+	   const FiniteElement<dim>& fe)
+{
+  deallog << "FE=" << fe.get_name()
+	  << std::endl;
+
+  DoFHandler<dim> dof(tr);
+  dof.distribute_dofs(fe);
+
+  Vector<double> fe_function(dof.n_dofs());
+  for (unsigned int i=0; i<dof.n_dofs(); ++i)
+    fe_function(i) = i+1;
+  
+  const QGauss<dim> quadrature(2);
+  FEValues<dim> fe_values (fe, quadrature,
+			   update_values | update_gradients | update_hessians);
+  fe_values.reinit (dof.begin_active());
+
+  std::vector<Tensor<2,dim> > scalar_values (quadrature.n_quadrature_points);
+  std::vector<std::vector<Tensor<2,dim> > >
+    vector_values (quadrature.n_quadrature_points,
+		   std::vector<Tensor<2,dim> >(fe.n_components()));
+
+  fe_values.get_function_hessians (fe_function, vector_values);
+  
+  for (unsigned int c=0; c<fe.n_components(); ++c)
+    {
+      FEValuesExtractors::Scalar single_component (c);
+      fe_values[single_component].get_function_hessians (fe_function,
+							 scalar_values);
+      deallog << "component=" << c << std::endl;
+      
+      for (unsigned int q=0; q<fe_values.n_quadrature_points; ++q)
+	{
+	  deallog << scalar_values[q] << std::endl;
+	  Assert ((scalar_values[q] - vector_values[q][c]).norm()
+		  <= 1e-12 * scalar_values[q].norm(),
+		  ExcInternalError());
+	}
+    }
+}
+
+
+
+template<int dim>
+void test_hyper_sphere()
+{
+  Triangulation<dim> tr;
+  GridGenerator::hyper_ball(tr);
+
+  static const HyperBallBoundary<dim> boundary;
+  tr.set_boundary (0, boundary);
+
+  FE_Nedelec<dim> fe(1);
+  test(tr, fe);
+}
+
+
+int main()
+{
+  std::ofstream logfile ("fe_values_view_10_single_02/output");
+  deallog << std::setprecision (3);
+
+  deallog.attach(logfile);
+  deallog.depth_console (0);
+  deallog.threshold_double(1.e-7);
+
+  test_hyper_sphere<2>();
+  test_hyper_sphere<3>();
+}
