3577012ef14d45887833c41f6c0d6e94122d99b1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-09-19 00:44:19 +0000

    Split test into two that are a bit more atomic.
    git-svn-id: https://svn.dealii.org/trunk@22042 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/constraints.cc
+++ tests/deal.II/constraints.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2003, 2004, 2005, 2008 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2003, 2004, 2005, 2008, 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -12,7 +12,7 @@
 //----------------------------  constraints.cc  ---------------------------
 
 
-// create, merge, and print a bunch of ConstrainMatrices
+// create and print a bunch of ConstrainMatrices
 
 #include "../tests.h"
 #include <dofs/dof_handler.h>
@@ -241,55 +241,6 @@ void make_tria (Triangulation<3> &tria, int step)
 
 
 
-void merge_check ()
-{
-  deallog << "Checking ConstraintMatrix::merge" << std::endl;
-
-				   // check twice, once with closed
-				   // objects, once with open ones
-  for (unsigned int run=0; run<2; ++run)
-    {
-      deallog << "Checking with " << (run == 0 ? "open" : "closed")
-	      << " objects" << std::endl;
-      
-				       // check that the `merge' function
-				       // works correctly
-      ConstraintMatrix c1, c2;
-
-				       // enter simple line
-      c1.add_line (0);
-      c1.add_entry (0, 11, 1.);
-				       // add more complex line
-      c1.add_line (1);
-      c1.add_entry (1, 3, 0.5);
-      c1.add_entry (1, 4, 0.5);
-  
-				       // fill second constraints
-				       // object with one trivial line
-				       // and one which further
-				       // constrains one of the
-				       // entries in the first object
-      c2.add_line (10);
-      c2.add_entry (10, 11, 1.);
-      c2.add_line (3);
-      c2.add_entry (3, 12, 0.25);
-      c2.add_entry (3, 13, 0.75);
-
-				       // in one of the two runs,
-				       // close the objects
-      if (run == 1)
-	{
-	  c1.close ();
-	  c2.close ();
-	};
-
-				       // now merge the two and print the
-				       // results
-      c1.merge (c2);
-      c1.print (logfile);
-    };
-}
-
 
 int main ()
 {
@@ -338,7 +289,5 @@ int main ()
 
       delete fe;
     };
-
-  merge_check ();
 }
 
