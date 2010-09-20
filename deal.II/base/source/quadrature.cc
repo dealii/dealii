a868382568051594b45d696c216ad88295bfbded
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-09-20 21:37:22 +0000

    Add one instantiation that we now apparently need.
    git-svn-id: https://svn.dealii.org/trunk@22096 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/base/source/quadrature.cc
+++ deal.II/base/source/quadrature.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2006, 2007, 2008, 2009 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2006, 2007, 2008, 2009, 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -1596,6 +1596,7 @@ QIterated<dim>::QIterated (const Quadrature<1> &base_quadrature,
 
 
 // explicit instantiations; note: we need them all for all dimensions
+template class Quadrature<0>;
 template class Quadrature<1>;
 template class Quadrature<2>;
 template class Quadrature<3>;
