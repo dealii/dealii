42e682b3be96625684abf31151bc9cd9f1b7806d
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-09-24 14:27:08 +0000

    start testing cnsistency of basic functions
    git-svn-id: https://svn.dealii.org/trunk@22143 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/functions_01.cc
+++ tests/base/functions_01.cc
@@ -11,7 +11,7 @@
 //
 //-----------------------------------------------------------------------------
 
-// Plot flow functions in library and check their consistency
+// Check consistency of function implementations
 
 #include "../tests.h"
 #include <base/data_out_base.h>
@@ -30,10 +30,34 @@
 #include <fstream>
 #include <string>
 
+#define CHECK(F) { deallog << #F << std::endl;	\
+  F f;								\
+  check_function_value_consistency(f, 5);				\
+  check_function_gradient_consistency(f, 5);			\
+}  
+  
+
+#define CHECKN(F,arg) { deallog << #F << '(' << arg << ')' << std::endl;	\
+  F f(arg);								\
+  check_function_value_consistency(f, arg+1);				\
+  check_function_gradient_consistency(f, arg+1);			\
+}  
+  
+
 int main()
 {
   std::string logname = JobIdentifier::base_name(__FILE__) + std::string("/output");
   std::ofstream logfile(logname.c_str());
   deallog.attach(logfile);
   deallog.depth_console(0);
+
+  CHECK(Functions::SquareFunction<1>);
+  CHECK(Functions::SquareFunction<2>);
+  CHECK(Functions::SquareFunction<3>);
+  CHECK(Functions::CosineFunction<1>);
+  CHECK(Functions::CosineFunction<2>);
+  CHECK(Functions::CosineFunction<3>);
+  CHECK(Functions::ExpFunction<1>);
+  CHECK(Functions::ExpFunction<2>);
+  CHECK(Functions::ExpFunction<3>);  
 }
