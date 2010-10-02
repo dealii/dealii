f0c390f8560a755b68db826032101663bc98613c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-10-02 03:56:11 +0000

    Build the boost serialization library.
    git-svn-id: https://svn.dealii.org/trunk@22233 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/base/source/boost_serialization.cc
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2009, 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+#include <base/config.h>
+
+#include "../../contrib/boost/libs/serialization/src/basic_archive.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_iarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_iserializer.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_oarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_oserializer.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_pointer_iserializer.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_pointer_oserializer.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_serializer_map.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_text_iprimitive.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_text_oprimitive.cpp"
+#include "../../contrib/boost/libs/serialization/src/basic_xml_archive.cpp"
+#include "../../contrib/boost/libs/serialization/src/binary_iarchive.cpp"
+
+#include "../../contrib/boost/libs/serialization/src/binary_oarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/extended_type_info.cpp"
+#include "../../contrib/boost/libs/serialization/src/extended_type_info_typeid.cpp"
+#include "../../contrib/boost/libs/serialization/src/extended_type_info_no_rtti.cpp"
+#include "../../contrib/boost/libs/serialization/src/polymorphic_iarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/polymorphic_oarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/stl_port.cpp"
+#include "../../contrib/boost/libs/serialization/src/text_iarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/text_oarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/void_cast.cpp"
+#include "../../contrib/boost/libs/serialization/src/archive_exception.cpp"
+#include "../../contrib/boost/libs/serialization/src/xml_grammar.cpp"
+#include "../../contrib/boost/libs/serialization/src/xml_iarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/xml_oarchive.cpp"
+#include "../../contrib/boost/libs/serialization/src/xml_archive_exception.cpp"
+#include "../../contrib/boost/libs/serialization/src/shared_ptr_helper.cpp"
