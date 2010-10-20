6f2e4d8e7949f28c572e7b620a0ab7437adc35dc
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-10-20 14:26:36 +0000

    add headers for general use
    git-svn-id: https://svn.dealii.org/trunk@22395 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/tests.h
+++ tests/tests.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 2004, 2005, 2006, 2009 by the deal.II authors
+//    Copyright (C) 2004, 2005, 2006, 2009, 2010 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -17,10 +17,11 @@
 // common definitions used in all the tests
 
 #include <base/config.h>
+#include <base/job_identifier.h>
 #include <base/logstream.h>
 #include <base/exceptions.h>
 #include <cmath>
-
+#include <fstream>
 
 // implicitly use the deal.II namespace everywhere, without us having to say
 // so in each and every testcase
