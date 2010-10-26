b4d452344dc8231e8cda33361d14856539e267ce
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-10-26 19:22:32 +0000

    Be explicit about namespaces.
    git-svn-id: https://svn.dealii.org/trunk@22503 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/source/numerics/mesh_worker.all_dimensions.cc
+++ deal.II/source/numerics/mesh_worker.all_dimensions.cc
@@ -15,43 +15,45 @@
 
 DEAL_II_NAMESPACE_OPEN
 
-using namespace MeshWorker;
-
-template <typename number>
-void
-LocalResults<number>::reinit(const BlockIndices& bi)
+namespace MeshWorker
 {
-  for (unsigned int i=0;i<J.size();++i)
+  template <typename number>
+  void
+  LocalResults<number>::reinit(const BlockIndices& bi)
+  {
+    for (unsigned int i=0;i<J.size();++i)
       J[i] = 0.;
-  for (unsigned int i=0;i<R.size();++i)
-    R[i].reinit(bi);
-  for (unsigned int i=0;i<M1.size();++i)
-    M1[i].matrix.reinit(bi.block_size(M1[i].row),
-			bi.block_size(M1[i].column));
-  for (unsigned int i=0;i<M2.size();++i)
-    M2[i].matrix.reinit(bi.block_size(M2[i].row),
-			bi.block_size(M2[i].column));
-  for (unsigned int k=0;k<quadrature_values.size();++k)
-    for (unsigned int i=0;i<quadrature_values[k].size();++i)
-      quadrature_values[k][i] = 0.;
+    for (unsigned int i=0;i<R.size();++i)
+      R[i].reinit(bi);
+    for (unsigned int i=0;i<M1.size();++i)
+      M1[i].matrix.reinit(bi.block_size(M1[i].row),
+			  bi.block_size(M1[i].column));
+    for (unsigned int i=0;i<M2.size();++i)
+      M2[i].matrix.reinit(bi.block_size(M2[i].row),
+			  bi.block_size(M2[i].column));
+    for (unsigned int k=0;k<quadrature_values.size();++k)
+      for (unsigned int i=0;i<quadrature_values[k].size();++i)
+	quadrature_values[k][i] = 0.;
+  }
+
+
+  template <typename number>
+  unsigned int
+  LocalResults<number>::memory_consumption () const
+  {
+    unsigned int mem = sizeof(*this)
+		       + MemoryConsumption::memory_consumption(J)
+		       + MemoryConsumption::memory_consumption(R)
+		       + MemoryConsumption::memory_consumption(M1)
+		       + MemoryConsumption::memory_consumption(M2)
+		       + MemoryConsumption::memory_consumption(quadrature_values);
+    return mem;
+  }
+
+
+  template class LocalResults<float>;
+  template class LocalResults<double>;
 }
 
-
-template <typename number>
-unsigned int
-LocalResults<number>::memory_consumption () const
-{
-  unsigned int mem = sizeof(*this)
-		     + MemoryConsumption::memory_consumption(J)
-		     + MemoryConsumption::memory_consumption(R)
-		     + MemoryConsumption::memory_consumption(M1)
-		     + MemoryConsumption::memory_consumption(M2)
-		     + MemoryConsumption::memory_consumption(quadrature_values);
-  return mem;
-}
-
-
-template class LocalResults<float>;
-template class LocalResults<double>;
 
 DEAL_II_NAMESPACE_CLOSE
