b4da3d1c118914af29cf28265ec12633cd0798dc
Guido Kanschat <dr.guido.kanschat@gmail.com>
2010-10-27 21:08:29 +0000

    test tangential contiunity in a more intuitive way
    git-svn-id: https://svn.dealii.org/trunk@22529 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/fe/nedelec_2.cc
+++ tests/fe/nedelec_2.cc
@@ -28,6 +28,39 @@
 #define PRECISION 2
 
 
+void
+plot_diff(const Vector<double>& v1, const Vector<double>& v2, const Tensor<1,2>& n)
+{
+  AssertDimension(v1.size(), 2);
+  AssertDimension(v2.size(), 2);
+
+  double p1 = v1(0)*n[1] - v1(1)*n[0];
+  double p2 = v2(0)*n[1] - v2(1)*n[0];
+
+  deallog << " tangential  diff " << p1-p2 << " (" << p1 << '-' << p2 << ')' << std::endl;
+}
+
+
+void
+plot_diff(const Vector<double>& v1, const Vector<double>& v2, const Tensor<1,3>& n)
+{
+  AssertDimension(v1.size(), 3);
+  AssertDimension(v2.size(), 3);
+  
+  Tensor<1,3> p1, p2;
+  for (unsigned int d=0;d<3;++d)
+    {
+      const unsigned int d1 = (d+1)%3;
+      const unsigned int d2 = (d+2)%3;
+      
+      p1[d] = v1(d1)*n[d2] - v1(d2)*n[d1];
+      p2[d] = v2(d1)*n[d2] - v2(d2)*n[d1];
+    }
+  
+  deallog << " tangential  diff " << p1-p2 << " (" << p1 << '-' << p2 << ')' << std::endl;
+}
+
+
 template <int dim>
 void
 plot (const Triangulation<dim> &tr, const unsigned int p)
@@ -35,72 +68,91 @@ plot (const Triangulation<dim> &tr, const unsigned int p)
   deallog << dim << "d, "
           << tr.n_active_cells() << " CELLS" << std::endl;
 
-  FE_Nedelec<dim> fe_ned (p);
-  
+				   // Create the finite element space
+  FE_Nedelec<dim> fe_ned (p);  
   DoFHandler<dim> dof(tr);
   dof.distribute_dofs(fe_ned);
+  ConstraintMatrix cm;
+  DoFTools::make_hanging_node_constraints (dof, cm);
+  cm.close ();
   
                                    // generate some numbers for the
                                    // degrees of freedom on this mesh
   Vector<double> values (dof.n_dofs());
   for (unsigned int i=0; i<values.size(); ++i)
     values(i) = i;
-
-  std::vector<unsigned int> face_dofs(fe_ned.dofs_per_face);
-
-				   // Uncomment this to isolate
-				   // boundary degrees of freedom
-//   if (p==1)
-//     for (typename DoFHandler<dim>::active_cell_iterator
-// 	   c = dof.begin_active();
-// 	 c!=dof.end(); ++c)
-//       for(unsigned int f=0;f<GeometryInfo<dim>::faces_per_cell;++f)
-// 	{
-// 	  c->face(f)->get_dof_indices(face_dofs);
-// 	  values(face_dofs[0]) = 0;
-// 	  values(face_dofs[1]) = 1;
-// 	}
+  cm.distribute (values);
+  
                                    // then make sure that hanging node
                                    // constraints are taken care of
-  ConstraintMatrix cm;
-  DoFTools::make_hanging_node_constraints (dof, cm);
-  cm.close ();
-   cm.distribute (values);
 
                                    // now take these values, and print
                                    // the values of this so defined
                                    // function on each cell and on
                                    // each quadrature point
-  QTrapez<dim> quadrature;
-  std::vector<Vector<double> > shape_values (quadrature.n_quadrature_points,
+  QTrapez<dim-1> quadrature;
+  std::vector<Vector<double> > shape_values1 (quadrature.n_quadrature_points,
                                              Vector<double>(dim));
+  std::vector<Vector<double> > shape_values2 (quadrature.n_quadrature_points,
+                                             Vector<double>(dim));
+  
   MappingCartesian<dim> mapping;
-  FEValues<dim> fe(mapping, fe_ned, quadrature,
-                   update_values|update_q_points);
-
+  FEFaceValues<dim> feface(mapping, fe_ned, quadrature,
+			   update_values|update_q_points|update_normal_vectors);
+  FEFaceValues<dim> feneighbor(mapping, fe_ned, quadrature,
+			       update_values|update_q_points|update_normal_vectors);
+  FESubfaceValues<dim> fesubface(mapping, fe_ned, quadrature,
+				 update_values|update_q_points|update_normal_vectors);
+  
+  
   for (typename DoFHandler<dim>::active_cell_iterator
          c = dof.begin_active();
        c!=dof.end(); ++c)
-    {
-      deallog << "  CELL " << c << std::endl;
-      fe.reinit(c);
-      fe.get_function_values (values, shape_values);
+    for (unsigned int face=0;face<GeometryInfo<dim>::faces_per_cell;++face)
+      {
+	deallog << "cell " << c << " face " << face;
+	if (c->at_boundary(face))
+	  {
+	    deallog << " boundary" << std::endl;
+	    continue;
+	  }
 
-      for (unsigned int q=0; q<quadrature.n_quadrature_points; ++q)
-        {
-          deallog << ", xq=" << fe.quadrature_point(q)
-                  << ", f=[";
-          for (unsigned int d=0; d<dim; ++d)
-            deallog << (d==0 ? "" : " ")
-                    << shape_values[q](d);
-          
-          deallog << "]" << std::endl;
-        };
-                
-      deallog << std::endl;
-    }
-
-				   // Uncomment this to plost solutions
+	if (c->neighbor(face)->has_children())
+	  {
+	    deallog << " neighbor " << c->neighbor(face) << " refined" << std::endl;
+	    continue;
+	  }
+	
+	deallog << " neighbor " << c->neighbor(face) << std::endl;
+	
+	feface.reinit(c, face);
+	feface.get_function_values (values, shape_values1);
+	
+	if (c->neighbor(face)->level() < c->level())
+	  {
+	    std::pair<unsigned int, unsigned int> neighbor_face
+	      = c->neighbor_of_coarser_neighbor(face);
+	    
+	    fesubface.reinit(c->neighbor(face), neighbor_face.first, neighbor_face.second);
+	    fesubface.get_function_values (values, shape_values2);
+	    for (unsigned int k=0;k<feface.n_quadrature_points;++k)
+	      {
+		    deallog << feface.quadrature_point(k);
+		    plot_diff(shape_values1[k], shape_values2[k], feface.normal_vector(k));
+	      }
+	  }
+	else
+	  {
+	    feneighbor.reinit(c->neighbor(face), c->neighbor_of_neighbor(face));
+	    feneighbor.get_function_values (values, shape_values2);
+	    for (unsigned int k=0;k<feface.n_quadrature_points;++k)
+	      {
+		deallog << feface.quadrature_point(k);
+		plot_diff(shape_values1[k], shape_values2[k], feface.normal_vector(k));
+	      }
+	  }
+      }
+				   // Uncomment this to plot solutions
 //   if (dim==2 && p==1)
 //     {
 //       std::ofstream gnuplot_output("test.gpl");
@@ -124,7 +176,7 @@ check (const unsigned int p)
                                    // first everything on a
                                    // once-refined grid
   tr.refine_global (1);
-  plot (tr, p);
+//   plot (tr, p);
 
                                    // then same with one cell refined
   tr.begin_active()->set_refine_flag ();
