c2ec8efe9695b15a2d6ff546eb52fc589674626a
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-11-02 02:52:48 +0000

    Add another test.
    git-svn-id: https://svn.dealii.org/trunk@22582 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/patterns_01.cc
@@ -0,0 +1,44 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2005, 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+#include "../tests.h"
+#include <base/parameter_handler.h>
+#include <base/logstream.h>
+#include <memory>
+
+int main()
+{
+  std::ofstream logfile("patterns_01/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+				   // create a pattern and let it
+				   // output its description
+  Patterns::Integer pattern(-1,42);
+  const std::string desc = pattern.description();
+
+				   // now let the same class re-create
+				   // a pattern object from the
+				   // description and verify that the
+				   // result is the same as what we
+				   // started out with
+  Patterns::Integer * pattern2 = Patterns::Integer::create (desc);
+
+  Assert (pattern2 != 0, ExcInternalError());
+  Assert (desc == pattern2->description(), ExcInternalError());
+
+  deallog << desc << std::endl;
+
+  delete pattern2;
+}
