2a15f38bfd2b097eeea5c8bf9910bd13fe8d31a8
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-11-10 12:34:38 +0000

    Add documentation. Simplify.
    git-svn-id: https://svn.dealii.org/trunk@22664 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/fe/nedelec_crash_01.cc
+++ tests/fe/nedelec_crash_01.cc
@@ -10,36 +10,18 @@
 //
 //--------------------------------------------------------------------
 
-// Test, whether differential operators produce a cochain complex on
-// the standard Hilbert space sequence
+// A test extracted from integrators/cochain_01 that crashed with the
+// FE_Nedelec at the time
 
 #include "../tests.h"
 #include "../lib/test_grids.h"
 
 #include <base/logstream.h>
 
-#include <lac/matrix_block.h>
-#include <lac/sparse_matrix.h>
-#include <lac/block_sparsity_pattern.h>
-#include <lac/compressed_sparsity_pattern.h>
-#include <lac/solver_cg.h>
-#include <lac/precondition.h>
-
 #include <dofs/dof_handler.h>
-#include <dofs/dof_renumbering.h>
 #include <dofs/dof_tools.h>
 
-#include <fe/fe_q.h>
 #include <fe/fe_nedelec.h>
-#include <fe/fe_raviart_thomas.h>
-#include <fe/fe_dgq.h>
-#include <fe/fe_system.h>
-#include <fe/fe_values.h>
-#include <fe/mapping_q1.h>
-
-#include <numerics/mesh_worker_info.h>
-#include <numerics/mesh_worker_assembler.h>
-#include <numerics/mesh_worker_loop.h>
 
 
 int main()
@@ -50,6 +32,9 @@ int main()
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
 
+				   // generate a version of the
+				   // Nedelec element but force it to
+				   // use the old-style constraints
   struct MyFE : FE_Nedelec<3> {
       MyFE () : FE_Nedelec<3>(0) {};
       virtual bool hp_constraints_are_implemented () const
