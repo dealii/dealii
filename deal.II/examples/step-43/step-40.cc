016b934cb05352e65dfaa8d93ea03ed87d385a6b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-11-23 02:18:43 +0000

    Add.
    git-svn-id: https://svn.dealii.org/trunk@22843 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/examples/step-43/step-40.cc
@@ -0,0 +1,15 @@
+/* $Id$ */
+/* Author: Chih-Che Chueh, University of Victoria, 2010 */
+/*         Wolfgang Bangerth, Texas A&M University, 2010 */
+
+/*    $Id$       */
+/*                                                                */
+/*    Copyright (C) 2010 by Chih-Che Chueh and the deal.II authors */
+/*                                                                */
+/*    This file is subject to QPL and may not be  distributed     */
+/*    without copyright and license information. Please refer     */
+/*    to the file deal.II/doc/license.html for the  text  and     */
+/*    further information on this license.                        */
+
+int main ()
+{}
