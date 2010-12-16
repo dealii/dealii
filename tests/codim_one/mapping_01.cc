fa629d5c91e88882e3789db8e77086c9160737a8
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-12-16 23:45:10 +0000

    New test.
    git-svn-id: https://svn.dealii.org/trunk@22988 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/codim_one/mapping_01.cc
@@ -0,0 +1,83 @@
+//----------------------------  mapping_01.cc  ---------------------------
+//    $Id: bem.cc 22693 2010-11-11 20:11:47Z kanschat $
+//    Version: $Name$
+//
+//    Copyright (C) 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  mapping_01.cc  ---------------------------
+
+
+// test mapping surfaces in higher dimensions. when we use the MappingQ1
+// class, each 1d cell in 2d space is mapped to a straight line and so all
+// cell normals should be parallel. likewise, if the four vertices of a 2d
+// cell in 3d space are in a plane, then the cell normal vectors at all
+// quadrature points of the same cell should be parallel, even though the true
+// surface is curved
+
+#include "../tests.h"
+
+#include <base/quadrature_lib.h>
+#include <grid/tria.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_boundary_lib.h>
+#include <grid/grid_out.h>
+#include <grid/grid_tools.h>
+#include <fe/fe_values.h>
+#include <fe/fe_q.h>
+#include <fe/mapping_q1.h>
+
+
+template <int dim>
+void test ()
+{
+  deallog << "Testing hyper_ball in dim: " << dim << "..."<< std::endl;
+
+  Triangulation<dim> volume_mesh;
+  GridGenerator::hyper_ball(volume_mesh);
+
+  const HyperBallBoundary<dim-1,dim> surface_description;
+  Triangulation<dim-1,dim> boundary_mesh;
+  boundary_mesh.set_boundary (0, surface_description);
+
+  GridTools::extract_boundary_mesh (volume_mesh, boundary_mesh);
+
+  QGauss<dim-1> quadrature(2);
+  MappingQ1<dim-1,dim> mapping;
+  FE_Q<dim-1,dim> fe (1);
+  
+  FEValues<dim-1,dim> fe_values (mapping, fe, quadrature, update_cell_normal_vectors);
+  
+  for (typename Triangulation<dim-1,dim>::active_cell_iterator
+	 cell = boundary_mesh.begin_active(); cell != boundary_mesh.end();
+       ++cell)
+    {
+      deallog << "Cell = " << cell
+	      << ", with center at " << cell->center()
+	      << std::endl;
+      fe_values.reinit (cell);
+
+      for (unsigned int q=0; q<quadrature.size(); ++q)
+	deallog << "  cell_normal[" << q << "] = "
+		<< fe_values.cell_normal_vector(q)
+		<< std::endl;
+    }
+}
+
+  
+
+int main ()
+{
+  std::ofstream logfile("mapping_01/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  test<2> ();
+  test<3> ();
+
+  return 0;
+}
