5d71164be2070d00c509c5e4eb4a3c3b32a6bc22
Wolfgang Bangerth <bangerth@math.tamu.edu>
2010-12-16 21:18:52 +0000

    Set properties.
    git-svn-id: https://svn.dealii.org/trunk@22982 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/step-38/step-38.cc
+++ deal.II/examples/step-38/step-38.cc
@@ -1,3 +1,14 @@
+/* $Id$ */
+/* Authors: Andrea Bonito, Sebastian Pauletti. */
+
+/*    $Id$       */
+/*                                                                */
+/*    Copyright (C) 2010 by the deal.II authors */
+/*                                                                */
+/*    This file is subject to QPL and may not be  distributed     */
+/*    without copyright and license information. Please refer     */
+/*    to the file deal.II/doc/license.html for the  text  and     */
+/*    further information on this license.                        */
 
 
 #include <base/quadrature_lib.h>
