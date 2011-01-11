f0bfea2b280f519225f974d59af5d9ff820d60f7
Toby D. Young <tyoung@ippt.pan.pl>
2011-01-11 12:04:58 +0000

    Added group doc slepc.h
    git-svn-id: https://svn.dealii.org/trunk@23168 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/doxygen/headers/slepc.h
@@ -0,0 +1,29 @@
+//-------------------------------------------------------------------------
+//    $Id: slepc.h 22855 2011-01-01 15:01:47Z young $
+//    Version: $Name$
+//
+//    Copyright (C) 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-------------------------------------------------------------------------
+
+
+/**
+ * @defgroup SLEPcWrappers SLEPcWrappers
+ * 
+ * The classes in this module are wrappers around functionality
+ * provided by the SLEPc library. All classes and functions in this
+ * group reside in a namespace @p PETScWrappers.
+ *
+ * These classes are only available if a SLEPc installation and a
+ * PETSc installation was detected during configuration of
+ * deal.II. Refer to the README file for more details about this.
+ *
+ * @author Toby D. Young, 2011
+ *
+ * @ingroup LAC PETScWrappers
+ */
