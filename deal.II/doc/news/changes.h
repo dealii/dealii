59e30099b23d3fe831a1d376a8c5e2885fc41ad9
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-01-15 21:36:28 +0000

    Add a few instantiations.
    git-svn-id: https://svn.dealii.org/trunk@23199 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/news/changes.h
+++ deal.II/doc/news/changes.h
@@ -4,9 +4,7 @@
 <p>
 This is the list of changes made after the release of
 deal.II version 7.0.0.
-All entries are signed with the names of the author. Regular
-contributor's names are abbreviated by WB (Wolfgang Bangerth), GK
-(Guido Kanschat), RH (Ralf Hartmann).
+All entries are signed with the names of the author.
 </p>
 
 
@@ -31,7 +29,18 @@ inconvenience this causes.
 <h3>General</h3>
 
 <ol>
-<li> Fixed: Boundary conditions in the step-23 tutorial program are now 
+<li> Extended: Several missing instantiations of functions for triangulations and DoF handlers embedded in higher dimensional space have been added.
+<br>
+(Wolfgang Bangerth, 2011/01/15)
+</ol>
+
+
+
+<a name="specific"></a>
+<h3>Specific improvements</h3>
+
+<ol>
+<li> Fixed: Boundary conditions in the step-23 tutorial program are now
 applied correctly. Matrix columns get eliminated with the used method
 and introduce some contribution to the right hand side coming from
 inhomogeneous boundary values. The old implementation did not reset the
@@ -39,20 +48,11 @@ matrix columns before applying new boundary values.<br>
 (Martin Stoll, Martin Kronbichler, 2011/01/14)
 </ol>
 
-<ol>
 <li> Extended: <code>base/tensor.h</code> has an additional collection of
 contractions between three tensors (<i>ie</i>. <code>contract3</code>).
 This can be useful for writing matrix/vector assembly in a more compact
 form than before.<br>
 (Toby D. Young, 2011/01/12)
-</ol>
-
-<a name="specific"></a>
-<h3>Specific improvements</h3>
-
-
-<ol>
-  <li>
 </ol>
 
 
