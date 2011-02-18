e3a5fe9951ada151c5bb21eabe5420500625d2d3
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-02-18 02:53:44 +0000

    Mark up input parameters as we usually do.
    git-svn-id: https://svn.dealii.org/trunk@23390 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/lac/constraint_matrix.h
+++ deal.II/include/deal.II/lac/constraint_matrix.h
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -562,7 +562,7 @@ class ConstraintMatrix : public Subscriptor
                                       * line. Unconstrained dofs also return a
                                       * zero value.
                                       */
-    double get_inhomogeneity (unsigned int line) const;
+    double get_inhomogeneity (const unsigned int line) const;
 
 				     /**
 				      * Print the constraint lines. Mainly
@@ -1906,7 +1906,7 @@ ConstraintMatrix::get_constraint_entries (unsigned int line) const
 
 inline
 double
-ConstraintMatrix::get_inhomogeneity (unsigned int line) const
+ConstraintMatrix::get_inhomogeneity (const unsigned int line) const
 {
 				// check whether the entry is
 				// constrained. could use is_constrained, but
