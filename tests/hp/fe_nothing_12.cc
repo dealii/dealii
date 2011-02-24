ed49228f97efd67d7d6afbef96c44ae6432b37d8
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-02-24 02:38:22 +0000

    Add a test that currently fails so that I can look at it at home.
    git-svn-id: https://svn.dealii.org/trunk@23443 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/fe_nothing_12.cc
@@ -0,0 +1,126 @@
+//----------------------------  fe_nothing_12.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2009, 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  fe_nothing_12.cc  ---------------------------
+
+
+// check the case where the two children on a face with a hanging node
+// have different FENothing structure:
+
+// active_fe_index
+// *-----*--*--*
+// |     | 1| 1|
+// |  0  *--*--*
+// |     | 0| 0|
+// *-----*--*--*
+
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <base/function.h>
+#include <fe/fe_nothing.h>
+#include <fe/fe_q.h>
+#include <hp/fe_collection.h>
+#include <hp/dof_handler.h>
+#include <grid/tria.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_accessor.h>
+#include <grid/tria_iterator.h>
+#include <grid/grid_refinement.h>
+#include <dofs/dof_accessor.h>
+#include <dofs/dof_tools.h>
+#include <fe/fe_q.h>
+#include <fe/fe_system.h>
+#include <hp/dof_handler.h>
+#include <hp/fe_values.h>
+#include <numerics/vectors.h>
+
+
+#include <fstream>
+
+
+template <int dim>
+void test ()
+{
+  Triangulation<dim>       triangulation;
+  std::vector<unsigned int> sub(dim, 1);
+  sub[dim-1] = 2;
+
+  GridGenerator::subdivided_hyper_rectangle (triangulation, sub, Point<dim>(), Point<dim>());
+  triangulation.begin_active()->set_refine_flag();
+  triangulation.execute_coarsening_and_refinement();
+
+  hp::FECollection<dim>    fe_collection;
+  fe_collection.push_back (FESystem<dim>(FE_Q<dim>(1), 1,
+					 FE_Nothing<dim>(), 1));
+  fe_collection.push_back (FESystem<dim>(FE_Nothing<dim>(), 1,
+					 FE_Q<dim>(1), 1));
+
+  hp::DoFHandler<dim>      dof_handler (triangulation);
+
+  dof_handler.begin_active()->set_active_fe_index(1);
+  typename hp::DoFHandler<dim>::active_cell_iterator
+    cell = dof_handler.begin_active(1),
+    endc = dof_handler.end();
+  for (; cell!=endc; ++cell)
+    if (cell->index() % 2 == 0)
+      cell->set_active_fe_index (0);
+    else
+      cell->set_active_fe_index (1);
+
+  dof_handler.distribute_dofs (fe_collection);
+
+  ConstraintMatrix constraints;
+
+  DoFTools::make_hanging_node_constraints (dof_handler, constraints);
+
+  constraints.close();
+
+  deallog << "   Number of constraints:        "
+          << constraints.n_constraints()
+          << std::endl;
+  {
+    typename hp::DoFHandler<dim>::active_cell_iterator
+      cell = dof_handler.begin_active(),
+      endc = dof_handler.end();
+
+    for(; cell != endc; cell++)
+      {
+	deallog << cell << ' ' << cell->active_fe_index() << std::endl
+		<< "   ";
+	std::vector<unsigned int> local_dof_indices (cell->get_fe().dofs_per_cell);
+	cell->get_dof_indices (local_dof_indices);
+
+	for (unsigned int i=0; i<cell->get_fe().dofs_per_cell; ++i)
+	  deallog << local_dof_indices[i]
+		  << (constraints.is_constrained(local_dof_indices[i]) ?
+		      "*" : "")
+		  << ' ';
+	deallog << std::endl;
+      }
+  }
+}
+
+
+
+int main ()
+{
+  std::ofstream logfile("fe_nothing_12/output");
+  logfile.precision(2);
+
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test<2> ();
+
+  deallog << "OK" << std::endl;
+}
