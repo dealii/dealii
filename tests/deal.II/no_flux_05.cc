9144488065e9bbe4f66e038d49d26124043239c7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-03-08 14:51:45 +0000

    New test.
    git-svn-id: https://svn.dealii.org/trunk@23470 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/deal.II/no_flux_05.cc
@@ -0,0 +1,103 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2007, 2008, 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+// check the creation of no-flux boundary conditions for a finite
+// element that consists of more than dim components and where
+// therefore we have to pick the vector components from somewhere in
+// the middle
+//
+// similar to _02, but for a Q^dim \times DGP element as used for
+// Stokes at times. the problem here is that the DGP element does not
+// have support points, which caused problems when this test was
+// written
+
+
+#include "../tests.h"
+#include <base/logstream.h>
+#include <base/function.h>
+#include <base/quadrature_lib.h>
+#include <lac/vector.h>
+#include <grid/grid_generator.h>
+#include <grid/tria_boundary_lib.h>
+#include <dofs/dof_handler.h>
+#include <dofs/dof_renumbering.h>
+#include <lac/constraint_matrix.h>
+#include <fe/fe_q.h>
+#include <fe/fe_dgp.h>
+#include <fe/fe_system.h>
+#include <fe/mapping_q1.h>
+#include <numerics/vectors.h>
+
+#include <fstream>
+
+
+
+template<int dim>
+void test (const Triangulation<dim>& tr,
+		      const FiniteElement<dim>& fe)
+{
+  DoFHandler<dim> dof(tr);
+  dof.distribute_dofs(fe);
+
+  DoFRenumbering::component_wise (dof);
+
+  for (unsigned int i=0; i<GeometryInfo<dim>::faces_per_cell; ++i)
+    {
+      deallog << "FE=" << fe.get_name()
+	      << ", case=" << i
+	      << std::endl;
+
+      std::set<unsigned char> boundary_ids;
+      for (unsigned int j=0; j<=i; ++j)
+	boundary_ids.insert (j);
+
+      ConstraintMatrix cm;
+      VectorTools::compute_no_normal_flux_constraints (dof, 0, boundary_ids, cm);
+
+      cm.print (deallog.get_file_stream ());
+    }
+}
+
+
+template<int dim>
+void test_hyper_cube()
+{
+  Triangulation<dim> tr;
+  GridGenerator::hyper_ball(tr);
+
+  static const HyperBallBoundary<dim> boundary;
+  tr.set_boundary (0, boundary);
+
+  tr.refine_global(1);
+
+  for (unsigned int degree=1; degree<4; ++degree)
+    {
+      FESystem<dim> fe (FE_Q<dim>(degree), dim,
+			FE_DGP<dim>(degree+1), 1);
+      test(tr, fe);
+    }
+}
+
+
+int main()
+{
+  std::ofstream logfile ("no_flux_05/output");
+  deallog << std::setprecision (2);
+  deallog << std::fixed;
+  deallog.attach(logfile);
+  deallog.depth_console (0);
+  deallog.threshold_double(1.e-12);
+
+  test_hyper_cube<2>();
+  test_hyper_cube<3>();
+}
