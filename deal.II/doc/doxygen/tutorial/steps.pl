5a688ddb1b9962b2f3fecf3ad81274dccaee2c1b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-04-22 23:45:53 +0000

    Link in step-46.
    git-svn-id: https://svn.dealii.org/trunk@23629 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/tutorial/steps.pl
+++ deal.II/doc/doxygen/tutorial/steps.pl
@@ -12,8 +12,8 @@ my @steps = (1,2,3,4,5,6,7,8,9,
 	     10,11,12,13,14,15,16,17,18,19,
 	     20,21,22,23,24,25,   27,28,29,
 	     30,31,   33,34,35,36,   38,39,
-             40,45);
-;
+             40,45,46);
+
 
 # List of additional node attributes to highlight purpose and state of the example
 my %style = (
