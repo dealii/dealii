069d6b8243e5b97447874ef26294fbfbc7613b4c
Luca Heltai <luca.heltai@sissa.it>
2011-05-16 10:50:02 +0000

    Added tetst for persistent triangulation
    git-svn-id: https://svn.dealii.org/trunk@23697 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/codim_one/grid_in_out_03.cc
@@ -0,0 +1,66 @@
+
+//----------------------------  template.cc  ---------------------------
+//    $Id: grid_in_out_02.cc 22595 2010-11-03 11:30:11Z bangerth $
+//    Version: $Name$
+//
+//    Copyright (C) 2005, 2008, 2010, 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  template.cc  ---------------------------
+
+
+// like grid_in_out but test persistent triangulation 
+
+#include "../tests.h"
+#include <fstream>
+#include <base/logstream.h>
+#include <grid/tria.h>
+#include <grid/tria_accessor.h>
+#include <grid/persistent_tria.h>
+#include <grid/grid_in.h>
+#include <grid/grid_out.h>
+
+#include <string>
+
+std::ofstream logfile("grid_in_out_03/output");
+
+template <int dim, int spacedim>
+void test(std::string filename) {
+  logfile << " Tria<" << dim << "," << spacedim << ">: "
+	  << filename << std::endl;
+  Triangulation<dim, spacedim> tria;
+  GridIn<dim, spacedim> gi;
+  gi.attach_triangulation (tria);
+  std::ifstream in (filename.c_str());
+  gi.read_ucd (in);
+  
+  PersistentTriangulation<dim, spacedim> ptria(tria);
+  typename Triangulation<dim,spacedim>::active_cell_iterator cell;
+
+  ptria.restore();
+  for(unsigned int i=0; i<2; ++i) 
+    {
+      for(cell=ptria.begin_active(); cell != ptria.end(); ++cell)
+	if(cell->center()[0]<.5)
+	  cell->set_refine_flag();
+      ptria.execute_coarsening_and_refinement();
+    }
+  
+  ptria.write_flags(logfile);
+}
+
+int main ()
+{
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  test<2,3>("grids/square.inp");
+  test<2,3>("grids/sphere_1.inp");
+
+  return 0;
+}
+
