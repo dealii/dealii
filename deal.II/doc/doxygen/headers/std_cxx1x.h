2bb07bdf67c5ef5388ff560750cad68091ba1ec4
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-06-02 03:45:28 +0000

    Update.
    git-svn-id: https://svn.dealii.org/trunk@23758 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/headers/std_cxx1x.h
+++ deal.II/doc/doxygen/headers/std_cxx1x.h
@@ -12,7 +12,7 @@
 //-------------------------------------------------------------------------
 
 /**
- * A namespace that contains classes and functions that will be part of the
+ * A namespace that contains a selection of classes and functions that will be part of the
  * next C++ standard (tentatively called C++1x) and that are currently
  * available via the <a href="http://www.boost.org/">BOOST</a> library. The
  * elements that are available through the current namespace are either
