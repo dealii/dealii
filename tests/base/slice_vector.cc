3bf04b7ca9f4bafc38ccb7b10b286e37bf4012f7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-06-30 21:14:12 +0000

    Don't compute stuff we never use. In particular, don't create variables then access beyond their end.
    git-svn-id: https://svn.dealii.org/trunk@23890 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/slice_vector.cc
+++ tests/base/slice_vector.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2004, 2005, 2006, 2008 by the deal.II authors
+//    Copyright (C) 2004, 2005, 2006, 2008, 2011 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -56,6 +56,4 @@ int main()
   f(v);
 
   make_slice(v, 3, 5);
-  int n = s[4];
-  n += 3;
 }
