470349528e391d37a82c97ac5eacd9d1310a5c7d
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2011-07-11 12:15:19 +0000

    Test for avoiding bug in FE_Q with arbitrary nodes: the hp line/quad identities need to be computed from support points, not equidistant points.
    git-svn-id: https://svn.dealii.org/trunk@23940 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/hp_quad_dof_identities_q_gl.cc
@@ -0,0 +1,75 @@
+//--------------------  hp_quad_dof_identities_q_gl.cc  ----------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2005, 2006 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//--------------------  hp_quad_dof_identities_q_gl.cc  ----------------------
+
+
+// check FE_Q::hp_quad_dof_identities for Gauss-Lobatto nodes
+
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/hp/fe_collection.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <fstream>
+
+
+template <int dim>
+void test ()
+{
+  hp::FECollection<dim> fe_collection;
+  for (unsigned int i=1; i<8-dim; ++i)
+    fe_collection.push_back (FE_Q<dim>(QGaussLobatto<1>(i+1)));
+
+  for (unsigned int i=0; i<fe_collection.size(); ++i)
+    for (unsigned int j=0; j<fe_collection.size(); ++j)
+      {
+	const std::vector<std::pair<unsigned int, unsigned int> >
+	  identities = fe_collection[i].hp_quad_dof_identities (fe_collection[j]);
+
+	deallog << "Identities for "
+		<< fe_collection[i].get_name() << " and "
+		<< fe_collection[j].get_name() << ": "
+		<< identities.size()
+		<< std::endl;
+
+	for (unsigned int k=0; k<identities.size(); ++k)
+	  {
+	    Assert (identities[k].first < fe_collection[i].dofs_per_quad,
+		    ExcInternalError());
+	    Assert (identities[k].second < fe_collection[j].dofs_per_quad,
+		    ExcInternalError());
+
+	    deallog << identities[k].first << ' '
+		    << identities[k].second
+		    << std::endl;
+	  }
+      }
+}
+
+
+
+int main ()
+{
+  std::ofstream logfile("hp_quad_dof_identities_q_gl/output");
+  logfile.precision(2);
+
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test<2> ();
+  test<3> ();
+
+  deallog << "OK" << std::endl;
+}
