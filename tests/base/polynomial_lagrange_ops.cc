219698412ae563d05c7ae6a8f02557b6a9215f77
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2011-07-26 12:58:28 +0000

    Polynomial evaluation at high orders: old implementation based on form a_0 + a_1 x^1 + ... + a_n x^n produces bad results.
    git-svn-id: https://svn.dealii.org/trunk@23959 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/polynomial_lagrange_ops.cc
@@ -0,0 +1,229 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+// Tests operations similar to polynomial_lagrange_order when the polynomial
+// is modified
+
+#include "../tests.h"
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/polynomial.h>
+#include <deal.II/base/quadrature_lib.h>
+
+
+using namespace Polynomials;
+
+
+void check_scale (const std::vector<Polynomial<double> > &p)
+{
+  deallog << "Scale operation";
+  for (unsigned int i=0; i<p.size(); ++i)
+    {
+      Polynomial<double> q = p[i];
+      double x = (double)rand()/RAND_MAX;
+      double factor = 5.*(double)rand()/RAND_MAX;
+      q.scale (factor);
+      double value1 = p[i].value (factor * x);
+      double value2 = q   .value (x);
+      if (std::fabs(value1-value2) > std::max(1e-13,1e-13*std::fabs(value1)))
+	deallog << "Error scale at x=" << x << ": p(t)=" << value1
+		<< ", q(x)=" << value2 << std::endl;
+      deallog << ".";
+    }
+  deallog << std::endl;
+}
+
+
+
+void check_shift (const std::vector<Polynomial<double> > &p)
+{
+  deallog << "Shift operation";
+  for (unsigned int i=0; i<p.size(); ++i)
+    {
+      Polynomial<double> q = p[i];
+      double x = (double)rand()/RAND_MAX;
+      double a = 10.*(-1.+2.*(double)rand()/RAND_MAX);
+      q.shift (a);
+      double value1 = p[i].value (x+a);
+      double value2 = q   .value (x);
+      if (std::fabs(value1-value2) > std::max(1e-13,1e-13*std::fabs(value1)))
+	deallog << "Error shift at x=" << x << ": p(t)=" << value1
+		<< ", q(x)=" << value2 << std::endl;
+      deallog << ".";
+    }
+  deallog << std::endl;
+}
+
+
+
+void check_mult_scalar (const std::vector<Polynomial<double> > &p)
+{
+  deallog << "Multiply by scalar";
+  for (unsigned int i=0; i<p.size(); ++i)
+    {
+      Polynomial<double> q = p[i];
+      double x = (double)rand()/RAND_MAX;
+      double a = (double)rand()/RAND_MAX;
+      q *= a;
+      double value1 = p[i].value (x) * a;
+      double value2 = q   .value (x);
+      if (std::fabs(value1-value2) > std::max(1e-13,1e-13*std::fabs(value1)))
+	deallog << "Error multiply at x=" << x << ": a*p(x)=" << value1
+		<< ", q(x)=" << value2 << std::endl;
+      deallog << ".";
+    }
+  deallog << std::endl;
+}
+
+
+
+void check_mult (const std::vector<Polynomial<double> > &p)
+{
+  deallog << "Multiply by polynomial";
+  for (unsigned int i=0; i<p.size(); ++i)
+    {
+      for (unsigned int j=0; j<p.size(); ++j)
+	{
+	  Polynomial<double> q = p[i];
+	  q *= p[j];
+	  double x = (double)rand()/RAND_MAX;
+	  double value1 = p[i].value (x) * p[j].value(x);
+	  double value2 = q   .value (x);
+	  if (std::fabs(value1-value2) > std::max(1e-13,1e-13*std::fabs(value1)))
+	    deallog << "Error multiply at x=" << x << ": p_1(x)*p_2(x)=" << value1
+		    << ", q(x)=" << value2 << std::endl;
+	}
+      deallog << ".";
+    }
+  deallog << std::endl;
+}
+
+
+
+void check_expand (const std::vector<Polynomial<double> > &p)
+{
+  if (p.size() > 10)
+    return;
+				// this checks whether the Lagrange product
+				// form and the usual form with factors for
+				// different powers does the same
+				// thing. Realize this by adding the
+				// polynomial '0' to the current
+				// polynomial. That destroys the product form
+				// and triggers the usual form. do not do this
+				// for higher order because then the standard
+				// form is unstable
+  deallog << "Expansion operation";
+  Monomial<double> zero(0, 0.);
+  for (unsigned int i=0; i<p.size(); ++i)
+    {
+      Polynomial<double> q = p[i];
+      double x = (double)rand()/RAND_MAX;
+      q += zero;
+      double value1 = p[i].value (x);
+      double value2 = q   .value (x);
+      if (std::fabs(value1-value2) > std::max(1e-10,1e-10*std::fabs(value1)))
+	deallog << "Error expansion at x=" << x << ": p(x)=" << value1
+		<< ", q(x)=" << value2 << std::endl;
+      deallog << ".";
+    }
+  deallog << std::endl;
+}
+
+
+
+void check_mult_expand (const std::vector<Polynomial<double> > &p)
+{
+  if (p.size() > 6)
+    return;
+  deallog << "Multiply by polynomial expanding";
+  Monomial<double> zero(0, 0.);
+  for (unsigned int i=0; i<p.size(); ++i)
+    {
+      for (unsigned int j=0; j<p.size(); ++j)
+	{
+	  Polynomial<double> q = p[i];
+	  q += zero;
+	  q *= p[j];
+	  double x = (double)rand()/RAND_MAX;
+	  double value1 = p[i].value (x) * p[j].value(x);
+	  double value2 = q   .value (x);
+	  if (std::fabs(value1-value2) > std::max(1e-10,1e-10*std::fabs(value1)))
+	    deallog << "Error multiply at x=" << x
+		    << ": p_"<<i<< "(x)*p_"<<j<<"(x)=" << value1
+		    << ", q(x)=" << value2 << std::endl;
+	}
+      deallog << ".";
+    }
+  deallog << std::endl;
+}
+
+
+
+void
+check_lge (unsigned int n)
+{
+  deallog << "Points: " << n+1 << std::endl;
+  std::vector<Polynomial<double> > p = LagrangeEquidistant::generate_complete_basis(n);
+  check_scale (p);
+  check_shift (p);
+  check_mult_scalar(p);
+  check_mult  (p);
+  check_expand(p);
+  check_mult_expand(p);
+  deallog << std::endl;
+}
+
+
+
+void
+check_poly (const Quadrature<1>& q)
+{
+  deallog << "Points: " << q.size() << std::endl;
+  std::vector<Polynomial<double> > p = generate_complete_Lagrange_basis(q.get_points());
+  check_scale (p);
+  check_shift (p);
+  check_mult_scalar(p);
+  check_mult  (p);
+  check_expand(p);
+  check_mult_expand(p);
+  deallog << std::endl;
+}
+
+
+
+int main()
+{
+  std::ofstream logfile("polynomial_lagrange_ops/output");
+  deallog << std::setprecision(3);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  deallog.push("LagrangeEquidistant");
+  for (unsigned i=1; i<10; i+=2)
+    check_lge (i);
+  deallog.pop();
+  deallog << std::endl;
+
+				// Lagrange elements on GL points have good
+				// conditioning, so test to some very high
+				// orders
+  deallog.push("GaussLobatto");
+  for (unsigned i=1; i<40; i+=3)
+    check_poly (QGaussLobatto<1>(i+1));
+  deallog.pop();
+}
---
+++ tests/base/polynomial_lagrange_product.cc
@@ -0,0 +1,112 @@
+//-----------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------------------------
+
+// Tests correctness of values and derivatives for polynomials derived from
+// Lagrange product form
+
+#include "../tests.h"
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/polynomial.h>
+#include <deal.II/base/quadrature_lib.h>
+
+
+using namespace Polynomials;
+
+
+void check_derivatives (const std::vector<Polynomial<double> >& p,
+			const unsigned int                      n_deriv)
+{
+				// check whether the values and derivatives
+				// are evaluated correctly some randomly
+				// generated points. compare with a polynomial
+				// that is not in product form (we get the
+				// expanded form by adding a dummy polynomial;
+				// addition of polynomials destroys the
+				// product form in the current implementation)
+  deallog << "Representation of derivatives up to order " << n_deriv-1 << std::endl;
+  std::vector<double> values(n_deriv), values_ref(n_deriv);
+  Monomial<double> zero (0,0);
+  for (unsigned int j=0; j<p.size(); ++j)
+    {
+      double x = (double)rand()/RAND_MAX;
+      p[j].value (x, values);
+      Polynomial<double> q = p[j];
+      q += zero;
+      q.value (x, values_ref);
+      for (unsigned int i=0; i<n_deriv; ++i)
+	{
+	  deallog << ".";
+	  if (std::fabs (values[i]-values_ref[i]) >
+	      std::max(1e-11,1e-11*std::fabs(values[i])))
+	    deallog << "Error deriv" << i << "  lg y="
+		    << std::log10(std::fabs(values[i]-values_ref[i]))
+		    << ", is: " << values[i] << ", should be: " << values_ref[i]
+		    << std::endl;
+	}
+    }
+  deallog << std::endl;
+
+}
+
+
+
+void
+check_poly (const Quadrature<1>& q)
+{
+  deallog << "Points: " << q.size() << std::endl;
+  std::vector<Polynomial<double> > p = generate_complete_Lagrange_basis(q.get_points());
+  for (unsigned int i=1; i<6; ++i)
+    check_derivatives(p, i);
+  deallog << std::endl;
+}
+
+
+
+void
+check_lge (unsigned int n)
+{
+  deallog << "Points: " << n+1 << std::endl;
+  std::vector<Polynomial<double> > p = LagrangeEquidistant::generate_complete_basis(n);
+  for (unsigned int i=1; i<6; ++i)
+    check_derivatives(p, i);
+  deallog << std::endl;
+}
+
+
+
+int main()
+{
+  std::ofstream logfile("polynomial_lagrange_product/output");
+  deallog << std::setprecision(3);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  deallog.push("LagrangeEquidistant");
+  for (unsigned i=1; i<8; i+=2)
+    check_lge (i);
+  deallog.pop();
+  deallog << std::endl;
+
+				// Lagrange elements on GL points have good
+				// conditioning, so test to some very high
+				// orders
+  deallog.push("GaussLobatto");
+  for (unsigned i=1; i<8; i+=2)
+    check_poly (QGaussLobatto<1>(i+1));
+  deallog.pop();
+}
