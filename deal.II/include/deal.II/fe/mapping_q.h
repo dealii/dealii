64d253ac4542cd30f60fa201110759e95d18e07b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-08-21 04:19:01 +0000

    Make the copy constructor of MappingQ explicit.
    git-svn-id: https://svn.dealii.org/trunk@24158 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/fe/mapping_q.h
+++ deal.II/include/deal.II/fe/mapping_q.h
@@ -82,7 +82,7 @@ class MappingQ : public MappingQ1<dim,spacedim>
 				      * #tensor_pols pointer as done
 				      * by a default copy constructor.
 				      */
-    MappingQ (const MappingQ<dim,spacedim> &mapping);
+    explicit MappingQ (const MappingQ<dim,spacedim> &mapping);
 
 				     /**
 				      * Destructor.
