72f9e436eb3403578ff40d2826cc18643e1cacc7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2011-08-31 11:30:33 +0000

    Add missing header file and update test output.
    git-svn-id: https://svn.dealii.org/trunk@24226 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/grid/tria.h
+++ deal.II/include/deal.II/grid/tria.h
@@ -25,6 +25,7 @@
 
 #include <boost/signals2.hpp>
 #include <boost/serialization/vector.hpp>
+#include <boost/serialization/map.hpp>
 #include <boost/serialization/split_member.hpp>
 
 #include <vector>
