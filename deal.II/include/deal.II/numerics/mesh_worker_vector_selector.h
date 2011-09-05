2d6c1a4b6c36a18ba2da6794eace2c31bbe25f5b
Guido Kanschat <dr.guido.kanschat@gmail.com>
2011-09-05 19:05:36 +0000

    svn had swallowed these
    git-svn-id: https://svn.dealii.org/trunk@24253 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/include/deal.II/numerics/mesh_worker_vector_selector.h
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//
+//    Copyright (C) 2010, 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+/**
+ * @file
+ * @deprecated The contents of this file has moved to <tt>meshworker/local_results.h>
+ */
+
+#include <deal.II/meshworker/vector_selector.h>
