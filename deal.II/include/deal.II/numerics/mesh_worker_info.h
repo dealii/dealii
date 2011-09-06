b070955a5dd3f1dcf7b32f6dfc11dd71c60b7927
Guido Kanschat <dr.guido.kanschat@gmail.com>
2011-09-06 15:43:55 +0000

    compatibility file for last move
    git-svn-id: https://svn.dealii.org/trunk@24270 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/include/deal.II/numerics/mesh_worker_info.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//
+//    Copyright (C) 2010, 2011 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+/**
+ * @file
+ * @deprecated The contents of this file has been split into
+ * <tt>meshworker/dof_info.h</tt> and  <tt>meshworker/integration_info.h</tt>
+ */
+
+#include <deal.II/meshworker/dof_info.h>
+#include <deal.II/meshworker/integration_info.h>
