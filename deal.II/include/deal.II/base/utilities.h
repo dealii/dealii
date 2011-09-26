89c9587bd51f76a859e7b38d3efaeb13aa5d4994
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2011-09-26 15:04:09 +0000

    Make compile without MPI.
    git-svn-id: https://svn.dealii.org/trunk@24427 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/base/utilities.h
+++ deal.II/include/deal.II/base/utilities.h
@@ -23,8 +23,7 @@
 #if defined(DEAL_II_COMPILER_SUPPORTS_MPI) || defined(DEAL_II_USE_PETSC)
 #include <mpi.h>
 #else
-
-extern boost::mutex m;typedef int MPI_Comm;
+  typedef int MPI_Comm;
 #endif
 
 #ifdef DEAL_II_USE_TRILINOS
