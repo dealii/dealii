7cd04785691d15b1a7e59bf594ea80c76d3fe513
Guido Kanschat <dr.guido.kanschat@gmail.com>
2011-09-26 20:32:32 +0000

    additional output
    git-svn-id: https://svn.dealii.org/trunk@24441 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/block_list_01.cc
+++ tests/deal.II/block_list_01.cc
@@ -2,7 +2,7 @@
 //    $Id$
 //    Version: $Name$ 
 //
-//    Copyright (C) 2009, 2010 by the deal.II authors
+//    Copyright (C) 2009, 2010, 2011 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -14,13 +14,17 @@
 #include "../tests.h"
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
-#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/multigrid/mg_dof_handler.h>
 #include <deal.II/grid/tria_iterator.h>
-#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/multigrid/mg_dof_accessor.h>
 #include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_dgq.h>
 #include <deal.II/fe/fe_raviart_thomas.h>
+#include <deal.II/fe/fe_nedelec.h>
 
 #include <deal.II/lac/block_list.h>
+
+#include <algorithm>
 
 using namespace dealii;
 
@@ -31,17 +35,26 @@ test_block_list(const Triangulation<dim>& tr, const FiniteElement<dim>& fe)
 {
   deallog << fe.get_name() << std::endl;
   
-  DoFHandler<dim> dof;
+  MGDoFHandler<dim> dof;
   dof.initialize(tr, fe);
   
   BlockList bl;
-  bl.initialize(tr.n_active_cells(), dof.begin_active(), dof.end());
+
+  const unsigned int level = tr.n_levels()-1;
+  
+  bl.initialize_mg(tr.n_cells(level), dof.begin(level), dof.end(level));
 
   for (unsigned int i=0;i<bl.size();++i)
     {
       deallog << "Block " << std::setw(3) << i;
+      std::vector<unsigned int> entries;
       for (BlockList::const_iterator b = bl.begin(i);b != bl.end(i);++b)
-	deallog << ' ' << std::setw(4) << (*b);
+	entries.push_back(*b);
+
+      std::sort(entries.begin(), entries.end());
+
+      for (unsigned int i=0;i<entries.size();++i)
+	deallog << ' ' << std::setw(4) << entries[i];
       deallog << std::endl;
     }
 }
@@ -54,14 +67,33 @@ int main()
   deallog.attach(logfile);
   deallog.depth_console(0);
   
-  Triangulation<2> tr;
-  GridGenerator::hyper_cube(tr);
-  tr.refine_global(2);
+  Triangulation<2> trc, trl;
+  GridGenerator::hyper_cube(trc);
+  trc.refine_global(2);
+  GridGenerator::hyper_L(trl);
+  trl.refine_global(2);
+  
   FE_Q<2> fe1(1);
   FE_Q<2> fe2(2);
   FE_RaviartThomas<2> fe3(0);
+  FE_RaviartThomas<2> fe4(1);
+  FE_DGQ<2> fe5(2);
+  FE_Nedelec<2> fe6(1);
   
-  test_block_list(tr, fe1);
-  test_block_list(tr, fe2);
-  test_block_list(tr, fe3);
+  deallog.push("Square");
+  test_block_list(trc, fe1);
+  test_block_list(trc, fe2);
+  test_block_list(trc, fe3);
+  test_block_list(trc, fe4);
+  test_block_list(trc, fe5);
+  test_block_list(trc, fe6);
+  deallog.pop();
+  deallog.push("L");
+  test_block_list(trl, fe1);
+  test_block_list(trl, fe2);
+  test_block_list(trl, fe3);
+  test_block_list(trl, fe4);
+  test_block_list(trl, fe5);
+  test_block_list(trl, fe6);
+  deallog.pop();
 }
