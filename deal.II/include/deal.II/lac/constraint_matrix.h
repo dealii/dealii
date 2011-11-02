13cdc53a5c6b827d794da77472f4e8f17876cc7c
Joerg Frohne <frohne@mathematik.uni-siegen.de>
2011-11-02 19:45:18 +0000

    fixed line 2054 again
    git-svn-id: https://svn.dealii.org/trunk@24720 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/lac/constraint_matrix.h
+++ deal.II/include/deal.II/lac/constraint_matrix.h
@@ -2051,9 +2051,8 @@ distribute_local_to_global (const FullMatrix<double>        &local_matrix,
                                    // function actually implementing this
                                    // feature in the cm.templates.h file.
   Vector<double> dummy(0);
-  bool           bool_dummy = false;
   distribute_local_to_global (local_matrix, dummy, local_dof_indices,
-                              global_matrix, dummy, bool_dummy,
+                              global_matrix, dummy, false,
                               internal::bool2type<IsBlockMatrix<MatrixType>::value>());
 }
 
