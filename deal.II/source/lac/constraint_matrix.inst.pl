63367d8afc8e8f5817c33d50ddfbbe2f0ee2b93e
Guido Kanschat <dr.guido.kanschat@gmail.com>
2011-12-11 03:04:41 +0000

    add copyright
    git-svn-id: https://svn.dealii.org/trunk@24814 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/source/lac/constraint_matrix.inst.pl
+++ deal.II/source/lac/constraint_matrix.inst.pl
@@ -4,11 +4,15 @@
 #
 # Copyright (C) 2011 by the deal.II authors
 #
+# This file is subject to QPL and may not be  distributed
+# without copyright and license information. Please refer
+# to the file deal.II/doc/license.html for the  text  and
+# further information on this license.
+#
 ######################################################################
 
 use strict;
 use warnings;
-
 use dealiitemplates;
 
 my $vector_functions = <<'EOT'
