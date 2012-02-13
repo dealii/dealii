48a57ea279d0d99834924633c3658fabc96cce03
Guido Kanschat <dr.guido.kanschat@gmail.com>
2012-02-13 19:48:01 +0000

    fix documentation
    git-svn-id: https://svn.dealii.org/trunk@25061 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/dofs/dof_handler.h
+++ deal.II/include/deal.II/dofs/dof_handler.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 //    $Id$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -1099,7 +1099,7 @@ class DoFHandler  :  public Subscriptor
 				      * the block structure on each
 				      * cell can be generated in this
 				      * object by calling
-				      * initialize_local_block_indices().
+				      * initialize_local_block_info().
 				      */
     const BlockInfo& block_info() const;
 
