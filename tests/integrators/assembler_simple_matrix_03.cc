ddee5c94081873c28f8bff32c801cd2ec4c9b27f
Guido Kanschat <dr.guido.kanschat@gmail.com>
2012-03-13 15:42:31 +0000

    test correct local block association
    git-svn-id: https://svn.dealii.org/trunk@25273 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/integrators/assembler_simple_matrix_03.cc
+++ tests/integrators/assembler_simple_matrix_03.cc
@@ -18,19 +18,40 @@
 #include "../tests.h"
 #include <deal.II/base/logstream.h>
 #include <deal.II/lac/full_matrix.h>
+#include <lac/sparse_matrix.h>
 #include <deal.II/lac/block_indices.h>
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
 #include <deal.II/meshworker/local_results.h>
 #include <deal.II/meshworker/dof_info.h>
 #include <deal.II/meshworker/simple.h>
 
 #include <deal.II/fe/fe_dgp.h>
+#include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_raviart_thomas.h>
 #include <deal.II/fe/fe_system.h>
 
 using namespace dealii;
+
+template <typename number>
+void fill_matrices(MeshWorker::LocalResults<number>& results, bool face)
+{
+  for (unsigned int k=0;k<results.n_matrices();++k)
+    {
+      FullMatrix<number>& M = results.matrix(k, false).matrix;
+      double base = 1000*(results.matrix(k).row+1) + 100*(results.matrix(k).column+1);
+      for (unsigned int i=0;i<M.m();++i)
+	for (unsigned int j=0;j<M.n();++j)
+	  {
+	    M(i,j) = base + 10*i+j;
+	    if (face)
+	      results.matrix(k, true).matrix(i,j) = base + 10*i+j;
+	  }
+    }
+}
+
 
 template <int dim>
 void test(FiniteElement<dim>& fe)
@@ -40,36 +61,47 @@ void test(FiniteElement<dim>& fe)
   Triangulation<dim> tr;
   GridGenerator::hyper_cube(tr);
   tr.refine_global(1);
-  tr.begin_active()->neighbor(1)->set_refine_flag();
-  tr.execute_coarsening_and_refinement();
-
+  
   DoFHandler<dim> dof(tr);
   dof.distribute_dofs(fe);
   dof.initialize_local_block_info();
+  DoFRenumbering::component_wise(dof);
+  
+  deallog << "DoFs " << dof.n_dofs() << std::endl;
   
   typename DoFHandler<dim>::active_cell_iterator cell = dof.begin_active();
   typename DoFHandler<dim>::face_iterator face = cell->face(1);
+  typename DoFHandler<dim>::active_cell_iterator neighbor = cell->neighbor(1);
   
-  MeshWorker::Assembler::MatrixSimple<FullMatrix<double> > ass;
+  CompressedSparsityPattern csp(dof.n_dofs(),dof.n_dofs());
+  DoFTools::make_flux_sparsity_pattern(dof, csp);
+  SparsityPattern sparsity;
+  sparsity.copy_from(csp);
+  SparseMatrix<double> M(sparsity);
+  
+  MeshWorker::Assembler::MatrixSimple<SparseMatrix<double> > ass;
+  ass.initialize(M);
   ass.initialize_local_blocks(dof.block_info().local());
   MeshWorker::DoFInfo<dim> info(dof.block_info());
   ass.initialize_info(info, false);
+  MeshWorker::DoFInfo<dim> infon(dof.block_info());
+  ass.initialize_info(infon, true);
 
-  deallog.push("cell");
+  deallog << "cell" << std::endl;
   info.reinit(cell);
-  info.print_debug(deallog);
-  deallog.pop();
+  fill_matrices(info, false);
+  ass.assemble(info);
+  M.print_formatted(deallog.get_file_stream(), 0, false, 6);
+  M = 0.;
   
-  deallog.push("face1");
-  info.reinit(cell, face, 1);
-  info.print_debug(deallog);
-  deallog.pop();
-
-  deallog.push("face2");
+  deallog << "face" << std::endl;
   ass.initialize_info(info, true);
   info.reinit(cell, face, 1);
-  info.print_debug(deallog);
-  deallog.pop();
+  infon.reinit(neighbor, neighbor->face(0), 0);
+  fill_matrices(info, true);
+  fill_matrices(infon, true);
+  ass.assemble(info, infon);
+  M.print_formatted(deallog.get_file_stream(), 0, false, 6);
 }
 
 int main()
@@ -81,14 +113,18 @@ int main()
 
   FE_DGP<2> p0(0);
   FE_DGP<2> p1(1);
-  FE_DGP<2> p2(2);
   FE_RaviartThomas<2> rt0(0);
-
-  FESystem<2> sys1(p0,1, p1, 1);
-  FESystem<2> sys2(p2,2,p0,3,p1,1);
-  FESystem<2> sys3(p0, 2, rt0, 1);
+  FE_Q<2> q2(2);
+  
+  FESystem<2> sys1(p0, 2, p1, 1);
+  FESystem<2> sys2(p0, 2, rt0, 1);
+  FESystem<2> sys3(rt0, 1, p0, 2);
+  FESystem<2> sys4(p1, 2, q2, 2);
+  FESystem<2> sys5(q2, 2, p1, 2);
   
   test(sys1);
   test(sys2);
   test(sys3);
+  test(sys4);
+  test(sys5);
 }
