b4d67be2daf5db5570704bf1237b4c5c1e5c68ba
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-03-14 16:45:14 +0000

    Reindent and simplify.
    git-svn-id: https://svn.dealii.org/trunk@25277 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/no_flux_10.cc
+++ tests/deal.II/no_flux_10.cc
@@ -2,7 +2,7 @@
 //    $Id: vectors_rhs_hp_02.cc 23710 2011-05-17 04:50:10Z bangerth $
 //    Version: $Name$
 //
-//    Copyright (C) 2000, 2001, 2003, 2004, 2006, 2007, 2011 by the deal.II authors
+//    Copyright (C) 2000, 2001, 2003, 2004, 2006, 2007, 2011, 2012 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -15,11 +15,11 @@
 //
 // An error occurred in line <4677> of file </scratch/deal-trunk/deal.II/include/deal.II/numerics/vectors.templates.h> in function
 //     void dealii::VectorTools::compute_no_normal_flux_constraints(const DH<dim, spacedim>&, unsigned int, const std::set<unsigned char>&, dealii::ConstraintMatrix&, const dealii::Mapping<dim, spacedim>&) [with int dim = 2, DH = dealii::DoFHandler, int spacedim = 2]
-// The violated condition was: 
+// The violated condition was:
 //     std::fabs(determinant (t)) > 1e-3
 // The name and call sequence of the exception was:
 //     ExcMessage("Found a set of normal vectors that are nearly collinear.")
-// Additional Information: 
+// Additional Information:
 // Found a set of normal vectors that are nearly collinear.
 
 // the crash does not happen without a boundary object. Same error occures with HyperShellBoundary instead of HalfHyperShellBoundary.
@@ -39,28 +39,29 @@ using namespace dealii;
 template <int dim>
 void run()
 {
- Triangulation<dim> triangulation;
- FESystem<dim> fe(FE_Q<dim>(1), dim);
- DoFHandler<dim> dof_handler (triangulation);
- ConstraintMatrix constraints;
- MappingQ<dim> mapping(1);
+  Triangulation<dim> triangulation;
+  FESystem<dim> fe(FE_Q<dim>(1), dim);
+  DoFHandler<dim> dof_handler (triangulation);
+  ConstraintMatrix constraints;
+  MappingQ<dim> mapping(1);
 
- GridGenerator::half_hyper_shell (triangulation,
-                                  Point<dim>(),
-                                  0.5,
-                                  1);
+  GridGenerator::half_hyper_shell (triangulation,
+				   Point<dim>(),
+				   0.5,
+				   1,
+				   2);
 
- static HalfHyperShellBoundary<dim> boundary;
- triangulation.set_boundary (0, boundary);
+  static HalfHyperShellBoundary<dim> boundary;
+  triangulation.set_boundary (0, boundary);
 
- dof_handler.distribute_dofs (fe);
+  dof_handler.distribute_dofs (fe);
 
- std::set<unsigned char> no_normal_flux_boundaries;
- no_normal_flux_boundaries.insert (0);
- VectorTools::compute_no_normal_flux_constraints
-   (dof_handler, 0,
-    no_normal_flux_boundaries,
-    constraints, mapping);
+  std::set<unsigned char> no_normal_flux_boundaries;
+  no_normal_flux_boundaries.insert (0);
+  VectorTools::compute_no_normal_flux_constraints
+    (dof_handler, 0,
+     no_normal_flux_boundaries,
+     constraints, mapping);
 }
 
 int main (int argc, char *argv[])
