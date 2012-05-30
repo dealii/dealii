05b502502f101b1b16e87847e9019d1bace04381
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-05-30 14:57:40 +0000

    One more test.
    git-svn-id: https://svn.dealii.org/trunk@25576 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/fe/mapping_real_to_unit_q4_straight.cc
+++ tests/fe/mapping_real_to_unit_q4_straight.cc
@@ -109,7 +109,7 @@ void test_real_to_unit_cell()
 int
 main()
 {
-  std::ofstream logfile ("mapping_real_to_unit_q4/output");
+  std::ofstream logfile ("mapping_real_to_unit_q4_straight/output");
   deallog.attach(logfile);
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
