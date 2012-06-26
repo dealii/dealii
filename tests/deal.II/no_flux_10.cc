a85caf0d70504c95237fcf69b2faeda214bb91f1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-06-26 14:05:22 +0000

    Reduce it to one cell.
    git-svn-id: https://svn.dealii.org/trunk@25647 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/no_flux_10.cc
+++ tests/deal.II/no_flux_10.cc
@@ -233,8 +233,6 @@ void run()
   triangulation.set_boundary (0, boundary);
   triangulation.set_boundary (1, boundary);
 
-  triangulation.refine_global(2);
-
   dof_handler.distribute_dofs (fe);
 
   std::set<unsigned char> no_normal_flux_boundaries;
