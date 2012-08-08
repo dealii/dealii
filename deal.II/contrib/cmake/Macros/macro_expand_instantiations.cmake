96afd75ff25e1c9ca7964154347e95653d6edca0
Matthias Maier <tamiko@kyomu.43-1.org>
2012-08-08 21:50:21 +0000

    Process all necessary *.in files
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@25790 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/contrib/cmake/Macros/macro_expand_instantiations.cmake
@@ -0,0 +1,25 @@
+MACRO(macro_expand_instantiations target inst_in_files)
+
+  FOREACH (inst_in_file ${inst_in_files})
+    STRING(REGEX REPLACE "\\.in$" "" inst_file "${inst_in_file}" )
+
+    ADD_CUSTOM_COMMAND (
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${inst_file}
+      DEPENDS expand_instantiations
+      COMMAND expand_instantiations
+      ARGS ${CMAKE_BINARY_DIR}/configured/template-arguments
+           < ${CMAKE_CURRENT_SOURCE_DIR}/${inst_in_file}
+           > ${CMAKE_CURRENT_BINARY_DIR}/${inst_file}
+      )
+
+    SET(inst_targets
+      ${CMAKE_CURRENT_BINARY_DIR}/${inst_file}
+      ${inst_targets}
+      )
+  ENDFOREACH()
+
+  ADD_CUSTOM_TARGET(${target}.inst ALL DEPENDS ${inst_targets})
+
+  ADD_DEPENDENCIES(${target} ${target}.inst)
+
+ENDMACRO()
