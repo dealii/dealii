f4e7214c87cda521842c554cfee82287dbe980bd
Timo Heister <timo.heister@gmail.com>
2012-08-10 08:46:58 +0000

    add missing files
    git-svn-id: https://svn.dealii.org/trunk@25856 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/include/deal.II/numerics/vectors.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+//    $Id: vectors.h -1   $
+//
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+//this files is deprecated, use vector_tools.h instead
+
+#ifndef __deal2__vectors_h
+#define __deal2__vectors_h
+
+#include <deal.II/numerics/vector_tools.h>
+
+#endif
---
+++ deal.II/include/deal.II/numerics/vectors.templates.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+//    $Id: vector_tools.templates.h -1   $
+//
+//    Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+//this files is deprecated, use vectors.templates.h instead
+
+#ifndef _deal2__vectors_templates_h
+#define _deal2__vectors_templates_h
+
+#include <deal.II/numerics/vector_tools.templates.h>
+
+#endif
