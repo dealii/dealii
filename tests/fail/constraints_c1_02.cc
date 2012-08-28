cb5c208064c4bb1f2164f5890a1143da90138c58
Timo Heister <timo.heister@gmail.com>
2012-08-28 15:37:14 +0000

    add reduced testcase
    git-svn-id: https://svn.dealii.org/trunk@26142 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fail/constraints_c1_02.cc
@@ -0,0 +1,63 @@
+/**********************************************************************
+ * $Id: constraint_test.cc -1   $
+ *
+ * Copyright Guido Kanschat, 2010, 2012
+ *
+ **********************************************************************/
+
+// reduced test case from constraint_c1.cc, causes a hang in close()
+
+
+#include <deal.II/base/job_identifier.h>
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace dealii;
+
+
+
+template <int dim>
+void
+run()
+{
+  ConstraintMatrix     constraints;
+
+  {
+  constraints.add_line(7);
+  std::vector<std::pair<unsigned int, double> > rhs;
+  rhs.push_back(std::pair<unsigned int, double>(41,1.0));
+  rhs.push_back(std::pair<unsigned int, double>(42,1.0));
+  constraints.add_entries(7, rhs);
+  }
+
+  {
+  constraints.add_line(41);
+  std::vector<std::pair<unsigned int, double> > rhs;
+  rhs.push_back(std::pair<unsigned int, double>(42,1.0));
+  constraints.add_entries(41, rhs);
+  }
+
+  {
+  constraints.add_line(42);
+  std::vector<std::pair<unsigned int, double> > rhs;
+  rhs.push_back(std::pair<unsigned int, double>(41,1.0));
+  constraints.add_entries(42, rhs);
+  }
+
+  deallog << "Closing" << std::endl;
+  constraints.close();
+  deallog << "Closed" << std::endl;
+
+}
+
+
+int main()
+{
+  const std::string logname = JobIdentifier::base_name(__FILE__) + std::string("/output");
+  std::ofstream logfile(logname.c_str());
+  deallog.attach(logfile);
+  
+  run();
+}
