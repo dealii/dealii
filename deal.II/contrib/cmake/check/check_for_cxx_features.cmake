13fc63d0e158e58b656f3cfe67cf58898f8b02c8
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-11 17:02:11 +0000

    Get rid of unnecessary tests
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26293 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/cmake/check/check_for_cxx_features.cmake
+++ deal.II/contrib/cmake/check/check_for_cxx_features.cmake
@@ -8,46 +8,6 @@ INCLUDE(CheckIncludeFiles)
 
 
 #
-# gcc2.95 doesn't have the std::iterator class, but the standard
-# requires it, so check whether we have to work around it
-#
-CHECK_CXX_SOURCE_COMPILES(
-  "
-  #include <iterator>
-  class MyIterator : public std::iterator<std::bidirectional_iterator_tag,int>{};
-  int main(){return 0;}
-  "
-  HAVE_STD_ITERATOR_CLASS)
-
-
-#
-# Up to early gcc2.95 releases, the i/ostringstream classes were not
-# available. check their availability, or whether we have to fall back
-# to the old strstream classes.
-#
-CHECK_CXX_SOURCE_COMPILES(
-  "
-  #include <sstream>
-  std::istringstream i;
-  std::ostringstream o;
-  int main(){return 0;}
-  "
-  HAVE_STD_STRINGSTREAM)
-
-
-#
-# Check whether the numeric_limits classes are available
-#
-CHECK_CXX_SOURCE_COMPILES(
-  "
-  #include <limits>
-  unsigned int i = std::numeric_limits<unsigned int>::min();
-  int main(){return 0;}
-  "
-  HAVE_STD_NUMERIC_LIMITS)
-
-
-#
 # Check whether the std::vector::iterator is just a plain pointer
 #
 CHECK_CXX_SOURCE_COMPILES(
