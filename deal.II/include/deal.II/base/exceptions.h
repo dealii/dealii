36acd75b0ad217cb0d031492382d1b48f59f1df2
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-12 11:32:53 +0000

    Remove obsolete HAVE_STD_OSTREAM checks
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26307 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/base/exceptions.h
+++ deal.II/include/deal.II/base/exceptions.h
@@ -21,14 +21,7 @@
 
 #include <exception>
 #include <string>
-
-// we only need output streams, but older compilers did not provide
-// them in a separate include file
-#ifdef HAVE_STD_OSTREAM_HEADER
-#  include <ostream>
-#else
-#  include <iostream>
-#endif
+#include <ostream>
 
 DEAL_II_NAMESPACE_OPEN
 
