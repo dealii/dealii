1dcf010227eaaf35ab828c3873649b32af12cce2
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-13 08:23:32 +0000

    Small changes on the BUILD_TYPE specific configuration
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26339 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/cmake/configure/configure_build.cmake
+++ deal.II/contrib/cmake/configure/configure_build.cmake
@@ -1,4 +1,11 @@
+#
+# Setup the build type and compiler flags
+#
+
+
+
 #TODO:
+
 SET(CMAKE_CXX_FLAGS "-Wfatal-errors -D_REENTRANT -fPIC -O2 -march=native")
 SET(CMAKE_C_FLAGS "-Wfatal-errors -D_REENTRANT -fPIC -O2 -march=native")
 
