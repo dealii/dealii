b60d6c3e7144d5a67963b43352cc2044b4e12735
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-14 14:32:05 +0000

    Remove obsolete file
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26382 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/cmake/macros/macro_list_contains.cmake
+++
@@ -1,9 +0,0 @@
-# grml...
-MACRO(LIST_CONTAINS var value)
-  SET(${var})
-  FOREACH (value2 ${ARGN})
-    IF (${value} STREQUAL ${value2})
-      SET(${var} TRUE)
-    ENDIF (${value} STREQUAL ${value2})
-  ENDFOREACH (value2)
-ENDMACRO(LIST_CONTAINS)
