a81d84fea352b0b54ec762ee2fbfc9601ce889e1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2012-09-14 12:32:17 +0000

    Step one in breaking apart our monster testcase 'internals'. That testcase had an output of over 6MB with more than 12000 lines, a good fraction of which is susceptible to round-off errors. It is time to split this testcase by finite element to be able to tell more easily what is going on. These new testcases take over the part where we test support points.
    git-svn-id: https://svn.dealii.org/trunk@26376 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fe/fe_support_points_common.h
@@ -0,0 +1,82 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2007, 2008, 2010, 2012 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+#include "../tests.h"
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/qprojector.h>
+#include <deal.II/base/logstream.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_q_hierarchical.h>
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_dgp.h>
+#include <deal.II/fe/fe_nedelec.h>
+#include <deal.II/fe/fe_bdm.h>
+#include <deal.II/fe/fe_raviart_thomas.h>
+#include <deal.II/fe/fe_system.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+
+template <int dim>
+inline void
+check_support (const FiniteElement<dim>& finel, const char* name)
+{
+  Triangulation<dim> tr;
+  GridGenerator::hyper_cube(tr, 0., 1.);
+  DoFHandler<dim> dof (tr);
+  dof.distribute_dofs (finel);
+
+  deallog << name << '<' << dim << '>' << " cell support points" << std::endl;
+
+  const std::vector<Point<dim> > &cell_points = finel.get_unit_support_points ();
+
+  for (unsigned int k=0;k<cell_points.size();++k)
+    deallog << std::setprecision(3) << cell_points[k] << std::endl;
+
+  const std::vector<Point<dim-1> > &face_points = finel.get_unit_face_support_points ();
+  const std::vector<double> dummy_weights (face_points.size());
+
+  Quadrature<dim-1> q(face_points, dummy_weights);
+
+  for (unsigned int i=0;i<GeometryInfo<dim>::faces_per_cell;++i)
+    {
+      std::vector<Point<dim> > q_points (q.get_points().size());
+      QProjector<dim>::project_to_face (q, i, q_points);
+      Quadrature<dim> qp(q_points);
+      deallog << name << '<' << dim << '>' << " face " << i
+		<< " support points" << std::endl;
+
+      for (unsigned int k=0; k<face_points.size(); ++k)
+	deallog << std::setprecision(3) << qp.point(k)
+		<< std::endl;
+    }
+}
+
+
+#define CHECK_ALL(EL,deg,dim) { FE_ ## EL<dim> EL(deg); check_support(EL, #EL #deg); }
+#define CHECK_SYS1(sub1,N1,dim) { FESystem<dim> q(sub1, N1); check_support(q, #sub1 #N1); }
+#define CHECK_SYS2(sub1,N1,sub2,N2,dim) { FESystem<dim> q(sub1, N1, sub2, N2); \
+                                          check_support(q, #sub1 #N1 #sub2 #N2); }
+#define CHECK_SYS3(sub1,N1,sub2,N2,sub3,N3,dim) { FESystem<dim> q(sub1, N1, sub2, N2, sub3, N3); \
+                                          check_support(q, #sub1 #N1 #sub2 #N2 #sub3 #N3); }
+
+
