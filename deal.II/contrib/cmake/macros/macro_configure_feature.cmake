877997327f864acfc266c07d39d3781403bf153d
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-14 14:32:25 +0000

    DEAL_II_FEATURE_AUTODETECTION
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26384 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/cmake/macros/macro_configure_feature.cmake
+++ deal.II/contrib/cmake/macros/macro_configure_feature.cmake
@@ -47,7 +47,9 @@
 #
 
 
+#
 # Some black magic to have substitution in command names:
+#
 MACRO(RUN_COMMAND the_command)
   FILE(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/macro_configure_feature.tmp"
     "${the_command}")
@@ -55,76 +57,29 @@ MACRO(RUN_COMMAND the_command)
 ENDMACRO()
 
 
+#
+# A small macro to set the DEAL_II_WITH_${feature} variables:
+#
+MACRO(SET_CACHED_OPTION feature value)
+  SET(DEAL_II_WITH_${feature}
+    ${value}
+    CACHE STRING
+    "Automatically set due to DEAL_II_FEATURE_AUTODETECTION"
+    FORCE)
+ENDMACRO()
+
+
 MACRO(CONFIGURE_FEATURE feature)
 
-  #
-  # First case: DEAL_II_FORCE_CONTRIB_${feature} is defined:
-  #
-  IF(DEAL_II_FORCE_CONTRIB_${feature})
-
-    IF(HAVE_CONTRIB_FEATURE_${feature})
-      RUN_COMMAND(
-        "
-        CONFIGURE_FEATURE_${feature}_CONTRIB(
-          FEATURE_${feature}_CONTRIB_CONFIGURED
-          )
-        "
-        )
-      IF(FEATURE_${feature}_CONTRIB_CONFIGURED)
-        MESSAGE(STATUS
-          "DEAL_II_WITH_${feature} successfully set up with contrib packages."
-          )
-      ELSE()
-        MESSAGE(SEND_ERROR
-          "Failed to set up DEAL_II_WITH_${feature} with contrib packages."
-          )
-      ENDIF()
-    ELSE()
-      MESSAGE(FATAL_ERROR
-        "Internal build system error: DEAL_II_FORCE_CONTRIB_${feature} defined, but HAVE_CONTRIB_FEATURE_${feature} not present."
-        )
-    ENDIF()
-
-  ELSE()
+  # Only (try) to configure ${feature} if we have to:
+  IF(DEAL_II_FEATURE_AUTODETECTION OR DEAL_II_WITH_${feature})
 
     #
-    # Second case: We may search for an external library:
+    # First case: DEAL_II_FORCE_CONTRIB_${feature} is defined:
     #
-    RUN_COMMAND(
-      "FIND_FEATURE_${feature}_EXTERNAL(FEATURE_${feature}_EXTERNAL_FOUND)"
-      )
+    IF(DEAL_II_FORCE_CONTRIB_${feature})
 
-    IF(FEATURE_${feature}_EXTERNAL_FOUND)
-
-      MESSAGE(STATUS
-        "All external dependencies for DEAL_II_WITH_${feature} are fullfilled."
-        )
-
-      RUN_COMMAND(
-        "
-        CONFIGURE_FEATURE_${feature}_EXTERNAL(
-          FEATURE_${feature}_EXTERNAL_CONFIGURED
-          )
-        "
-        )
-
-      IF(FEATURE_${feature}_EXTERNAL_CONFIGURED)
-        MESSAGE(STATUS
-          "DEAL_II_WITH_${feature} successfully set up with external dependencies."
-          )
-      ELSE()
-        MESSAGE(SEND_ERROR
-          "Failed to set up DEAL_II_WITH_${feature} with external dependencies."
-          )
-      ENDIF()
-
-    ELSE()
-
-      MESSAGE(STATUS
-        "DEAL_II_WITH_${feature} has unmet external dependencies."
-        )
-
-      IF(HAVE_CONTRIB_FEATURE_${feature} AND DEAL_II_ALLOW_CONTRIB)
+      IF(HAVE_CONTRIB_FEATURE_${feature})
         RUN_COMMAND(
           "
           CONFIGURE_FEATURE_${feature}_CONTRIB(
@@ -136,19 +91,96 @@ MACRO(CONFIGURE_FEATURE feature)
           MESSAGE(STATUS
             "DEAL_II_WITH_${feature} successfully set up with contrib packages."
             )
+          IF(DEAL_II_FEATURE_AUTODETECTION)
+            SET_CACHED_OPTION(DEAL_II_WITH_${feature} on)
+          ENDIF()
         ELSE()
-          # TODO: Opt out!
+          # This should not happen. So give an error
           MESSAGE(SEND_ERROR
             "Failed to set up DEAL_II_WITH_${feature} with contrib packages."
             )
         ENDIF()
       ELSE()
-        # TODO: Opt out!
-        RUN_COMMAND(
-          "CONFIGURE_FEATURE_${feature}_ERROR_MESSAGE()"
+        MESSAGE(FATAL_ERROR
+          "Internal build system error: DEAL_II_FORCE_CONTRIB_${feature} defined, but HAVE_CONTRIB_FEATURE_${feature} not present."
           )
       ENDIF()
 
+    ELSE()
+
+      #
+      # Second case: We are allowed to search for an external library:
+      #
+      RUN_COMMAND(
+        "FIND_FEATURE_${feature}_EXTERNAL(FEATURE_${feature}_EXTERNAL_FOUND)"
+        )
+
+      IF(FEATURE_${feature}_EXTERNAL_FOUND)
+
+        MESSAGE(STATUS
+          "All external dependencies for DEAL_II_WITH_${feature} are fullfilled."
+          )
+
+        RUN_COMMAND(
+          "
+          CONFIGURE_FEATURE_${feature}_EXTERNAL(
+            FEATURE_${feature}_EXTERNAL_CONFIGURED
+            )
+          "
+          )
+
+        IF(FEATURE_${feature}_EXTERNAL_CONFIGURED)
+          MESSAGE(STATUS
+            "DEAL_II_WITH_${feature} successfully set up with external dependencies."
+            )
+          IF(DEAL_II_FEATURE_AUTODETECTION)
+            SET_CACHED_OPTION(DEAL_II_WITH_${feature} on)
+          ENDIF()
+        ELSE()
+          # This should not happen. So give an error
+          MESSAGE(SEND_ERROR
+            "Failed to set up DEAL_II_WITH_${feature} with external dependencies."
+            )
+        ENDIF()
+
+      ELSE()
+
+        MESSAGE(STATUS
+          "DEAL_II_WITH_${feature} has unmet external dependencies."
+          )
+
+        IF(HAVE_CONTRIB_FEATURE_${feature} AND DEAL_II_ALLOW_CONTRIB)
+          RUN_COMMAND(
+            "
+            CONFIGURE_FEATURE_${feature}_CONTRIB(
+              FEATURE_${feature}_CONTRIB_CONFIGURED
+              )
+            "
+            )
+          IF(FEATURE_${feature}_CONTRIB_CONFIGURED)
+            MESSAGE(STATUS
+              "DEAL_II_WITH_${feature} successfully set up with contrib packages."
+              )
+            IF(DEAL_II_FEATURE_AUTODETECTION)
+              SET_CACHED_OPTION(DEAL_II_WITH_${feature} on)
+            ENDIF()
+          ELSE()
+            # This should not happen. So give an error
+            MESSAGE(SEND_ERROR
+              "Failed to set up DEAL_II_WITH_${feature} with contrib packages."
+              )
+          ENDIF()
+        ELSE()
+            IF(DEAL_II_FEATURE_AUTODETECTION)
+              SET_CACHED_OPTION(DEAL_II_WITH_${feature} on)
+            ELSE()
+              RUN_COMMAND(
+                "CONFIGURE_FEATURE_${feature}_ERROR_MESSAGE()"
+                )
+            ENDIF()
+        ENDIF()
+
+      ENDIF()
     ENDIF()
 
   ENDIF()
