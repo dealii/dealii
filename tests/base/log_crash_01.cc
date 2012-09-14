e25ce9841fae073dea2f92d493cfca33df63a121
Timo Heister <timo.heister@gmail.com>
2012-09-14 16:26:38 +0000

    add failing test for deallog
    git-svn-id: https://svn.dealii.org/trunk@26388 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/base/log_crash_01.cc
@@ -0,0 +1,38 @@
+//----------------------------  logtest.cc  ---------------------------
+//    $Id: logtest.cc 23710 2011-05-17 04:50:10Z bangerth $
+//    Version: $Name$ 
+//
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2007 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  logtest.cc  ---------------------------
+
+
+// document crash in deallog related to missing newline
+
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+
+
+int main()
+{
+  std::ofstream logfile("log_crash_01/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+
+  deallog.push("1");
+  {
+  deallog.threshold_double(1.e-10);
+  deallog.push("2");
+  deallog << "no newline here!";
+  deallog.pop();
+  }
+}
