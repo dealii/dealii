c1a9f47242f28f95598bf8a43175275a06391431
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-18 21:26:08 +0000

    Major refactoring of old files
    ./common/template-arguments.in --> ./contrib/config/template-arguments.in
    ./common/scripts --> ./scripts
    
    Removal of clearly obsolete files
    
    update of the build system to reflect those changes
    
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26485 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/cmake/macros/macro_expand_instantiations.cmake
+++ deal.II/contrib/cmake/macros/macro_expand_instantiations.cmake
@@ -1,3 +1,22 @@
+#
+# A macro for the inst.in file expansion:
+#
+# Usage:
+#
+# macro_expand_instantiations(target inst_in_files)
+#
+# Options:
+#
+# target
+#
+#    a target that will depend on the generation of all .inst files.
+#    (To ensure that all .inst files are generated prior to compiling.)
+#
+# inst_in_files
+#
+#    a list of inst.in files that will be expanded
+#
+
 MACRO(macro_expand_instantiations target inst_in_files)
 
   FOREACH (inst_in_file ${inst_in_files})
@@ -8,7 +27,7 @@ MACRO(macro_expand_instantiations target inst_in_files)
       DEPENDS expand_instantiations
               ${CMAKE_CURRENT_SOURCE_DIR}/${inst_in_file}
       COMMAND expand_instantiations
-      ARGS ${CMAKE_BINARY_DIR}/common/template-arguments
+      ARGS ${CMAKE_BINARY_DIR}/contrib/config/template-arguments
            < ${CMAKE_CURRENT_SOURCE_DIR}/${inst_in_file}
            > ${CMAKE_CURRENT_BINARY_DIR}/${inst_file}
       )
@@ -19,8 +38,16 @@ MACRO(macro_expand_instantiations target inst_in_files)
       )
   ENDFOREACH()
 
+  #
+  # Define a custom target that depends on the generation of all inst.in
+  # files.
+  #
   ADD_CUSTOM_TARGET(${target}.inst ALL DEPENDS ${inst_targets})
 
+  #
+  # Add a dependency to target so that target.inst is fully generated
+  # before target will be processed.
+  #
   ADD_DEPENDENCIES(${target} ${target}.inst)
 
 ENDMACRO()
