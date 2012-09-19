4b9574d22e3f20b6a3a1d0aca42fc3e81905c4e4
Timo Heister <timo.heister@gmail.com>
2012-09-19 22:35:38 +0000

    work on benchmark script
    git-svn-id: https://svn.dealii.org/trunk@26533 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/benchmarks/bench.sh
+++ tests/benchmarks/bench.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-TESTS=step-22 tablehandler
+TESTS="step-22 tablehandler"
 
 PREVREVISION="`svn info deal.II | grep Revision | sed s/Revision://`"
 HEADREVISION="`svn info http://www.dealii.org/svn/dealii | grep Revision | sed s/Revision://`"
-MAKECMD="make -j16"
+MAKECMD="make -j10"
 
 echo "previous $PREVREVISION"
 echo "HEAD: $HEADREVISION"
 
 while [ $PREVREVISION -lt $HEADREVISION ] ; do
 
-  NEXTREVISION=`expr $PREVREVISION "+" 1`
+  NEXTREVISION=`expr $PREVREVISION "+" 100`
   echo "Updating from $PREVREVISION to $NEXTREVISION"
-  pause
   cd deal.II
-  svn up deal.II -r$NEXTREVISION
-  if test -z "`svn diff -r$PREVREVISION:$NEXTREVISION . tests`" ; then
+  svn up -r$NEXTREVISION
+  if test -z "`svn diff -r$PREVREVISION:$NEXTREVISION .`" ; then
       echo "Skipping revision $NEXTREVISION" ;
       continue ;
   fi
@@ -26,7 +25,6 @@ while [ $PREVREVISION -lt $HEADREVISION ] ; do
   echo "compiling" 
   nice make optimized -j 10>/dev/null
   
-  $MAKECMD optimized
   cd ..
 
   for test in $TESTS ; do
@@ -34,13 +32,14 @@ while [ $PREVREVISION -lt $HEADREVISION ] ; do
       echo "** working on $test"
       make clean
       make run | grep "|" > temp.txt
-      ./your_code $NEXTREVISION >>datatable.$test
+      cat temp.txt >>datatable.$test
+      #./your_code $NEXTREVISION >>datatable.$test
       # collect info
       cd ..      
   
   done  
 
-fi
+done
 
 echo "DONE WITH REGRESSION TESTS ON `date`"
 
