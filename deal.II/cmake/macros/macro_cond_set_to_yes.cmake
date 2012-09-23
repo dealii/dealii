79890b510ccd8e1c5c6c2459dbaa47ee19e71efa
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-23 18:11:25 +0000

    On the quest to get a working Make.global_options
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26647 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/cmake/macros/macro_cond_set_to_yes.cmake
@@ -0,0 +1,14 @@
+
+#
+# IF(bool), set variable to "yes".
+#
+# Usage:
+#     COND_SET_TO_YES(bool variable)
+#
+
+MACRO(COND_SET_TO_YES bool variable)
+  IF(${bool})
+    SET(${variable} "yes")
+  ENDIF()
+ENDMACRO()
+
