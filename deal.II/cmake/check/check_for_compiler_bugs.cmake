36c1a84905237c552aabf8dbc0633b470f31898e
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-25 19:35:52 +0000

    Remove an obsolete test
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26738 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/check/check_for_compiler_bugs.cmake
+++ deal.II/cmake/check/check_for_compiler_bugs.cmake
@@ -10,47 +10,6 @@
 
 
 #
-# TODO: Obsolete. Remove and clean source.
-# Versions of GCC before 3.0 had a problem with the explicit
-# instantiation of member templates when the member was in fact
-# an operator. In that case, they needed the "template" keyword,
-# which is actually not allowed at this place. Test case is
-#
-# struct X
-# {
-#     template <typename T2>
-#     X operator = (T2 &) { return X(); };
-# };
-#
-# template X X::operator=<float> (float &);
-#
-# The compiler only groks this if the "operator=" is prepended
-# by "template". We detect this, and either set the
-# DEAL_II_MEMBER_OP_TEMPLATE_INST to "template" or nothing, so
-# that it gets expanded to the right string needed in this place.
-#
-CHECK_CXX_SOURCE_COMPILES(
-  "
-  struct X
-  {
-      template <typename T2>
-      X operator = (T2 &) { return X(); }
-  };
-
-  template X X::operator=<float> (float &);
-  int main(){return 0;}
-  "
-  DEAL_II_MEMBER_OP_TEMPLATE_INST_OK)
-
-IF(DEAL_II_MEMBER_OP_TEMPLATE_INST_OK)
-  SET(DEAL_II_MEMBER_OP_TEMPLATE_INST "")
-ELSE()
-  SET(DEAL_II_MEMBER_OP_TEMPLATE_INST "template")
-ENDIF()
-
-
-
-#
 # Some compiler versions, notably ICC, have trouble with the
 # following code in which we explicitly call a destructor.
 # This has to be worked around with a typedef. The problem is
