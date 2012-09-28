4b3badc680a85e7d31a2cbffbea3d878288ae19d
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-28 14:45:46 +0000

    Add the last bunch of system checks
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26838 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/checks/check_for_system_features.cmake
+++ deal.II/cmake/checks/check_for_system_features.cmake
@@ -2,6 +2,14 @@
 # Check for various system features:
 #
 
+INCLUDE(TestBigEndian)
+
+
+#
+# Check for various posix specific header files:
+#
+
+CHECK_INCLUDE_FILE("unistd.h" HAVE_UNISTD_H)
 
 CHECK_INCLUDE_FILE("sys/stat.h" HAVE_SYS_STAT_H)
 
@@ -11,4 +19,18 @@ CHECK_INCLUDE_FILE("sys/times.h" HAVE_SYS_TIMES_H)
 
 CHECK_INCLUDE_FILE("sys/types.h" HAVE_SYS_TYPES_H)
 
-CHECK_INCLUDE_FILE("unistd.h" HAVE_UNISTD_H)
+
+#
+# Check for various posix specific functions:
+#
+
+CHECK_FUNCTION_EXISTS(gethostname HAVE_GETHOSTNAME)
+
+CHECK_FUNCTION_EXISTS(getpid HAVE_GETPID)
+
+CHECK_FUNCTION_EXISTS(rand_r HAVE_RAND_R)
+
+CHECK_FUNCTION_EXISTS(times HAVE_TIMES)
+
+TEST_BIG_ENDIAN(DEAL_II_WORDS_BIGENDIAN)
+
