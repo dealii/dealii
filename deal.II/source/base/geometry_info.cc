fc92afd5201ea46a777be992bbf5cecb9878ed83
Matthias Maier <tamiko@kyomu.43-1.org>
2012-09-28 15:31:33 +0000

    Remove workarounds for DEAL_II_ARRAY_ARG_BUG from the source
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26845 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/source/base/geometry_info.cc
+++ deal.II/source/base/geometry_info.cc
@@ -1680,13 +1680,8 @@ template <int spacedim>
 void
 GeometryInfo<dim>::
 alternating_form_at_vertices
-#ifndef DEAL_II_ARRAY_ARG_BUG
     (const Point<spacedim> (&vertices)[vertices_per_cell],
      Tensor<spacedim-dim,spacedim> (&forms)[vertices_per_cell])
-#else
-    (const Point<spacedim> *vertices,
-     Tensor<spacedim-dim,spacedim> *forms)
-#endif
 {
                                    // for each of the vertices,
                                    // compute the alternating form
@@ -1744,52 +1739,32 @@ template
 void
 GeometryInfo<1>::
 alternating_form_at_vertices
-#ifndef DEAL_II_ARRAY_ARG_BUG
 (const Point<1> (&vertices)[vertices_per_cell],
  Tensor<1-1,1> (&forms)[vertices_per_cell])
-#else
-  (const Point<1> *vertices,
-   Tensor<1-1,1> *forms)
-#endif
   ;
 
 template
 void
 GeometryInfo<1>::
 alternating_form_at_vertices
-#ifndef DEAL_II_ARRAY_ARG_BUG
 (const Point<2> (&vertices)[vertices_per_cell],
  Tensor<2-1,2> (&forms)[vertices_per_cell])
-#else
-  (const Point<2> *vertices,
-   Tensor<2-1,2> *forms)
-#endif
   ;
 
 template
 void
 GeometryInfo<2>::
 alternating_form_at_vertices
-#ifndef DEAL_II_ARRAY_ARG_BUG
 (const Point<2> (&vertices)[vertices_per_cell],
  Tensor<2-2,2> (&forms)[vertices_per_cell])
-#else
-  (const Point<2> *vertices,
-   Tensor<2-2,2> *forms)
-#endif
   ;
 
 template
 void
 GeometryInfo<2>::
 alternating_form_at_vertices
-#ifndef DEAL_II_ARRAY_ARG_BUG
 (const Point<3> (&vertices)[vertices_per_cell],
  Tensor<3-2,3> (&forms)[vertices_per_cell])
-#else
-(const Point<3> *vertices,
- Tensor<3-2,3> *forms)
-#endif
   ;
 
 
@@ -1797,13 +1772,8 @@ template
 void
 GeometryInfo<3>::
 alternating_form_at_vertices
-#ifndef DEAL_II_ARRAY_ARG_BUG
 (const Point<3> (&vertices)[vertices_per_cell],
  Tensor<3-3,3> (&forms)[vertices_per_cell])
-#else
-(const Point<3> *vertices,
- Tensor<3-3,3> *forms)
-#endif
  ;
 
 
