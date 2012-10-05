2e3c79e2ea4f5c2d35ae84e8f204656950e27e25
Matthias Maier <tamiko@kyomu.43-1.org>
2012-10-05 18:33:15 +0000

    Add the old configuration behaviour as well
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@26975 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/config/setup_template-arguments.in.cmake
@@ -0,0 +1,20 @@
+#####
+##
+## Copyright (C) 2012 by the deal.II authors
+##
+## This file is part of the deal.II library.
+##
+## <TODO: Full License information>
+## This file is dual licensed under QPL 1.0 and LGPL 2.1 or any later
+## version of the LGPL license.
+##
+## Author: Matthias Maier <matthias.maier@iwr.uni-heidelberg.de>
+##
+#####
+
+
+CONFIGURE_FILE(
+  ${CMAKE_SOURCE_DIR}/config/template-arguments.in
+  ${CMAKE_BINARY_DIR}/config/template-arguments
+  )
+
