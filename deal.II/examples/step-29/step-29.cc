dealii/dealii

commit b81266a53300f38d395098508d612c2bda526bd1

Fix a misleading sentence in the documentation.
git-svn-id: https://svn.dealii.org/trunk@27140 0785d39b-7218-0410-832d-ea1e28bc413d

deal.II/examples/step-29/step-29.cc
@@ -474,7 +474,7 @@ namespace Step29
                                    // here vector-valued) representing
                                    // the data vector given to
                                    // DataOut::add_data_vector,
-                                   // evaluated at all quadrature points
+                                   // evaluated at all evaluation points
                                    // where we generate output, and some
                                    // tensor objects representing
                                    // derivatives (that we don't use
@@ -810,9 +810,9 @@ namespace Step29
     QGauss<dim>    quadrature_formula(2);
     QGauss<dim-1>  face_quadrature_formula(2);
 
-    const unsigned int n_q_points             = quadrature_formula.size(),
-                             n_face_q_points  = face_quadrature_formula.size(),
-                             dofs_per_cell    = fe.dofs_per_cell;
+    const unsigned int n_q_point        = quadrature_formula.size(),
+		       n_face_q_points  = face_quadrature_formula.size(),
+		       dofs_per_cell    = fe.dofs_per_cell;
 
                                      // The FEValues objects will
                                      // evaluate the shape functions for

commit 0998b9dd674ae849762a44e5df13a2251e7a8d8b

Fix a typo introduced with r27140
git-svn-id: https://svn.dealii.org/trunk@27196 0785d39b-7218-0410-832d-ea1e28bc413d

deal.II/examples/step-29/step-29.cc
@@ -810,9 +810,9 @@ namespace Step29
     QGauss<dim>    quadrature_formula(2);
     QGauss<dim-1>  face_quadrature_formula(2);
 
-    const unsigned int n_q_point        = quadrature_formula.size(),
-		       n_face_q_points  = face_quadrature_formula.size(),
-		       dofs_per_cell    = fe.dofs_per_cell;
+    const unsigned int n_q_points       = quadrature_formula.size(),
+                       n_face_q_points  = face_quadrature_formula.size(),
+                       dofs_per_cell    = fe.dofs_per_cell;
 
                                      // The FEValues objects will
                                      // evaluate the shape functions for
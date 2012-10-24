0998b9dd674ae849762a44e5df13a2251e7a8d8b
Matthias Maier <tamiko@kyomu.43-1.org>
2012-10-24 16:53:30 +0000

    Fix a typo introduced with r27140
    git-svn-id: https://svn.dealii.org/trunk@27196 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/examples/step-29/step-29.cc
+++ deal.II/examples/step-29/step-29.cc
@@ -810,9 +810,9 @@ namespace Step29
     QGauss<dim>    quadrature_formula(2);
     QGauss<dim-1>  face_quadrature_formula(2);
 
-    const unsigned int n_q_point        = quadrature_formula.size(),
-		       n_face_q_points  = face_quadrature_formula.size(),
-		       dofs_per_cell    = fe.dofs_per_cell;
+    const unsigned int n_q_points       = quadrature_formula.size(),
+                       n_face_q_points  = face_quadrature_formula.size(),
+                       dofs_per_cell    = fe.dofs_per_cell;
 
                                      // The FEValues objects will
                                      // evaluate the shape functions for
