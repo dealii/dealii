fd7f1f479e8e8dcfaa438a7cd5ea769fa28ea928
Timo Heister <timo.heister@gmail.com>
2012-10-26 20:01:22 +0000

    avoid printing NaN in TimerOutput.
    git-svn-id: https://svn.dealii.org/trunk@27218 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/source/base/timer.cc
+++ deal.II/source/base/timer.cc
@@ -499,7 +499,10 @@ TimerOutput::print_summary () const
           out_stream << i->second.total_cpu_time << "s |";
           out_stream << std::setw(10);
           out_stream << std::setprecision(2);
-          out_stream << i->second.total_cpu_time/total_cpu_time * 100 << "% |";
+          double value = i->second.total_cpu_time/total_cpu_time * 100;
+          if (!numbers::is_finite(value))
+            value = 0.0;
+          out_stream << value << "% |";
         }
       out_stream << std::endl
                  << "+---------------------------------+-----------+"
@@ -566,7 +569,10 @@ TimerOutput::print_summary () const
           out_stream << i->second.total_wall_time << "s |";
           out_stream << std::setw(10);
           out_stream << std::setprecision(2);
-          out_stream << i->second.total_wall_time/total_wall_time * 100 << "% |";
+          double value = i->second.total_wall_time/total_wall_time * 100;
+          if (!numbers::is_finite(value))
+            value = 0.0;
+          out_stream << value << "% |";
         }
       out_stream << std::endl
                  << "+---------------------------------+-----------+"
