0c2e3dd1e8d349d7ccd54df65de8a87cec7970ab
Matthias Maier <tamiko@kyomu.43-1.org>
2012-10-31 09:33:13 +0000

    Remove the "autopilot mode" for in-source builds
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@27250 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/cmake/README.cmake
@@ -0,0 +1,41 @@
+This folder contains the deal.II CMake build system
+
+It is structured as follows:
+
+./checks
+========
+
+Contains checks for platform features and compiler bugs and features
+
+./config
+=======
+
+Contains configuration templates for installing deal.IIConfig.cmake and for
+the c++ template expansion mechanism
+
+./configure
+===========
+
+Contains files configure_<feature>.cmake for configuration and setup of
+all features the deal.II library supports
+
+./macros
+========
+
+CMake script macros for several purposes
+
+./modules
+=========
+
+Contains Find<Library>.cmake modules for finding external libraries
+
+./scripts
+=========
+
+Contains script files needed for the build system
+
+
+./setup_*.cmake
+===============
+
+Setup files included by the top level CMakeLists.txt file
