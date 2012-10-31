b71cc1f726aea4dac0ecc62fe72843e4f35063c5
Matthias Maier <tamiko@kyomu.43-1.org>
2012-10-31 09:33:01 +0000

    Reorganize the top level directory structure
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@27249 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/macros/macro_expand_instantiations.cmake
+++ deal.II/cmake/macros/macro_expand_instantiations.cmake
@@ -40,7 +40,7 @@ MACRO(EXPAND_INSTANTIATIONS _target _inst_in_files)
       DEPENDS expand_instantiations
               ${CMAKE_CURRENT_SOURCE_DIR}/${_inst_in_file}
       COMMAND expand_instantiations
-      ARGS ${CMAKE_BINARY_DIR}/config/template-arguments
+      ARGS ${CMAKE_BINARY_DIR}/cmake/config/template-arguments
            < ${CMAKE_CURRENT_SOURCE_DIR}/${_inst_in_file}
            > ${CMAKE_CURRENT_BINARY_DIR}/${_inst_file}
       )
