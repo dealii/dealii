2a0cb71c579f26f7e521804bf97ba8b7dd6509d8
Matthias Maier <tamiko@kyomu.43-1.org>
2012-12-13 17:59:25 +0000

    Avoid face_to_equivalent_cell_index
    git-svn-id: https://svn.dealii.org/branches/branch_deprecated@27812 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/numerics/vector_tools.templates.h
+++ deal.II/include/deal.II/numerics/vector_tools.templates.h
@@ -1688,7 +1688,7 @@ namespace VectorTools
               std::vector<Point<dim-1> > unit_support_points (fe.dofs_per_face);
 
               for (unsigned int i=0; i<fe.dofs_per_face; ++i)
-                if (fe.is_primitive (fe.face_to_equivalent_cell_index(i)))
+                if (fe.is_primitive (fe.face_to_cell_index(0,i)))
                   if (component_mask[fe.face_system_to_component_index(i).first]
                       == true)
                     unit_support_points[i] = fe.unit_face_support_point(i);
