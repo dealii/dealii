459bbb746a60c48b141c43fed447387ffb983eee
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-02-19 04:26:24 +0000

    Add something to the documentation.
    git-svn-id: https://svn.dealii.org/trunk@28459 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/fe/fe_values.h
+++ deal.II/include/deal.II/fe/fe_values.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 //    $Id$
 //
-//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 by the deal.II authors
+//    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 by the deal.II authors
 //
 //    This file is subject to QPL and may not be  distributed
 //    without copyright and license information. Please refer
@@ -2648,7 +2648,7 @@ public:
 
   /**
    * Return a pointer to the vector of
-   * quadrature points.
+   * quadrature points in real space.
    */
   const std::vector<Point<spacedim> > &get_quadrature_points () const;
 
