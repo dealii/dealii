488bc34f24dea793978d4c93e91a92c2998763ab
Timo Heister <timo.heister@gmail.com>
2013-03-01 20:15:02 +0000

    add new gla directory for parallel unit tests for parallel LA, first test
    git-svn-id: https://svn.dealii.org/branches/branch_unify_linear_algebra@28704 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/gla/gla.h
@@ -0,0 +1,79 @@
+
+#include <deal.II/lac/abstract_linear_algebra.h>
+
+class LA_PETSc
+{
+  public:
+    class MPI
+    {
+      public:
+	typedef LinearAlgebraPETSc::MPI::Vector Vector;
+    };
+};
+
+class LA_Trilinos
+{
+  public:
+    class MPI
+    {
+      public:
+	typedef LinearAlgebraPETSc::MPI::Vector Vector;
+    };
+};
+
+class LA_Dummy
+{
+  public:
+    class MPI
+    {
+      public:
+	class Vector
+	{
+	  public:
+	    
+	    Vector(const IndexSet local, const MPI_Comm &comm=MPI_COMM_WORLD)
+	      {}
+	    
+	    Vector(const IndexSet &local, const IndexSet &ghost, const MPI_Comm &comm=MPI_COMM_WORLD) 
+	      {}
+	    
+	    Vector(const MPI_Comm &comm, const IndexSet local) 
+	      {}
+	    
+	    Vector(const MPI_Comm &commm, const IndexSet &local, const IndexSet &ghost)
+	      {}
+	    
+	    void compress(VectorOperation::values op)
+	      {}
+	    
+	    bool has_ghost_elements()
+	      {
+		return false;}
+
+	    unsigned int size()
+	      {return 0;}
+	    
+	    
+	    const Vector & operator*=(const double factor)
+	      {
+		return *this;
+	      }
+	    
+	    double & operator()(unsigned int)
+	      {
+		static double d;
+		return d;
+	      }
+	    
+	    const double & operator()(unsigned int) const
+	      {
+		static double d;
+		return d;
+	      }
+	    
+	    
+	};
+	
+	
+    };
+};
---
+++ tests/gla/vec_01.cc
@@ -0,0 +1,122 @@
+//---------------------------------------------------------------------------
+//    $Id: ghost_01.cc 28440 2013-02-17 14:35:08Z heister $
+//    Version: $Name$ 
+//
+//    Copyright (C) 2004, 2005, 2010 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+
+
+#include "../tests.h"
+#include <deal.II/lac/abstract_linear_algebra.h>
+#include <deal.II/base/index_set.h>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+#include "gla.h"
+
+template <class LA> 
+void test ()
+{
+  unsigned int myid = Utilities::MPI::this_mpi_process (MPI_COMM_WORLD);
+  unsigned int numproc = Utilities::MPI::n_mpi_processes (MPI_COMM_WORLD);
+  
+  if (myid==0)
+    deallog << "numproc=" << numproc << std::endl;
+
+				   // each processor owns 2 indices and all
+				   // are ghosting Element 1 (the second)
+
+  IndexSet local_active(numproc*2);
+  local_active.add_range(myid*2,myid*2+2);
+  IndexSet local_relevant(numproc*2);
+  local_relevant.add_range(1,2);
+
+  typename LA::MPI::Vector vb(MPI_COMM_WORLD, local_active);
+  typename LA::MPI::Vector v(MPI_COMM_WORLD, local_active, local_relevant);
+
+				   // set local values
+  vb(myid*2)=myid*2.0;
+  vb(myid*2+1)=myid*2.0+1.0;
+
+  vb.compress(VectorOperation::insert);
+  vb*=2.0;
+  v=vb;
+
+  Assert(vb.size() == numproc*2, ExcInternalError());
+  Assert(v.size() == numproc*2, ExcInternalError());
+
+  Assert(!vb.has_ghost_elements(), ExcInternalError());
+  Assert(v.has_ghost_elements(), ExcInternalError());
+  
+				   // check local values
+  if (Utilities::MPI::this_mpi_process (MPI_COMM_WORLD) == 0)
+    {
+      deallog << myid*2 << ":" << v(myid*2) << std::endl;
+      deallog << myid*2+1 << ":" << v(myid*2+1) << std::endl;
+    }
+  
+  Assert(v(myid*2) == myid*4.0, ExcInternalError());
+  Assert(v(myid*2+1) == myid*4.0+2.0, ExcInternalError());
+  
+
+				   // check ghost values
+  if (Utilities::MPI::this_mpi_process (MPI_COMM_WORLD) == 0)
+    deallog << "ghost: " << v(1) << std::endl;
+  Assert(v(1) == 2.0, ExcInternalError());
+
+				   // done
+  if (myid==0)
+    deallog << "OK" << std::endl;
+}
+
+
+
+int main (int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv);
+  unsigned int myid = Utilities::MPI::this_mpi_process (MPI_COMM_WORLD);
+
+  deallog.push(Utilities::int_to_string(myid));
+
+  if (myid == 0)
+    {
+      std::ofstream logfile(output_file_for_mpi("ghost_01").c_str());
+      deallog.attach(logfile);
+      deallog << std::setprecision(4);
+      deallog.depth_console(0);
+      deallog.threshold_double(1.e-10);
+
+      {	
+	deallog.push("PETSc");
+	test<LA_PETSc>();
+	deallog.pop();	
+	deallog.push("Trilinos");
+	test<LA_Trilinos>();
+	deallog.pop();	
+      }
+      
+    }
+  else
+      {	
+	deallog.push("PETSc");
+	test<LA_PETSc>();
+	deallog.pop();	
+	deallog.push("Trilinos");
+	test<LA_Trilinos>();
+	deallog.pop();	
+      }
+
+  if (myid==9999)
+    test<LA_Dummy>();
+  
+
+}
