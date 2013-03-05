196ec8e02ef780ceca9ef8dafba14751cfcb50e1
Matthias Maier <tamiko@kyomu.43-1.org>
2013-03-05 10:33:12 +0000

    Bugfixes
    git-svn-id: https://svn.dealii.org/branches/branch_cmake@28735 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/configure/configure_hdf5.cmake
+++ deal.II/cmake/configure/configure_hdf5.cmake
@@ -16,4 +16,21 @@
 # Configuration for the hdf5 library:
 #
 
+MACRO(FEATURE_HDF5_FIND_EXTERNAL var)
+  FIND_PACKAGE(HDF5)
+
+  IF(HDF5_FOUND)
+    IF( (HDF5_WITH_MPI AND DEAL_II_WITH_MPI)
+         OR
+         (NOT HDF5_WITH_MPI AND NOT DEAL_II_WITH_MPI))
+      SET(${var} TRUE)
+    ELSE()
+      MESSAGE(WARNING "\n"
+        "Could not find a sufficient hdf5 installation: "
+        "hdf5 has to be configured with the same MPI configuration as deal.II.\n\n"
+        )
+    ENDIF()
+  ENDIF()
+ENDMACRO()
+
 CONFIGURE_FEATURE(HDF5)
