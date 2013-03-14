aa35e315c25fa51521afbbd73b4d8aa6d0ba155b
Kainan Wang <kennan.wong@gmail.com>
2013-03-14 00:24:11 +0000

    changes in mpi.h to add an mpi type for unsigned long long int
    git-svn-id: https://svn.dealii.org/branches/branch_bigger_global_dof_indices_4@28895 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/include/deal.II/base/mpi.h
+++ deal.II/include/deal.II/base/mpi.h
@@ -363,7 +363,13 @@ namespace Utilities
       {
         return MPI_UNSIGNED_LONG;
       }
+      
 
+      inline MPI_Datatype mpi_type_id (const unsigned long long int *)
+      {
+	return MPI_UNSIGNED_LONG;
+      }
+      
 
       inline MPI_Datatype mpi_type_id (const unsigned long long int *)
       {
