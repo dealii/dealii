a9d7617a5b323bd35cc78800baec07be8948f056
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-03-15 18:31:03 +0000

    Allow default construction of FEValuesExtractor objects, but make sure they are then invalid when used.
    git-svn-id: https://svn.dealii.org/trunk@28915 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/deal.II/fe_values_view_invalid_03.cc
@@ -0,0 +1,86 @@
+//----------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2007, 2008, 2013 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------------------------------------------------
+
+
+// make sure FEValuesExtractors::SymmetricTensor can be default-constructed
+// but that it produces an invalid, unusable object
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_values.h>
+
+#include <fstream>
+
+
+
+template<int dim>
+void test (const Triangulation<dim>& tr,
+	   const FiniteElement<dim>& fe)
+{
+  DoFHandler<dim> dof(tr);
+  dof.distribute_dofs(fe);
+
+  const QGauss<dim> quadrature(2);
+  FEValues<dim> fe_values (fe, quadrature,
+			   update_values);
+  fe_values.reinit (dof.begin_active());
+
+  FEValuesExtractors::SymmetricTensor<2> extr; // invalid object
+  try
+    {
+      fe_values[extr];             // invalid access
+    }
+  catch (const std::exception &exc)
+    {
+      goto ok;
+    }
+
+  Assert (false, ExcMessage ("No exception!?"));
+
+  ok:
+  ;
+}
+
+
+
+template<int dim>
+void test()
+{
+  Triangulation<dim> tr;
+  GridGenerator::hyper_cube(tr);
+
+  FESystem<dim> fe (FE_Q<dim>(1), dim*(dim+1)/2);
+  test(tr, fe);
+}
+
+
+int main()
+{
+  std::ofstream logfile ("fe_values_view_invalid_03/output");
+  deallog << std::setprecision (2);
+
+  deallog.attach(logfile);
+  deallog.depth_console (0);
+  deallog.threshold_double(1.e-7);
+
+  deal_II_exceptions::disable_abort_on_exception();
+
+  test<1>();
+  test<2>();
+  test<3>();
+}
