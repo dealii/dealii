3f6d759199785afd1a44eb396d90223db33a74bc
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-03-27 12:22:58 +0000

    Add a test that currently crashes.
    git-svn-id: https://svn.dealii.org/branches/branch_bigger_global_dof_indices_4@29063 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/mpi/trilinos_vector_ghosts_01.cc
@@ -0,0 +1,100 @@
+//---------------------------------------------------------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2009, 2010, 2011, 2013 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//---------------------------------------------------------------------------
+
+
+// Test VectorTools::trilinos_vector_ghosts_01 for parallel computations. we
+// interpolate a linear function onto the grid with a symmetric mesh. the mean
+// value of the interpolation must be the mean of the linear function
+
+#include "../tests.h"
+#include "coarse_grid_common.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/tensor.h>
+#include <deal.II/lac/trilinos_vector.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/dofs/dof_accessor.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <fstream>
+
+
+template <int dim>
+class LinearFunction : public Function<dim>
+{
+  public:
+    double value (const Point<dim> &p,
+		  const unsigned int) const
+      {
+	return p[0] + 2;
+      }
+};
+
+
+void test()
+{
+  parallel::distributed::Triangulation<2> tr(MPI_COMM_WORLD);
+
+  GridGenerator::hyper_cube(tr);
+  tr.refine_global (2);
+
+  const FE_Q<2> fe(2);
+  DoFHandler<2> dofh(tr);
+  dofh.distribute_dofs (fe);
+
+  TrilinosWrappers::MPI::Vector interpolated(dofh.locally_owned_dofs(),
+					     MPI_COMM_WORLD);
+  Assert (interpolated.has_ghost_elements() == false,
+	  ExcInternalError());
+}
+
+
+int main(int argc, char *argv[])
+{
+#ifdef DEAL_II_WITH_MPI
+  MPI_Init (&argc,&argv);
+#else
+  (void)argc;
+  (void)argv;
+#endif
+
+  unsigned int myid = Utilities::MPI::this_mpi_process (MPI_COMM_WORLD);
+
+
+  deallog.push(Utilities::int_to_string(myid));
+
+  if (myid == 0)
+    {
+      std::ofstream logfile(output_file_for_mpi("trilinos_vector_ghosts_01").c_str());
+      deallog.attach(logfile);
+      deallog.depth_console(0);
+      deallog.threshold_double(1.e-10);
+    }
+
+  test();
+
+#ifdef DEAL_II_WITH_MPI
+  MPI_Finalize();
+#endif
+}
