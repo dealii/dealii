20359774102d4d9e47b616427b3f3a1c6da9952e
Matthias Maier <tamiko@kyomu.43-1.org>
2013-03-27 17:11:26 +0000

    CMake: Bugfix: Implement ENABLE_IF_LINKS with CHECK_CXX_COMPILER_FLAG
    git-svn-id: https://svn.dealii.org/trunk@29080 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/macros/macro_enable_if_links.cmake
+++ deal.II/cmake/macros/macro_enable_if_links.cmake
@@ -23,18 +23,15 @@
 
 MACRO(ENABLE_IF_LINKS _variable _flag)
   STRING(STRIP "${_flag}" _flag_stripped)
-  STRING(REGEX REPLACE "^-" "" _flag_name "${_flag_stripped}")
-  STRING(REPLACE "," "" _flag_name "${_flag_name}")
-  STRING(REPLACE "-" "_" _flag_name "${_flag_name}")
-
   IF(NOT "${_flag_stripped}" STREQUAL "")
+    STRING(REGEX REPLACE "^-" "" _flag_name "${_flag_stripped}")
+    STRING(REPLACE "," "" _flag_name "${_flag_name}")
+    STRING(REPLACE "--" "__" _flag_name "${_flag_name}")
     ADD_FLAGS(CMAKE_REQUIRED_FLAGS "${_flag_stripped}")
-    CHECK_CXX_SOURCE_COMPILES(
-    "
-    int main() { return 0; }
-    "
-    DEAL_II_HAVE_FLAG_${_flag_name}
-    )
+    CHECK_CXX_COMPILER_FLAG(
+      ""
+      DEAL_II_HAVE_FLAG_${_flag_name}
+      )
     STRIP_FLAG(CMAKE_REQUIRED_FLAGS "${_flag_stripped}")
     IF(DEAL_II_HAVE_FLAG_${_flag_name})
       SET(${_variable} "${${_variable}} ${_flag_stripped}")
@@ -42,4 +39,3 @@ MACRO(ENABLE_IF_LINKS _variable _flag)
     ENDIF()
   ENDIF()
 ENDMACRO()
-
--- deal.II/cmake/macros/macro_enable_if_supported.cmake
+++ deal.II/cmake/macros/macro_enable_if_supported.cmake
@@ -25,7 +25,7 @@ MACRO(ENABLE_IF_SUPPORTED _variable _flag)
   IF(NOT "${_flag_stripped}" STREQUAL "")
     STRING(REGEX REPLACE "^-" "" _flag_name "${_flag_stripped}")
     STRING(REPLACE "," "" _flag_name "${_flag_name}")
-    STRING(REPLACE "-" "_" _flag_name "${_flag_name}")
+    STRING(REPLACE "--" "__" _flag_name "${_flag_name}")
     CHECK_CXX_COMPILER_FLAG(
       "${_flag_stripped}"
       DEAL_II_HAVE_FLAG_${_flag_name}
