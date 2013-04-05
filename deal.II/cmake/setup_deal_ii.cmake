d53ae32f14d744140cdbc2856214add01593ff5b
Matthias Maier <tamiko@kyomu.43-1.org>
2013-04-05 06:45:39 +0000

    CMake: Bugfix. Don't rename header destination in an in-source build
    git-svn-id: https://svn.dealii.org/trunk@29195 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/setup_deal_ii.cmake
+++ deal.II/cmake/setup_deal_ii.cmake
@@ -105,11 +105,12 @@ IF(DEAL_II_COMPONENT_COMPAT_FILES)
   SET_IF_EMPTY(DEAL_II_DOCHTML_RELDIR "doc")
   SET_IF_EMPTY(DEAL_II_EXAMPLES_RELDIR "examples")
   SET_IF_EMPTY(DEAL_II_EXECUTABLE_RELDIR "bin")
-  IF("${CMAKE_INSTALL_PREFIX}" STREQUAL "${CMAKE_BINARY_DIR}")
+  IF( "${CMAKE_INSTALL_PREFIX}" STREQUAL "${CMAKE_BINARY_DIR}" AND
+      (NOT "${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}") )
     #
-    # Ensure that always BINARY_DIR/include != INSTALL_PREFIX/include.
-    # Otherwise stale headers might get included resulting in a failing
-    # build.
+    # Ensure that in case of an out of source build BINARY_DIR/include !=
+    # INSTALL_PREFIX/include us always true. Otherwise stale headers might
+    # get included resulting in a failing build.
     #
     SET_IF_EMPTY(DEAL_II_INCLUDE_RELDIR "include/install")
   ELSE()
