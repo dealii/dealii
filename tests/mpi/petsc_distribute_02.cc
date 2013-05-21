fa24b42561dc182a56d1ced475dd8b10b6ccebb7
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-05-21 14:39:21 +0000

    Rename test to reflect the function it tests.
    git-svn-id: https://svn.dealii.org/trunk@29541 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/mpi/petsc_distribute_02.cc
+++ tests/mpi/petsc_distribute_02.cc
@@ -160,7 +160,7 @@ int main(int argc, char *argv[])
 
   if (myid == 0)
     {
-      std::ofstream logfile(output_file_for_mpi("petsc_condense_02").c_str());
+      std::ofstream logfile(output_file_for_mpi("petsc_distribute_02").c_str());
       deallog.attach(logfile);
       deallog.depth_console(0);
       deallog.threshold_double(1.e-10);
