003068e4af923786cc8e9efceac2eac8cb0ab64a
Guido Kanschat <dr.guido.kanschat@gmail.com>
2013-06-24 11:44:08 +0000

    test ordering and SVG subdomain output on lower levels
    git-svn-id: https://svn.dealii.org/trunk@29870 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/grid/grid_out_svg_02.cc
@@ -0,0 +1,70 @@
+#include "../tests.h"
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria_boundary_lib.h>
+#include <deal.II/grid/grid_out.h>
+
+#include <fstream>
+#include <cmath>
+
+using namespace dealii;
+
+Triangulation<2,2> create_grid()
+{
+	Triangulation<2,2> triangulation;
+
+	double inner_radius = .5;
+	double outer_radius = 1.;
+
+	Point<2> center(0., 0.);
+
+	GridGenerator::hyper_cube_with_cylindrical_hole(triangulation, inner_radius, outer_radius);
+	triangulation.refine_global(1);
+
+	Triangulation<2>::active_cell_iterator
+
+	cell = triangulation.begin_active(),
+	endc = triangulation.end();
+
+	for(; cell!=endc; ++cell)
+	{
+		for (unsigned int v=0; v < GeometryInfo<2>::vertices_per_cell; ++v)
+		{
+			const double distance_from_center = center.distance(cell->vertex(v));
+
+			if (std::fabs(distance_from_center - inner_radius) < .25)
+			{
+				cell->set_refine_flag();
+				break;
+			}
+		}
+	}
+
+	triangulation.execute_coarsening_and_refinement();
+
+	return triangulation;
+}
+
+int main()
+{
+	initlog(__FILE__);
+
+	GridOut grid_out;
+	GridOutFlags::Svg svg_flags;
+
+	svg_flags.coloring = GridOutFlags::Svg::level_number;
+	svg_flags.label_material_id = true;
+	svg_flags.label_subdomain_id = true;
+	svg_flags.label_level_subdomain_id = true;
+	svg_flags.background = GridOutFlags::Svg::transparent;
+	svg_flags.polar_angle = 45;
+	svg_flags.convert_level_number_to_height = true;
+	
+	grid_out.set_flags(svg_flags);
+	grid_out.write_svg(create_grid(), deallog.get_file_stream());
+
+	return 0;
+}
