60c468a4a1442b5d9d6acbb172785090802d3391
Matthias Maier <tamiko@kyomu.43-1.org>
2013-07-18 16:55:40 +0000

    Update License Headers in Source files
    git-svn-id: https://svn.dealii.org/trunk@30037 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/source/lac/sparse_decomposition.cc
+++ deal.II/source/lac/sparse_decomposition.cc
@@ -1,13 +1,19 @@
-//---------------------------------------------------------------------------
-//    Copyright (C) 2002, 2003, 2005, 2006, 2012 by the deal.II authors
-//    by the deal.II authors and Stephen "Cheffo" Kolaroff
+// ---------------------------------------------------------------------
+// $Id$
 //
-//    This file is subject to QPL and may not be  distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// Copyright (C) 2002 - 2013 by the deal.II authors
 //
-//---------------------------------------------------------------------------
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
 
 #include <deal.II/lac/sparse_decomposition.templates.h>
 
--- deal.II/source/multigrid/mg_transfer_block.cc
+++ deal.II/source/multigrid/mg_transfer_block.cc
@@ -1,15 +1,19 @@
-//---------------------------------------------------------------------------
-//    $Id$
-//    Version: $Name$
+// ---------------------------------------------------------------------
+// $Id$
 //
-//    Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2010, 2012 by the deal.II authors
+// Copyright (C) 2003 - 2013 by the deal.II authors
 //
-//    This file is subject to QPL and may not be  distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// This file is part of the deal.II library.
 //
-//---------------------------------------------------------------------------
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
 
 #include <deal.II/base/logstream.h>
 
