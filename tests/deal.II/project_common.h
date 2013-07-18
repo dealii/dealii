5ad54d35481509fdf48a87b2e0f90a8c56e1dc45
Matthias Maier <tamiko@kyomu.43-1.org>
2013-07-18 19:42:36 +0000

    Don't forget some header files as well...
    git-svn-id: https://svn.dealii.org/trunk@30048 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/project_common.h
+++ tests/deal.II/project_common.h
@@ -1,15 +1,18 @@
-//----------------------------  project_common.h  ---------------------------
-//    $Id$
-//    Version: $Name$
+// ---------------------------------------------------------------------
+// $Id$
 //
-//    Copyright (C) 2006, 2010 by the deal.II authors
+// Copyright (C) 2006 - 2013 by the deal.II authors
 //
-//    This file is subject to QPL and may not be  distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// This file is part of the deal.II library.
 //
-//----------------------------  project_common.h  ---------------------------
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
 
 
 // common framework to check whether an element of polynomial order p can
