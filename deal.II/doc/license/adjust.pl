0e76edf786aab77049a9a8d21a2e71b7d4379230
Guido Kanschat <dr.guido.kanschat@gmail.com>
2013-07-18 19:39:29 +0000

    adjusting script
    git-svn-id: https://svn.dealii.org/trunk@30047 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/doc/license/adjust.pl
@@ -0,0 +1,29 @@
+#!/bin/perl
+
+my $header=1;
+
+print << 'EOT'
+//----------------------------------------------------------------------
+// $Id$
+//
+// Copyright (C) 1998 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+//----------------------------------------------------------------------
+EOT
+    ;
+
+while(<>)
+{
+    $header=0 unless m/\/\//;
+    next if $header;
+    print;
+}
