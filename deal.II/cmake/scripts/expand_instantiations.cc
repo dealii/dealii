85d9c3cf9ca24293c949e6a11195f39da66a4280
Matthias Maier <tamiko@kyomu.43-1.org>
2013-07-18 19:42:40 +0000

    Process a whole bunch of remaining copyright notices
    git-svn-id: https://svn.dealii.org/trunk@30049 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/scripts/expand_instantiations.cc
+++ deal.II/cmake/scripts/expand_instantiations.cc
@@ -1,15 +1,19 @@
-//----------------------------  expand_instantiations.cc  -------------------
-//    $Id$
-//    Version: $Name$
+// ---------------------------------------------------------------------
+// $Id$
 //
-//    Copyright (C) 2007, 2008, 2009, 2010, 2011 by the deal.II authors
+// Copyright (C) 2007 - 2013 by the deal.II authors
 //
-//    This file is subject to QPL and may not be  distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// This file is part of the deal.II library.
 //
-//----------------------------  expand_instantiations.cc  -------------------
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
 
 // This is the program that we use to generate explicit instantiations for a
 // variety of template arguments. It takes two kinds of input files. The first
--- tests/lib/test_grids.h
+++ tests/lib/test_grids.h
@@ -13,18 +13,6 @@
 // the top level of the deal.II distribution.
 //
 // ---------------------------------------------------------------------
-//----------------------------------------------------------------------
-//    $Id$
-//    Version: $Name$
-//
-//    Copyright (C) 2006, 2010 by the deal.II authors
-//
-//    This file is subject to QPL and may not be  distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
-//
-//----------------------------------------------------------------------
 
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_iterator.h>
