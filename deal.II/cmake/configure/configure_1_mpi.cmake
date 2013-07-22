86fd4f3dd4a37d49cb4d15e5be4973fb77f3f63b
Matthias Maier <tamiko@kyomu.43-1.org>
2013-07-22 19:35:24 +0000

    CMake: A small workaround
    git-svn-id: https://svn.dealii.org/trunk@30103 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/cmake/configure/configure_1_mpi.cmake
+++ deal.II/cmake/configure/configure_1_mpi.cmake
@@ -20,14 +20,6 @@
 
 MACRO(FEATURE_MPI_FIND_EXTERNAL var)
   #
-  # Enable C and Fortran support so that MPI_Fortran_LIBRARIES is set up.
-  #
-  # Disabled wrt to http://public.kitware.com/Bug/view.php?id=9220
-  #IF(NOT CMAKE_Fortran_COMPILER_WORKS)
-  #  ENABLE_LANGUAGE(Fortran OPTIONAL)
-  #ENDIF()
-
-  #
   # Obey a manual user override: If MPI_CXX_FOUND is set to true in the
   # cache, we skip the FIND_PACKAGE calls:
   #
