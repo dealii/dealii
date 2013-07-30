8945b9edecef618b067c969b69cfe5a093407f36
Timo Heister <timo.heister@gmail.com>
2013-07-30 02:35:34 +0000

    add test
    git-svn-id: https://svn.dealii.org/trunk@30180 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/fe/fe_q_01.cc
@@ -0,0 +1,44 @@
+// ---------------------------------------------------------------------
+// $Id: fe_data_test.cc 30045 2013-07-18 19:18:00Z maier $
+//
+// Copyright (C) 1998 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// document a bug that would fill all memory if you try to create a FE_Q<dim>(0).
+
+#include "../tests.h"
+#include <iostream>
+#include <fstream>
+
+#include <deal.II/base/logstream.h>
+#include <deal.II/fe/fe_q.h>
+
+int main()
+{
+  std::ofstream logfile("fe_q_01/output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deal_II_exceptions::disable_abort_on_exception();
+  try
+    {
+      FE_Q<3> fe(0);
+    }
+  catch (ExceptionBase &e)
+    {
+      deallog << e.get_exc_name() << std::endl;
+    }  
+}
+
+
+
