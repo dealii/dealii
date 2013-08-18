1975c04c209fee635c1c15e593f79612bb796fbf
Matthias Maier <tamiko@kyomu.43-1.org>
2013-08-18 21:08:11 +0000

    First commit (tm)
    git-svn-id: https://svn.dealii.org/branches/branch_port_the_testsuite@30337 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/cmake/macros/macro_deal_ii_add_test.cmake
@@ -0,0 +1,52 @@
+## ---------------------------------------------------------------------
+## $Id$
+##
+## Copyright (C) 2013 by the deal.II authors
+##
+## This file is part of the deal.II library.
+##
+## The deal.II library is free software; you can use it, redistribute
+## it, and/or modify it under the terms of the GNU Lesser General
+## Public License as published by the Free Software Foundation; either
+## version 2.1 of the License, or (at your option) any later version.
+## The full text of the license can be found in the file LICENSE at
+## the top level of the deal.II distribution.
+##
+## ---------------------------------------------------------------------
+
+#
+# Usage:
+#     DEAL_II_ADD_TEST(category test_name [configurations])
+#
+
+MACRO(DEAL_II_ADD_TEST _category _test_target)
+
+  FOREACH(_build ${DEAL_II_BUILD_TYPES})
+
+    ITEM_MATCHES(_match "${_build}" ${ARGN})
+    IF(_match OR "${ARGN}" STREQUAL "")
+
+      STRING(TOLOWER ${_build} _build_lowercase)
+      ADD_EXECUTABLE(${_test_target}.${_build_lowercase}
+        ${_test_target}.cc
+        )
+      SET_TARGET_PROPERTIES(${_test_target}.${_build_lowercase} PROPERTIES
+        LINK_FLAGS "${DEAL_II_LINKER_FLAGS} ${DEAL_II_LINKER_FLAGS_${_build}}"
+        COMPILE_DEFINITIONS "${DEAL_II_DEFINITIONS};${DEAL_II_DEFINITIONS_${_build}}"
+        COMPILE_FLAGS "${DEAL_II_CXX_FLAGS_${_build}}"
+        LINKER_LANGUAGE "CXX"
+        )
+      SET_PROPERTY(TARGET ${_target} APPEND PROPERTY
+        INCLUDE_DIRECTORIES
+          "${CMAKE_BINARY_DIR}/include"
+          "${CMAKE_SOURCE_DIR}/include"
+        )
+
+      TARGET_LINK_LIBRARIES(${_test_target}.${_build_lowercase}
+        ${DEAL_II_BASE_NAME}${DEAL_II_${_build}_SUFFIX}
+        )
+    ENDIF()
+
+  ENDFOREACH()
+
+ENDMACRO()
