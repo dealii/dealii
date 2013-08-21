10d548a4622a719c6d95b16e9aceef4ffebfd364
Matthias Maier <tamiko@kyomu.43-1.org>
2013-08-21 23:58:09 +0000

    Port tests/metis
    git-svn-id: https://svn.dealii.org/branches/branch_port_the_testsuite@30398 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/metis/metis_02.cc
+++ tests/metis/metis_02.cc
@@ -89,7 +89,7 @@ void test ()
 
 int main ()
 {
-  std::ofstream logfile("metis_02/output");
+  std::ofstream logfile("output");
   deallog.attach(logfile);
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
