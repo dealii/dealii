879bdf780f6696b8473ca36b4f2a9bbc918f7df7
Bruno Turcksin <bruno.turcksin@gmail.com>
2013-09-19 22:17:34 +0000

    Fix a test.
    git-svn-id: https://svn.dealii.org/branches/branch_port_the_testsuite@30857 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/deal.II/grid_in_msh_03.cc
+++ tests/deal.II/grid_in_msh_03.cc
@@ -46,7 +46,9 @@ void check_file (const std::string name,
   GridIn<dim> gi;
   gi.attach_triangulation (tria);
   gi.read(name, format);
-  deallog << name
+  std::string source_dir(SOURCE_DIR "/");
+  std::string relative_name(name.begin()+source_dir.size(),name.end());
+  deallog << relative_name
           << '\t' << tria.n_vertices()
           << '\t' << tria.n_cells()
           << std::endl;
