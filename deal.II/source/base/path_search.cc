88b53da870a9e495fe767f9feebb645db083bf03
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-10-08 12:41:27 +0000

    Run second part of test only if it's different from the first. Adjust test output.
    git-svn-id: https://svn.dealii.org/trunk@31169 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/source/base/path_search.cc
+++ deal.II/source/base/path_search.cc
@@ -146,22 +146,24 @@ PathSearch::find (const std::string &filename,
 	  }
       }
 
-      // try again with the suffix appended
-      {
-	real_name = *path + filename + suffix;
-	if (debug > 1)
-	  deallog << "PathSearch[" << cls << "] trying "
-		  << real_name << std::endl;
-	FILE *fp = fopen(real_name.c_str(), open_mode);
-	if (fp != 0)
-	  {
-	    if (debug > 0)
-	      deallog << "PathSearch[" << cls << "] opened "
-		      << real_name << std::endl;
-	    fclose(fp);
-	    return real_name;
-	  }
-      }
+      // try again with the suffix appended, unless there is
+      // no suffix
+      if (suffix != "")
+        {
+          real_name = *path + filename + suffix;
+          if (debug > 1)
+            deallog << "PathSearch[" << cls << "] trying "
+            << real_name << std::endl;
+          FILE *fp = fopen(real_name.c_str(), open_mode);
+          if (fp != 0)
+            {
+              if (debug > 0)
+                deallog << "PathSearch[" << cls << "] opened "
+                << real_name << std::endl;
+              fclose(fp);
+              return real_name;
+            }
+        }
     }
   AssertThrow(false, ExcFileNotFound(filename, cls));
   return std::string("");
