49e43e3c548b460dc1b7ad08e53e03f0b51b0ac0
Timo Heister <timo.heister@gmail.com>
2013-11-05 15:47:55 +0000

    clean up quicktest script
    git-svn-id: https://svn.dealii.org/trunk@31545 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/tests/quick_tests/mpi.cc
+++ deal.II/tests/quick_tests/mpi.cc
@@ -31,6 +31,8 @@ int main(int argc, char *argv[] )
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
+  std::cout << " Hi from " << myrank << "/" << nproc << std::endl;
+  
   if (nproc != 2)
     {
       std::cerr << "ERROR: process does not see nproc=2!" << std::endl;
