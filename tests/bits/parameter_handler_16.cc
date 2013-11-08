8f343817421b676f61d6feef87bf7a0bbe68de36
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-11-08 04:55:17 +0000

    Add another test.
    git-svn-id: https://svn.dealii.org/trunk@31581 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/parameter_handler_16.cc
@@ -0,0 +1,59 @@
+// ---------------------------------------------------------------------
+// $Id: parameter_handler_14.cc 31349 2013-10-20 19:07:06Z maier $
+//
+// Copyright (C) 2002 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// ParameterHandler could not deal with missing endline at end of file
+// or can it?
+// http://code.google.com/p/dealii/issues/detail?id=126
+
+// this is a variant of parameter_handler_15 but in fact reads data
+// from a file
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/parameter_handler.h>
+#include <fstream>
+
+void test ()
+{
+  ParameterHandler foo;
+  foo.enter_subsection("bar");
+  foo.declare_entry("val", "1.0", dealii::Patterns::Double(), "");
+  foo.leave_subsection();
+  foo.declare_entry("val2", "2.0", dealii::Patterns::Double(), "");
+
+  foo.read_input(SOURCE_DIR "/parameter_handler_16.prm");
+
+
+
+  foo.enter_subsection("bar");
+  deallog << foo.get ("val") << std::endl;
+  foo.leave_subsection();
+  deallog << foo.get ("val2") << std::endl;
+}
+
+int main ()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test ();
+
+  return 0;
+}
