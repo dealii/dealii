ab88b971bd20952db9106e75663a55ec1f895a8a
Wolfgang Bangerth <bangerth@math.tamu.edu>
2013-11-08 05:06:55 +0000

    Add a few tests.
    git-svn-id: https://svn.dealii.org/trunk@31583 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/bits/parameter_handler_1_include_fail.cc
@@ -0,0 +1,54 @@
+// ---------------------------------------------------------------------
+// $Id$
+//
+// Copyright (C) 2002 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// check that we can do include statements. the current test verifies what
+// happens if such an include statement fails
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/parameter_handler.h>
+#include <fstream>
+
+void check (const char *p)
+{
+  ParameterHandler prm;
+  prm.declare_entry ("test_1", "-1,0",
+                     Patterns::List(Patterns::Integer(-1,1),2,3));
+
+  std::ifstream in(p);
+  bool status = prm.read_input (in);
+  Assert (status == false, ExcInternalError());
+  
+  deallog << "test_1=" << prm.get ("test_1") << std::endl;
+}
+
+
+int main ()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  // go into the source dir to read files there. this
+  // is necessary so that we can include files there
+  chdir (SOURCE_DIR);
+  check ("parameter_handler_1_include_fail.prm");
+
+  return 0;
+}
