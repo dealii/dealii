d653de7b7430b4da73fcdb9c2ecb15e63dbfb7b2
Guido Kanschat <dr.guido.kanschat@gmail.com>
2013-11-27 14:04:50 +0000

    automatically generate version info
    git-svn-id: https://svn.dealii.org/trunk@31810 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/doxygen/scripts/mod_header.pl
+++ deal.II/doc/doxygen/scripts/mod_header.pl
@@ -1,8 +1,8 @@
 
 
 # Modify these to enter the current data automatically
-my $year = 2013;
-my $version = "8.1.pre";
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+$year += 1900;
 
 if (m'</head>')
 {
@@ -10,7 +10,7 @@ if (m'</head>')
     print '<link rel="SHORTCUT ICON" href="http://www.dealii.org/deal.ico"></link>', "\n";
     print '<meta name="author" content="The deal.II Authors <authors@dealii.org>"></meta>', "\n";
     print '<meta name="copyright" content="Copyright (C) 1998 - ', $year, ' by the deal.II authors"></meta>', "\n";
-    print '<meta name="deal.II-version" content="', $version, '"></meta>', "\n";
+    print '<meta name="deal.II-version" content="@DEAL_II_PACKAGE_VERSION@"></meta>', "\n";
 }
 
 s/\$projectname// unless (m/<title>/);
