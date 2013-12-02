439a133f0988842f6a4d63a6945ce604533239b8
Matthias Maier <tamiko@kyomu.43-1.org>
2013-12-02 10:42:48 +0000

    Try to fix the timing issues in base/timer_02
    git-svn-id: https://svn.dealii.org/trunk@31843 0785d39b-7218-0410-832d-ea1e28bc413d

--- tests/base/timer_02.cc
+++ tests/base/timer_02.cc
@@ -33,7 +33,7 @@ int main ()
   std::ofstream logfile("output");
   deallog.attach(logfile);
   deallog.depth_console(0);
-  deallog.threshold_double(1.e-10);
+  deallog.threshold_double(1.e-1);
 
   {
 
