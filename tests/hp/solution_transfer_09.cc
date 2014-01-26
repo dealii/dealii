5e6cf1ea0108468912c587362666c32048588f3c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2014-01-26 18:47:48 +0000

    Fix a forgotten case with FENothing (thanks to a testcase by Krzysztof Bzowski).
    git-svn-id: https://svn.dealii.org/trunk@32316 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/hp/solution_transfer_09.cc
@@ -0,0 +1,105 @@
+// ---------------------------------------------------------------------
+// $Id$
+//
+// Copyright (C) 2011 - 2014 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// Like _07 but with all same fe indices. This triggered yet another place
+// where we had the same kind of error.
+
+#include "../tests.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <deal.II/base/logstream.h>
+
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/hp/dof_handler.h>
+#include <deal.II/grid/tria_iterator.h>
+
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_nothing.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/hp/fe_collection.h>
+
+#include <deal.II/numerics/solution_transfer.h>
+
+#include <deal.II/numerics/data_out.h>
+
+using namespace dealii;
+
+
+template <int dim>
+void test ()
+{
+  Triangulation<dim> triangulation;
+  GridGenerator::hyper_cube (triangulation);
+
+  hp::FECollection<dim>     fe_collection;
+  fe_collection.push_back(FE_Q<dim>(1));
+  fe_collection.push_back(FE_Q<dim>(2));
+
+  hp::DoFHandler<dim> dof_handler(triangulation);
+  dof_handler.begin_active()->set_active_fe_index(0);
+  dof_handler.distribute_dofs (fe_collection);
+
+  // Init solution
+  Vector<double> solution(dof_handler.n_dofs());
+  solution = 1.0;
+
+
+  // set refine flag for the only cell we have, then do the refinement
+  SolutionTransfer<dim, Vector<double>, hp::DoFHandler<dim> >
+    solution_trans(dof_handler);
+  dof_handler.begin_active()->set_refine_flag ();
+  solution_trans.prepare_for_pure_refinement();
+  triangulation.execute_coarsening_and_refinement ();
+
+  // now set the active_fe_index flags on the new set of fine level cells
+  for (unsigned int c=0; c<dof_handler.begin(0)->n_children(); ++c)
+    dof_handler.begin(0)->child(c)->set_active_fe_index(0);
+
+  // distribute dofs and transfer solution there
+  dof_handler.distribute_dofs (fe_collection);
+  
+  Vector<double> new_solution(dof_handler.n_dofs());
+  solution_trans.refine_interpolate(solution, new_solution);
+
+  // we should now have only 1s in the new_solution vector
+  for (unsigned int i=0; i<new_solution.size(); ++i)
+    Assert (new_solution[i] == 1, ExcInternalError());
+  
+  // we are good if we made it to here
+  deallog << "OK" << std::endl;
+}
+
+
+int main()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  test<1> ();
+  test<2> ();
+  test<3> ();
+}
---
+++ tests/hp/solution_transfer_10.cc
@@ -0,0 +1,119 @@
+// ---------------------------------------------------------------------
+// $Id$
+//
+// Copyright (C) 2014 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// A test by Krzysztof Bzowski that verifies something in SolutionTransfer
+// that didn't work for a few days
+
+#include <deal.II/base/logstream.h>
+
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/hp/dof_handler.h>
+#include <deal.II/grid/tria_iterator.h>
+
+#include <deal.II/fe/fe_q.h> 
+#include <deal.II/fe/fe_nothing.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/hp/fe_collection.h>
+
+#include <deal.II/numerics/solution_transfer.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace dealii;
+
+int main()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  Triangulation<2> triangulation(Triangulation<2>::none);
+  GridGenerator::hyper_cube (triangulation); 
+  triangulation.refine_global(1);
+	 
+  hp::FECollection<2> fe_collection;
+  fe_collection.push_back(FE_Q<2>(1));
+  fe_collection.push_back(FE_Nothing<2>());
+
+  hp::DoFHandler<2> dof_handler(triangulation);
+
+  // Assign FEQ to all cells
+  hp::DoFHandler<2>::active_cell_iterator cell = dof_handler.begin_active();
+  hp::DoFHandler<2>::active_cell_iterator endc = dof_handler.end();  
+
+
+
+  /*
+   * -----------
+   * |  0 |  0 |
+   * -----------
+   * |  1 |  1 |		0 - FEQ, 1 - FE_Nothing
+   * -----------
+   */
+
+  cell->set_active_fe_index(1);
+  cell++;
+  cell->set_active_fe_index(1);
+  cell++;
+  cell->set_active_fe_index(0);
+  cell++;
+  cell->set_active_fe_index(0);
+
+  dof_handler.distribute_dofs (fe_collection);
+
+  // Init solution
+  Vector<double> solution(dof_handler.n_dofs());
+  solution = 1.0;
+
+
+  /* Set refine flags:
+   * -----------
+   * |    |  R |
+   * -----------
+   * |  R |    |		
+   * -----------
+   */
+
+
+  cell = dof_handler.begin_active();
+  cell->set_refine_flag();
+  cell++;
+  cell++;
+  cell++;
+  cell->set_refine_flag();
+
+  triangulation.prepare_coarsening_and_refinement();
+
+  // Interpolate solution
+  SolutionTransfer<2, Vector<double>, hp::DoFHandler<2> > solultion_trans(dof_handler);
+  solultion_trans.prepare_for_coarsening_and_refinement(solution);
+
+  triangulation.execute_coarsening_and_refinement ();
+
+  dof_handler.distribute_dofs(fe_collection);
+
+  Vector<double> new_solution(dof_handler.n_dofs());
+  solultion_trans.interpolate(solution, new_solution);
+
+  deallog << "OK" << std::endl;
+}
+
