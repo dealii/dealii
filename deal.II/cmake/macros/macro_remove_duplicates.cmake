e43b06d681ff1d7b4ffe336f887a9befbf540015
Matthias Maier <tamiko@kyomu.43-1.org>
2014-01-28 18:58:22 +0000

    CMake: Refactor list deduplication into a macro
    git-svn-id: https://svn.dealii.org/trunk@32336 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/cmake/macros/macro_remove_duplicates.cmake
@@ -0,0 +1,35 @@
+## ---------------------------------------------------------------------
+## $Id$
+##
+## Copyright (C) 2014 by the deal.II authors
+##
+## This file is part of the deal.II library.
+##
+## The deal.II library is free software; you can use it, redistribute
+## it, and/or modify it under the terms of the GNU Lesser General
+## Public License as published by the Free Software Foundation; either
+## version 2.1 of the License, or (at your option) any later version.
+## The full text of the license can be found in the file LICENSE at
+## the top level of the deal.II distribution.
+##
+## ---------------------------------------------------------------------
+
+#
+# Remove duplicate entries from a list. Optionally do this in reverse
+# order, keeping the rightmost element
+#
+# Usage:
+#     REMOVE_DUPLICATES(list [REVERSE])
+#
+
+MACRO(REMOVE_DUPLICATES _list)
+  IF(NOT "${${_list}}" STREQUAL "")
+    IF("${ARGN}" STREQUAL "REVERSE")
+      LIST(REVERSE ${_list})
+    ENDIF()
+    LIST(REMOVE_DUPLICATES ${_list})
+    IF("${ARGN}" STREQUAL "REVERSE")
+      LIST(REVERSE ${_list})
+    ENDIF()
+  ENDIF()
+ENDMACRO()
