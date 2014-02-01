61160090ad6832e4ab98f3771a575b2bab3226a5
Matthias Maier <tamiko@kyomu.43-1.org>
2014-02-01 21:53:31 +0000

    CMake: Break everything (tm)
     - Provide a quick sanity check to test the final link interface.
    
    git-svn-id: https://svn.dealii.org/trunk@32370 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/doc/news/changes.h
+++ deal.II/doc/news/changes.h
@@ -124,6 +124,12 @@ inconvenience this causes.
 <h3>Specific improvements</h3>
 
 <ol>
+  <li>Added: A sanity check for the full link interface at configure time.
+  Hopefully this prevents some people from compiling the whole library just
+  to hit a link error.
+  <br>
+  (Matthias Maier, 2014/02/01)
+
   <li>Fixed: The build system does no longer record full paths to system
   libraries but uses the appropriate short names instead.
   <br>
