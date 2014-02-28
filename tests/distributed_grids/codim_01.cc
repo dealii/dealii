b88c5edf97b8358c68ce31d5ffb242894264ca89
Timo Heister <timo.heister@gmail.com>
2014-02-28 14:49:15 +0000

    add simple distributed codim 1 test case
    git-svn-id: https://svn.dealii.org/trunk@32585 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/distributed_grids/codim_01.cc
@@ -0,0 +1,57 @@
+// ---------------------------------------------------------------------
+// $Id: 2d_refinement_01.cc 31349 2013-10-20 19:07:06Z maier $
+//
+// Copyright (C) 2008 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// Test Tria<2,3>
+
+#include "../tests.h"
+#include "coarse_grid_common.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/tensor.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+
+#include <fstream>
+
+
+template<int dim, int spacedim>
+void test(std::ostream & /*out*/)
+{
+  parallel::distributed::Triangulation<dim,spacedim> tr(MPI_COMM_WORLD);
+
+  GridGenerator::torus(tr, 1, 0.2);
+  tr.begin_active()->set_refine_flag();
+  tr.execute_coarsening_and_refinement ();
+
+  write_vtk(tr, "file");
+}
+
+
+int main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+  initlog();
+
+  deallog.push("2-3");
+  test<2,3>(deallog.get_file_stream());  
+  deallog.pop();
+}
