fb6cd924f4d502aa643fd73be2075a3f1db47615
dakshina.ilangova <dakshina.ilangova@0785d39b-7218-0410-832d-ea1e28bc413d>
2014-04-05 19:31:21 +0000

    Added tests for lac/PointerMatrix
    git-svn-id: https://svn.dealii.org/trunk@32719 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ tests/lac/pointer_matrix_04.cc
@@ -0,0 +1,54 @@
+// ---------------------------------------------------------------------
+// $Id: pointer_matrix_01.cc 2014-03-14 dilangov $
+//
+// Copyright (C) 2006 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// check PointerMatrix:checkConstructor4
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/lac/pointer_matrix.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/vector.h>
+
+template<typename number>
+  void
+  checkConstructor4(const FullMatrix<number> &A, char *name)
+  {
+    deallog << "Init with matrix name and matrix" << std::endl;
+    PointerMatrix<FullMatrix<number>, Vector<number> > P(&A, name);
+    deallog << "Is matrix empty:" << P.empty() << std::endl;
+  }
+
+int
+main()
+{
+
+  std::ofstream logfile("output");
+  deallog << std::fixed;
+  deallog << std::setprecision(4);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  const double Adata[] =
+    { 2, 3, 4, 5 };
+
+  FullMatrix<double> A(2, 2);
+  A.fill(Adata);
+
+  char *name = "Matrix A";
+
+  checkConstructor4<double>(A, name);
+}
