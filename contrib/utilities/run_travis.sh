bfdab4be4613d4999bc07a1f29ea68a9ba889d9f
Luca Heltai <luca.heltai@sissa.it>
2014-08-25 16:28:01 +0200

    Removed all builds. Only indent test.

--- contrib/utilities/run_travis.sh
+++ contrib/utilities/run_travis.sh
@@ -1,32 +1,17 @@
 #!/bin/sh
-case $1 in
-build)
-    if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
-	echo "Running build tests."
-	mkdir build_test 
-	cd build_test
-	ctest -DCMAKE_BUILD_TYPE=$2 -V -j4 -S ../tests/run_buildtest.cmake
-    else 
-	echo "Build test is only run when merging to master branch. Exiting."
-    fi
-    ;;
-indent)
-     if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
-	echo "Running indentation test on master merge."
-     else 
-        echo "Running indentation test on Pull Request #${TRAVIS_PULL_REQUEST}"
-     fi
-     wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz > /dev/null
-     tar xvfz astyle_2.04_linux.tar.gz > /dev/null
-     pushd astyle/build/gcc
-     make -j4 > /dev/null
-     popd
-     export PATH=`pwd`/astyle/build/gcc/bin:$PATH
-     ./contrib/utilities/indent
-     git diff-files --quiet || (git diff && failing_missing_command)	
-     ;;
-*)
-    echo "Unrecognized test type! [$1]"
-    exit 1
-esac
 
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
+	echo "Running indentation test on master merge."
+else 
+	echo "Running indentation test on Pull Request #${TRAVIS_PULL_REQUEST}"
+fi
+
+wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz > /dev/null
+tar xvfz astyle_2.04_linux.tar.gz > /dev/null
+cd astyle/build/gcc
+make -j4 > /dev/null
+cd ../../../
+export PATH=`pwd`/astyle/build/gcc/bin:$PATH
+./contrib/utilities/indent
+git diff
+git diff-files --quiet 
