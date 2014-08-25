5dc666b6aea69a7cb61d3853c3f23e7713f43f73
Luca Heltai <luca.heltai@sissa.it>
2014-08-25 17:01:48 +0200

    Moved download and install of astyle into before_install section. Renamed files.

--- contrib/utilities/check_indentation.sh
+++ contrib/utilities/check_indentation.sh
@@ -6,12 +6,8 @@ else
 	echo "Running indentation test on Pull Request #${TRAVIS_PULL_REQUEST}"
 fi
 
-wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz > /dev/null
-tar xvfz astyle_2.04_linux.tar.gz > /dev/null
-cd astyle/build/gcc
-make -j4 > /dev/null
-cd ../../../
 export PATH=`pwd`/astyle/build/gcc/bin:$PATH
+
 ./contrib/utilities/indent
 git diff
 git diff-files --quiet 
