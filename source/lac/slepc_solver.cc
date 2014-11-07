33ceae9251110b9c7b0d81185bb28d01345c58e5
Denis Davydov <davydden@gmail.com>
2014-11-07 16:47:29 +0100

    use EPSComputeRelativeError in SLEPc SolverBase.

--- source/lac/slepc_solver.cc
+++ source/lac/slepc_solver.cc
@@ -211,7 +211,11 @@ namespace SLEPcWrappers
       // only if at least one eigenvector has converged.
       if ((*n_converged)>0)
         {
-          ierr = EPSComputeResidualNorm (solver_data->eps, 0, &residual_norm);
+          // EPSComputeRelativeError is consistent with the one
+          // used during the solution process. Given EPS_CONV_ABS set above,
+          // this can be either the l2 norm or the mass-matrix induced norm
+          // when EPS_GHEP is set.
+          ierr = EPSComputeRelativeError (solver_data->eps, 0, &residual_norm);
           AssertThrow (ierr == 0, ExcSLEPcError(ierr));
         }
 
