05e39e1f0dbd9ba5e19a74771306fa99b7ad3214
Wolfgang Bangerth <bangerth@math.tamu.edu>
2014-11-09 11:38:51 -0600

    Allow using the old std_cxx1x namespace.
    This was supposed to work in a backward compatible way but I broke
    it for everyone who didn't explicitly #included the old base/std_cxx1x/*h
    headers but got stuff through indirect #includes.

--- include/deal.II/base/std_cxx11/type_traits.h
+++ include/deal.II/base/std_cxx11/type_traits.h
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2012 - 2013 by the deal.II authors
+// Copyright (C) 2012 - 2014 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
@@ -62,5 +62,11 @@ namespace std_cxx11
 DEAL_II_NAMESPACE_CLOSE
 
 #endif
+
+
+// then allow using the old namespace name instead of the new one
+DEAL_II_NAMESPACE_OPEN
+namespace std_cxx1x = std_cxx11;
+DEAL_II_NAMESPACE_CLOSE
 
 #endif
