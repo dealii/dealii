27153e25ec131bc5218ed886922398418058d137
Lukas Korous <lukas.korous@gmail.com>
2014-11-25 15:01:13 +0100

    Workaround for known VS 2013 missing support for defaulted functions.

--- bundled/boost-1.56.0/include/boost/config/compiler/visualc.hpp
+++ bundled/boost-1.56.0/include/boost/config/compiler/visualc.hpp
@@ -168,6 +168,16 @@
 #  define BOOST_NO_CXX11_DECLTYPE_N3276
 #endif
 
+// Apply the bugfix from this patch: https ://svn.boost.org/trac/boost/ticket/10215
+// 
+//// Although VC 2013 claims to support defaulted functions, it 
+//// does not support the =default construct. See 
+//// h t t p : / / m s d n . m i c r o s o f t . c o m / e n - u s / l i b r a r y / h h 4 0 9 2 9 3 . a s p x 
+//// 
+#ifdef _MSC_VER
+  #define BOOST_NO_CXX11_DEFAULTED_FUNCTIONS 
+#endif
+
 // C++11 features not supported by any versions
 #define BOOST_NO_CXX11_CHAR16_T
 #define BOOST_NO_CXX11_CHAR32_T
