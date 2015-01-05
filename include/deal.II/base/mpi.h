156abe7ae6ec8647e11095f096bbf3c3cc4ddc2f
Wolfgang Bangerth <bangerth@math.tamu.edu>
2015-01-05 09:01:45 -0600

    Fix a typo.

--- include/deal.II/base/mpi.h
+++ include/deal.II/base/mpi.h
@@ -279,7 +279,7 @@ namespace Utilities
        * main.
        * @param[in] max_num_threads The maximal number of threads this MPI
        * process should utilize. If this argument is set to
-       * numbers::invalid_unsigned_int, the number of threads is determined by
+       * numbers::invalid_unsigned_int, the number of threads is determined
        * automatically in the following way: the number of threads to run on
        * this MPI process is set in such a way that all of the cores in your
        * node are spoken for. In other words, if you have started one MPI
