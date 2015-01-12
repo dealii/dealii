0d810b50f18d44cbf7de9c346b4d9831978fac68
Matthias Maier <tamiko@kyomu.43-1.org>
2015-01-12 23:14:45 +0100

    Provide a 8.2.0-vs-8.2.1.h changelog

---
+++ doc/news/8.2.0-vs-8.2.1.h
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2014 - 2015 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+/**
+@page changes_between_8_2_0_and_8_2_1 Changes between Version 8.2.0 and 8.2.1
+
+<p>
+This is the list of changes made between the release of deal.II version
+8.2.0 and that of 8.2.1. All entries are signed with the names of the
+authors.
+</p>
+
+<!-- ----------- SPECIFIC IMPROVEMENTS ----------------- -->
+
+<a name="specific"></a>
+<h3>Specific improvements</h3>
+
+<ol>
+  <li> Ported: The build system now supports CMake up to version 3.1.
+  <br>
+  (Matthias Maier, 2015/01/06)
+  </li>
+
+  <li> Fixed: CMake now also handles and exports the subminor version
+  number correctly ("pre" and "rc?" are replaced by "0").
+  <br>
+  (Matthias Maier, 2015/01/02)
+  </li>
+</ol>
+
+*/
