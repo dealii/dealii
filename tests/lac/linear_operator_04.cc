0167b6550bc9e39b8fdb6250e57a6ec10fc87d01
Matthias Maier <tamiko@43-1.org>
2015-04-08 12:13:36 +0200

    Implement a LinearOperator class

---
+++ tests/lac/linear_operator_04.cc
@@ -0,0 +1,48 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2015 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// Test that it is possible to instantiate a LinearOperator object for all
+// different kinds of Trilinos matrices and vectors
+
+#include "../tests.h"
+
+#include <deal.II/lac/linear_operator.h>
+#include <deal.II/lac/trilinos_block_sparse_matrix.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
+
+using namespace dealii;
+
+int main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+  initlog();
+  deallog << std::setprecision(10);
+
+  TrilinosWrappers::SparseMatrix a;
+
+  auto op_a  = linop<TrilinosWrappers::MPI::Vector>(a);
+  auto op_a2  = linop<TrilinosWrappers::Vector>(a);
+
+  TrilinosWrappers::BlockSparseMatrix b;
+
+  auto op_b = linop<TrilinosWrappers::MPI::BlockVector>(b);
+  auto op_b3 = linop<TrilinosWrappers::BlockVector>(b);
+
+  deallog << "OK" << std::endl;
+
+  return 0;
+}
