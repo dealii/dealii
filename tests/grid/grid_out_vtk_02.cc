c0854dd046e875f7f9d552ab913ab971b148178f
Guido Kanschat <dr.guido.kanschat@gmail.com>
2015-07-05 13:34:35 +0200

    remove include

--- tests/grid/grid_out_vtk_02.cc
+++ tests/grid/grid_out_vtk_02.cc
@@ -18,7 +18,6 @@
 // check level_subdomain_id for distributed Triangulation
 
 #include "../tests.h"
-#include "coarse_grid_common.h"
 #include <deal.II/base/logstream.h>
 #include <deal.II/base/tensor.h>
 #include <deal.II/grid/tria.h>
