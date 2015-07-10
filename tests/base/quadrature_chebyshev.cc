991f62222158583d0dd3e504f734e31c3f5225cd
Wolfgang Bangerth <bangerth@math.tamu.edu>
2015-07-10 13:34:13 -0500

    Adjust the precision of the output of a test.

--- tests/base/quadrature_chebyshev.cc
+++ tests/base/quadrature_chebyshev.cc
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 1998 - 2014 by the deal.II authors
+// Copyright (C) 1998 - 2015 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
@@ -79,7 +79,8 @@ int main()
   deallog.attach(logfile);
   deallog.depth_console(0);
   deallog.threshold_double(1.e-10);
-
+  deallog << std::setprecision(8);
+  
   deallog.push("1d Gauss-Chebyshev\n");
   deallog.pop();
   check_quadrature<QGaussChebyshev<1>,1>(&exact_monomials[0]);
