940129df494037eada8dd7afa2fd614fdaf07a89
Luca Heltai <luca.heltai@sissa.it>
2015-07-25 17:13:53 +0200

    Fixed indentation.

--- source/base/quadrature_lib.cc
+++ source/base/quadrature_lib.cc
@@ -771,10 +771,10 @@ QGaussLogR<1>::QGaussLogR(const unsigned int n,
       {
         Assert( this->quadrature_points[i] != origin,
                 ExcMessage("The singularity cannot be on a Gauss point of the same order!") );
-	double denominator = std::log(std::abs( (this->quadrature_points[i]-origin)[0] )/alpha);
-	Assert( denominator != 0.0,
-		ExcMessage("The quadrature formula you are using does not allow to "
-			   "factor out the singularity, which is zero at one point."));
+        double denominator = std::log(std::abs( (this->quadrature_points[i]-origin)[0] )/alpha);
+        Assert( denominator != 0.0,
+                ExcMessage("The quadrature formula you are using does not allow to "
+                           "factor out the singularity, which is zero at one point."));
         this->weights[i] /= denominator;
       }
 }
