3aaa6860418f17174fb10004473a465c0a649cb0
Luca Heltai <luca.heltai@sissa.it>
2015-07-26 20:59:05 +0200

    Updated travis config.

--- contrib/utilities/setup_astyle.sh
+++ contrib/utilities/setup_astyle.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-echo "Downloading and installing astyle."
-wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz  > /dev/null
-tar xvfz astyle_2.04_linux.tar.gz > /dev/null
-cd astyle/build/gcc
-make -j4 > /dev/null
-cd ../../../
-export PATH=`pwd`/astyle/build/gcc/bin:$PATH
+
+PRG=$PWD/programs
+
+if [ ! -d $PRG ] 
+then
+    echo "create folder `$PRG`"
+    mkdir $PRG
+fi
+
+# astyle
+if [ ! -d $PRG/astyle ]
+then
+    echo "Downloading and installing astyle."
+    mkdir $PRG/astyle
+    wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz  > /dev/null
+    tar xfz astyle_2.04_linux.tar.gz -C $PRG > /dev/null
+    cd $PRG/astyle/build/gcc
+    make -j4 > /dev/null
+fi
