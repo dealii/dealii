959df468f23e3d13286fa468542f0b5f52542357
Wolfgang Bangerth <bangerth@math.tamu.edu>
2015-07-29 16:41:25 -0500

    Fix division by zero.
    Instead of first dividing by zero and then cleaning up the mess,
    simply test whether we're going to divide by zero and do the
    right thing based on that test.

--- source/base/timer.cc
+++ source/base/timer.cc
@@ -506,10 +506,10 @@ TimerOutput::print_summary () const
           out_stream << i->second.total_cpu_time << "s |";
           out_stream << std::setw(10);
           out_stream << std::setprecision(2);
-          double value = i->second.total_cpu_time/total_cpu_time * 100;
-          if (!numbers::is_finite(value))
-            value = 0.0;
-          out_stream << value << "% |";
+          if (total_cpu_time != 0)
+            out_stream << i->second.total_cpu_time/total_cpu_time * 100 << "% |";
+          else
+            out_stream << 0.0 << "% |";
         }
       out_stream << std::endl
                  << "+---------------------------------+-----------+"
