6e4a9ea2ff181e0d35edc3a8f9d8d3eaffb1ef03
Matthias Maier <tamiko@43-1.org>
2015-08-26 21:59:05 -0500

    Refactor template definitions into function_time.templates.h

--- include/deal.II/base/function_time.templates.h
+++ include/deal.II/base/function_time.templates.h
@@ -13,12 +13,44 @@
 //
 // ---------------------------------------------------------------------
 
-#include <deal.II/base/function_time.templates.h>
+#ifndef dealii__function_time_templates_h
+#define dealii__function_time_templates_h
+
+#include <deal.II/base/function_time.h>
 
 DEAL_II_NAMESPACE_OPEN
 
-// Explicit instantiation
-template class FunctionTime<double>;
+
+template<typename Number>
+FunctionTime<Number>::FunctionTime(const Number initial_time)
+  :
+  time(initial_time)
+{}
+
+
+
+template<typename Number>
+FunctionTime<Number>::~FunctionTime()
+{}
+
+
+
+template<typename Number>
+void
+FunctionTime<Number>::set_time (const Number new_time)
+{
+  time = new_time;
+}
+
+
+template<typename Number>
+void
+FunctionTime<Number>::advance_time (const Number delta_t)
+{
+  set_time (time+delta_t);
+}
+
 
 DEAL_II_NAMESPACE_CLOSE
 
+#endif /* dealii__function_time_templates_h */
