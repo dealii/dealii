e4606e4abcda7ede508a3815b0410d0218129de6
Luca Heltai <luca.heltai@sissa.it>
2015-11-10 19:48:57 +0100

    Fixed header.

--- include/deal.II/base/sacado_product_type.h
+++ include/deal.II/base/sacado_product_type.h
@@ -17,6 +17,7 @@
 #define dealii__sacado_product_type_h
 
 #include <deal.II/base/config.h>
+#include <deal.II/base/tensor.h>
 #include <deal.II/base/template_constraints.h>
 
 #ifdef DEAL_II_WITH_TRILINOS
