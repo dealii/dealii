3439c89b6ae475230814a28d009d2bfcfe8f494b
Wolfgang Bangerth <bangerth@math.tamu.edu>
2015-11-28 15:57:45 -0600

    Add tests.

---
+++ tests/base/signaling_nan_tensor.cc
@@ -0,0 +1,58 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2015 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// check numbers::signaling_nan<Tensor>
+//
+// the test only checks that the function can be called. It would have
+// been nicer to output the tensors (which would also verify the
+// correct output type, as well as that indeed *each* element is
+// correctly filled), but outputting a sNaN triggers a floating point
+// exception as well
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/signaling_nan.h>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+#include <fenv.h>
+
+
+template <typename T>
+void check ()
+{
+  numbers::signaling_nan<Tensor<1,2,T> >();
+  numbers::signaling_nan<Tensor<2,2,T> >();
+  numbers::signaling_nan<Tensor<3,2,T> >();
+
+  deallog << "OK" << std::endl;
+}
+
+
+int main ()
+{
+  std::ofstream logfile("output");
+  deallog << std::setprecision(3);
+  deallog.attach(logfile);
+  deallog.depth_console(0);
+  deallog.threshold_double(1.e-10);
+
+  check<float> ();
+  check<double> ();
+
+  return 0;
+}
+
