b6d28d3060132380a852dedba51dda68bc23db33
Wolfgang Bangerth <bangerth@math.tamu.edu>
2015-12-28 16:40:53 -0600

    Fix markup of references.
    doxygen interpreted our bibtex-references as C++ code which led
    to funny HTML markup. Fortunately, there is a way to tell
    doxygen to interpret the code block as just some piece of text
    it will not understand regardless of how hard it tries.

--- include/deal.II/numerics/error_estimator.h
+++ include/deal.II/numerics/error_estimator.h
@@ -75,7 +75,7 @@ namespace hp
  *
  * The full reference for the paper in which this error estimator is defined
  * is as follows:
- * @code
+ * @code{.bib}
  * @Article{KGZB83,
  *   author =       {Kelly, D. W. and {De S. R. Gago}, J. P. and Zienkiewicz, O. C.
  *                   and Babu\v{s}ka, I.},
