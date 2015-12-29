0e093cee70bc0c0369a2d317a6b0d72b30929acc
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2015-12-29 21:16:12 +0100

    Change minimum parallel grainsize of vector to power of 2

--- source/base/parallel.cc
+++ source/base/parallel.cc
@@ -34,13 +34,13 @@ namespace internal
     // large to split the work load into
     // enough chunks and the problem becomes
     // badly balanced)
-    unsigned int minimum_parallel_grain_size = 1000;
+    unsigned int minimum_parallel_grain_size = 1024;
   }
 
 
   namespace SparseMatrix
   {
-    // set this value to 1/5 of the value of
+    // set this value to 1/4 of the value of
     // the minimum grain size of
     // vectors. this rests on the fact that
     // we have to do a lot more work per row
@@ -50,7 +50,7 @@ namespace internal
     // worth it any more for anything but
     // very small matrices that we don't care
     // that much about anyway.
-    unsigned int minimum_parallel_grain_size = 200;
+    unsigned int minimum_parallel_grain_size = 256;
   }
 }
 
