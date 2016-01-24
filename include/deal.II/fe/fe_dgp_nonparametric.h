7670a4859381b13448c5b370341bb4417d0a5edd
Wolfgang Bangerth <bangerth@math.tamu.edu>
2016-01-23 18:54:29 -0600

    Remove now no longer used FE_DGP_Nonparametric::update_once/each.

--- include/deal.II/fe/fe_dgp_nonparametric.h
+++ include/deal.II/fe/fe_dgp_nonparametric.h
@@ -595,29 +595,9 @@ private:
    * within the constructor to be passed to the constructor of @p
    * FiniteElementData.
    */
-  static std::vector<unsigned int> get_dpo_vector (const unsigned int degree);
-
-  /**
-   * Given a set of flags indicating what quantities are requested from a @p
-   * FEValues object, return which of these can be precomputed once and for
-   * all. Often, the values of shape function at quadrature points can be
-   * precomputed, for example, in which case the return value of this function
-   * would be the logical and of the input @p flags and @p update_values.
-   *
-   * For the present kind of finite element, this is exactly the case.
-   */
-  UpdateFlags update_once (const UpdateFlags flags) const;
-
-  /**
-   * This is the opposite to the above function: given a set of flags
-   * indicating what we want to know, return which of these need to be
-   * computed each time we visit a new cell.
-   *
-   * If for the computation of one quantity something else is also required
-   * (for example, we often need the covariant transformation when gradients
-   * need to be computed), include this in the result as well.
-   */
-  UpdateFlags update_each (const UpdateFlags flags) const;
+  static
+  std::vector<unsigned int>
+  get_dpo_vector (const unsigned int degree);
 
   /**
    * Degree of the polynomials.
