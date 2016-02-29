a773a514430f7f23a9d890b08bd34a09628d603f
Denis Davydov <davydden@gmail.com>
2016-02-29 18:11:59 +0100

    fix a few bugs introduced in the recent complex PETSc related pull-requests

--- source/numerics/vector_tools_boundary.cc
+++ source/numerics/vector_tools_boundary.cc
@@ -21,4 +21,13 @@ DEAL_II_NAMESPACE_OPEN
 // ---------------------------- explicit instantiations --------------------
 #include "vector_tools_boundary.inst"
 
+// extra instantiations to make unit tests pass:
+template
+void VectorTools::interpolate_boundary_values (
+  const dealii::DoFHandler<2, 2> &,
+  const types::boundary_id,
+  const Function<2,float> &,
+  std::map<types::global_dof_index,float> &,
+  const ComponentMask &);
+
 DEAL_II_NAMESPACE_CLOSE
