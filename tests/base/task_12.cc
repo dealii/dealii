fd1cbdb27193e0fd988f469d36f868de6cafe30c
Wolfgang Bangerth <bangerth@math.tamu.edu>
2016-03-08 20:08:54 -0600

    Add tests.

---
+++ tests/base/task_12.cc
@@ -0,0 +1,62 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// verify that we can create tasks both via lambdas and via std::bind
+// expressions. this obviously requires C++11
+
+#include "../tests.h"
+#include <iomanip>
+#include <fstream>
+#include <unistd.h>
+
+#include <deal.II/base/thread_management.h>
+
+
+// return a double, to make sure we correctly identify the return type
+// of the expressions used in new_task(...)
+double test (int i)
+{
+  deallog << "Task " << i << " starting..." << std::endl;
+  sleep (1);
+  deallog << "Task " << i << " finished!" << std::endl;
+
+  return 3.141;
+}
+
+
+
+
+int main()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.threshold_double(1.e-10);
+
+  Threads::TaskGroup<double> tg;
+  tg += Threads::new_task (std::bind (test, 1));
+  tg += Threads::new_task ([]()
+  {
+    return test(2);
+  });
+
+  tg.join_all ();
+
+  deallog << "OK" << std::endl;
+
+  deallog.detach ();
+  logfile.close ();
+  sort_file_contents ("output");
+}
