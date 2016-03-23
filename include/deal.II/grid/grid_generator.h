927eb3044a83ece879127ea15b28caa67368053e
Nicola Demo <demo.nicola@gmail.com>
2016-03-23 14:21:16 +0100

    Remove auto as typename, added comment for dimensional problem

--- include/deal.II/grid/grid_generator.h
+++ include/deal.II/grid/grid_generator.h
@@ -576,7 +576,7 @@ namespace GridGenerator
    * @p colorize flag is set, the @p boundary_ids of the surfaces are
    * assigned, such that the left boundary is 0, and the others are set with
    * growing number accordingly to the counterclockwise. Colorize option works
-   * only with 2-dimensional problem. This function will create the classical 
+   * only with 2-dimensional problem. This function will create the classical
    * L-shape in 2d and it will look like the following in 3d:
    *
    * @image html hyper_l.png
