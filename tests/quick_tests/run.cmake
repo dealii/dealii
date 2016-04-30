dda20217b0ffb7a166369add3bf8a4a6a47f3368
David Wells <wellsd2@rpi.edu>
2016-04-30 12:18:50 -0400

    Make the quick test error message more accurate.
    Errors occured, so this should be labeled as an error with the same
    style used elsewhere in the CMake configuration.

--- tests/quick_tests/run.cmake
+++ tests/quick_tests/run.cmake
@@ -21,12 +21,11 @@ SEPARATE_ARGUMENTS(ALL_TESTS)
 
 EXECUTE_PROCESS(COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process --output-on-failure -O quicktests.log RESULT_VARIABLE res_var)
 
-if(NOT "${res_var}" STREQUAL "0")
-  MESSAGE( "
-
-*******************************     WARNING     *******************************
-
-Some of the tests failed!
+IF(NOT "${res_var}" STREQUAL "0")
+  MESSAGE("
+***************************************************************************
+**                 Error: Some of the quick tests failed.
+***************************************************************************
 
 Please scroll up or check the file tests/quick_tests/quicktests.log for the
 error messages. If you are unable to fix the problems, see the FAQ or write
