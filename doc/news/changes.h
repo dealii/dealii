33670773d30f4e786e11788722ae392cb753abc1
Matthias Maier <tamiko@43-1.org>
2016-07-13 09:59:06 -0500

    add a news/changes.h entry

--- doc/news/changes.h
+++ doc/news/changes.h
@@ -150,6 +150,14 @@ inconvenience this causes.
 <h3>General</h3>
 
 <ol>
+ <li> Improved: The build system now checks for usable compiler/linker
+ flags during various stages of the configure run. This should catch the
+ majority of issues by user supplied flags/libraries and unusable final
+ link interfaces before we actually proceed to compile the library.
+ <br>
+ (Matthias Maier, 2016/07/13)
+ </li>
+
  <li> Improved: The testsuite now supports fine grained feature constraints
  of the form <code>test.with_[feature]_with_[...]=true</code> corresponding
  to variables <code>DEAL_II_<FEATURE>_WITH_[...]</code> exported to
