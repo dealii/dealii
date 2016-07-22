78482ef0437eb66f23614d65c6d62a8ae4e51078
Jaeryun Yim <jaeryun.yim@gmail.com>
2016-07-22 16:23:57 +0900

    Edit copyright year and correct the name of element on comments.

--- tests/fe/p1nc_08.cc
+++ tests/fe/p1nc_08.cc
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2015 by the deal.II authors
+// Copyright (C) 2015 - 2016 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
@@ -15,7 +15,7 @@
 
 
 
-// check the P1NC nonparametric element's gradient on a genuine quadrilateral.
+// check the P1NC element's gradient on a genuine quadrilateral.
 
 #include "../tests.h"
 #include <deal.II/base/logstream.h>
