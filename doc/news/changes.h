2f4210cde71ebab6f767c82ec2d35d120c8215d0
Jonathan Robey <class4kayaker@gmail.com>
2016-07-24 10:55:50 -0700

    Fix changes.h entry formatting

--- doc/news/changes.h
+++ doc/news/changes.h
@@ -151,7 +151,7 @@ inconvenience this causes.
 
 <ol>
  <li>
- Improved: VectorTools:interpolate now takes a ComponentMask to select the
+ Improved: VectorTools::interpolate() now takes a ComponentMask to select the
  components to interpolate.
  <br>
  (Jonathan Robey, 2016/07/21)
