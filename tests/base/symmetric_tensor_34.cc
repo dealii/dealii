39e1d8c597c6ba7081f593f69014d5bcd67388a1
Wolfgang Bangerth <bangerth@math.tamu.edu>
2016-08-11 12:07:21 -0500

    Add test.

---
+++ tests/base/symmetric_tensor_34.cc
@@ -0,0 +1,78 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// Test that we can convert a SymmetricTensor<4,dim> to a Tensor<4,dim>
+
+#include "../tests.h"
+#include <deal.II/base/symmetric_tensor.h>
+#include <deal.II/base/logstream.h>
+
+
+template <int dim>
+void initialize (SymmetricTensor<2,dim> &st)
+{
+  for (unsigned int i=0; i<dim; ++i)
+    for (unsigned int j=i; j<dim; ++j)
+      st[i][j] = (i+1)*dim + (j-i);
+}
+
+
+template <int dim>
+void initialize (SymmetricTensor<4,dim> &st)
+{
+  for (unsigned int i=0; i<dim; ++i)
+    for (unsigned int j=i; j<dim; ++j)
+      for (unsigned int k=0; k<dim; ++k)
+        for (unsigned int l=k; l<dim; ++l)
+          st[i][j][k][l] = (i+1)*dim + (j-i)  +  (k+1)*dim + (l-k);
+}
+
+
+
+template <int rank, int dim>
+void check ()
+{
+  // build a regular tensor
+  SymmetricTensor<rank,dim> st;
+  initialize (st);
+  deallog << "st=" << st << std::endl;
+
+  Tensor<rank,dim>          t (st);   // check conversion constructor
+  deallog << "t =" << t << std::endl;
+
+  t = st;                             // check assignment operator
+  deallog << "t =" << t << std::endl;
+}
+
+
+int main ()
+{
+  std::ofstream logfile("output");
+  deallog << std::setprecision(3);
+  deallog.attach(logfile);
+  deallog.threshold_double(1.e-10);
+
+  deallog << "checking rank 2 tensors" << std::endl;
+  check<2,1>();
+  check<2,2>();
+  check<2,3>();
+
+  deallog << "checking rank 4 tensors" << std::endl;
+  check<4,1>();
+  check<4,2>();
+  check<4,3>();
+
+  deallog << "OK" << std::endl;
+}
