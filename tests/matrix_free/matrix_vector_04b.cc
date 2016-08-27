8fb5035bf8d0b2559a9a6646ba165fc9fabf5c45
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2016-08-27 15:51:16 +0200

    Add similar test to matrix_vector_14b for the FE_Q case.

---
+++ tests/matrix_free/matrix_vector_04b.cc
@@ -0,0 +1,48 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// Same as matrix_vector_04, but using more quadrature points than the FE
+// degree would suggest
+
+#include "../tests.h"
+
+std::ofstream logfile("output");
+
+#include "matrix_vector_common.h"
+
+
+template <int dim, int fe_degree>
+void test ()
+{
+  const SphericalManifold<dim> manifold;
+  Triangulation<dim> tria;
+  GridGenerator::hyper_shell (tria, Point<dim>(),
+                              0.5, 1., 96, true);
+  tria.set_all_manifold_ids(0);
+  tria.set_manifold (0, manifold);
+  if (dim == 2)
+    tria.refine_global (2);
+
+  FE_Q<dim> fe (fe_degree);
+  DoFHandler<dim> dof (tria);
+  dof.distribute_dofs(fe);
+  ConstraintMatrix constraints;
+  DoFTools::make_hanging_node_constraints(dof, constraints);
+  constraints.close();
+
+  do_test<dim, fe_degree, double, fe_degree+3> (dof, constraints);
+}
