527eca83f8b1d27576f800b9a09fc6f1f936d8cc
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2016-09-06 14:14:41 +0200

    Compare with Triangulation::none instead of true

--- source/dofs/dof_handler.cc
+++ source/dofs/dof_handler.cc
@@ -1156,7 +1156,8 @@ void DoFHandler<dim, spacedim>::distribute_mg_dofs (const FiniteElement<dim, spa
   (void)old_fe;
   Assert(old_fe == &fe, ExcMessage("You are required to use the same FE for level and active DoFs!") );
 
-  Assert((tria->get_mesh_smoothing() & Triangulation<dim, spacedim>::limit_level_difference_at_vertices)==true,
+  Assert(((tria->get_mesh_smoothing() & Triangulation<dim, spacedim>::limit_level_difference_at_vertices)
+          != Triangulation<dim, spacedim>::none),
          ExcMessage("The mesh smoothing requirement 'limit_level_difference_at_vertices' has to be set for using multigrid!"));
 
   clear_mg_space();
