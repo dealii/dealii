725eaad031a1389353150cd7af653ba998e39c32
Matthias Maier <tamiko@43-1.org>
2016-09-08 16:18:50 -0500

    add news/changes.h entry

--- doc/news/changes.h
+++ doc/news/changes.h
@@ -219,6 +219,13 @@ inconvenience this causes.
 
 <ol>
 
+ <li> Fixed: The CMake macros <code>DEAL_II_(ADD_TEST|SETUP_TARGET)</code>
+ now enforce a stricter <code>CMAKE_BUILD_TYPE</code> handling. This helps
+ to avoid situations where targets with different build flavors might
+ accidentally get linked against each other.
+ <br>
+ (Matthias Maier, 2016/09/08)
+
  <li> Improved: the doxygen documentation now contains nicely formatted
  boxes containing the text message of each exception. Several messages
  haven been clarified and improved.
