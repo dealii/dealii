76f374efa9e647fb5899dcd5bc530a65f1f417e9
Kent Van Vels <kvanvel@gmail.com>
2016-09-14 14:23:25 -0400

    moved to template file

--- include/deal.II/lac/sparse_matrix.templates.h
+++ include/deal.II/lac/sparse_matrix.templates.h
@@ -1898,6 +1898,80 @@ SparseMatrix<number>::get_sparsity_pattern () const
 }
 
 
+namespace internal
+{
+  namespace SparseMatrix
+  {
+    template <class StreamType, typename realType>
+    inline
+    void print_value(StreamType &out, const realType&val)
+    {
+      out << val;
+    }
+
+    template<class StreamType, typename realType>
+    inline
+    void print_value(StreamType &out, const std::complex<realType> &val)
+    {
+      // the following format is what is required by the matrix market format.
+      out << val.real << ' ' << val.imag;
+    }
+
+    template<class StreamType, class numberType>
+    void
+    print_header(StreamType &out,const numberType & dummy)
+    {
+      out << "%%MatrixMarket matrix coordinate real general\n";
+    }
+
+    //template<typename realType>
+    template<class StreamType, class realType>
+    void
+    print_header(StreamType &out, const std::complex<realType> & dummy )
+    {
+      out << "%%MatrixMarket matrix coordinate complex general\n";
+    }  
+  } // namespace SparseMatrix
+} // namespace internal
+
+template <typename number>
+template <class StreamType>
+void SparseMatrix<number>::print_matrix_market(StreamType &out,
+					       const double threshold) const
+{
+  Assert(cols != 0, ExcNotInitialized());
+  Assert(val != 0, ExcNotInitialized());
+  Assert(threshold >= 0, ExcMessage("Negative threshold!") );  
+
+  //Print the header.
+  {
+    const number dummy = (number) 1.0; 
+    print_header(out,dummy);
+  }
+  
+  const size_type nnz = n_actually_nonzero_elements(threshold);
+  out << m() << ' ' << n() << ' ' << nnz << '\n';
+
+  //Print the body
+  for (unsigned int i = 0; i < m(); ++i)
+    for (const_iterator it = begin(i); it != end(i); ++it)
+      {
+	const number value = it->value();
+	if (std::fabs<number>(value) > threshold)
+	  {
+	    const unsigned int j = it->column();
+	    //the following " + 1"s are to convert to ones base indexing
+	    out << i + 1 << ' ' << j + 1 << ' ';
+	    print_value(out, value);
+	    out << value << '\n';
+	  }
+      }
+}
+
+  
+
+
+    
 
 template <typename number>
 void SparseMatrix<number>::print_formatted (std::ostream &out,
