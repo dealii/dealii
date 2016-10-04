7080c024d6c7d195bbef642acceda3f009d1bd31
Wolfgang Bangerth <bangerth@colostate.edu>
2016-10-04 14:54:36 -0600

    Do not use 'typename' outside templates.

--- tests/manifold/spherical_manifold_04.cc
+++ tests/manifold/spherical_manifold_04.cc
@@ -72,7 +72,7 @@ void test (const double R)
                            update_quadrature_points |
                            update_JxW_values);
 
-  typename DoFHandler<dim>::active_cell_iterator
+  DoFHandler<dim>::active_cell_iterator
   cell = dof_handler.begin_active (),
   endc = dof_handler.end ();
   const unsigned int n_q_points = quadrature_formula.size();
