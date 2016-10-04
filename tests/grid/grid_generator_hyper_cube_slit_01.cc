3bb9b4205f817e3cef2c5b19c7868f94f058656e
Timo Heister <timo.heister@gmail.com>
2016-10-04 10:17:15 -0400

    indent

--- tests/grid/grid_generator_hyper_cube_slit_01.cc
+++ tests/grid/grid_generator_hyper_cube_slit_01.cc
@@ -34,20 +34,20 @@ void test()
   Triangulation<dim> tr;
   GridGenerator::hyper_cube_slit(tr, -1, 1, true);
   typename Triangulation<dim>::active_cell_iterator
-    cell = tr.begin_active(),
-    endc = tr.end();
+  cell = tr.begin_active(),
+  endc = tr.end();
   for (; cell!=endc; ++cell)
     {
       deallog << "cell:" << std::endl;
-      
+
       for (unsigned int face=0; face<GeometryInfo<dim>::faces_per_cell; ++face)
-	{
-	  if (cell->face(face)->at_boundary() && cell->face(face)->boundary_id() != 0)
-	    deallog << "boundary id = " << (int)cell->face(face)->boundary_id()
-		    << " center = " << cell->face(face)->center()
-		    << " faceidx = " << face
-		    << std::endl;
-	}
+        {
+          if (cell->face(face)->at_boundary() && cell->face(face)->boundary_id() != 0)
+            deallog << "boundary id = " << (int)cell->face(face)->boundary_id()
+                    << " center = " << cell->face(face)->center()
+                    << " faceidx = " << face
+                    << std::endl;
+        }
     }
 }
 
