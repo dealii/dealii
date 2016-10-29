9588242b94fc463fb64f8b52a50d381734f82095
Denis Davydov <davydden@gmail.com>
2016-10-29 16:40:31 +0200

    add VectorTools::project() for scalar-valued quadrature data

---
+++ tests/numerics/project_parallel_qp_q_01.cc
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// check VectorTools::project_parallel() of quadrature data for
+// FE_Q on a mesh without hanging nodes.
+
+#include "project_parallel_qp_common.h"
+
+namespace LA
+{
+  using namespace ::LinearAlgebraTrilinos;
+}
+
+template <int dim>
+void test ()
+{
+  for (unsigned int p=1; p<7-dim; ++p)
+    test_no_hanging_nodes<LA::MPI::Vector> (FE_Q<dim>(p), p);
+}
+
+int main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
+
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.threshold_double(1.e-10);
+
+  test<2>();
+  test<3>();
+}
+
---
+++ tests/numerics/project_parallel_qpmf_q_01.cc
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// check VectorTools::project_parallel() for matrix-free quadrature data for
+// FE_Q on a mesh without hanging nodes.
+
+
+#include "project_parallel_qpmf_common.h"
+
+template <int dim>
+void test ()
+{
+  test_no_hanging_nodes<1, 2,dim> (FE_Q<dim>(1), 1);
+  test_no_hanging_nodes<2, 3,dim> (FE_Q<dim>(2), 2);
+  test_no_hanging_nodes<3, 4,dim> (FE_Q<dim>(3), 3);
+}
+
+int main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv,
+                                                       numbers::invalid_unsigned_int);
+
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.threshold_double(1.e-10);
+
+  test<2>();
+  test<3>();
+}
+
