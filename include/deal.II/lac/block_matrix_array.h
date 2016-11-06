28ee6f867887b49c6c423cd0cc3807350bd0a402
David Wells <wellsd2@rpi.edu>
2016-11-06 07:46:52 -0500

    Add (private) BlockMatrixArray::Entry::operator=.
    This class implements a (destructive) copy constructor more or less so
    that one can use it with std::vector::push_back, so it should disable
    operator=.
    
    This fixes a warning caught by PVS studio.

--- include/deal.II/lac/block_matrix_array.h
+++ include/deal.II/lac/block_matrix_array.h
@@ -334,6 +334,13 @@ private:
    * number of blocks per row.
    */
   unsigned int block_cols;
+
+  /**
+   * Assignment operator. Since the copy constructor is destructive (see its
+   * documentation) and only exists for convenience there is no reasonable way
+   * to implement this. Hence this operator is both private and unimplemented.
+   */
+  Entry &operator= (const Entry &);
 };
 
 /*@}*/
