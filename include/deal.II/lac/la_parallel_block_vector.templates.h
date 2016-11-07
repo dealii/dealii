f043283a7be4fac6e36d110c14b654118fed106d
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2016-11-07 12:48:37 +0100

    Correct LinearAlgebra::distributed::BlockVector::memory_consumption()

--- include/deal.II/lac/la_parallel_block_vector.templates.h
+++ include/deal.II/lac/la_parallel_block_vector.templates.h
@@ -759,11 +759,9 @@ namespace LinearAlgebra
     std::size_t
     BlockVector<Number>::memory_consumption () const
     {
-      std::size_t mem = sizeof(this->n_blocks());
-      for (size_type i=0; i<this->components.size(); ++i)
-        mem += MemoryConsumption::memory_consumption (this->components[i]);
-      mem += MemoryConsumption::memory_consumption (this->block_indices);
-      return mem;
+      return (MemoryConsumption::memory_consumption (this->block_indices)
+              +
+              MemoryConsumption::memory_consumption (this->components));
     }
 
   } // end of namespace distributed
