2b172e7b9d79a39e01343c4f77c49cfbd30d6bd6
David Wells <wellsd2@rpi.edu>
2017-01-12 10:23:53 -0500

    Write 'is analogous to', not 'is analogy to'.

--- include/deal.II/lac/trilinos_parallel_block_vector.h
+++ include/deal.II/lac/trilinos_parallel_block_vector.h
@@ -320,7 +320,7 @@ namespace TrilinosWrappers
        * the same number of blocks. If needed, the numbers of blocks should be
        * exchanged, too.
        *
-       * This function is analog to the the swap() function of all C++
+       * This function is analogous to the the swap() function of all C++
        * standard containers. Also, there is a global function swap(u,v) that
        * simply calls <tt>u.swap(v)</tt>, again in analogy to standard
        * functions.
