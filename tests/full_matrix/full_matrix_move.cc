f9ce9594dcaf8059669272445773ea24580c7ee3
danshapero <shapero.daniel@gmail.com>
2017-01-16 23:11:35 -0800

    Test for FullMatrix move operations

---
+++ tests/full_matrix/full_matrix_move.cc
@@ -0,0 +1,52 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// Check that FullMatrix objects can be move constructed and assigned
+
+#include "../tests.h"
+
+#include <deal.II/lac/full_matrix.h>
+
+int main()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+  deallog.threshold_double(1.0e-10);
+
+  size_t m = 2, n = 3;
+  FullMatrix<double> A(m, n);
+  for (size_t i = 0; i < m; ++i)
+    for (size_t j = 0; j < n; ++j)
+      A(i, j) = n * i + j;
+
+  deallog << "Size of A:" << std::endl
+          << A.m() << " " << A.n() << std::endl;
+
+  FullMatrix<double> B = std::move(A);
+
+  deallog << "Size of B:" << std::endl
+          << B.m() << " " << B.n() << std::endl;
+  deallog << "Size of A:" << std::endl
+          << A.m() << " " << A.n() << std::endl;
+
+  A = std::move(B);
+  deallog << "Size of B:" << std::endl
+          << B.m() << " " << B.n() << std::endl;
+  deallog << "Size of A:" << std::endl
+          << A.m() << " " << A.n() << std::endl;
+
+  return 0;
+}
