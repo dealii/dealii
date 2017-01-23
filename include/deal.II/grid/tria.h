f53c9e6a864410cfd896dc41081508700d285ff2
Timo Heister <timo.heister@gmail.com>
2017-01-22 22:13:01 -0500

    MSVC fixes
    - enable compilation of bundled boost with MSVC 2017rc1
    - remove static_assert that gets wrongly triggered during normal compilation

--- include/deal.II/grid/tria.h
+++ include/deal.II/grid/tria.h
@@ -3526,11 +3526,14 @@ private:
   template <typename>
   friend class dealii::internal::Triangulation::TriaObjects;
 
-  // explicitly check for sensible template arguments
+  // explicitly check for sensible template arguments, but not on windows
+  // because MSVC creates bogus warnings during normal compilation
 #ifdef DEAL_II_WITH_CXX11
+#ifndef DEAL_II_MSVC
   static_assert (dim<=spacedim,
                  "The dimension <dim> of a Triangulation must be less than or "
                  "equal to the space dimension <spacedim> in which it lives.");
+#endif
 #endif
 
 };
