fa9b6e81bc07513a49a14d74490b24e56d985620
Matthias Maier <tamiko@43-1.org>
2017-03-21 13:52:24 -0500

    Opencascade: Bugfix. Call Handle macro in a compatible fashion

--- source/opencascade/boundary_lib.cc
+++ source/opencascade/boundary_lib.cc
@@ -59,12 +59,12 @@ namespace OpenCASCADE
               (shape.ShapeType() == TopAbs_EDGE),
               ExcUnsupportedShape());
       if (shape.ShapeType() == TopAbs_WIRE)
-        return (Handle(BRepAdaptor_HCompCurve(new BRepAdaptor_HCompCurve(TopoDS::Wire(shape)))));
+        return Handle(BRepAdaptor_HCompCurve)(new BRepAdaptor_HCompCurve(TopoDS::Wire(shape)));
       else if (shape.ShapeType() == TopAbs_EDGE)
-        return (Handle(BRepAdaptor_HCurve(new BRepAdaptor_HCurve(TopoDS::Edge(shape)))));
+        return Handle(BRepAdaptor_HCurve)(new BRepAdaptor_HCurve(TopoDS::Edge(shape)));
 
       Assert(false, ExcInternalError());
-      return Handle(BRepAdaptor_HCurve(new BRepAdaptor_HCurve()));
+      return Handle(BRepAdaptor_HCurve)(new BRepAdaptor_HCurve());
     }
 
 
