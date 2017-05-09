2221ee63b65fa1e14bc1473faf40b3aa996173f3
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-05-09 16:47:45 -0400

    add tests.

---
+++ tests/lac/la_vector_all_zero.cc
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// Check the function all_zero
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/lac/la_vector.h>
+
+
+template <typename Number>
+void check_all_zero()
+{
+  LinearAlgebra::Vector<Number> v(10);
+
+  AssertThrow(v.all_zero() == true, ExcInternalError());
+
+  v[0] = 1.;
+  AssertThrow(v.all_zero() == false, ExcInternalError());
+}
+
+int main()
+{
+  std::ofstream logfile("output");
+  deallog.attach(logfile);
+
+  check_all_zero<float>();
+  check_all_zero<double>();
+
+  deallog << "OK" << std::endl;
+}
