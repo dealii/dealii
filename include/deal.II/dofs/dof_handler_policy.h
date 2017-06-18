bdf3e4032886c018da7b606d0cb2073190afd403
Wolfgang Bangerth <bangerth@colostate.edu>
2017-06-17 18:39:47 -0600

    Remove an empty 'private:' section.

--- include/deal.II/dofs/dof_handler_policy.h
+++ include/deal.II/dofs/dof_handler_policy.h
@@ -176,8 +176,6 @@ namespace internal
         renumber_dofs (const std::vector<types::global_dof_index>  &new_numbers,
                        dealii::DoFHandler<dim,spacedim> &dof_handler,
                        NumberCache &number_cache) const;
-      private:
-
       };
 
 
