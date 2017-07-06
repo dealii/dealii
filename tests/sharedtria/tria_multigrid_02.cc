79fb210aae3b1037a266383c431f93089ac2ccfb
Wolfgang Bangerth <bangerth@colostate.edu>
2017-07-06 14:09:05 -0600

    Remove unnecessary #include files.

--- tests/sharedtria/tria_multigrid_02.cc
+++ tests/sharedtria/tria_multigrid_02.cc
@@ -26,9 +26,6 @@
 #include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_out.h>
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/numerics/data_out.h>
 
 #include <fstream>
 
