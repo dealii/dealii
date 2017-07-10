7f424d86a84267e383740c4c12f35d3b99444876
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2017-07-10 21:37:01 +0200

    Implement hp_line_dof_identities in 2D and hp_quad_dof_identities in 3D for FE_FaceQ.

--- include/deal.II/fe/fe_face.h
+++ include/deal.II/fe/fe_face.h
@@ -262,6 +262,50 @@ public:
   virtual bool hp_constraints_are_implemented () const;
 
   /**
+   * If, on a vertex, several finite elements are active, the hp code first
+   * assigns the degrees of freedom of each of these FEs different global
+   * indices. It then calls this function to find out which of them should get
+   * identical values, and consequently can receive the same global DoF index.
+   * This function therefore returns a list of identities between DoFs of the
+   * present finite element object with the DoFs of @p fe_other, which is a
+   * reference to a finite element object representing one of the other finite
+   * elements active on this particular vertex. The function computes which of
+   * the degrees of freedom of the two finite element objects are equivalent,
+   * both numbered between zero and the corresponding value of dofs_per_vertex
+   * of the two finite elements. The first index of each pair denotes one of
+   * the vertex dofs of the present element, whereas the second is the
+   * corresponding index of the other finite element.
+   *
+   * This being a discontinuous element, the set of such constraints is of
+   * course empty.
+   */
+  virtual
+  std::vector<std::pair<unsigned int, unsigned int> >
+  hp_vertex_dof_identities (const FiniteElement<1, spacedim> &fe_other) const;
+
+  /**
+   * Same as hp_vertex_dof_indices(), except that the function treats degrees
+   * of freedom on lines.
+   *
+   * This being a discontinuous element, the set of such constraints is of
+   * course empty.
+   */
+  virtual
+  std::vector<std::pair<unsigned int, unsigned int> >
+  hp_line_dof_identities (const FiniteElement<1, spacedim> &fe_other) const;
+
+  /**
+   * Same as hp_vertex_dof_indices(), except that the function treats degrees
+   * of freedom on quads.
+   *
+   * This being a discontinuous element, the set of such constraints is of
+   * course empty.
+   */
+  virtual
+  std::vector<std::pair<unsigned int, unsigned int> >
+  hp_quad_dof_identities (const FiniteElement<1, spacedim> &fe_other) const;
+
+  /**
    * Return whether this element dominates the one given as argument when they
    * meet at a common face, whether it is the other way around, whether
    * neither dominates, or if either could dominate.
---
+++ tests/hp/hp_quad_dof_identities_faceq.cc
@@ -0,0 +1,72 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// check FE_FaceQ::hp_quad_dof_identities
+
+
+#include "../tests.h"
+#include <deal.II/base/logstream.h>
+#include <deal.II/hp/fe_collection.h>
+#include <deal.II/fe/fe_face.h>
+
+#include <fstream>
+
+
+template <int dim>
+void test ()
+{
+  hp::FECollection<dim> fe_collection;
+  for (unsigned int i=1; i<8-dim; ++i)
+    fe_collection.push_back (FE_FaceQ<dim>(i));
+
+  for (unsigned int i=0; i<fe_collection.size(); ++i)
+    for (unsigned int j=0; j<fe_collection.size(); ++j)
+      {
+        const std::vector<std::pair<unsigned int, unsigned int> >
+        identities = fe_collection[i].hp_quad_dof_identities (fe_collection[j]);
+
+        deallog << "Identities for "
+                << fe_collection[i].get_name() << " and "
+                << fe_collection[j].get_name() << ": "
+                << identities.size()
+                << std::endl;
+
+        for (unsigned int k=0; k<identities.size(); ++k)
+          {
+            Assert (identities[k].first < fe_collection[i].dofs_per_quad,
+                    ExcInternalError());
+            Assert (identities[k].second < fe_collection[j].dofs_per_quad,
+                    ExcInternalError());
+
+            deallog << identities[k].first << ' '
+                    << identities[k].second
+                    << std::endl;
+          }
+      }
+}
+
+
+
+int main ()
+{
+  initlog();
+
+  test<2> ();
+  test<3> ();
+
+  deallog << "OK" << std::endl;
+}
