e050bd80648eae12c3b7d6eff3bc0a55814c3798
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2017-07-20 17:14:16 +0200

    Add new test

---
+++ tests/mpi/mg_ghost_dofs_periodic_07.cc
@@ -0,0 +1,89 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// this tests building the parallel distributed triangulation with multigrid
+// hierarchy and distribute_mg_dofs on a mesh with periodic boundary
+// conditions. we used to miss some vertices in periodicity connection
+// mechanism before July 2017 that lead to a deadlock on 43 cores and upwards
+// in 3D when the locally owned layer was sufficiently far away from the other
+// end of the computational domain and no cells were owned.
+
+#include "../tests.h"
+
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/utilities.h>
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_tools.h>
+
+#include <deal.II/distributed/tria.h>
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_q.h>
+
+
+
+template <int dim>
+void test()
+{
+  const unsigned int myid = Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
+
+  parallel::distributed::Triangulation<dim>
+  tria(MPI_COMM_WORLD, dealii::Triangulation<dim>::limit_level_difference_at_vertices,
+       parallel::distributed::Triangulation<dim>::construct_multigrid_hierarchy);
+  GridGenerator::hyper_cube (tria, 0, 1);
+
+  // set periodic boundary conditions in all directions
+  for (typename Triangulation<dim>::cell_iterator cell=tria.begin();
+       cell != tria.end(); ++cell)
+    for (unsigned int f=0; f<GeometryInfo<dim>::faces_per_cell; ++f)
+      if (cell->at_boundary(f))
+        cell->face(f)->set_all_boundary_ids(f);
+
+  std::vector<GridTools::PeriodicFacePair<typename Triangulation<dim>::cell_iterator> > periodic_faces;
+  for (unsigned int d=0; d<dim; ++d)
+    GridTools::collect_periodic_faces(tria, 2*d, 2*d+1, d, periodic_faces);
+  tria.add_periodicity(periodic_faces);
+  tria.refine_global(3);
+
+  for (typename Triangulation<dim>::cell_iterator cell=tria.begin();
+       cell != tria.end(); ++cell)
+    deallog << cell->id().to_string() << " " << cell->level_subdomain_id() << std::endl;
+
+  FE_DGQ<dim> fe1(1);
+  FE_Q<dim> fe2(1);
+  DoFHandler<dim> dof_handler(tria);
+  dof_handler.distribute_dofs(fe1);
+  dof_handler.distribute_mg_dofs(fe1);
+  dof_handler.distribute_dofs(fe2);
+  dof_handler.distribute_mg_dofs(fe2);
+  deallog << "OK" << std::endl;
+}
+
+
+
+int main (int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
+  MPILogInitAll log;
+
+  test<2>();
+  test<3>();
+
+  return 0;
+}
