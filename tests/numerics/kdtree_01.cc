0f5953911a47328d9c7e75f7a56ae35ca17ef567
Luca Heltai <luca.heltai@sissa.it>
2017-08-13 18:40:36 -0600

    Removed reference to deal3lkit, fixed KDTree names.

--- tests/numerics/kdtree_01.cc
+++ tests/numerics/kdtree_01.cc
@@ -9,7 +9,7 @@
 // Public License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
 // The full text of the license can be found in the file LICENSE at
-// the top level of the deal2lkit distribution.
+// the top level of the deal.II distribution.
 //
 //---------------------------------------------------------------------
 
@@ -24,7 +24,7 @@ int main ()
 {
   initlog();
 
-  KDTreeDistance<2> kdtree;
+  KDTree<2> kdtree;
 
   std::vector<Point<2> > points;
 
