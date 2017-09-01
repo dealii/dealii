93e4734a0ac238ae1df0dd952cd6fa3faec480a1
Matthias Maier <tamiko@43-1.org>
2017-08-31 22:09:55 -0500

    Add an interpolation test for FE_Nedelec

---
+++ tests/fe/interpolate_nedelec.cc
@@ -0,0 +1,70 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+#include "interpolate_common.h"
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/fe/fe_nedelec.h>
+
+
+// FE_Nedelec<dim>::interpolate(...)
+
+template <int dim>
+void check1(const Function<dim> &f,
+            const unsigned int degree)
+{
+  FE_Nedelec<dim> fe(degree);
+  deallog << fe.get_name() << ' ';
+  deallog << fe.get_generalized_support_points().size() << ' ';
+
+  std::vector<double> dofs(fe.dofs_per_cell);
+
+  std::vector<Vector<double> > values (fe.get_generalized_support_points().size(),
+                                       Vector<double>(dim));
+  f.vector_value_list(fe.get_generalized_support_points(), values);
+  fe.convert_generalized_support_point_values_to_dof_values(values, dofs);
+  deallog << " vector " << vector_difference(fe,dofs,f,0) << std::endl;
+}
+
+int main()
+{
+  std::ofstream logfile ("output");
+  deallog.attach(logfile);
+
+  {
+    Q1WedgeFunction<2, 0, 2> w20;
+    check1(w20, 0);
+    check1(w20, 1);
+    check1(w20, 2);
+    Q1WedgeFunction<2, 1, 2> w21;
+    check1(w21, 2);
+    Q1WedgeFunction<2, 2, 2> w22;
+    check1(w22, 2);
+    Q1WedgeFunction<2, 3, 2> w23;
+    check1(w23, 3);
+  }
+
+  {
+    Q1WedgeFunction<3, 1, 3> w21;
+    check1(w21, 1);
+    check1(w21, 2);
+    // FIXME - higher order interpolation is currently broken
+    // Q1WedgeFunction<3, 2, 3> w22;
+    // check1(w22, 2);
+    // Q1WedgeFunction<3, 3, 3> w23;
+    // check1(w23, 3);
+  }
+}
