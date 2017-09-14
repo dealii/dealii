a28e7ad4eab65bf229a66d76746c68f1b58d673d
Wolfgang Bangerth <bangerth@colostate.edu>
2017-09-14 16:04:59 -0600

    Add a test.

---
+++ tests/fe/system_04.cc
@@ -0,0 +1,67 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2003 - 2015 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// like the _03 test, but use the variadic constructor with the
+// promotion rule that converts individual elements into pairs of the
+// element and multiplicity one
+
+#include "../tests.h"
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_dgp.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_tools.h>
+
+#include <string>
+
+template <int dim>
+void
+check(FESystem<dim> &fe)
+{
+  deallog << fe.get_name() << std::endl;
+  deallog << "components: " << fe.n_components() << std::endl;
+  deallog << "blocks: " << fe.n_blocks() << std::endl;
+  deallog << "conforms H1: " << fe.conforms(FiniteElementData<dim>::H1) << std::endl;
+  deallog << "n_base_elements: " << fe.n_base_elements() << std::endl;
+}
+
+int
+main()
+{
+  initlog();
+
+  {
+    FESystem<2> fe (FE_Q<2>(1)^2, FE_Q<2>(1));
+    check<2>(fe);
+  }
+  {
+    FESystem<2> fe (FE_Q<2>(1)^2, FE_DGQ<2>(2)^0, FE_Q<2>(1));
+    check<2>(fe);
+  }
+  {
+    FESystem<2> fe (FESystem<2>(FE_Q<2>(1)^2), FE_Q<2>(1));
+    check<2>(fe);
+  }
+  {
+    FESystem<2> fe (FESystem<2>(FE_Q<2>(1)^2)^1, FE_DGQ<2>(2)^0, FE_Q<2>(1));
+    check<2>(fe);
+  }
+
+  return 0;
+}
+
+
+
