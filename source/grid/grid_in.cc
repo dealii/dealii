912fef07bba85c12bb65a33b926a72feb1125dc8
Luca Heltai <luca.heltai@sissa.it>
2017-09-16 16:24:53 +0200

    GridIn::read_assimp + test.

--- source/grid/grid_in.cc
+++ source/grid/grid_in.cc
@@ -34,6 +34,30 @@
 #include <netcdfcpp.h>
 #endif
 
+#ifdef DEAL_II_WITH_ASSIMP
+DEAL_II_DISABLE_EXTRA_DIAGNOSTICS
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress-of-packed-member"
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+#pragma GCC diagnostic pop
+DEAL_II_ENABLE_EXTRA_DIAGNOSTICS
+
+#undef AI_CONFIG_PP_RVC_FLAGS
+#define AI_CONFIG_PP_RVC_FLAGS           \
+  aiComponent_NORMALS |            \
+  aiComponent_TANGENTS_AND_BITANGENTS |        \
+  aiComponent_COLORS    |        \
+  aiComponent_TEXCOORDS   |        \
+  aiComponent_BONEWEIGHTS |        \
+  aiComponent_ANIMATIONS |           \
+  aiComponent_TEXTURES  |          \
+  aiComponent_LIGHTS |             \
+  aiComponent_CAMERAS |            \
+  aiComponent_MATERIALS
+#endif
+
 
 DEAL_II_NAMESPACE_OPEN
 
@@ -2542,6 +2566,153 @@ void GridIn<dim, spacedim>::read_tecplot(std::istream &)
 }
 
 
+
+template <int dim, int spacedim>
+void GridIn<dim, spacedim>::read_assimp(const std::string &filename,
+                                        const unsigned int mesh_index,
+                                        const bool remove_duplicates,
+                                        const double tol,
+                                        const bool ignore_unsupported_types)
+{
+  // Only good for surface grids.
+  AssertThrow(dim<3, ExcImpossibleInDim(dim));
+
+  // Create an istance of the Importer class
+  Assimp::Importer importer;
+
+  // And have it read the given file with some  postprocessing
+  const aiScene *scene = importer.ReadFile( filename.c_str(),
+                                            aiProcess_RemoveComponent   |
+                                            aiProcess_JoinIdenticalVertices   |
+                                            aiProcess_ImproveCacheLocality  |
+                                            aiProcess_SortByPType   |
+                                            aiProcess_OptimizeGraph   |
+                                            aiProcess_OptimizeMeshes);
+
+  // If the import failed, report it
+  AssertThrow(scene, ExcMessage(importer.GetErrorString()))
+
+  AssertThrow(scene->mNumMeshes, ExcMessage("Input file contains no meshes."));
+
+  AssertThrow((mesh_index == numbers::invalid_unsigned_int) ||
+              (mesh_index < scene->mNumMeshes),
+              ExcMessage("Too few meshes in the file."));
+
+  int start_mesh = (mesh_index == numbers::invalid_unsigned_int ?
+                    0 : mesh_index);
+  int end_mesh = (mesh_index == numbers::invalid_unsigned_int ?
+                  scene->mNumMeshes : mesh_index+1);
+
+  // Deal.II objects are created empty, and then filled with imported file.
+  std::vector<Point<spacedim> > vertices;
+  std::vector<CellData<dim> > cells;
+  SubCellData subcelldata;
+
+  // A series of counters to merge cells.
+  unsigned int v_offset=0;
+  unsigned int c_offset=0;
+
+  // The index of the mesh will be used as a material index.
+  for (int m=start_mesh; m<end_mesh; ++m)
+    {
+      const aiMesh *mesh = scene->mMeshes[m];
+
+      // Check that we know what to do with this mesh, otherwise just
+      // ignore it
+      if ( (dim == 2) && mesh->mPrimitiveTypes != aiPrimitiveType_POLYGON)
+        {
+          std::stringstream s;
+          s << "Incompatible mesh "  << m
+            << "/" << scene->mNumMeshes;
+          AssertThrow(ignore_unsupported_types, ExcMessage(s.str()));
+          continue;
+        }
+      else if ( (dim == 1) && mesh->mPrimitiveTypes != aiPrimitiveType_LINE)
+        {
+          std::stringstream s;
+          s << "Incompatible mesh "  << m
+            << "/" << scene->mNumMeshes;
+          AssertThrow(ignore_unsupported_types, ExcMessage(s.str()));
+          continue;
+        }
+      // Vertices
+      const unsigned int n_vertices = mesh->mNumVertices;
+      const aiVector3D *mVertices = mesh->mVertices;
+
+      // Faces
+      const unsigned int n_faces = mesh->mNumFaces;
+      const aiFace *mFaces = mesh->mFaces;
+
+      vertices.resize(v_offset+n_vertices);
+      cells.resize(c_offset+n_faces);
+
+      for (unsigned int i=0; i<n_vertices; ++i)
+        for (unsigned int d=0; d<spacedim; ++d)
+          vertices[i+v_offset][d] = mVertices[i][d];
+
+      unsigned int valid_cell = c_offset;
+      for (unsigned int i=0; i<n_faces; ++i)
+        {
+          if (mFaces[i].mNumIndices == GeometryInfo<dim>::vertices_per_cell)
+            {
+              for (unsigned int f=0; f<GeometryInfo<dim>::vertices_per_cell; ++f)
+                {
+                  cells[valid_cell].vertices[f] = mFaces[i].mIndices[f]+v_offset;
+                }
+              cells[valid_cell].material_id = (types::material_id) m;
+              ++valid_cell;
+            }
+          else
+            {
+              std::stringstream s;
+              s << "Face " << i << " of mesh "
+                << m << " has " << mFaces[i].mNumIndices
+                << " vertices. We expected only "
+                << GeometryInfo<dim>::vertices_per_cell;
+              AssertThrow(ignore_unsupported_types, ExcMessage(s.str()));
+            }
+        }
+      cells.resize(valid_cell);
+
+      // The vertices are added all at once. Cells are check for
+      // validity, so only valid_cells are now present in the deal.II
+      // list of cells.
+      v_offset += n_vertices;
+      c_offset = valid_cell;
+    }
+
+  // No cells were read
+  if (cells.size() == 0)
+    return;
+
+  if (remove_duplicates)
+    {
+      // The function delete_duplicated_vertices() needs to be called more
+      // than once if a vertex is duplicated more than once. So we keep
+      // calling it until the number of vertices does not change any more.
+      unsigned int n_verts = 0;
+      while (n_verts != vertices.size())
+        {
+          n_verts = vertices.size();
+          std::vector<unsigned int> considered_vertices;
+          GridTools::delete_duplicated_vertices(vertices, cells, subcelldata,
+                                                considered_vertices, tol);
+        }
+    }
+
+  GridTools::delete_unused_vertices(vertices, cells, subcelldata);
+  if (dim == spacedim)
+    GridReordering<dim, spacedim>::invert_all_cells_of_negative_grid
+    (vertices,cells);
+
+  GridReordering<dim, spacedim>::reorder_cells(cells);
+  if (dim == 2)
+    tria->create_triangulation_compatibility(vertices, cells, subcelldata);
+  else
+    tria->create_triangulation(vertices, cells, subcelldata);
+}
+
+
 template <int dim, int spacedim>
 void GridIn<dim, spacedim>::skip_empty_lines (std::istream &in)
 {
@@ -2820,12 +2991,18 @@ void GridIn<dim, spacedim>::read (std::istream &in,
 
     case netcdf:
       Assert(false, ExcMessage("There is no read_netcdf(istream &) function. "
-                               "Use the read(_netcdf)(string &filename) "
+                               "Use the read_netcdf)(string &filename) "
                                "functions, instead."));
       return;
 
     case tecplot:
       read_tecplot (in);
+      return;
+
+    case assimp:
+      Assert(false, ExcMessage("There is no read_assimp(istream &) function. "
+                               "Use the read_netcdf)(string &filename, ...) "
+                               "functions, instead."));
       return;
 
     case Default:
@@ -2928,7 +3105,7 @@ GridIn<dim, spacedim>::parse_format (const std::string &format_name)
 template <int dim, int spacedim>
 std::string GridIn<dim, spacedim>::get_format_names ()
 {
-  return "dbmesh|msh|unv|vtk|ucd|abaqus|xda|netcdf|tecplot";
+  return "dbmesh|msh|unv|vtk|ucd|abaqus|xda|netcdf|tecplot|assimp";
 }
 
 namespace
---
+++ tests/grid/grid_in_assimp_01.cc
@@ -0,0 +1,33 @@
+//-----------------------------------------------------------
+//
+//    Copyright (C) 2017 by the deal.II authors
+//
+//    This file is part of the deal.II library.
+//
+//    The deal.II library is free software; you can use it, redistribute
+//    it, and/or modify it under the terms of the GNU Lesser General
+//    Public License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//    The full text of the license can be found in the file LICENSE at
+//    the top level of the deal.II distribution.
+//
+//-----------------------------------------------------------
+
+#include "../tests.h"
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
+
+
+int main ()
+{
+  initlog();
+  Triangulation<2,3> tria;
+  GridIn<2,3> gridin;
+  gridin.attach_triangulation(tria);
+  gridin.read_assimp(SOURCE_DIR "/grids/torus.obj", -1, true, 1e-3);
+
+  GridOut go;
+  go.write_msh(tria, deallog.get_file_stream());
+  return 0;
+}
