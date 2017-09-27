43833204e892bd78943a498f8a389d2efe02056f
Luca Heltai <luca.heltai@sissa.it>
2017-09-27 14:41:07 +0200

    deal2lkit -> deal.II, 2015 -> 2017

--- tests/sundials/harmonic_oscillator_05.cc
+++ tests/sundials/harmonic_oscillator_05.cc
@@ -1,15 +1,15 @@
 //-----------------------------------------------------------
 //
-//    Copyright (C) 2015 by the deal2lkit authors
+//    Copyright (C) 2017 by the deal.II authors
 //
-//    This file is part of the deal2lkit library.
+//    This file is part of the deal.II library.
 //
-//    The deal2lkit library is free software; you can use it, redistribute
+//    The deal.II library is free software; you can use it, redistribute
 //    it, and/or modify it under the terms of the GNU Lesser General
 //    Public License as published by the Free Software Foundation; either
 //    version 2.1 of the License, or (at your option) any later version.
 //    The full text of the license can be found in the file LICENSE at
-//    the top level of the deal2lkit distribution.
+//    the top level of the deal.II distribution.
 //
 //-----------------------------------------------------------
 
