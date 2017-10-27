ad1cab2f4fdb90c6e87bb60646187c4283db26a8
Wolfgang Bangerth <bangerth@colostate.edu>
2017-10-27 16:02:03 -0600

    Use appropriate data types to allow complex-valued vectors.

--- source/multigrid/mg_base.cc
+++ source/multigrid/mg_base.cc
@@ -33,7 +33,7 @@ MGSmootherBase<VectorType>::apply (const unsigned int level,
                                    VectorType         &u,
                                    const VectorType   &rhs) const
 {
-  u = 0;
+  u = typename VectorType::value_type(0.);
   smooth(level, u, rhs);
 }
 
