ac4e1f799df8b0fd4fe44f3c3defa3625c19daaa
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2017-10-29 11:14:19 +0100

    Fix Patterns::Tuple::description

---
+++ tests/base/patterns_12.cc
@@ -0,0 +1,60 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2005 - 2015 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// Check that the description of a patterns works for all OutputStyles
+// and number of PatternStyles.
+
+#include "../tests.h"
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/std_cxx14/memory.h>
+#include <memory>
+
+int main()
+{
+  initlog();
+
+  // one pattern
+  {
+    const auto &pattern = Patterns::Tuple(Patterns::Double());
+
+    deallog << pattern.description(Patterns::PatternBase::OutputStyle::Machine) << '\n'
+            << pattern.description(Patterns::PatternBase::OutputStyle::Text)    << '\n'
+            << pattern.description(Patterns::PatternBase::OutputStyle::LaTeX)   << '\n'
+            << std::endl;
+  }
+
+  // two patterns
+  {
+    const auto &pattern = Patterns::Tuple(Patterns::Double(),
+                                          Patterns::Anything());
+
+    deallog << pattern.description(Patterns::PatternBase::OutputStyle::Machine) << '\n'
+            << pattern.description(Patterns::PatternBase::OutputStyle::Text)    << '\n'
+            << pattern.description(Patterns::PatternBase::OutputStyle::LaTeX)   << '\n'
+            << std::endl;
+  }
+
+  // three patterns
+  {
+    const auto &pattern = Patterns::Tuple(Patterns::Double(),
+                                          Patterns::Anything(),
+                                          Patterns::Bool());
+
+    deallog << pattern.description(Patterns::PatternBase::OutputStyle::Machine) << '\n'
+            << pattern.description(Patterns::PatternBase::OutputStyle::Text)    << '\n'
+            << pattern.description(Patterns::PatternBase::OutputStyle::LaTeX)   << '\n'
+            << std::endl;
+  }
+}
