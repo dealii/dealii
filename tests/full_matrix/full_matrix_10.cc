c29e5849d8e0b22cbfb840459c9976b3877a24ea
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2017-11-05 18:33:40 +0100

    Filter away small numbers to avoid roundoff errors for floats.

--- tests/full_matrix/full_matrix_10.cc
+++ tests/full_matrix/full_matrix_10.cc
@@ -41,7 +41,7 @@ void test()
         D(i,j) += A(i,k) * B(j,k);
   C.add(-1., D);
 
-  deallog << "Difference: " << (double)C.l1_norm() << std::endl;
+  deallog << "Difference: " << filter_out_small_numbers(C.l1_norm(), std::numeric_limits<Number>::epsilon()*50.) << std::endl;
 }
 
 int main()
