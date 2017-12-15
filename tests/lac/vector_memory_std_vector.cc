1b02732d081a6f2519a46fa8c1ea7bace6e91a9f
David Wells <wellsd2@rpi.edu>
2017-12-15 17:27:59 -0500

    Add a test for VectorMemory<std::vector<T>>.

---
+++ tests/lac/vector_memory_std_vector.cc
@@ -0,0 +1,30 @@
+#include <deal.II/lac/vector_memory.h>
+#include <deal.II/lac/vector_memory.templates.h>
+
+#include <vector>
+
+#include "../tests.h"
+
+// Check that we can create VectorMemory objects with vector-like classes in
+// the standard library.
+
+template <typename VectorType>
+void test_std_vector_pointer()
+{
+  GrowingVectorMemory<VectorType> mem;
+
+  std::vector<typename VectorMemory<VectorType>::Pointer> va;
+  va.push_back(typename VectorMemory<VectorType>::Pointer(mem));
+  va.emplace_back(mem);
+}
+
+
+
+int main()
+{
+  initlog();
+  PrimitiveVectorMemory<std::vector<double>> primitive_memory;
+  test_std_vector_pointer<std::vector<double>>();
+
+  deallog << "OK" << std::endl;
+}
