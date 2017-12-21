3d21f333a6165916afa1b34e9f9c47a91fcbf6b5
Luca Heltai <luca.heltai@sissa.it>
2017-12-21 17:55:59 +0100

    Made old tests include simplex.h

--- tests/codim_one/integrate_one_over_r_telles_middle.cc
+++ tests/codim_one/integrate_one_over_r_telles_middle.cc
@@ -23,6 +23,7 @@
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/geometry_info.h>
 #include <deal.II/fe/fe_q.h>
+#include "../base/simplex.h"
 
 #include <string>
 
@@ -31,10 +32,6 @@ using namespace dealii;
 
 // We test the integration of singular kernels with a singularity of kind 1/R
 // We multiply this function with a polynomial up to degree 6.
-
-double
-exact_integral_one_over_r_middle (
-  const unsigned int i, const unsigned int j);
 
 int
 main ()
@@ -101,60 +98,4 @@ main ()
             }
         }
     }
-}
-
-double exact_integral_one_over_r_middle(const unsigned int i,
-                                        const unsigned int j)
-{
-  Assert(i<6, ExcNotImplemented());
-  Assert(j<6, ExcNotImplemented());
-
-// The integrals are computed using the following Mathematica snippet of
-// code:
-//
-// x0 = 0.5
-// y0 = 0.5
-// Do[Do[Print["v[0][", n, "][", m, "]=",
-//    NumberForm[
-//     NIntegrate[
-//      x^n*y^m/Sqrt[(x - x0)^2 + (y - y0)^2], {x, 0, 1}, {y, 0, 1},
-//      MaxRecursion -> 10000, PrecisionGoal -> 9], 9], ";"], {n, 0,
-//    4}], {m, 0, 4}]
-
-
-  static double v[1][6][6] =
-  {
-    {
-      { 0}
-    }
-  };
-  if (v[0][0][0] == 0)
-    {
-      v[0][0][0] = 3.52549435;;
-      v[0][1][0] = 1.76274717;
-      v[0][2][0]=1.07267252;
-      v[0][3][0]=0.727635187;
-      v[0][4][0]=0.53316959;
-      v[0][0][1]=1.76274717;
-      v[0][1][1]=0.881373587;
-      v[0][2][1]=0.536336258;
-      v[0][3][1]=0.363817594;
-      v[0][4][1]=0.266584795;
-      v[0][0][2]=1.07267252;
-      v[0][1][2]=0.536336258;
-      v[0][2][2]=0.329313861;
-      v[0][3][2]=0.225802662;
-      v[0][4][2]=0.167105787;
-      v[0][0][3]=0.727635187;
-      v[0][1][3]=0.363817594;
-      v[0][2][3]=0.225802662;
-      v[0][3][3]=0.156795196;
-      v[0][4][3]=0.117366283;
-      v[0][0][4]=0.53316959;
-      v[0][1][4]=0.266584795;
-      v[0][2][4]=0.167105787;
-      v[0][3][4]=0.117366283;
-      v[0][4][4]=0.0887410133;
-    }
-  return v[0][i][j];
 }
