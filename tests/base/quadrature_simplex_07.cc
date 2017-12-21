fa366c2e88d54a6dbc8016c61be38356da756813
Luca Heltai <luca.heltai@sissa.it>
2017-12-21 17:49:09 +0100

    Added tests with singular point in the middle.

---
+++ tests/base/quadrature_simplex_07.cc
@@ -0,0 +1,78 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// integrates the function *f(x,y)/R, where f(x,y) is a power of x and
+// y on the set [0,1]x[0,1]. dim = 2 only.
+
+#include "../tests.h"
+#include <deal.II/base/utilities.h>
+
+// all include files needed for the program
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/geometry_info.h>
+#include "simplex.h"
+
+
+int main()
+{
+  initlog();
+
+  deallog << std::endl
+          << "Calculation of the integral of f(x,y)*1/R on [0,1]x[0,1]" << std::endl
+          << "for f(x,y) = x^i y^j, with i,j ranging from 0 to 5, and R being" << std::endl
+          << "the distance from (x,y) to [0.5,0.5]." << std::endl
+          << std::endl;
+
+  double eps = 1e-10;
+
+  //           m  i  j
+  double error[5][6][6] = {{{0}}};
+
+  for (unsigned int m=0; m<5; ++m)
+    {
+      auto split_point = Point<2>(.5, .5);
+
+      QSplit<2> quad(QTrianglePolar(m+1), split_point);
+
+      for (unsigned int i=0; i<6; ++i)
+        for (unsigned int j=0; j<6; ++j)
+          {
+            double exact_integral  = exact_integral_one_over_r_middle(i,j);
+            double approx_integral = 0;
+
+            for (unsigned int q=0; q< quad.size(); ++q)
+              {
+                double x = quad.point(q)[0];
+                double y = quad.point(q)[1];
+                approx_integral += ( pow(x, (double)i) *
+                                     pow(y, (double)j) *
+                                     quad.weight(q) /
+                                     (quad.point(q)-split_point).norm());
+              }
+            error[m][i][j] = approx_integral - exact_integral;
+          }
+    }
+
+  for (unsigned int i=0; i<6; ++i)
+    for (unsigned int j=0; j<6; ++j)
+      {
+        deallog << "======= f(x,y) = x^" << i
+                << " y^" << j << std::endl;
+
+        for (unsigned int m=0; m<5; ++m)
+          deallog << "Order[" << m + 1 << "], error = "
+                  << error[m][i][j]  << std::endl;
+      }
+}
--- tests/base/simplex.h
+++ tests/base/simplex.h
@@ -236,4 +236,57 @@ double exact_integral_one_over_r(const unsigned int vertex_index,
 }
 
 
+
+double exact_integral_one_over_r_middle(const unsigned int i,
+                                        const unsigned int j)
+{
+  Assert(i<6, ExcNotImplemented());
+  Assert(j<6, ExcNotImplemented());
+
+// The integrals are computed using the following Mathematica snippet of
+// code:
+//
+// x0 = 0.5
+// y0 = 0.5
+// Do[Do[Print["v[", n, "][", m, "]=",
+//    NumberForm[
+//     NIntegrate[
+//      x^n*y^m/Sqrt[(x - x0)^2 + (y - y0)^2], {x, 0, 1}, {y, 0, 1},
+//      MaxRecursion -> 10000, PrecisionGoal -> 9], 9], ";"], {n, 0,
+//    4}], {m, 0, 4}]
+
+
+  static double v[6][6] = {{0}};
+
+  if (v[0][0] == 0)
+    {
+      v[0][0] = 3.52549435;;
+      v[1][0] = 1.76274717;
+      v[2][0]=1.07267252;
+      v[3][0]=0.727635187;
+      v[4][0]=0.53316959;
+      v[0][1]=1.76274717;
+      v[1][1]=0.881373587;
+      v[2][1]=0.536336258;
+      v[3][1]=0.363817594;
+      v[4][1]=0.266584795;
+      v[0][2]=1.07267252;
+      v[1][2]=0.536336258;
+      v[2][2]=0.329313861;
+      v[3][2]=0.225802662;
+      v[4][2]=0.167105787;
+      v[0][3]=0.727635187;
+      v[1][3]=0.363817594;
+      v[2][3]=0.225802662;
+      v[3][3]=0.156795196;
+      v[4][3]=0.117366283;
+      v[0][4]=0.53316959;
+      v[1][4]=0.266584795;
+      v[2][4]=0.167105787;
+      v[3][4]=0.117366283;
+      v[4][4]=0.0887410133;
+    }
+  return v[i][j];
+}
+
 #endif
