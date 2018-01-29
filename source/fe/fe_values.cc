aab5546309c93abfe6bf2334de991a99a3ab6634
David Wells <wellsd2@rpi.edu>
2018-01-28 22:59:04 -0500

    Split fe_values.cc into more instantiation files.
    This is currently the most expensive file to compile: with GCC 7.2 and all
    libraries but adol-c installed, this file requires 4.5 GB and 170s to (just for
    one of two instantiation files). This commit adds four additional instantiation
    files to lower the memory requirements below 3 GB per translation unit.
    
    Here is some profiling information:
    
    debug, master:
    165s, 4 GB
    170s, 4.5 GB
    (335 s total)
    
    release, master:
    182s, 3.6 GB
    194s, 3.9 GB
    (376s total)
    
    debug, feature:
    37s, 1.7 GB
    63s, 2.6 GB
    31s, 1.5 GB
    87s, 2.7 GB
    58s, 2.6 GB
    79s, 2.7 GB
    (355s total)
    
    release, feature:
     37s, 1.5 GB
     71s, 2.4 GB
     32s, 1.5 GB
    103s, 2.6 GB
     69s, 2.3 GB
     96s, 2.6 GB
    (408s total)

--- source/fe/fe_values.cc
+++ source/fe/fe_values.cc
@@ -4579,7 +4579,7 @@ void FESubfaceValues<dim,spacedim>::do_reinit (const unsigned int face_no,
 
 
 /*------------------------------- Explicit Instantiations -------------*/
-#define SPLIT_INSTANTIATIONS_COUNT 2
+#define SPLIT_INSTANTIATIONS_COUNT 6
 #ifndef SPLIT_INSTANTIATIONS_INDEX
 #define SPLIT_INSTANTIATIONS_INDEX 0
 #endif
