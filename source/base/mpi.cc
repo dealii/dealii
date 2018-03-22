0934bfa99fa900d1e0417e014805fbe308ef642e
Denis Davydov <davydden@gmail.com>
2018-03-22 11:48:39 +0100

    print PIDs in order of ranks

--- source/base/mpi.cc
+++ source/base/mpi.cc
@@ -64,14 +64,23 @@ namespace Utilities
     void wait(const MPI_Comm &mpi_communicator)
     {
       // see https://www.open-mpi.org/faq/?category=debugging#serial-debuggers
-      std::cout << "PID " << ::getpid()
-                << " rank " << Utilities::MPI::this_mpi_process(mpi_communicator)
-                << " on " << Utilities::System::get_hostname()
-                << " ready for attach." << std::endl;
-
-      char a;
-      if (Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
+      const unsigned int np = Utilities::MPI::n_mpi_processes(mpi_communicator);
+      const unsigned int myid = Utilities::MPI::this_mpi_process(mpi_communicator);
+      for (unsigned int p =0; p < np; ++p )
         {
+          if (p==myid)
+            std::cout << "PID " << ::getpid()
+                      << " rank " << myid
+                      << " on " << Utilities::System::get_hostname()
+                      << " ready for attach." << std::endl;
+#ifdef DEAL_II_WITH_MPI
+          MPI_Barrier(mpi_communicator);
+#endif
+        }
+
+      if (myid == 0)
+        {
+          char a;
           std::cout << "Type any character and press Enter/Return to start: " << std::flush;
           std::cin >> a;
         }
