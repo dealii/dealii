c5314a4100bb2d171b62fdcf73f6c0cc14feab30
Timo Heister <timo.heister@gmail.com>
2018-04-23 13:45:08 -0400

    blacklist MSVC 2015 or older
    because we can no longer support it

--- cmake/setup_compiler_flags_msvc.cmake
+++ cmake/setup_compiler_flags_msvc.cmake
@@ -27,6 +27,18 @@
 #                      #
 ########################
 
+# Notice how intelligent the version numbering of "Microsoft Visual Studio 2017
+# version 15.0" is, the c++ compiler is advertised as "MSVC++ 14.1" but the
+# version information is 19.10.x (this is the numbering used by CMake), see
+# https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B#Internal_version_numbering
+IF(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "19.10" )
+  MESSAGE(FATAL_ERROR "\n"
+    "You're using an old version of the Visual Studio C++ Compiler!\n"
+    "You need at least version Visual Studio 2017.\n"
+    )
+ENDIF()
+
+
 # enable exception handling:
 ENABLE_IF_SUPPORTED(DEAL_II_CXX_FLAGS "/EHsc")
 
