154073f82adc46828ebfd8cdba752a2eb12d7b9e
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2018-04-25 19:46:07 +0200

    Add 37 test files checking many aspects of FEFaceEvaluation.

---
+++ tests/matrix_free/matrix_vector_faces_04.cc
@@ -0,0 +1,50 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+
+// tests matrix-free face evaluation, matrix-vector products as compared to
+// the same implementation with MeshWorker. This example uses a hyperball mesh
+// with both hanging nodes and curved boundaries
+
+#include "../tests.h"
+#include <deal.II/base/function.h>
+#include <deal.II/fe/fe_dgq.h>
+
+std::ofstream logfile("output");
+
+#include "matrix_vector_faces_common.h"
+
+template <int dim, int fe_degree>
+void test ()
+{
+  Triangulation<dim> tria;
+  GridGenerator::hyper_ball (tria);
+  static const HyperBallBoundary<dim> boundary;
+  tria.set_boundary (0, boundary);
+  tria.refine_global(4-dim);
+  tria.begin_active()->set_refine_flag();
+  tria.last()->set_refine_flag();
+  tria.execute_coarsening_and_refinement();
+
+  FE_DGQ<dim> fe (fe_degree);
+  DoFHandler<dim> dof (tria);
+  dof.distribute_dofs(fe);
+  ConstraintMatrix constraints;
+  constraints.close();
+
+  // test with threads enabled as well
+  do_test<dim, fe_degree, fe_degree+1, double> (dof, constraints, true);
+}
--- tests/matrix_free/thread_correctness_dg.cc
+++ tests/matrix_free/thread_correctness_dg.cc
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2013 - 2018 by the deal.II authors
+// Copyright (C) 2018 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
@@ -20,11 +20,12 @@
 
 #include "../tests.h"
 #include <deal.II/base/function.h>
+#include <deal.II/fe/fe_dgq.h>
 #include "create_mesh.h"
 
 std::ofstream logfile("output");
 
-#include "matrix_vector_common.h"
+#include "matrix_vector_faces_common.h"
 
 
 template <int dim, int fe_degree, typename number>
@@ -41,6 +42,7 @@ void sub_test()
     if (cell->center().norm()<0.5)
       cell->set_refine_flag();
   tria.execute_coarsening_and_refinement();
+  tria.refine_global(1);
 #ifndef DEBUG
   if (dim < 3 || fe_degree < 2)
     tria.refine_global(1);
@@ -53,7 +55,7 @@ void sub_test()
     tria.refine_global(1);
 #endif
 
-  FE_Q<dim> fe (fe_degree);
+  FE_DGQ<dim> fe (fe_degree);
   DoFHandler<dim> dof (tria);
   deallog << "Testing " << fe.get_name() << std::endl;
 
@@ -70,9 +72,6 @@ void sub_test()
 
       dof.distribute_dofs(fe);
       ConstraintMatrix constraints;
-      DoFTools::make_hanging_node_constraints(dof, constraints);
-      VectorTools::interpolate_boundary_values (dof, 0, Functions::ZeroFunction<dim>(),
-                                                constraints);
       constraints.close();
 
       //std::cout << "Number of cells: " << dof.get_triangulation().n_active_cells() << std::endl;
@@ -92,15 +91,16 @@ void sub_test()
                               typename MatrixFree<dim,number>::AdditionalData
                               (MatrixFree<dim,number>::AdditionalData::partition_color,
                                3));
+
         mf_data_partition.reinit (dof, constraints, quad,
                                   typename MatrixFree<dim,number>::AdditionalData
                                   (MatrixFree<dim,number>::AdditionalData::partition_partition,
                                    3));
       }
 
-      MatrixFreeTest<dim,fe_degree,number> mf_ref (mf_data);
-      MatrixFreeTest<dim,fe_degree,number> mf_color (mf_data_color);
-      MatrixFreeTest<dim,fe_degree,number> mf_partition (mf_data_partition);
+      MatrixFreeTest<dim,fe_degree,fe_degree+1,number> mf_ref (mf_data);
+      MatrixFreeTest<dim,fe_degree,fe_degree+1,number> mf_color (mf_data_color);
+      MatrixFreeTest<dim,fe_degree,fe_degree+1,number> mf_partition (mf_data_partition);
       Vector<number> in_dist (dof.n_dofs());
       Vector<number> out_dist (in_dist), out_color (in_dist),
              out_partition(in_dist);
@@ -109,7 +109,7 @@ void sub_test()
         {
           if (constraints.is_constrained(i))
             continue;
-          const double entry = random_value<double>();
+          const double entry = Testing::rand()/(double)RAND_MAX;
           in_dist(i) = entry;
         }
 
@@ -124,13 +124,17 @@ void sub_test()
 
           out_color -= out_dist;
           double diff_norm = out_color.linfty_norm();
+          if (std::is_same<number,float>::value && diff_norm < 5e-6)
+            diff_norm = 0;
           deallog << "Sweep " << sweep
-                  << ", error in partition/color:     " << diff_norm
+                  << ", error in partition/color:                  " << diff_norm
                   << std::endl;
           out_partition -= out_dist;
           diff_norm = out_partition.linfty_norm();
+          if (std::is_same<number,float>::value && diff_norm < 5e-6)
+            diff_norm = 0;
           deallog << "Sweep " << sweep
-                  << ", error in partition/partition: " << diff_norm
+                  << ", error in partition/partition:              " << diff_norm
                   << std::endl;
         }
       deallog << std::endl;
