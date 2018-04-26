f9774e62f2897f0aa51124e49cb0902b2aa85a9e
Matthias Maier <tamiko@43-1.org>
2018-04-26 16:15:50 -0500

    update copyright

--- tests/opencascade/create_tria_02.cc
+++ tests/opencascade/create_tria_02.cc
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2017 by the deal.II authors
+// Copyright (C) 2017 - 2018 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
