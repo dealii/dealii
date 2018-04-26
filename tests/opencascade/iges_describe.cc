376e78dbeaa1b363281ec423b18795b38bb4c66d
David Wells <wellsd2@rpi.edu>
2018-04-25 22:12:56 -0400

    Reformat some copyright headers.

--- tests/opencascade/iges_describe.cc
+++ tests/opencascade/iges_describe.cc
@@ -1,13 +1,18 @@
-//-----------------------------------------------------------
+// ---------------------------------------------------------------------
 //
-//    Copyright (C) 2014 - 2017 by the deal.II authors
+// Copyright (C) 2014 - 2017 by the deal.II authors
 //
-//    This file is subject to LGPL and may not be distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// This file is part of the deal.II library.
 //
-//-----------------------------------------------------------
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
 
 // Read goteborg.iges and dump its topological structure to the
 // logfile.
@@ -52,4 +57,3 @@ int main ()
 
   return 0;
 }
-
