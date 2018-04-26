0cf8fa94a082710bfbd5e637b7e41ee2d8c1da99
David Wells <wellsd2@rpi.edu>
2018-04-25 22:12:32 -0400

    Reassign copyright from mathlab to deal.II.
    Done with the permission of Luca Heltai.

--- tests/manifold/flat_manifold_04.cc
+++ tests/manifold/flat_manifold_04.cc
@@ -1,13 +1,17 @@
-//----------------------------  manifold_id_01.cc  ---------------------------
-//    Copyright (C) 2011 - 2018 by the mathLab team.
+// ---------------------------------------------------------------------
 //
-//    This file is subject to LGPL and may not be  distributed
-//    without copyright and license information. Please refer
-//    to the file deal.II/doc/license.html for the  text  and
-//    further information on this license.
+// Copyright (C) 2011 - 2018 by the deal.II authors
 //
-//----------------------------  flat_manifold_01.cc  ---------------------------
-
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
 
 // Test interior mapping of flat manifold
 
@@ -90,4 +94,3 @@ int main ()
 
   return 0;
 }
-
