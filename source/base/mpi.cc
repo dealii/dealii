13f86ae169258a20a53aa46ccbf353b4c399e43d
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2018-04-29 11:48:03 +0200

    Link p4est issue in

--- source/base/mpi.cc
+++ source/base/mpi.cc
@@ -444,8 +444,9 @@ namespace Utilities
       //Initialize p4est and libsc components
 #if !(DEAL_II_P4EST_VERSION_GTE(2,0,0,0))
       // This feature is broken in version 2.0.0 for calls to
-      // MPI_Comm_create_group. Disabling it leads to more verbose
-      // p4est error messages which should be fine.
+      // MPI_Comm_create_group (see cburstedde/p4est#30).
+      // Disabling it leads to more verbose p4est error messages
+      // which should be fine.
       sc_init(MPI_COMM_WORLD, 0, 0, nullptr, SC_LP_SILENT);
 #endif
       p4est_init (nullptr, SC_LP_SILENT);
