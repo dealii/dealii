818cb7e99cf131f612e08b85904747b6a462f2c0
David Wells <wellsd2@rpi.edu>
2018-05-03 20:30:10 -0400

    Fix some header inclusion symbols.

--- include/deal.II/fe/fe_rt_bubbles.h
+++ include/deal.II/fe/fe_rt_bubbles.h
@@ -13,8 +13,8 @@
 //
 // ---------------------------------------------------------------------
 
-#ifndef dealii__fe_raviart_thomas_bubbles_h
-#define dealii__fe_raviart_thomas_bubbles_h
+#ifndef dealii_fe_raviart_thomas_bubbles_h
+#define dealii_fe_raviart_thomas_bubbles_h
 
 #include <deal.II/base/config.h>
 #include <deal.II/base/table.h>
