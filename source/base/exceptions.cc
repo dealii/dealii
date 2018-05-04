74a8f79feb0cb513a4dd2d144aaecc77b12938eb
David Wells <wellsd2@rpi.edu>
2018-05-04 12:46:23 -0400

    Revert "Mark a few file-specific variables as static."
    This reverts commit 28611bfaf785b9459fc2c7259e31fb097496e24b.
    
    tests.h adds is own extern declaration to access variables declared in
    exceptions.cc, so they cannot be static.

--- source/base/exceptions.cc
+++ source/base/exceptions.cc
@@ -42,21 +42,21 @@ DEAL_II_NAMESPACE_OPEN
 namespace deal_II_exceptions
 {
 
-  static std::string additional_assert_output;
+  std::string additional_assert_output;
 
   void set_additional_assert_output (const char *const p)
   {
     additional_assert_output = p;
   }
 
-  static bool show_stacktrace = true;
+  bool show_stacktrace = true;
 
   void suppress_stacktrace_in_exceptions ()
   {
     show_stacktrace = false;
   }
 
-  static bool abort_on_exception = true;
+  bool abort_on_exception = true;
 
   void disable_abort_on_exception ()
   {
