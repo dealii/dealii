c9145726bb5b4040be963883d94a344d819474f2
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2018-05-24 15:22:00 +0200

    Fix compiling with complex PETSc

--- source/multigrid/mg_transfer_prebuilt.cc
+++ source/multigrid/mg_transfer_prebuilt.cc
@@ -311,7 +311,7 @@ MGTransferPrebuilt<VectorType>::build_matrices(
         mg_dof);
       dsp.reinit(0, 0);
 
-      FullMatrix<double> prolongation;
+      FullMatrix<typename VectorType::value_type> prolongation;
 
       // now actually build the matrices
       for (cell = mg_dof.begin(level); cell != endc; ++cell)
