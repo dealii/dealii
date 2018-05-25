c5a144919acaf422a628a216468a29fd6a24b25c
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2018-05-25 17:37:48 +0200

    Fix typo

--- include/deal.II/base/bounding_box.h
+++ include/deal.II/base/bounding_box.h
@@ -170,7 +170,7 @@ inline BoundingBox<spacedim, Number>::BoundingBox(
   // We check the Bounding Box is not degenerate
   for (unsigned int i = 0; i < spacedim; ++i)
     Assert(boundary_points.first[i] <= boundary_points.second[i],
-           ExcMessage("Bounding Box can't be created: the point's "
+           ExcMessage("Bounding Box can't be created: the points' "
                       "order should be bottom left, top right!"));
 
   this->boundary_points = boundary_points;
