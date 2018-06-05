beabe8881269bebb3a6146a15dabe73e0f496c06
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2018-06-05 23:12:11 +0200

    Fix muparser

--- bundled/muparser_v2_2_4/src/muParserTokenReader.cpp
+++ bundled/muparser_v2_2_4/src/muParserTokenReader.cpp
@@ -902,7 +902,7 @@ namespace mu
     std::size_t iEnd(0), iSkip(0);
 
     // parser over escaped '\"' end replace them with '"'
-    for(iEnd=(int)strBuf.find( _T("\"") ); iEnd!=0 && iEnd!=string_type::npos; iEnd=(int)strBuf.find( _T("\""), iEnd))
+    for(iEnd=(int)strBuf.find( _T('\"') ); iEnd!=0 && iEnd!=string_type::npos; iEnd=(int)strBuf.find( _T('\"'), iEnd))
     {
       if (strBuf[iEnd-1]!='\\') break;
       strBuf.replace(iEnd-1, 2, _T("\"") );
