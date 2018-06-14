32ba5251a2c22f01651dab3fdce87ef44c35db66
Pi-Yueh Chuang <pychuang@gwu.edu>
2018-06-13 20:34:12 -0400

    fix #6756

--- examples/step-51/step-51.cc
+++ examples/step-51/step-51.cc
@@ -192,7 +192,7 @@ namespace Step51
   {
   public:
     SolutionAndGradient()
-      : Function<dim>(dim)
+      : Function<dim>(dim+1)
     {}
 
     virtual void vector_value(const Point<dim> &p,
