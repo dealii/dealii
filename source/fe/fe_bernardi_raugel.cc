e4aeefa1b5558b1567e5da5d92307f0837057b73
Graham Harper <grahambenharper@gmail.com>
2018-07-26 22:49:17 +0000

    Removed override from function definitions

--- source/fe/fe_bernardi_raugel.cc
+++ source/fe/fe_bernardi_raugel.cc
@@ -70,7 +70,7 @@ FE_BernardiRaugel<dim>::FE_BernardiRaugel(const unsigned int p)
 
 template <int dim>
 std::string
-FE_BernardiRaugel<dim>::get_name() const override
+FE_BernardiRaugel<dim>::get_name() const
 {
   std::ostringstream namebuf;
   namebuf << "FE_BR<" << dim << ">(" << 1 << ")";
@@ -81,7 +81,7 @@ FE_BernardiRaugel<dim>::get_name() const override
 
 template <int dim>
 std::unique_ptr<FiniteElement<dim, dim>>
-FE_BernardiRaugel<dim>::clone() const override
+FE_BernardiRaugel<dim>::clone() const
 {
   return std_cxx14::make_unique<FE_BernardiRaugel<dim>>(*this);
 }
@@ -92,7 +92,7 @@ template <int dim>
 void
 FE_BernardiRaugel<dim>::convert_generalized_support_point_values_to_dof_values(
   const std::vector<Vector<double>> &support_point_values,
-  std::vector<double> &              nodal_values) const override
+  std::vector<double> &              nodal_values) const
 {
   Assert(support_point_values.size() == this->generalized_support_points.size(),
          ExcDimensionMismatch(support_point_values.size(),
