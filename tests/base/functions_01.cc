e3f3efb3bcfb1c0b89e5ef1ddbf4975f96b0f44a
David Wells <drwells@email.unc.edu>
2018-07-28 13:00:35 -0400

    Use a C++17 function instead of jn.
    This is not guaranteed to be in the math library, but it is in C++17 (or in
    boost).

--- tests/base/functions_01.cc
+++ tests/base/functions_01.cc
@@ -19,54 +19,58 @@
 #include <deal.II/base/auto_derivative_function.h>
 #include <deal.II/base/data_out_base.h>
 #include <deal.II/base/flow_function.h>
+#include <deal.II/base/function_bessel.h>
 #include <deal.II/base/function_lib.h>
 #include <deal.II/base/job_identifier.h>
 #include <deal.II/base/quadrature_lib.h>
 
 #include <deal.II/lac/vector.h>
 
+#include <boost/core/demangle.hpp>
+
 #include <string>
+#include <typeinfo>
 #include <vector>
 
 #include "../tests.h"
 #include "functions.h"
 
-#define CHECK(F)                               \
-  {                                            \
-    deallog << #F << std::endl;                \
-    F f;                                       \
-    check_function_value_consistency(f, 5);    \
-    check_function_gradient_consistency(f, 5); \
-    check_gradient(f, 5);                      \
-  }
+template <typename FunctionType, typename... Args>
+void
+check(Args... args)
+{
+  FunctionType f(args...);
+  std::string  function_name = boost::core::demangle(typeid(f).name());
+  // remove the preceding dealii:: to match older output
+  function_name.erase(0, sizeof("dealii::") - 1 /*remember the NUL*/);
+  deallog << function_name << std::endl;
+  check_function_value_consistency(f, 5);
+  check_function_gradient_consistency(f, 5);
+  check_gradient(f, 5);
+}
 
-
-#define CHECKN(F, arg)                               \
-  {                                                  \
-    deallog << #F << '(' << arg << ')' << std::endl; \
-    F f(arg);                                        \
-    check_function_value_consistency(f, arg + 1);    \
-    check_function_gradient_consistency(f, arg + 1); \
-  }
 
 
 int
 main()
 {
-  std::string   logname = "output";
-  std::ofstream logfile(logname.c_str());
-  deallog.attach(logfile);
+  initlog();
 
-  CHECK(Functions::SquareFunction<1>);
-  CHECK(Functions::SquareFunction<2>);
-  CHECK(Functions::SquareFunction<3>);
-  CHECK(Functions::CosineFunction<1>);
-  CHECK(Functions::CosineFunction<2>);
-  CHECK(Functions::CosineFunction<3>);
-  CHECK(Functions::CosineGradFunction<1>);
-  CHECK(Functions::CosineGradFunction<2>);
-  CHECK(Functions::CosineGradFunction<3>);
-  CHECK(Functions::ExpFunction<1>);
-  CHECK(Functions::ExpFunction<2>);
-  CHECK(Functions::ExpFunction<3>);
+  check<Functions::SquareFunction<1>>();
+  check<Functions::SquareFunction<2>>();
+  check<Functions::SquareFunction<3>>();
+  check<Functions::CosineFunction<1>>();
+  check<Functions::CosineFunction<2>>();
+  check<Functions::CosineFunction<3>>();
+  check<Functions::CosineGradFunction<1>>();
+  check<Functions::CosineGradFunction<2>>();
+  check<Functions::CosineGradFunction<3>>();
+  check<Functions::ExpFunction<1>>();
+  check<Functions::ExpFunction<2>>();
+  check<Functions::ExpFunction<3>>();
+  // Bessel1 is only implemented in 2D right now
+  // check<Functions::Bessel1<1>>(3, 2.0, Point<1>(1.0));
+  check<Functions::Bessel1<2>>(3, 2.0, Point<2>(1.0, 2.0));
+  // check<Functions::Bessel1<3>>(3, 2.0, Point<2>(1.0, 2.0));
+  // check(Functions::Bessel1<3>);
 }
