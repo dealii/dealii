3be1ee50f0e2a61535e8accc524dcdba5bf3b109
David Wells <drwells@email.unc.edu>
2018-09-03 17:22:01 -0400

    Execute quick tests in parallel.
    The quick tests are currently set up as a custom target which immediately
    launches ctest. The difficulty with this is that its not possible (AFAICT) to
    get the number of jobs available from make at that point, so there is no way to
    run these in parallel without giving an explicit number of jobs to ctest.
    
    This PR sets the number of jobs to the number of CPUs on the machine: while
    hardcoding this is not ideal, this is the right usage pattern for a build script
    like
    
        make -j20
        make test
    
    where all processors on the machine should be available.

--- tests/quick_tests/run.cmake
+++ tests/quick_tests/run.cmake
@@ -13,13 +13,22 @@
 ##
 ## ---------------------------------------------------------------------
 
-# This file is run when "make test" is executed by the user and is 
+# This file is run when "make test" is executed by the user and is
 # responsible for running the tests and printing some helpful
 # error messages.
+include(ProcessorCount)
+PROCESSORCOUNT(_n_processors)
+IF(_n_processors EQUAL 0)
+  SET(_n_processors "1")
+ENDIF()
 
 SEPARATE_ARGUMENTS(ALL_TESTS)
 
-EXECUTE_PROCESS(COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process --output-on-failure -O quicktests.log RESULT_VARIABLE res_var)
+EXECUTE_PROCESS(COMMAND ${CMAKE_CTEST_COMMAND} -j${_n_processors}
+  --force-new-ctest-process
+  --output-on-failure
+  -O quicktests.log
+  RESULT_VARIABLE res_var)
 
 IF(NOT "${res_var}" STREQUAL "0")
   MESSAGE("
