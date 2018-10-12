bef55e11535598acbccc7fe1503fa837c55aebd8
Jean-Paul Pelteret <jppelteret@gmail.com>
2018-10-12 15:16:35 +0200

    Add tests to check compilation with AD numbers: Tensor class

---
+++ tests/ad_common_tests/tensor_functions_01.h
@@ -0,0 +1,76 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 - 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// Header file:
+// Test to check that the various Tensor functions compile
+// with the various auto-differentiable number types
+
+#include <deal.II/base/tensor.h>
+
+#include <deal.II/differentiation/ad.h>
+
+#include <iostream>
+
+#include "../tests.h"
+
+using namespace dealii;
+namespace AD = dealii::Differentiation::AD;
+
+template <int dim, typename number_t, enum AD::NumberTypes ad_type_code>
+void
+test_tensor()
+{
+  typedef
+    typename AD::NumberTraits<number_t, ad_type_code>::ad_type ADNumberType;
+
+  std::cout << "*** Test Tensor functions, "
+            << "dim = " << Utilities::to_string(dim) << ", "
+            << "Type code: " << static_cast<int>(ad_type_code) << std::endl;
+
+  const ADNumberType                 a = 1.0;
+  const Tensor<1, dim, ADNumberType> v1;
+  const Tensor<1, dim, ADNumberType> v2;
+  const Tensor<2, dim, ADNumberType> A(
+    unit_symmetric_tensor<dim, ADNumberType>());
+  const Tensor<2, dim, ADNumberType> B(
+    unit_symmetric_tensor<dim, ADNumberType>());
+
+  const Tensor<2, dim, ADNumberType> C1 = A + B;
+  const Tensor<2, dim, ADNumberType> C2 = A - B;
+  const Tensor<2, dim, ADNumberType> C3 = A * B;
+  const Tensor<2, dim, ADNumberType> C4 = a * A;
+  const Tensor<2, dim, ADNumberType> C5 = A * a;
+  const Tensor<2, dim, ADNumberType> C6 = A / a;
+
+  const ADNumberType                 det_A       = determinant(A);
+  const ADNumberType                 tr_A        = trace(A);
+  const Tensor<2, dim, ADNumberType> A_inv       = invert(A);
+  const Tensor<2, dim, ADNumberType> A_T         = transpose(A);
+  const Tensor<2, dim, ADNumberType> A_adj       = adjugate(A);
+  const Tensor<2, dim, ADNumberType> A_cof       = cofactor(A);
+  const ADNumberType                 A_l1_norm   = l1_norm(A);
+  const ADNumberType                 A_linf_norm = linfty_norm(A);
+
+  const ADNumberType                 A_ddot_B = double_contract(A, B);
+  const Tensor<2, dim, ADNumberType> A_dot_B  = contract<1, 0>(A, B);
+  const ADNumberType                 sp_A_B   = scalar_product(A, B);
+  const Tensor<4, dim, ADNumberType> op_A_B   = outer_product(A, B);
+
+  if (dim == 2)
+    const Tensor<1, dim, ADNumberType> v3 = cross_product_2d(v1);
+  if (dim == 3)
+    const Tensor<1, dim, ADNumberType> v3 = cross_product_3d(v1, v2);
+}
---
+++ tests/adolc/tensor_functions_01_1.cc
@@ -0,0 +1,39 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 - 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// Test to check that the various Tensor functions compile
+// with the various auto-differentiable number types
+//
+// AD number type: ADOL-C taped
+
+#include "../ad_common_tests/tensor_functions_01.h"
+#include "../tests.h"
+
+int
+main()
+{
+  initlog();
+
+  deallog.push("Double");
+  {
+    test_tensor<2, double, AD::NumberTypes::adolc_taped>();
+    test_tensor<3, double, AD::NumberTypes::adolc_taped>();
+    deallog << "OK" << std::endl;
+  }
+  deallog.pop();
+
+  deallog << "OK" << std::endl;
+}
