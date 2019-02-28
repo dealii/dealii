e9c20e609eb6f7ff6ba75bf0377466b98dfbcc62
Matthias Maier <tamiko@43-1.org>
2019-02-28 14:34:24 -0600

    add a test

---
+++ tests/sparsity/sparsity_pattern_iterator_09.cc
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2004 - 2017 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+
+// test that i++ and i+n operations work with SparsityPattern::iterator.
+
+#include <deal.II/lac/sparsity_pattern.h>
+
+#include "../tests.h"
+
+void
+test()
+{
+  SparsityPattern sp(5, 5, 3);
+  for (unsigned int i = 0; i < 5; ++i)
+    for (unsigned int j = 0; j < 5; ++j)
+      if ((i + 2 * j + 1) % 3 == 0)
+        sp.add(i, j);
+  sp.compress();
+
+  SparsityPattern::const_iterator i = sp.begin();
+
+  ++i;
+  i++;
+  i + 2;
+
+  deallog << "OK" << std::endl;
+}
+
+
+
+int
+main()
+{
+  initlog();
+
+  test();
+}
