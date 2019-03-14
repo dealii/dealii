8f713e8f0f889cea3269404fb65742ddefd17d3a
Jean-Paul Pelteret <jppelteret@gmail.com>
2019-03-14 18:27:51 +0100

    Add some initial tests for SymEngine

---
+++ tests/symengine/basic_02.cc
@@ -0,0 +1,152 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 - 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+
+// Check that SymEngine can perform symbolic differentiation correctly
+
+// References:
+// https://github.com/symengine/symengine/blob/master/symengine/tests/basic/test_basic.cpp
+// https://github.com/symengine/symengine/blob/master/symengine/tests/basic/test_number.cpp
+// https://github.com/symengine/symengine/blob/master/symengine/tests/basic/test_subs.cpp
+// https://github.com/symengine/symengine/blob/master/symengine/symengine_casts.h
+// https://github.com/symengine/symengine/blob/master/symengine/symengine_rcp.h
+// https://github.com/symengine/symengine/blob/master/symengine/derivative.h
+// https://github.com/symengine/symengine/blob/master/symengine/subs.h
+
+#include "../tests.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wextra-semi"
+#include <symengine/complex_double.h>
+#include <symengine/derivative.h>
+#include <symengine/eval_double.h>
+#include <symengine/integer.h>
+#include <symengine/rational.h>
+#include <symengine/real_double.h>
+#include <symengine/symbol.h>
+#include <symengine/symengine_casts.h>
+#include <symengine/visitor.h>
+#pragma clang diagnostic pop
+
+#include <fstream>
+#include <iomanip>
+
+using namespace dealii;
+namespace SE = SymEngine;
+
+SE::RCP<const SE::Number>
+make_symengine_rcp(const int &val)
+{
+  return SE::integer(val);
+}
+
+SE::RCP<const SE::Number>
+make_symengine_rcp(const float &val)
+{
+  return SE::real_double(val);
+}
+
+SE::RCP<const SE::Number>
+make_symengine_rcp(const double &val)
+{
+  return SE::real_double(val);
+}
+
+template <typename NumberType>
+SE::RCP<const SE::Number>
+make_symengine_rcp(const std::complex<NumberType> &val)
+{
+  // Build complex from two SymEngine numbers
+  return SE::Complex::from_two_nums(*make_symengine_rcp(val.real()),
+                                    *make_symengine_rcp(val.imag()));
+}
+
+SE::RCP<const SE::Symbol>
+make_symengine_rcp(const std::string &name)
+{
+  return SE::symbol(name);
+}
+
+template <typename NumberType>
+void
+test_number()
+{
+  SE::RCP<const SE::Number> a = make_symengine_rcp(NumberType(3.1));
+  SE::RCP<const SE::Basic>  b = make_symengine_rcp(NumberType(7.5));
+  deallog << "a: " << *a << std::endl;
+  deallog << "b: " << *b << std::endl;
+
+  SE::RCP<const SE::Symbol> x(make_symengine_rcp("x"));
+  SE::RCP<const SE::Basic>  y(make_symengine_rcp("y"));
+  deallog << "x: " << *x << std::endl;
+  deallog << "y: " << *y << std::endl;
+
+  SE::RCP<const SE::Basic> c;
+  // deallog << "c (no constructor): " << *c << std::endl; // Fails
+
+  // Construction of symbolic function
+  c = SE::mul(y, SE::mul(SE::sub(y, b), SE::add(a, x)));
+  deallog << "c = y*(y-b)*(a+x): " << *c << std::endl;
+
+  // Perform symbolic differentiation
+  SE::RCP<const SE::Basic> dc_dx = c->diff(x);
+  // SE::RCP<const SE::Basic> dc_dy = diff(c,SE::implicit_cast<const
+  // SE::RCP<const SE::Symbol> &>(y));
+  SE::RCP<const SE::Basic> dc_dy =
+    diff(c, SE::rcp_static_cast<const SE::Symbol>(y));
+  SE::RCP<const SE::Basic> dc_dy_2 = sdiff(c, y);
+
+  deallog << "dc_dx = a*y*(y-b): " << *dc_dx << std::endl;
+  deallog << "dc_dy = 2*y*(a+x): " << *dc_dy << std::endl;
+  deallog << "dc_dy = 2*y*(a+x): " << *dc_dy_2 << std::endl;
+
+  // Substitute values
+  SE::map_basic_basic sub_vals;
+  sub_vals[x] = make_symengine_rcp(NumberType(1));
+  sub_vals[y] = make_symengine_rcp(NumberType(2.2));
+  deallog << "dc_dx(x=1,y=2.2): " << *(dc_dx->subs(sub_vals)) << std::endl;
+
+  sub_vals[x] = make_symengine_rcp(NumberType(0.75));
+  sub_vals[y] = make_symengine_rcp(NumberType(-2.08));
+  deallog << "dc_dy(x=0.75,y=-1.08): " << *(dc_dy->subs(sub_vals)) << std::endl;
+
+  deallog << std::endl;
+}
+
+int
+main()
+{
+  initlog();
+
+  deallog.push("Integer");
+  test_number<int>();
+  deallog.pop();
+
+  deallog.push("Float");
+  test_number<float>();
+  deallog.pop();
+
+  deallog.push("Double");
+  test_number<double>();
+  deallog.pop();
+
+  // Not available yet
+  // SymEngine::SymEngineException: Invalid Format: Expected Integer or Rational
+  // deallog << "Complex double" << std::endl;
+  // test_number<std::complex<double>>();
+  // deallog.pop();
+
+  deallog << "OK" << std::endl;
+}
---
+++ tests/symengine/basic_06.cc
@@ -0,0 +1,115 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 - 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
+// Check that SymEngine can do delayed substitution of explicitly dependent
+// symbolic functions and evaluate their derivatives as well.
+// This is inline with what is required to implement some material laws that
+// have internal variables
+
+#include "../tests.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wextra-semi"
+
+#include <symengine/add.h>
+#include <symengine/basic.h>
+#include <symengine/dict.h>
+#include <symengine/eval_double.h>
+#include <symengine/functions.h>
+#include <symengine/integer.h>
+#include <symengine/mul.h>
+#include <symengine/pow.h>
+#include <symengine/real_double.h>
+#include <symengine/symbol.h>
+
+#pragma GCC diagnostic pop
+
+#include <iostream>
+#include <vector>
+
+using namespace dealii;
+namespace SE = SymEngine;
+
+int
+main(int argc, char *argv[])
+{
+  initlog();
+
+  // 0. Define some independent variables
+  // Here we have two definitions of Q: One it is treated as a completely
+  // indepdent variable, and the other where its dependence on C is
+  // explicitly defined
+  SE::RCP<const SE::Symbol> C  = SE::symbol("C");  // Primary variable
+  SE::RCP<const SE::Symbol> Qi = SE::symbol("Qi"); // Some internal variable
+  SE::RCP<const SE::Basic>  Q =
+    SE::function_symbol("Q", C); // This sets up the dependence of Q on C
+
+  deallog << *C << ",  " << *Qi << ",  " << *Q << std::endl;
+
+  // 1. Define a function with Q being independent of C
+  SE::RCP<const SE::Basic> f_CQ_symb =
+    SE::mul(SE::real_double(0.5), SE::mul(Qi, SE::pow(C, SE::integer(2))));
+
+  deallog << "f_CQ_symb: " << *f_CQ_symb << std::endl;
+
+  // 2. Compute the partial derivative of f wrt C. This is a "partial
+  // derivative" as Q != Q(C).
+  SE::RCP<const SE::Basic> df_CQ_dC_symb = f_CQ_symb->diff(C);
+
+  deallog << "df_CQ_dC_symb: " << *df_CQ_dC_symb << std::endl;
+
+  // 3. Substitute Qi -> Q=Q(C) to now make SymEngine aware of Q's dependence
+  // on C.
+  SE::map_basic_basic int_var_dict;
+  int_var_dict[Qi] = Q;
+  SE::RCP<const SE::Basic> df_CQ_dC_symb_subs =
+    df_CQ_dC_symb->subs(int_var_dict);
+
+  deallog << "df_CQ_dC_symb_subs: " << *df_CQ_dC_symb_subs << std::endl;
+
+  // 4. Compute total derivative of df_dC wrt C.
+  SE::RCP<const SE::Basic> D2f_CQ_DC_dC_symb = df_CQ_dC_symb_subs->diff(C);
+
+  deallog << "D2f_CQ_DC_dC_symb: " << *D2f_CQ_DC_dC_symb << std::endl;
+
+  // 5. Perform some numerical substitutions
+  SE::map_basic_basic all_var_dict;
+  all_var_dict[C]  = SE::real_double(5);
+  all_var_dict[Qi] = SE::real_double(3);
+  all_var_dict[Q] =
+    SE::mul(SE::real_double(0.6),
+            C); // This explicitly defines the relationship between Q and C
+
+  SE::RCP<const SE::Basic> f_CQ_subs = f_CQ_symb->subs(all_var_dict);
+  deallog << "f_CQ_subs: " << *f_CQ_subs << std::endl;
+
+  SE::RCP<const SE::Basic> df_CQ_dC_subs = df_CQ_dC_symb->subs(all_var_dict);
+  deallog << "df_CQ_dC: subs: " << *df_CQ_dC_subs << std::endl
+          << "df_CQ_dC: eval: " << SE::eval_double(*df_CQ_dC_subs) << std::endl;
+
+  // This first substitution should presumably convert Q(C)->0.6C and thus
+  // dQ(C)_dC into 0.6
+  SE::RCP<const SE::Basic> D2f_CQ_DC_dC_subs_1 =
+    D2f_CQ_DC_dC_symb->subs(all_var_dict);
+  // This second substitution should fill out the remaining entries for C
+  SE::RCP<const SE::Basic> D2f_CQ_DC_dC_subs =
+    D2f_CQ_DC_dC_subs_1->subs(all_var_dict);
+  deallog << "D2f_CQ_DC_dC: subs 1: " << *D2f_CQ_DC_dC_subs_1 << std::endl
+          << "D2f_CQ_DC_dC: subs 2: " << *D2f_CQ_DC_dC_subs << std::endl
+          << "D2f_CQ_DC_dC: eval: " << SE::eval_double(*D2f_CQ_DC_dC_subs)
+          << std::endl;
+
+  return 0;
+}
