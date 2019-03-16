df1a79ab3abd6f126e7000972eea4c1c3779bb42
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2019-03-16 01:11:58 +0100

    Set cell manifold ids when the apply_all_indicators_to_manifold flag is used.

---
+++ tests/grid/grid_in_ucd_02.cc
@@ -0,0 +1,77 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2019 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+// Test that cell manifold ids are also set for the
+// "apply_all_indicators_to_manifolds" option in GridIn::read_ucd
+
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+
+#include <sstream>
+#include <string>
+
+#include "../tests.h"
+
+template <int dim, int spacedim>
+void
+test(const std::string &filename)
+{
+  std::ifstream                tmp_in(filename);
+  Triangulation<dim, spacedim> tria;
+  GridIn<dim, spacedim>        gi;
+  gi.attach_triangulation(tria);
+  gi.read_ucd(tmp_in, true);
+  deallog << "Testing dim=" << dim << " spacedim=" << spacedim << std::endl;
+  for (const auto &cell : tria.active_cell_iterators())
+    {
+      deallog << "cell->manifold_id: " << cell->manifold_id() << std::endl;
+      if (dim > 1)
+        for (unsigned int face_no = 0;
+             face_no < GeometryInfo<dim>::faces_per_cell;
+             ++face_no)
+          deallog << "cell->face(" << face_no
+                  << ")->manifold_id: " << cell->face(face_no)->manifold_id()
+                  << std::endl;
+      if (dim > 1)
+        for (unsigned int line_no = 0;
+             line_no < GeometryInfo<dim>::lines_per_cell;
+             ++line_no)
+          deallog << "cell->line(" << line_no
+                  << ")->manifold_id: " << cell->line(line_no)->manifold_id()
+                  << std::endl;
+    }
+  deallog << std::endl;
+}
+
+int
+main()
+{
+  initlog();
+  deallog.get_file_stream() << std::setprecision(2);
+
+  test<1, 1>(SOURCE_DIR "/grid_in_ucd_02_grids/grid_1.inp");
+  test<1, 2>(SOURCE_DIR "/grid_in_ucd_02_grids/grid_1.inp");
+  test<1, 3>(SOURCE_DIR "/grid_in_ucd_02_grids/grid_1.inp");
+  test<2, 2>(SOURCE_DIR "/grid_in_ucd_02_grids/grid_2.inp");
+  test<2, 3>(SOURCE_DIR "/grid_in_ucd_02_grids/grid_2.inp");
+  test<3, 3>(SOURCE_DIR "/grid_in_ucd_02_grids/grid_3.inp");
+}
