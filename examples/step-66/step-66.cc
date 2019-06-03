41eaeaee40352791c6b04a68e19f4d990fd6ef3c
gfcas <50630942+gfcas@users.noreply.github.com>
2019-06-03 17:09:49 +0200

    Include headers

--- examples/step-66/step-66.cc
+++ examples/step-66/step-66.cc
@@ -1,5 +1,5 @@
 /* ---------------------------------------------------------------------
- *
+ * 
  * Copyright (C) 2003 - 2018 by the deal.II authors
  *
  * This file is part of the deal.II library.
@@ -13,41 +13,56 @@
  *
  * ---------------------------------------------------------------------
  *
- * Authors: Fabian Castelli, KIT
+ * Authors: Fabian Castelli, Karlsruhe Institute of Technology (KIT)
  *          Timo Heister, University of Utah
  */
 
-// @note: This is work in progress and will be an example for a non-linear
-// problem solved in parallel with matrix-free geometric multigrid. For now,
-// this is just step-1.
+
+
+// First we include the typical headers of the deal.II library needed for this
+// tutroial:
+#include <deal.II/base/function.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/timer.h>
+#include <deal.II/base/vectorization.h>
+
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
 
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/manifold_lib.h>
 
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/mapping_c1.h>
+
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/data_out.h>
+
+// In particular, we need to include the headers for the matrix-free framework:
+#include <deal.II/matrix_free/matrix_free.h>
+#include <deal.II/matrix_free/operators.h>
+#include <deal.II/matrix_free/fe_evaluation.h>
+
+// And since we want to use a geometric multigrid preconditioner, we need also
+// the multilevel headers:
+#include <deal.II/multigrid/multigrid.h>
+#include <deal.II/multigrid/mg_transfer_matrix_free.h>
+#include <deal.II/multigrid/mg_tools.h>
+#include <deal.II/multigrid/mg_coarse.h>
+#include <deal.II/multigrid/mg_smoother.h>
+#include <deal.II/multigrid/mg_matrix.h>
+#include <deal.II/multigrid/mg_constrained_dofs.h>
+
+
+// Finally some common C++ headers for in and output:
 #include <iostream>
 #include <fstream>
-#include <cmath>
-
-using namespace dealii;
-
-void first_grid()
-{
-  Triangulation<2> triangulation;
-
-  GridGenerator::hyper_cube(triangulation);
-  triangulation.refine_global(4);
-
-  std::ofstream out("grid-1.eps");
-  GridOut       grid_out;
-  grid_out.write_eps(triangulation, out);
-  std::cout << "Grid written to grid-1.eps" << std::endl;
-}
-
-
-int main()
-{
-  first_grid();
-}
