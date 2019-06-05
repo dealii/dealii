da6dae2b72a55ddfc9cbc226e4d1a5cea8255437
Mathias Anselmann <mathias.anselmann@posteo.de>
2019-06-05 21:24:57 +0200

    Changed dof_tools_12a to include more complicated FE systems and output all locally owned components (not mapped to global DoFs)

--- tests/dofs/dof_tools_12a.cc
+++ tests/dofs/dof_tools_12a.cc
@@ -20,8 +20,12 @@
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
 
+#include <deal.II/fe/fe_dgp.h>
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_nedelec.h>
 #include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/mapping_q_generic.h>
+#include <deal.II/fe/fe_raviart_thomas.h>
+#include <deal.II/fe/fe_system.h>
 
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
@@ -50,27 +54,18 @@ template <int dim, typename DoFHandlerType>
 void
 check_this(const DoFHandlerType &dof_handler)
 {
-  IndexSet locally_relevant_dofs;
-  DoFTools::extract_locally_relevant_dofs(dof_handler, locally_relevant_dofs);
-
   std::vector<bool> selected_dofs(dof_handler.n_locally_owned_dofs());
   std::vector<bool> mask(dof_handler.get_fe().n_components(), false);
 
   // only select first component
   mask[0] = true;
   DoFTools::extract_dofs(dof_handler, ComponentMask(mask), selected_dofs);
-  if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
-    {
-      output_bool_vector(selected_dofs);
-    }
+  output_bool_vector(selected_dofs);
 
   // also select last component
   mask.back() = true;
   DoFTools::extract_dofs(dof_handler, ComponentMask(mask), selected_dofs);
-  if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
-    {
-      output_bool_vector(selected_dofs);
-    }
+  output_bool_vector(selected_dofs);
 }
 
 
@@ -95,16 +90,17 @@ check(const FiniteElement<dim> &fe, const std::string &name)
       tria.execute_coarsening_and_refinement();
     }
 
+  // setup DoFHandler
   DoFHandler<dim> dof_handler(tria);
   dof_handler.distribute_dofs(fe);
 
   // setup hp DoFHandler
   hp::FECollection<dim> fe_collection(fe);
-  hp::DoFHandler<dim>   hp_dof_handler(tria);
+  hp::DoFHandler<dim> hp_dof_handler(tria);
   hp_dof_handler.distribute_dofs(fe_collection);
 
-  check_this<dim, DoFHandler<dim>>(dof_handler);
-  check_this<dim, hp::DoFHandler<dim>>(hp_dof_handler);
+  check_this<dim, DoFHandler<dim> >(dof_handler);
+  check_this<dim, hp::DoFHandler<dim> >(hp_dof_handler);
 }
 
 
@@ -114,20 +110,131 @@ check(const FiniteElement<dim> &fe, const std::string &name)
     check(EL, #EL #deg);    \
   }
 
+#define CHECK_SYS1(sub1, N1, dim) \
+  {                               \
+    FESystem<dim> q(sub1, N1);    \
+    check(q, #sub1 #N1);          \
+  }
+
+#define CHECK_SYS2(sub1, N1, sub2, N2, dim) \
+  {                                         \
+    FESystem<dim> q(sub1, N1, sub2, N2);    \
+    check(q, #sub1 #N1 #sub2 #N2);          \
+  }
+
+#define CHECK_SYS3(sub1, N1, sub2, N2, sub3, N3, dim) \
+  {                                                   \
+    FESystem<dim> q(sub1, N1, sub2, N2, sub3, N3);    \
+    check(q, #sub1 #N1 #sub2 #N2 #sub3 #N3);          \
+  }
+
 #define CHECK_ALL(EL, deg) \
   {                        \
     CHECK(EL, deg, 2);     \
     CHECK(EL, deg, 3);     \
   }
 
+
 int
 main(int argc, char **argv)
 {
-  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
-  mpi_initlog();
-  CHECK_ALL(Q, 1);
+  try
+    {
+      Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+      MPILogInitAll logfile;
 
-  CHECK_ALL(Q, 2);
+      CHECK_ALL(Q, 1)
+      CHECK_ALL(Q, 2)
+      CHECK_ALL(Q, 3)
 
-  return 0;
+      CHECK_ALL(DGQ, 0)
+      CHECK_ALL(DGQ, 1)
+      CHECK_ALL(DGQ, 3)
+
+      CHECK_ALL(DGP, 0)
+      CHECK_ALL(DGP, 1)
+      CHECK_ALL(DGP, 3)
+
+      CHECK(Nedelec, 0, 2)
+      CHECK(Nedelec, 0, 3)
+
+      CHECK(RaviartThomas, 0, 2)
+      CHECK(RaviartThomas, 1, 2)
+      CHECK(RaviartThomas, 2, 2)
+
+      CHECK(RaviartThomasNodal, 0, 2)
+      CHECK(RaviartThomasNodal, 1, 2)
+      CHECK(RaviartThomasNodal, 2, 2)
+      CHECK(RaviartThomasNodal, 0, 3)
+      CHECK(RaviartThomasNodal, 1, 3)
+      CHECK(RaviartThomasNodal, 2, 3)
+
+      CHECK_SYS1(FE_Q<2>(1), 3, 2)
+      CHECK_SYS1(FE_DGQ<2>(2), 2, 2)
+      CHECK_SYS1(FE_DGP<2>(3), 1, 2)
+
+      CHECK_SYS1(FE_Q<3>(1), 3, 3)
+      CHECK_SYS1(FE_DGQ<3>(2), 2, 3)
+      CHECK_SYS1(FE_DGP<3>(3), 1, 3)
+
+      CHECK_SYS2(FE_Q<2>(1), 3, FE_DGQ<2>(2), 2, 2)
+      CHECK_SYS2(FE_DGQ<2>(2), 2, FE_DGP<2>(3), 1, 2)
+      CHECK_SYS2(FE_DGP<2>(3), 1, FE_DGQ<2>(2), 2, 2)
+
+      CHECK_SYS3(FE_Q<2>(1), 3, FE_DGP<2>(3), 1, FE_Q<2>(1), 3, 2)
+      CHECK_SYS3(FE_DGQ<2>(2), 2, FE_DGQ<2>(2), 2, FE_Q<2>(3), 3, 2)
+      CHECK_SYS3(FE_DGP<2>(3), 1, FE_DGP<2>(3), 1, FE_Q<2>(2), 3, 2)
+
+      CHECK_SYS3(FE_DGQ<3>(1), 3, FE_DGP<3>(3), 1, FE_Q<3>(1), 3, 3)
+
+      // systems of systems
+      CHECK_SYS3(
+        (FESystem<2>(FE_Q<2>(1), 3)), 3, FE_DGQ<2>(0), 1, FE_Q<2>(1), 3, 2)
+      CHECK_SYS3(FE_DGQ<2>(3),
+                 1,
+                 FESystem<2>(FE_DGQ<2>(0), 3),
+                 1,
+                 FESystem<2>(FE_Q<2>(2), 1, FE_DGQ<2>(0), 1),
+                 2,
+                 2)
+
+      // systems with Nedelec elements
+      CHECK_SYS2(FE_DGQ<2>(3), 1, FE_Nedelec<2>(0), 2, 2)
+      CHECK_SYS3(FE_Nedelec<2>(0),
+                 1,
+                 FESystem<2>(FE_DGQ<2>(1), 2),
+                 1,
+                 FESystem<2>(FE_Q<2>(2), 1, FE_Nedelec<2>(0), 2),
+                 2,
+                 2)
+
+      return 0;
+    }
+  catch (std::exception &exc)
+    {
+      deallog << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+      deallog << "Exception on processing: " << std::endl
+              << exc.what() << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      deallog << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+      deallog << "Unknown exception!" << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+      return 1;
+    };
+
 }
+
