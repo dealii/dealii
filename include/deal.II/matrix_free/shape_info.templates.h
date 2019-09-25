1bbdcbedc9f9b482824de3c47df961178ab9a80a
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-09-25 12:45:12 -0400

    Store 1D quadrature

--- include/deal.II/matrix_free/shape_info.templates.h
+++ include/deal.II/matrix_free/shape_info.templates.h
@@ -75,6 +75,7 @@ namespace internal
                               const FiniteElement<dim> &fe_in,
                               const unsigned int        base_element_number)
     {
+      this->quadrature             = quad;
       const FiniteElement<dim> *fe = &fe_in.base_element(base_element_number);
 
       Assert(fe->n_components() == 1,
