980a2c0182d2df37f60a65d45105291477eb6d4b
Roland Richter <Roland.Richter2@gmx.de>
2019-10-15 10:26:19 +0200

    Safety check for usable eigenvalues extended to non-symmetric matrices

--- include/deal.II/lac/arpack_solver.h
+++ include/deal.II/lac/arpack_solver.h
@@ -483,6 +483,21 @@ inline ArpackSolver::AdditionalData::AdditionalData(
         ExcMessage(
           "'smallest imaginary part' can only be used for non-symmetric problems!"));
     }
+  // Check for possible options for asymmetric problems
+  else
+    {
+      Assert(
+        eigenvalue_of_interest != algebraically_largest,
+        ExcMessage(
+          "'largest algebraic part' can only be used for symmetric problems!"));
+      Assert(
+        eigenvalue_of_interest != algebraically_smallest,
+        ExcMessage(
+          "'smallest algebraic part' can only be used for symmetric problems!"));
+      Assert(eigenvalue_of_interest != both_ends,
+             ExcMessage(
+               "'both ends' can only be used for symmetric problems!"));
+    }
 }
 
 
