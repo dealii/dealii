30df5d2033e8fa9a3eb0b0e01da04d34c090b270
Nicola Giuliani <ngiuliani@sissa.it>
2019-10-30 14:06:33 +0100

    added stl read/write functions
    post comments from wolfgang
    
    added changes

---
+++ tests/opencascade/stl_write.cc
@@ -0,0 +1,46 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2014 - 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+// Read a file in stl format, and write it out again in the same
+// format.
+
+#include <deal.II/opencascade/utilities.h>
+
+#include <Standard_Stream.hxx>
+#include <TopTools.hxx>
+#include <TopoDS_Shape.hxx>
+
+#include "../tests.h"
+
+using namespace OpenCASCADE;
+
+int
+main()
+{
+  TopoDS_Shape sh = read_STL(SOURCE_DIR "/stl_files/sphere_refined.stl");
+  write_STL(sh, "tmp.stl", 0.001, 0.001);
+  std::ifstream in("tmp.stl");
+  std::ofstream out("output");
+  std::string   line;
+  unsigned int  counter = 5;
+  while (counter--)
+    std::getline(in, line);
+  while (std::getline(in, line))
+    out << line << std::endl;
+  in.close();
+  out.close();
+  return 0;
+}
