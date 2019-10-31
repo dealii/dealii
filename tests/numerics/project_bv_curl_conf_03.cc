53966d9c4a289bd360048b00f2139b60e17832d7
Winnifried Wollner <wollner@mathematik.tu-darmstadt.de>
2019-10-31 19:22:20 +0100

    Indentation fixed

--- tests/numerics/project_bv_curl_conf_03.cc
+++ tests/numerics/project_bv_curl_conf_03.cc
@@ -15,56 +15,72 @@
 // The test checks that project_boundary_values_curl_conforming
 // works for FESystems with FE_Nedelec of different order.
 
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/fe/fe_system.h>
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/fe_nedelec.h>
-#include <deal.II/fe/mapping_q.h>
-#include <deal.II/lac/constraint_matrix.h>
 #include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/fe/fe_nedelec.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/mapping_q.h>
+
+#include <deal.II/grid/grid_generator.h>
+
+#include <deal.II/lac/constraint_matrix.h>
+
 #include <deal.II/numerics/vector_tools.h>
 
 #include "../tests.h"
 
 using namespace dealii;
 
-int main(int argc, char **argv) {
+int
+main(int argc, char **argv)
+{
   initlog();
-  const unsigned int dim=3;
+  const unsigned int dim = 3;
 
   Triangulation<dim> triangulation;
   GridGenerator::hyper_cube(triangulation, 0, 1, true);
-  MappingQ<dim> mapping(1);
+  MappingQ<dim>    mapping(1);
   ConstraintMatrix constraints;
 
-  //Testing cases with only Nedelec Elements in FE_System
+  // Testing cases with only Nedelec Elements in FE_System
   {
     deallog << "Equal order Nedelec degree 0 \t";
-    //Equal-order elements
-    FESystem<dim> fe_system(FE_Nedelec<dim>(0), 1, FE_Nedelec<dim>(0), 1);
+    // Equal-order elements
+    FESystem<dim>   fe_system(FE_Nedelec<dim>(0), 1, FE_Nedelec<dim>(0), 1);
     DoFHandler<dim> dof_handler(triangulation);
     dof_handler.distribute_dofs(fe_system);
-    
+
     constraints.clear();
-    VectorTools::project_boundary_values_curl_conforming_l2(dof_handler, dim,
-							    Functions::ZeroFunction<dim>(dim + dim), 0, constraints, mapping);
+    VectorTools::project_boundary_values_curl_conforming_l2(
+      dof_handler,
+      dim,
+      Functions::ZeroFunction<dim>(dim + dim),
+      0,
+      constraints,
+      mapping);
     constraints.close();
     deallog << "OK" << std::endl;
   }
 
   {
     deallog << "Mixed order Nedelec (degree 1,degree 0) \t";
-    //Elements of mixed order
-    FESystem<dim> fe_system(FE_Nedelec<dim>(1), 1, FE_Nedelec<dim>(0), 1);
+    // Elements of mixed order
+    FESystem<dim>   fe_system(FE_Nedelec<dim>(1), 1, FE_Nedelec<dim>(0), 1);
     DoFHandler<dim> dof_handler(triangulation);
     dof_handler.distribute_dofs(fe_system);
-    
+
     constraints.clear();
-    VectorTools::project_boundary_values_curl_conforming_l2(dof_handler, dim,
-							    Functions::ZeroFunction<dim>(dim + dim), 0, constraints, mapping);
+    VectorTools::project_boundary_values_curl_conforming_l2(
+      dof_handler,
+      dim,
+      Functions::ZeroFunction<dim>(dim + dim),
+      0,
+      constraints,
+      mapping);
     constraints.close();
     deallog << "OK" << std::endl;
   }
-  
+
   return 0;
 }
