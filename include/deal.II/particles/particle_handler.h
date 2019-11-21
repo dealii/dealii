f00f61315a09f22467268f98b52d16953e6c5cd5
Luca Heltai <luca.heltai@sissa.it>
2019-11-21 19:30:17 +0100

    Insert particles globally.
    Create and insert a number of particles into the collection of
    particles.
    This function takes a list of positions and creates a set of particles
    
    at these positions, which are then distributed and added to the local
    
    particle collection of a procesor. Note that this function uses
    
    GridTools::distributed_compute_point_locations. Consequently, it can
    
    require intense communications between the processors.
    
    Co-authored-by: Bruno Blais <blais.bruno@gmail.com>

--- include/deal.II/particles/particle_handler.h
+++ include/deal.II/particles/particle_handler.h
@@ -19,13 +19,19 @@
 #include <deal.II/base/config.h>
 
 #include <deal.II/base/array_view.h>
+#include <deal.II/base/data_out_base.h>
 #include <deal.II/base/mpi.h>
 #include <deal.II/base/smartpointer.h>
 #include <deal.II/base/subscriptor.h>
+#include <deal.II/base/utilities.h>
 
 #include <deal.II/distributed/tria.h>
 
 #include <deal.II/fe/mapping.h>
+
+#include <deal.II/grid/filtered_iterator.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/grid_tools_cache.h>
 
 #include <deal.II/particles/particle.h>
 #include <deal.II/particles/particle_iterator.h>
@@ -229,6 +235,250 @@ namespace Particles
      */
     void
     insert_particles(const std::vector<Point<spacedim>> &positions);
+
+    /**
+     * Create and insert a number of particles into the collection of particles.
+     * This function takes a list of positions and creates a set of particles
+     * at these positions, which are then distributed and added to the local
+     * particle collection of a procesor. Note that this function uses
+     * GridTools::distributed_compute_point_locations. Consequently, it can
+     * require intense communications between the processors.
+     *
+     * This function figures out what mpi process owns all points that do not
+     * fall within the locally owned part of the triangulation, it sends
+     * to that process the points passed to this function on this process,
+     * and receives from the points that fall within the locally owned cells of
+     * the triangulation from whoever owns them.
+     *
+     * In order to keep track of what mpi process recieved what points, a maps
+     * from mpi process to IndexSet is returned by the function, that contains
+     * the local indices of the points that were passed to this function on the
+     * calling mpi process, and that falls within the part of triangulation
+     * owned by this mpi process.
+     *
+     * @param[in] A vector of points that do not need to be on the local
+     * processor
+     *
+     * @param[in] A vector of vectors of bounding boxes. The bounding boxes
+     * global_bboxes[rk] describe which part of the mesh is locally owned by
+     * the mpi process with rank rk. The local description can be obtained from
+     * GridTools::compute_mesh_predicate_bounding_box, and the global one can
+     * be obtained by passing the local ones to Utilities::MPI::all_gather.
+     *
+     * @param[in] (Optional) a vector of properties associated with each
+     * local point. The size of the vector should be either zero (no
+     * properties will be transfered nor attached to the generated particles)
+     * or it should be `positions.size()*this->n_properties_per_particle()`.
+     * Notice that this function call will tranfer the properties from the
+     * local mpi process to the final mpi process that will own each of the
+     * particle, and it may therefore be communication intensive.
+     *
+     * @return A pair of maps from owner to IndexSet, that contains the local
+     * indices of the points that other mpi processes have sent to the current
+     * processor, and a map that identifies the new owner of the points that
+     * were originally located on this processor.
+     *
+     * @author : Bruno Blais, Luca Heltai 2019
+     */
+    std::map<unsigned int, IndexSet>
+    insert_global_particles(
+      const std::vector<Point<spacedim>> &positions,
+      const std::vector<std::vector<BoundingBox<spacedim>>>
+        &                        global_bounding_boxes,
+      const std::vector<double> &properties = std::vector<double>())
+    {
+      if (!properties.empty())
+        AssertDimension(properties.size(),
+                        positions.size() * n_properties_per_particle());
+
+      const auto my_cpu =
+        Utilities::MPI::this_mpi_process(triangulation->get_communicator());
+
+      const auto n_cpus =
+        Utilities::MPI::n_mpi_processes(triangulation->get_communicator());
+
+      GridTools::Cache<dim, spacedim> cache(*triangulation, *mapping);
+
+      // Gather the number of points per processor
+      auto n_particles_per_proc =
+        Utilities::MPI::all_gather(triangulation->get_communicator(),
+                                   positions.size());
+
+      // Calculate all starting points locally
+      std::vector<unsigned int> starting_points(n_cpus);
+
+      for (unsigned int i = 0; i < starting_points.size(); ++i)
+        {
+          starting_points[i] = std::accumulate(n_particles_per_proc.begin(),
+                                               n_particles_per_proc.begin() + i,
+                                               0u);
+        }
+
+      auto distributed_tuple =
+        GridTools::distributed_compute_point_locations(cache,
+                                                       positions,
+                                                       global_bounding_boxes);
+
+      // Finally create the particles
+      std::vector<typename Triangulation<dim, spacedim>::active_cell_iterator>
+                                           cell_iterators = std::get<0>(distributed_tuple);
+      std::vector<std::vector<Point<dim>>> dist_reference_points =
+        std::get<1>(distributed_tuple);
+      std::vector<std::vector<unsigned int>> dist_map =
+        std::get<2>(distributed_tuple);
+      std::vector<std::vector<Point<spacedim>>> dist_points =
+        std::get<3>(distributed_tuple);
+      std::vector<std::vector<unsigned int>> dist_procs =
+        std::get<4>(distributed_tuple);
+
+      // Create the multimap of particles
+      std::multimap<typename Triangulation<dim, spacedim>::active_cell_iterator,
+                    Particle<dim, spacedim>>
+        particles;
+
+      // Create the map of cpu to indices, indicating whom sent us what
+      // point
+      std::map<unsigned int, IndexSet> cpu_to_indices;
+
+      for (unsigned int i_cell = 0; i_cell < cell_iterators.size(); ++i_cell)
+        {
+          for (unsigned int i_particle = 0;
+               i_particle < dist_points[i_cell].size();
+               ++i_particle)
+            {
+              const auto &local_id = dist_map[i_cell][i_particle];
+              const auto &cpu      = dist_procs[i_cell][i_particle];
+
+              const unsigned int particle_id = local_id + starting_points[cpu];
+
+              particles.emplace(cell_iterators[i_cell],
+                                Particle<dim, spacedim>(
+                                  dist_points[i_cell][i_particle],
+                                  dist_reference_points[i_cell][i_particle],
+                                  particle_id));
+
+              if (cpu_to_indices.find(cpu) == cpu_to_indices.end())
+                cpu_to_indices.insert(
+                  {cpu, IndexSet(n_particles_per_proc[cpu])});
+
+              cpu_to_indices[cpu].add_index(local_id);
+            }
+        }
+
+      this->insert_particles(particles);
+      for (auto &c : cpu_to_indices)
+        c.second.compress();
+
+      // Take care of properties, if the input vector contains them.
+      const auto sum_pro =
+        Utilities::MPI::sum(properties.size(),
+                            triangulation->get_communicator());
+      if (sum_pro)
+        {
+          // [TODO]: fix this in some_to_some, to allow communication from
+          // my cpu to my cpu.
+          auto cpu_to_indices_to_send = cpu_to_indices;
+          if (cpu_to_indices_to_send.find(my_cpu) !=
+              cpu_to_indices_to_send.end())
+            cpu_to_indices_to_send.erase(cpu_to_indices_to_send.find(my_cpu));
+
+          // Gather whom I sent my own particles to, to decide whom to send
+          // the particle properties
+          auto send_to_cpu =
+            Utilities::MPI::some_to_some(triangulation->get_communicator(),
+                                         cpu_to_indices_to_send);
+          std::map<unsigned int, std::vector<double>>
+            non_locally_owned_properties;
+
+          // Prepare the vector of non_locally_owned properties,
+          for (const auto &it : send_to_cpu)
+            {
+              std::vector<double> properties_to_send;
+              properties_to_send.reserve(it.second.n_elements() *
+                                         n_properties_per_particle());
+
+              for (const auto &el : it.second)
+                properties_to_send.insert(
+                  properties_to_send.end(),
+                  properties.begin() + el * n_properties_per_particle(),
+                  properties.begin() + (el + 1) * n_properties_per_particle());
+
+              non_locally_owned_properties.insert(
+                {it.first, properties_to_send});
+            }
+
+          // Send the non locally owned properties to each mpi process
+          // that needs them
+          auto locally_owned_properties_from_other_cpus =
+            Utilities::MPI::some_to_some(triangulation->get_communicator(),
+                                         non_locally_owned_properties);
+
+          // Store all local properties in a single vector. This includes
+          // properties coming from my own mpi process, and properties that
+          // were sent to me in the call above.
+          std::vector<double> local_properties;
+          local_properties.reserve(n_locally_owned_particles() *
+                                   n_properties_per_particle());
+
+          // Compute the association between particle id and start of
+          // property data in the vector containing all local properties
+          std::map<types::particle_index, unsigned int> property_start;
+          for (const auto &it : cpu_to_indices)
+            if (it.first != my_cpu)
+              {
+                unsigned int sequential_index = 0;
+                // Process all properties coming from other mpi processes
+                for (const auto &el : it.second)
+                  {
+                    types::particle_index particle_id =
+                      el + starting_points[it.first];
+                    property_start.insert(
+                      {particle_id, local_properties.size()});
+
+                    local_properties.insert(
+                      local_properties.end(),
+                      locally_owned_properties_from_other_cpus.at(it.first)
+                          .begin() +
+                        sequential_index * n_properties_per_particle(),
+                      locally_owned_properties_from_other_cpus.at(it.first)
+                          .begin() +
+                        (sequential_index + 1) * n_properties_per_particle());
+                    sequential_index++;
+                  }
+              }
+            else
+              {
+                // Process all properties that we already own
+                for (const auto &el : it.second)
+                  {
+                    types::particle_index particle_id =
+                      el + starting_points[my_cpu];
+                    property_start.insert(
+                      {particle_id, local_properties.size()});
+
+                    local_properties.insert(local_properties.end(),
+                                            properties.begin() +
+                                              el * n_properties_per_particle(),
+                                            properties.begin() +
+                                              (el + 1) *
+                                                n_properties_per_particle());
+                  }
+              }
+          // Actually fill the property pool of each particle.
+          for (auto particle : *this)
+            {
+              particle.set_property_pool(get_property_pool());
+              const auto id = particle.get_id();
+              Assert(property_start.find(id) != property_start.end(),
+                     ExcInternalError());
+              const auto start = property_start[id];
+              particle.set_properties({local_properties.begin() + start,
+                                       local_properties.begin() + start +
+                                         n_properties_per_particle()});
+            }
+        }
+      return cpu_to_indices;
+    }
 
     /**
      * This function allows to register three additional functions that are
