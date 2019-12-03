c8eb173ef73aa3719c17b169779fe56485617f7b
Wolfgang Bangerth <bangerth@colostate.edu>
2019-12-02 17:05:19 -0700

    Use 5 refinements in step-61 as default.

--- examples/step-61/step-61.cc
+++ examples/step-61/step-61.cc
@@ -275,7 +275,7 @@ namespace Step61
   void WGDarcyEquation<dim>::make_grid()
   {
     GridGenerator::hyper_cube(triangulation, 0, 1);
-    triangulation.refine_global(2);
+    triangulation.refine_global(5);
 
     std::cout << "   Number of active cells: " << triangulation.n_active_cells()
               << std::endl
