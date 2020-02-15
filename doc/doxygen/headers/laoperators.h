02ea6d37bbddd45abe5352266c2951c888e2e0d6
Wolfgang Bangerth <bangerth@colostate.edu>
2020-02-14 22:58:18 -0700

    Use better markup when talking about header files.

--- doc/doxygen/headers/laoperators.h
+++ doc/doxygen/headers/laoperators.h
@@ -97,8 +97,8 @@
  * Consequently, the user program has to include header files for both classes
  * for compilation to be successful. In an attempt to make easier the decision of which
  * headers to include in what circumstances and to prevent hidden templates-related
- * compiler errors, all headers relevant to LinearOperator are grouped in
- * <deal.ii/lac/linear_operator_tools.h>.
+ * compiler errors, all headers relevant to LinearOperator are grouped in the
+ * `<deal.II/lac/linear_operator_tools.h>` header file.
  *
  * <h3>Packaged Operation</h3>
  *
