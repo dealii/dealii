c252c9f9f44dc7ce43c5371381e92992d1427f5a
Wolfgang Bangerth <bangerth@colostate.edu>
2020-03-23 16:58:23 -0600

    Avoid spaces before commas.

--- examples/step-67/step-67.cc
+++ examples/step-67/step-67.cc
@@ -1985,12 +1985,11 @@ namespace Euler_DG
       euler_operator.compute_errors(ExactSolution<dim>(time), solution);
     std::string quantity_name = testcase == 0 ? "error" : "norm";
     pcout << "Time:" << std::setw(8) << std::setprecision(3) << time
-          << " , dt: " << std::setw(8) << std::setprecision(2) << time_step
-          << " , " << quantity_name << " rho: " << std::setprecision(4)
-          << std::setw(10) << errors[0]
-          << " , rho * u: " << std::setprecision(4) << std::setw(10)
-          << errors[1] << " , energy:" << std::setprecision(4) << std::setw(10)
-          << errors[2] << std::endl;
+          << ", dt: " << std::setw(8) << std::setprecision(2) << time_step
+          << ", " << quantity_name << " rho: " << std::setprecision(4)
+          << std::setw(10) << errors[0] << ", rho * u: " << std::setprecision(4)
+          << std::setw(10) << errors[1] << ", energy:" << std::setprecision(4)
+          << std::setw(10) << errors[2] << std::endl;
 
     TimerOutput::Scope t(timer, "output");
 
