7adaf9f54ad27bafe2e26d380e1b9af757d89a32
Peter Munch <peterrmuench@gmail.com>
2020-03-25 10:54:32 +0100

    Introduce new namespace MatrixFreeTools

---
+++ include/deal.II/matrix_free/utilities.h
@@ -0,0 +1,28 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2020 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#ifndef dealii_matrix_free_utilities_h
+#define dealii_matrix_free_utilities_h
+
+#include <deal.II/base/config.h>
+
+/**
+ * A namespace for utility functions in the context of matrix-free operator
+ * evaluation.
+ */
+namespace MatrixFreeTools
+{} // namespace MatrixFreeTools
+
+#endif
