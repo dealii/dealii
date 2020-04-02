5c83a23db9449252e6cd802bcb2b5d419c3acbd7
Jean-Paul Pelteret <jppelteret@gmail.com>
2020-04-02 11:25:59 +0200

    Add BatchOptimizer for symbolic expressions

--- include/deal.II/differentiation/sd.h
+++ include/deal.II/differentiation/sd.h
@@ -23,6 +23,7 @@
 #  include <deal.II/differentiation/sd/symengine_math.h>
 #  include <deal.II/differentiation/sd/symengine_number_traits.h>
 #  include <deal.II/differentiation/sd/symengine_number_types.h>
+#  include <deal.II/differentiation/sd/symengine_optimizer.h>
 #  include <deal.II/differentiation/sd/symengine_product_types.h>
 #  include <deal.II/differentiation/sd/symengine_scalar_operations.h>
 #  include <deal.II/differentiation/sd/symengine_tensor_operations.h>
