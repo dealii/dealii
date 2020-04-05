276ff48bbff5c51e59f0e9f92b0be5dbcb1a6865
Paras Kumar <paras.kumar@fau.de>
2020-04-05 15:36:27 +0200

    added wrapper class for SuperLU-MT Currently, only written for SparseMatrix. There are issues with linking SuperLU-MT library.

---
+++ include/deal.II/lac/superlu_mt.h
@@ -0,0 +1,181 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2001 - 2020 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+#ifndef  dealii_superlu_mt_h
+#define  dealii_superlu_mt_h
+
+#include <deal.II/base/config.h>
+
+#include <deal.II/base/exceptions.h>
+#include <deal.II/base/subscriptor.h>
+
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+
+#include "slu_mt_ddefs.h"
+//#include "slu_mt_sdefs.h"
+
+#include <vector>
+
+DEAL_II_NAMESPACE_OPEN
+
+/**
+ * Wrapper for SuperLU-MT direct linear solver.
+ * \todo Complete the docu here.
+ * *
+ * @ingroup Solvers Preconditioners
+ *
+ * @author Paras Kumar, 2020
+ */
+class SuperLU_MT : public Subscriptor
+{
+public:
+    /**
+     * Declare type for container sizes.
+     */
+    using size_type = types::global_dof_index;
+
+
+    // \todo remove this issue below
+    /**
+     * in order to avoid the error with
+     * <c> invalid conversion from ‘unsigned int*’ to ‘int_t* {aka int*}’ </c>
+     * we create a temporary type. Ideally this should be same as \c size_type
+     */
+    using my_int = int_t;
+
+    /**
+     * Declare types for matrix and vectorm for extension to block sparse matrices
+     * (to be replaced by instantiation later)
+     */
+     using number_type = double;
+     using matrix_type = SparseMatrix<number_type>;
+     using vector_type = Vector<number_type>;
+
+    /**
+   * To contain additional data such as number of process for multi-threading.
+   */
+    struct AdditionalData //: public superlumt_options_t
+    {
+        public:
+            AdditionalData()
+            {}
+
+            explicit AdditionalData(const unsigned int& num_threads,
+                                    const unsigned int& factorize,
+                                    const size_type& col_order)
+                : column_ordering(col_order)
+            {
+                options_.nprocs = num_threads;
+                options_.fact = static_cast<fact_t>(factorize);
+            }
+        /**
+         * Object to store additional options for the solver
+         */
+        superlumt_options_t options_;
+        /**
+         * Parameter to define the type of column ordering for LU factorization.
+         * See \c ispec argument of \c pdgssv routine for details.
+         */
+         size_type column_ordering=0;
+    };
+
+    /**
+     * Construcotr.
+     */
+    SuperLU_MT(const AdditionalData& data = AdditionalData());
+
+    /**
+     * Desctructor
+     */
+     ~SuperLU_MT() override ;
+
+   /**
+    * Function to intialize the datastructures required by the SuperLU-MT
+    * solver.
+    */
+    void
+    initialize(const matrix_type& A, const vector_type& b);
+
+    void
+    solve(vector_type& x);
+
+private:
+
+    /**
+     * Function to clear the memory allocated.
+     */
+    void clear();
+
+    /**
+     * Function to generate the \ac SuperMatrix object as required by the
+     * SuperLU-MT solver.
+     */
+    void create_supermatrices();
+    /**
+     * The dimension of the range space, i.e., the number of rows of the matrix.
+     */
+    size_type n_rows;
+
+    /**
+     * The dimension of the domain space, i.e., the number of columns of the
+     * matrix.
+     */
+    size_type n_cols;
+
+    /**
+     * \c SuperMatrix objects to store the data as required by SuperLU.
+     * SuperLU is actually meant to solve several systems of linear
+     * equations of the form <c> AX = B <\c>, where \c X and \c B are dense
+     * matrices comprising of \c nrhs different right hand sides adn solutions
+     * respectively. Thus, we need to store the right hand side (which will be
+     * overwritten by the solution) as a \c Supermatrix with one column.
+     */
+    SuperMatrix A;
+    SuperMatrix b;
+
+    /**
+     * Object to store additional input arguments for the solver.
+     */
+    AdditionalData additional_data;
+
+    /**
+     * Arrays to store the sparse matrix in the compressed row (SLU_NR)
+     * format of SuperLU.
+     * \c colind: array of column indices of the nonzero values
+     * \c rowptr: array containing beginning of columns in \c nzval_NR and
+     * \c colind. Note: Zero-based indexing is used; \c rowptr has
+     * n_rows+1 entries, the last one pointing beyond the last row,
+     * so that rowptr[n_rows] = nnz.
+     * \c nzval_NR: array of nonzero values, packed by row
+     */
+    std::vector<my_int > colind;
+    std::vector<my_int > rowptr;
+    std::vector<number_type> nzval;
+
+     /**
+      * Since, the right hand side gets overwritten by solution we need to
+      * make a copy of it.
+      */
+     std::vector<number_type> rhs_vals;
+
+};
+
+
+DEAL_II_NAMESPACE_CLOSE
+
+# endif // dealii_superlu_mt_h
---
+++ source/lac/superlu_mt.cc
@@ -0,0 +1,175 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2001 - 2020 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#include <deal.II/lac/superlu_mt.h>
+
+// add ifdef for superlu-mt
+#include "slu_mt_ddefs.h"
+//#include "slu_mt_sdefs.h"
+
+#include <typeinfo>
+
+DEAL_II_NAMESPACE_OPEN
+
+SuperLU_MT::SuperLU_MT(const AdditionalData& data)
+:additional_data(data)
+{}
+
+
+
+SuperLU_MT::~SuperLU_MT()
+{
+    this->clear();
+}
+
+
+
+void
+SuperLU_MT::clear()
+{
+    // \todo complete this definition
+}
+
+
+
+void
+SuperLU_MT::initialize(const matrix_type& A, const vector_type& b)
+{
+    Assert(A.m() == A.n(), ExcNotQuadratic());
+    Assert(A.m() == b.size(), ExcDimensionMismatch(A.m(),b.size()));
+ //   Assert(typeid(number_type) == typeid(matrix_type::value_type),ExcMessage("Types mismatch"));
+
+    // \todo clear all datastructures here before going ahead
+
+    this->n_rows = A.m();
+    this->n_cols = A.n();
+
+   // Actually, the factorize routine of SuperLU-MT only support the
+   // compressed column format (SLU-NC). However, the solver routine also
+   // accepts the compressed row format (SLU-NR) and then internally converts
+   // it into SLU-NC format in order to generate the desired results.
+   // Since, dealii stores matrices in a (modified) crs format. We copy
+   // the data into the crs format  (SLU-NR) of
+   // SuperLU-MT in similar manner to SparseDirectUPFPACK solver.
+   this->rowptr.resize(this->n_rows + 1);
+   this->colind.resize(A.n_nonzero_elements());
+   this->nzval.resize(A.n_nonzero_elements());
+
+   // first fill the rowptr array
+   this->rowptr[0] = 0;
+   for(size_type row_ctr = 1; row_ctr < this->n_rows; ++row_ctr)
+       this->rowptr[row_ctr] = this->rowptr[row_ctr -1] + A.get_row_length(row_ctr-1);
+    Assert(static_cast<size_type>(this->rowptr.back()) == this->colind.size(), ExcInternalError());
+
+    // then copy over matrix elements. note that for sparse matrices,
+    // iterators are sorted so that they traverse each row from start to end
+    // before moving on to the next row. however, this isn't true for block
+    // matrices, so we have to do a bit of book keeping
+    {
+        // have an array that for each row points to the first entry not yet
+        // written to
+        std::vector<my_int> row_pointers = this->rowptr;
+
+        // loop over the elements of the matrix row by row, as suggested in the
+        // documentation of the sparse matrix iterator class
+        for (size_type row = 0; row < A.m(); ++row)
+        {
+            for (typename matrix_type ::const_iterator p = A.begin(row);
+                 p != A.end(row);
+                 ++p)
+            {
+                // write entry into the first free one for this row
+                this->colind[row_pointers[row]] = p->column();
+                this->nzval[row_pointers[row]] = std::real(p->value());
+
+                // then move pointer ahead
+                ++row_pointers[row];
+            }
+        }
+
+        // at the end, we should have written all rows completely
+        for (size_type i = 0; i < this->rowptr.size() - 1; ++i)
+        Assert(row_pointers[i] == this->rowptr[i + 1], ExcInternalError());
+    }
+
+    // now copy the right hand side values
+    std::copy(b.begin(), b.end(), this->rhs_vals.begin());
+
+    // now create the \c SuperMatrix objects needed by the solve routine.
+    this->create_supermatrices();
+}
+
+
+
+void SuperLU_MT::create_supermatrices()
+{
+    const Stype_t Stype_matrix = Stype_t::SLU_NR;
+    const Stype_t Stype_rhs = Stype_t::SLU_DN;
+    const Mtype_t Mtype = Mtype_t::SLU_GE;
+
+    if(typeid(number_type) == typeid(double))
+    {
+        const Dtype_t Dtype = Dtype_t::SLU_D;
+        dCreate_CompCol_Matrix(&A, this->n_rows, this->n_cols, this->nzval.size(), this->nzval.data(),
+                               this->colind.data(), this->rowptr.data(), Stype_matrix, Dtype, Mtype);
+        dCreate_Dense_Matrix(&b, this->n_rows, 1, rhs_vals.data(), this->n_rows, Stype_rhs, Dtype, Mtype);
+    }
+    else if(typeid(number_type) == typeid(float))
+    {
+     //   const auto& Dtype = Dtype_t::SLU_S;
+     //   sCreate_CompCol_Matrix(&A, this->n_rows, this->n_cols, this->nzval.size(), this->nzval.data(),
+     //                          this->colind.data(), this->rowptr.data(), Stype_matrix, Dtype, Mtype);
+     //   sCreate_Dense_Matrix(&b, this->n_rows, 1, rhs_vals.data(), this->n_rows, Stype_rhs, Dtype, Mtype);
+        Assert(false, ExcInternalError());
+    }
+    else
+    Assert(false, ExcMessage("Type not supported"));
+}
+
+
+
+void SuperLU_MT::solve(vector_type &x)
+{
+    // generate the required datastructures
+    int_t info;
+    std::vector<int_t> perm_c(this->n_rows);
+    std::vector<int_t> perm_r(this->n_cols);
+    SuperMatrix L;
+    SuperMatrix U;
+    get_perm_c(this->additional_data.column_ordering, &this->A, perm_c.data());
+
+    // solve the SLE in parallel
+    if(this->A.Dtype == Dtype_t::SLU_D)
+    {
+        pdgssv(this->additional_data.options_.nprocs, &this->A, perm_c.data(), perm_r.data(), &L, &U, &this->b, &info);
+    }
+    else if(this->A.Dtype == Dtype_t::SLU_S)
+    {
+      //  psgssv(this->additional_data.options_.nprocs, &this->A, perm_c.data(), perm_r.data(), &L, &U, &this->b,
+      //  &info);
+      Assert(false, ExcInternalError());
+    }
+
+    // copy solution
+    DNformat *bStore;
+    bStore = (DNformat *) b.Store;
+   // std::copy(bStore->nzval, bStore->nzval + this->nzval.size() * sizeof(number_type), x.begin());
+   // \todo remove error in this copy operation
+
+    // \todo clear vectors before leaving
+}
+
+
+DEAL_II_NAMESPACE_CLOSE
