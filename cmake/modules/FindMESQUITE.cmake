9f72a0365d224e1a5a107dbeb172098e4d6181e7
Jean-Paul Pelteret <jppelteret@gmail.com>
2020-04-05 22:59:45 +0200

    CMake: Add FindMESQUITE.cmake

---
+++ cmake/modules/FindMESQUITE.cmake
@@ -0,0 +1,20 @@
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2012 - 2016 by the deal.II authors
+##
+## This file is part of the deal.II library.
+##
+## The deal.II library is free software; you can use it, redistribute
+## it, and/or modify it under the terms of the GNU Lesser General
+## Public License as published by the Free Software Foundation; either
+## version 2.1 of the License, or (at your option) any later version.
+## The full text of the license can be found in the file LICENSE.md at
+## the top level directory of deal.II.
+##
+## ---------------------------------------------------------------------
+
+
+# This is a dummy file, required to stop CMake form emitting warnings
+# about it not being found.
+# Mesquite is either provided as a bundled package, or must be detected
+# as part of the Trilinos package.
