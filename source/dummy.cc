8cbe9a8cfdf5b4c6a96d5d379d3a561e75c5b6b1
Timo Heister <timo.heister@gmail.com>
2020-04-27 11:03:58 -0400

    CI: clang-tidy all headers
    Hijack the already existing dummy.cc to include a dynamically created
    header that includes all deal.II header files. This will run clang-tidy
    on all headers.
    I tried running clang-tidy on a .cc generated inside the build
    directory, but this doesn't work for some reason.
    
    closes #9933

--- source/dummy.cc
+++ source/dummy.cc
@@ -26,3 +26,12 @@ use_global_symbol_42()
 {
   (void)global_symbol_42;
 }
+
+/**
+ * If we are running the contrib/utilities/run_clang_tidy.sh script, we
+ * generate a header file allheaders.h that includes all deal.II
+ * headers. Include the file here so that all headers are being checked.
+ */
+#ifdef CLANG_TIDY
+#  include <deal.II/allheaders.h>
+#endif
