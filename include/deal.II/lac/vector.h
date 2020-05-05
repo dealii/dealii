3682ce6fd7f2eb1526ad83748212d43875ad21b8
Wolfgang Bangerth <bangerth@colostate.edu>
2020-05-05 08:42:25 -0600

    Implement operator<<(ostream,Vector) in a better way.

--- include/deal.II/lac/vector.h
+++ include/deal.II/lac/vector.h
@@ -1369,10 +1369,18 @@ swap(Vector<Number> &u, Vector<Number> &v)
  */
 template <typename number>
 inline std::ostream &
-operator<<(std::ostream &os, const Vector<number> &v)
+operator<<(std::ostream &out, const Vector<number> &v)
 {
-  v.print(os);
-  return os;
+  Assert(v.size() != 0, ExcEmptyObject());
+  AssertThrow(out, ExcIO());
+
+  for (typename Vector<number>::size_type i = 0; i < v.size() - 1; ++i)
+    out << v(i) << ' ';
+  out << v(v.size() - 1);
+
+  AssertThrow(out, ExcIO());
+
+  return out;
 }
 
 /*@}*/
