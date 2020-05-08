a8f26fc918d6c351a908f0cb9d531d6eec9cba36
Graham Harper <grahambenharper@gmail.com>
2020-05-07 18:39:26 -0600

    Indent

--- include/deal.II/fe/fe_poly.templates.h
+++ include/deal.II/fe/fe_poly.templates.h
@@ -569,15 +569,13 @@ std::vector<unsigned int>
 FE_Poly<dim, spacedim>::get_poly_space_numbering() const
 {
   auto *const space_tensor_prod =
-    dynamic_cast<TensorProductPolynomials<dim> *>(
-      this->poly_space.get());
+    dynamic_cast<TensorProductPolynomials<dim> *>(this->poly_space.get());
 
   if (space_tensor_prod != nullptr)
     return space_tensor_prod->get_numbering();
 
   auto *const space_tensor_prod_const =
-    dynamic_cast<TensorProductPolynomials<dim> *>(
-      this->poly_space.get());
+    dynamic_cast<TensorProductPolynomials<dim> *>(this->poly_space.get());
 
   if (space_tensor_prod_const != nullptr)
     return space_tensor_prod_const->get_numbering();
