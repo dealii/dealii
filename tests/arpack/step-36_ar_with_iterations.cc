bfeabab6b088a606f87fa7de28eb06cfb314496d
Matthias Maier <tamiko@43-1.org>
2020-05-10 18:50:32 -0500

    Tests arpack/step-36_ar* - use solver_within_range macro

--- tests/arpack/step-36_ar_with_iterations.cc
+++ tests/arpack/step-36_ar_with_iterations.cc
@@ -223,12 +223,15 @@ namespace Step36
     ArpackSolver::AdditionalData additional_data(
       num_arnoldi_vectors, ArpackSolver::largest_magnitude, true);
     ArpackSolver eigensolver(solver_control, additional_data);
-    eigensolver.solve(stiffness_matrix,
-                      mass_matrix,
-                      inverse,
-                      eigenvalues,
-                      eigenfunctions,
-                      eigenvalues.size());
+    check_solver_within_range(eigensolver.solve(stiffness_matrix,
+                                                mass_matrix,
+                                                inverse,
+                                                eigenvalues,
+                                                eigenfunctions,
+                                                eigenvalues.size()),
+                              solver_control.last_step(),
+                              2,
+                              10);
 
     // make sure that we have eigenvectors and they are mass-orthonormal:
     // a) (A*x_i-\lambda*B*x_i).L2() == 0
