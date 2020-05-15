b4858559902574d29b5facb10d165f7436c90b5e
Timo Heister <timo.heister@gmail.com>
2020-05-15 18:54:34 -0400

    update copyright years
    with fixed script from #10241 and after a lot of other changes came in.

--- include/deal.II/physics/elasticity/kinematics.h
+++ include/deal.II/physics/elasticity/kinematics.h
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2016 by the deal.II authors
+// Copyright (C) 2016 - 2019 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
