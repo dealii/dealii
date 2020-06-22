4506d263b79c833021dd7ecef7a388d349f45e89
Daniel Arndt <arndtd@ornl.gov>
2020-06-22 15:24:01 -0400

    Readd opencascade/boundary_lib.h

---
+++ include/deal.II/opencascade/boundary_lib.h
@@ -0,0 +1,27 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2014 - 2018 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+#ifndef dealii_occ_boundary_lib_h
+#define dealii_occ_boundary_lib_h
+
+#include <deal.II/base/config.h>
+
+#include <deal.II/opencascade/manifold_lib.h>
+
+DEAL_II_WARNING(
+  "This file is deprecated. Use deal.II/opencascade/manifold_lib.h and the classes therein instead.")
+
+#endif // dealii_occ_boundary_lib_h
