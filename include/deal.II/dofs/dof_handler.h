5fa5e8741590e3d70409c73bdf5d7779a2c1b790
Daniel Arndt <arndtd@ornl.gov>
2020-06-24 15:51:31 -0400

    Remove FunctionMap

--- include/deal.II/dofs/dof_handler.h
+++ include/deal.II/dofs/dof_handler.h
@@ -29,7 +29,6 @@
 #include <deal.II/distributed/tria_base.h>
 
 #include <deal.II/dofs/block_info.h>
-#include <deal.II/dofs/deprecated_function_map.h>
 #include <deal.II/dofs/dof_accessor.h>
 #include <deal.II/dofs/dof_faces.h>
 #include <deal.II/dofs/dof_iterator_selector.h>
