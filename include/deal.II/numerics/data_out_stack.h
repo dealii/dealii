d46d02b9cb51338fa89c01ab24b6783c134e1e4f
Marc Fehling <mafehling.git@gmail.com>
2020-11-20 19:20:45 -0700

    Deprecated DoFHandlerType in DataOutStack.

--- include/deal.II/numerics/data_out_stack.h
+++ include/deal.II/numerics/data_out_stack.h
@@ -38,6 +38,25 @@ class DoFHandler;
 #endif
 
 /**
+ * @deprecated Use DataOutStack<dim, spacedim> instead.
+ */
+template <int dim, int spacedim = dim, typename DoFHandlerType = void>
+class DataOutStack;
+
+#ifndef DOXYGEN
+// prevent doxygen from complaining about potential recursive class relations
+template <int dim, int spacedim, typename DoFHandlerType>
+class DataOutStack : public DataOutStack<dim, spacedim, void>
+{
+public:
+  DEAL_II_DEPRECATED
+  DataOutStack()
+    : DataOutStack<dim, spacedim, void>()
+  {}
+};
+#endif // DOXYGEN
+
+/**
  * This class is used to stack the output from several computations into one
  * output file by stacking the data sets in another co-ordinate direction
  * orthogonal to the space directions. The most common use is to stack the
@@ -111,10 +130,8 @@ class DoFHandler;
  *
  * @ingroup output
  */
-template <int dim,
-          int spacedim            = dim,
-          typename DoFHandlerType = DoFHandler<dim, spacedim>>
-class DataOutStack : public DataOutInterface<dim + 1>
+template <int dim, int spacedim>
+class DataOutStack<dim, spacedim, void> : public DataOutInterface<dim + 1>
 {
 public:
   /**
@@ -156,7 +173,7 @@ public:
    * value.
    */
   void
-  attach_dof_handler(const DoFHandlerType &dof_handler);
+  attach_dof_handler(const DoFHandler<dim, spacedim> &dof_handler);
 
   /**
    * Declare a data vector. The @p vector_type argument determines whether the
@@ -312,8 +329,8 @@ private:
    * DoF handler to be used for the data corresponding to the present
    * parameter value.
    */
-  SmartPointer<const DoFHandlerType,
-               DataOutStack<dim, spacedim, DoFHandlerType>>
+  SmartPointer<const DoFHandler<dim, spacedim>,
+               DataOutStack<dim, spacedim, void>>
     dof_handler;
 
   /**
