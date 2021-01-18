e53d9d4ad84ce8f0eab698e1920bf9f29fe90e05
Peter Munch <peterrmuench@gmail.com>
2021-01-18 20:09:20 +0100

    Add step-3-simplex

---
+++ examples/step-3-simplex/step-3-simplex.cc
@@ -0,0 +1,228 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 1999 - 2021 by the deal.II authors
+ *
+ * This file is part of the deal.II library.
+ *
+ * The deal.II library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE.md at
+ * the top level directory of deal.II.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Authors: Wolfgang Bangerth, 1999,
+ *          Guido Kanschat, 2011,
+ *          Peter Munch, 2021
+ */
+
+
+// @sect3{Many new include files}
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/numerics/data_out.h>
+#include <fstream>
+#include <iostream>
+#include <deal.II/simplex/fe_lib.h>
+#include <deal.II/simplex/quadrature_lib.h>
+#include <deal.II/fe/mapping_fe.h>
+
+using namespace dealii;
+
+// @sect3{The <code>Step3</code> class}
+
+class Step3
+{
+public:
+  Step3();
+
+  void run();
+
+private:
+  void make_grid();
+  void setup_system();
+  void assemble_system();
+  void solve();
+  void output_results() const;
+
+  Triangulation<2>         triangulation;
+  const MappingFE<2>       mapping;
+  const Simplex::FE_P<2>   fe;
+  const Simplex::QGauss<2> quadrature_formula;
+  DoFHandler<2>            dof_handler;
+
+  SparsityPattern      sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+
+  Vector<double> solution;
+  Vector<double> system_rhs;
+};
+
+
+// @sect4{Step3::Step3}
+
+Step3::Step3()
+  : mapping(Simplex::FE_P<2>(1))
+  , fe(1)
+  , quadrature_formula(fe.degree + 1)
+  , dof_handler(triangulation)
+{}
+
+
+// @sect4{Step3::make_grid}
+
+void Step3::make_grid()
+{
+  Triangulation<2> triangulation_temp;
+  GridGenerator::hyper_cube(triangulation_temp, -1, 1);
+  GridGenerator::convert_hypercube_to_simplex_mesh(triangulation_temp,
+                                                   triangulation);
+  triangulation.refine_global(5);
+
+  std::cout << "Number of active cells: " << triangulation.n_active_cells()
+            << std::endl;
+}
+
+
+// @sect4{Step3::setup_system}
+
+void Step3::setup_system()
+{
+  dof_handler.distribute_dofs(fe);
+  std::cout << "Number of degrees of freedom: " << dof_handler.n_dofs()
+            << std::endl;
+  DynamicSparsityPattern dsp(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dsp);
+  sparsity_pattern.copy_from(dsp);
+
+  system_matrix.reinit(sparsity_pattern);
+
+  solution.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+}
+
+
+// @sect4{Step3::assemble_system}
+
+void Step3::assemble_system()
+{
+  QGauss<2>          quadrature_formula(fe.degree + 1);
+  FEValues<2>        fe_values(mapping,
+                        fe,
+                        quadrature_formula,
+                        update_values | update_gradients | update_JxW_values);
+  const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+  FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+  Vector<double>     cell_rhs(dofs_per_cell);
+  std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
+  for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+      fe_values.reinit(cell);
+
+      cell_matrix = 0;
+      cell_rhs    = 0;
+
+      for (const unsigned int q_index : fe_values.quadrature_point_indices())
+        {
+          for (const unsigned int i : fe_values.dof_indices())
+            for (const unsigned int j : fe_values.dof_indices())
+              cell_matrix(i, j) +=
+                (fe_values.shape_grad(i, q_index) * // grad phi_i(x_q)
+                 fe_values.shape_grad(j, q_index) * // grad phi_j(x_q)
+                 fe_values.JxW(q_index));           // dx
+
+          for (const unsigned int i : fe_values.dof_indices())
+            cell_rhs(i) += (fe_values.shape_value(i, q_index) * // phi_i(x_q)
+                            1. *                                // f(x_q)
+                            fe_values.JxW(q_index));            // dx
+        }
+      cell->get_dof_indices(local_dof_indices);
+
+      for (const unsigned int i : fe_values.dof_indices())
+        for (const unsigned int j : fe_values.dof_indices())
+          system_matrix.add(local_dof_indices[i],
+                            local_dof_indices[j],
+                            cell_matrix(i, j));
+
+      for (const unsigned int i : fe_values.dof_indices())
+        system_rhs(local_dof_indices[i]) += cell_rhs(i);
+    }
+
+
+  std::map<types::global_dof_index, double> boundary_values;
+  VectorTools::interpolate_boundary_values(
+    mapping, dof_handler, 0, Functions::ZeroFunction<2>(), boundary_values);
+  MatrixTools::apply_boundary_values(boundary_values,
+                                     system_matrix,
+                                     solution,
+                                     system_rhs);
+}
+
+
+// @sect4{Step3::solve}
+
+void Step3::solve()
+{
+  SolverControl            solver_control(1000, 1e-12);
+  SolverCG<Vector<double>> solver(solver_control);
+  solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+}
+
+
+// @sect4{Step3::output_results}
+
+void Step3::output_results() const
+{
+  DataOut<2> data_out;
+  data_out.attach_dof_handler(dof_handler);
+  data_out.add_data_vector(solution, "solution");
+  data_out.build_patches(mapping);
+  std::ofstream output("solution.vtk");
+  data_out.write_vtk(output);
+}
+
+
+// @sect4{Step3::run}
+
+void Step3::run()
+{
+  make_grid();
+  setup_system();
+  assemble_system();
+  solve();
+  output_results();
+}
+
+
+// @sect3{The <code>main</code> function}
+
+int main()
+{
+  deallog.depth_console(2);
+
+  Step3 laplace_problem;
+  laplace_problem.run();
+
+  return 0;
+}
