636e7795698566a399ca3dfff50d51ca9df15971
Peter Munch <peterrmuench@gmail.com>
2021-02-28 19:30:51 +0100

    Add GridGenerator::MarchingCubeAlgorithm

---
+++ tests/grid/grid_generator_marching_cube_algorithm_01.cc
@@ -0,0 +1,186 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2021 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+// Like remote_point_evaluation_01.cc but normal and curvature vector living on
+// background mesh.
+
+#include <deal.II/base/mpi.h>
+
+#include <deal.II/distributed/shared_tria.h>
+
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_nothing.h>
+#include <deal.II/fe/fe_point_evaluation.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_q_iso_q1.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/mapping_fe_field.h>
+#include <deal.II/fe/mapping_q_generic.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/grid_tools_cache.h>
+
+#include <deal.II/lac/la_parallel_block_vector.h>
+
+#include <deal.II/matrix_free/fe_evaluation.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include "../tests.h"
+
+using namespace dealii;
+
+
+using VectorType = LinearAlgebra::distributed::Vector<double>;
+
+template <int dim>
+class LSFunction : public Function<dim>
+{
+public:
+  LSFunction()
+    : Function<dim>(1)
+  {}
+
+  double
+  value(const Point<dim> &p, const unsigned int component) const
+  {
+    const double v = p.distance(Point<dim>()) - 0.5;
+
+    if (v > +0.1)
+      return +0.1;
+
+    if (v < -0.1)
+      return -0.1;
+
+    return v;
+  }
+};
+
+
+
+template <typename MeshType>
+MPI_Comm
+get_mpi_comm(const MeshType &mesh)
+{
+  const auto *tria_parallel = dynamic_cast<
+    const parallel::TriangulationBase<MeshType::dimension,
+                                      MeshType::space_dimension> *>(
+    &(mesh.get_triangulation()));
+
+  return tria_parallel != nullptr ? tria_parallel->get_communicator() :
+                                    MPI_COMM_SELF;
+}
+
+
+
+template <int dim, int spacedim>
+std::shared_ptr<const Utilities::MPI::Partitioner>
+create_partitioner(const DoFHandler<dim, spacedim> &dof_handler)
+{
+  IndexSet locally_relevant_dofs;
+
+  DoFTools::extract_locally_relevant_dofs(dof_handler, locally_relevant_dofs);
+
+  return std::make_shared<const Utilities::MPI::Partitioner>(
+    dof_handler.locally_owned_dofs(),
+    locally_relevant_dofs,
+    get_mpi_comm(dof_handler));
+}
+
+
+
+template <int dim>
+void
+test()
+{
+  const unsigned int spacedim = dim + 1;
+
+  const unsigned int fe_degree      = 3;
+  const unsigned int mapping_degree = fe_degree;
+  const unsigned int n_refinements  = 5;
+
+
+  const unsigned int background_fe_degree = 3;
+
+  parallel::shared::Triangulation<spacedim> background_tria(
+    MPI_COMM_WORLD, Triangulation<spacedim>::none, true);
+  GridGenerator::subdivided_hyper_rectangle(background_tria,
+                                            {20, 20},
+                                            {-1.0, -1.0},
+                                            {+1.0, +1.0});
+
+  FE_Q<spacedim>       background_fe(background_fe_degree);
+  DoFHandler<spacedim> background_dof_handler(background_tria);
+  background_dof_handler.distribute_dofs(background_fe);
+
+  MappingQ1<spacedim> background_mapping;
+
+  VectorType ls_vector(create_partitioner(background_dof_handler));
+
+  VectorTools::interpolate(background_mapping,
+                           background_dof_handler,
+                           LSFunction<spacedim>(),
+                           ls_vector);
+
+  ls_vector.update_ghost_values();
+
+  const unsigned int n_subdivisions = 3;
+
+  GridGenerator::MarchingCubeAlgorithm<spacedim> mc(
+    background_mapping, background_dof_handler.get_fe(), n_subdivisions);
+
+  parallel::shared::Triangulation<dim, spacedim> tria(
+    MPI_COMM_WORLD, Triangulation<dim, spacedim>::none, true);
+
+  mc.create_triangulation(background_dof_handler, ls_vector, tria);
+
+  // write computed vectors to Paraview
+  if (true)
+    {
+      GridOut().write_mesh_per_processor_as_vtu(tria, "grid_surface");
+    }
+
+  if (true)
+    {
+      DataOutBase::VtkFlags flags;
+      flags.write_higher_order_cells = true;
+
+      DataOut<spacedim> data_out;
+      data_out.set_flags(flags);
+      data_out.add_data_vector(background_dof_handler, ls_vector, "curvature");
+
+      data_out.build_patches(background_mapping, background_fe_degree + 1);
+      data_out.write_vtu_with_pvtu_record("./",
+                                          "data_background",
+                                          0,
+                                          MPI_COMM_WORLD);
+    }
+}
+
+
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
+  MPILogInitAll                    all;
+
+  test<1>();
+}
