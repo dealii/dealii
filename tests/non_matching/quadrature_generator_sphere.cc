169092199e25b3e285bef6bfb425092e8d2570fa
Simon Sticko <simon@sticko.se>
2021-05-03 11:46:14 +0200

    Add class generating high-order immersed quadrature rules
    Add a class NonMatching::QuadratureGenerator that generates high-order
    immersed quadrature rules over a BoundingBox, B, when the domain is
    described by a level set function, \psi. Thus creating quadrature rules
    for the following 3 regions of the box:
    
    {x \in B : \psi(x) < 0},
    {x \in B : \psi(x) > 0},
    {x \in B : \psi(x) = 0}.

---
+++ tests/non_matching/quadrature_generator_sphere.cc
@@ -0,0 +1,101 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2021 - 2021 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#include <deal.II/base/function_level_set.h>
+#include <deal.II/base/quadrature.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/non_matching/quadrature_generator.h>
+
+#include <cmath>
+
+#include "../tests.h"
+
+using namespace dealii;
+
+/**
+ * Compute the volume and surface area of a ball/sphere by setting up a
+ * level set function immersed in a background mesh, generating the
+ * quadrature rules, and summing the weights.
+ */
+template <int dim>
+void
+calculate_volume_and_surface_area()
+{
+  // Set up a background mesh
+  Triangulation<dim> triangulation;
+  const int          n_subdivisions = 12;
+  const double       gridsize       = 2.07;
+  GridGenerator::subdivided_hyper_cube(triangulation,
+                                       n_subdivisions,
+                                       -gridsize / 2,
+                                       gridsize / 2);
+
+
+  // Description of the immersed domain.
+  const Functions::LevelSet::Sphere<dim> level_set;
+
+  // Create a quadrature generator.
+  const hp::QCollection<1>              q_collection1D(QGauss<1>(2));
+  NonMatching::QuadratureGenerator<dim> quadrature_generator(q_collection1D);
+
+  // Go over all cells and compute the volume and surface area.
+  double surface_area = 0, volume = 0;
+  for (const auto cell : triangulation.active_cell_iterators())
+    {
+      // Create a box corresponding to the cell.
+      std::pair<Point<dim>, Point<dim>> lower_upper_corner;
+      lower_upper_corner.first = cell->vertex(0);
+      lower_upper_corner.second =
+        cell->vertex(GeometryInfo<dim>::vertices_per_cell - 1);
+      const BoundingBox<dim> box(lower_upper_corner);
+
+      // Generate immersed quadrature rules.
+      quadrature_generator.generate(level_set, box);
+
+      // Get the quadrature rules.
+      const Quadrature<dim> &inside_quadrature =
+        quadrature_generator.get_inside_quadrature();
+      const NonMatching::ImmersedSurfaceQuadrature<dim> &surface_quadrature =
+        quadrature_generator.get_surface_quadrature();
+
+      // Sum the weights to get the area/volume of the sphere.
+      for (unsigned int i = 0; i < inside_quadrature.size(); ++i)
+        volume += inside_quadrature.weight(i);
+
+      // Sum the weights to get the circumference/surface area of the sphere.
+      for (unsigned int i = 0; i < surface_quadrature.size(); ++i)
+        surface_area += surface_quadrature.weight(i);
+    }
+
+  deallog << "dim = " << dim << std::endl;
+  deallog << (2 == dim ? "area = " : "volume = ");
+  deallog << volume / M_PI << " * pi" << std::endl;
+  deallog << (2 == dim ? "circumference = " : "surface area = ");
+  deallog << surface_area / M_PI << " * pi" << std::endl;
+  deallog << std::endl;
+}
+
+
+int
+main()
+{
+  initlog();
+  calculate_volume_and_surface_area<2>();
+  calculate_volume_and_surface_area<3>();
+}
