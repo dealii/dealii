9eb735326cd362577e7b20feacefd04db9e605e0
Vachan Potluri <vachanpotluri1997@gmail.com>
2021-05-29 10:40:38 +0530

    Move trailing backslash to next line

--- include/deal.II/fe/fe_dgq.h
+++ include/deal.II/fe/fe_dgq.h
@@ -438,11 +438,12 @@ public:
  * space Polynomials::Legendre. The tensor product is achieved using
  * TensorProductPolynomials and the ordering of shape functions, like in
  * TensorProductPolynomials, is lexicographic. For instance, the ordering in 2d
- * is $P_0(x)P_0(y),\ P_1(x)P_0(y),\ \ldots,\ P_n(x)P_0(y),\ P_0(x)P_1(y),\
- * \ldots,\ P_n(x)P_1(y),\ \ldots,\ P_0(x)P_n(y),\ \ldots,\ P_n(x)P_n(y)$ when
- * <tt>degree=n</tt> where $\{P_i\}_{i=0}^{n}$ are the one-dimensional Legendre
- * polynomials defined on $[0,1]$. As opposed to the basic FE_DGQ element,
- * these elements are not interpolatory and no support points are defined.
+ * is $P_0(x)P_0(y),\ P_1(x)P_0(y),\ \ldots,\ P_n(x)P_0(y),\ P_0(x)P_1(y),
+ * \ \ldots,\ P_n(x)P_1(y),\ \ldots,\ P_0(x)P_n(y),\ \ldots,\ P_n(x)P_n(y)$
+ * when <tt>degree=n</tt> where $\{P_i\}_{i=0}^{n}$ are the one-dimensional
+ * Legendre polynomials defined on $[0,1]$. As opposed to the basic FE_DGQ
+ * element, these elements are not interpolatory and no support points are
+ * defined.
  *
  * See the base class documentation in FE_DGQ for details.
  */
