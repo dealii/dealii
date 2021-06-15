be1f35a1fcb9000962b45ff1002a606156cd1539
David Wells <drwells@email.unc.edu>
2021-06-15 08:50:51 -0400

    Reserve step-84.

---
+++ examples/step-84/step-84.cc
@@ -0,0 +1,62 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2021 by the deal.II authors
+ *
+ * This file is part of the deal.II library.
+ *
+ * The deal.II library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE.md at
+ * the top level directory of deal.II.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Author: David Wells, University of North Carolina, Chapel Hill, 2021
+ */
+
+#include <iostream>
+
+namespace Step84
+{
+
+}
+
+// @sect3{The <code>main</code> Function}
+
+// Having made it this far, there is, again, nothing much to discuss for the
+// main function of this program: it looks like all such functions since step-6.
+int main()
+{
+  try
+    {
+      using namespace Step84;
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  return 0;
+}
