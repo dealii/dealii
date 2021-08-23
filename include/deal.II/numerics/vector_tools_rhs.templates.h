63974886a19080e2bf4a6715a7d70178564c4694
Fabian Castelli <50630942+gfcas@users.noreply.github.com>
2021-08-23 20:08:04 +0200

    Update include/deal.II/numerics/vector_tools_rhs.templates.h
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>

--- include/deal.II/numerics/vector_tools_rhs.templates.h
+++ include/deal.II/numerics/vector_tools_rhs.templates.h
@@ -534,7 +534,7 @@ namespace VectorTools
               dofs.resize(dofs_per_cell);
               cell_vector.reinit(dofs_per_cell);
 
-              const std::vector<double> &weights = fe_values.get_JxW_values();
+              const auto &weights = fe_values.get_JxW_values();
               rhs_function.value_list(fe_values.get_quadrature_points(),
                                       rhs_values);
 
