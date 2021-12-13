dd59a7d793c337b461a5cd35587ac9ad6afb3d64
Peter Munch <peterrmuench@gmail.com>
2021-12-13 16:34:03 +0100

    Geometric global coarsening: enable FE_Nothing

--- include/deal.II/multigrid/mg_transfer_global_coarsening.templates.h
+++ include/deal.II/multigrid/mg_transfer_global_coarsening.templates.h
@@ -1034,7 +1034,8 @@ namespace internal
       touch_count.reinit(transfer.partitioner_fine);
 
       for (const auto i : transfer.level_dof_indices_fine)
-        touch_count.local_element(i) += 1;
+        if (i != numbers::invalid_dof_index)
+          touch_count.local_element(i) += 1;
       touch_count.compress(VectorOperation::add);
 
       IndexSet locally_relevant_dofs;
@@ -1120,6 +1121,9 @@ namespace internal
 
       loop_over_active_or_level_cells(
         dof_handler_fine, mg_level_fine, [&](const auto &cell) {
+          if (cell->get_fe().n_dofs_per_cell() == 0) /*FE_Nothing*/
+            return;
+
           min_active_fe_indices[0] =
             std::min(min_active_fe_indices[0], cell->active_fe_index());
           max_active_fe_indices[0] =
@@ -1128,6 +1132,9 @@ namespace internal
 
       loop_over_active_or_level_cells(
         dof_handler_coarse, mg_level_coarse, [&](const auto &cell) {
+          if (cell->get_fe().n_dofs_per_cell() == 0) /*FE_Nothing*/
+            return;
+
           min_active_fe_indices[1] =
             std::min(min_active_fe_indices[1], cell->active_fe_index());
           max_active_fe_indices[1] =
@@ -1149,9 +1156,11 @@ namespace internal
       AssertDimension(min_active_fe_indices[0], max_active_fe_indices[0]);
       AssertDimension(min_active_fe_indices[1], max_active_fe_indices[1]);
 
-      const auto &fe_fine = dof_handler_fine.get_fe(min_active_fe_indices[0]);
-      const auto &fe_coarse =
-        dof_handler_coarse.get_fe(min_active_fe_indices[1]);
+      const unsigned int active_fe_index_fine   = min_active_fe_indices[0];
+      const unsigned int active_fe_index_coarse = min_active_fe_indices[1];
+
+      const auto &fe_fine   = dof_handler_fine.get_fe(active_fe_index_fine);
+      const auto &fe_coarse = dof_handler_coarse.get_fe(active_fe_index_coarse);
 
       // extract number of components
       AssertDimension(fe_fine.n_components(), fe_coarse.n_components());
@@ -1351,30 +1360,46 @@ namespace internal
         process_cells(
           [&](const auto &cell_coarse, const auto &cell_fine) {
             // parent
-            {
-              if (mg_level_coarse == numbers::invalid_unsigned_int)
-                cell_coarse->get_dof_indices(local_dof_indices);
-              else
-                cell_coarse->get_mg_dof_indices(local_dof_indices);
+            if (cell_coarse->active_fe_index() == active_fe_index_coarse)
+              {
+                if (mg_level_coarse == numbers::invalid_unsigned_int)
+                  cell_coarse->get_dof_indices(local_dof_indices);
+                else
+                  cell_coarse->get_mg_dof_indices(local_dof_indices);
 
-              for (unsigned int i = 0;
-                   i < transfer.schemes[0].n_dofs_per_cell_coarse;
-                   i++)
-                level_dof_indices_coarse_0[i] =
-                  transfer.partitioner_coarse->global_to_local(
-                    local_dof_indices[lexicographic_numbering_coarse[i]]);
-            }
+                for (unsigned int i = 0;
+                     i < transfer.schemes[0].n_dofs_per_cell_coarse;
+                     i++)
+                  level_dof_indices_coarse_0[i] =
+                    transfer.partitioner_coarse->global_to_local(
+                      local_dof_indices[lexicographic_numbering_coarse[i]]);
+              }
+            else
+              {
+                for (unsigned int i = 0;
+                     i < transfer.schemes[0].n_dofs_per_cell_coarse;
+                     i++)
+                  level_dof_indices_coarse_0[i] = numbers::invalid_dof_index;
+              }
 
             // child
-            {
-              cell_fine.get_dof_indices(local_dof_indices);
-              for (unsigned int i = 0;
-                   i < transfer.schemes[0].n_dofs_per_cell_coarse;
-                   i++)
-                level_dof_indices_fine_0[i] =
-                  transfer.partitioner_fine->global_to_local(
-                    local_dof_indices[lexicographic_numbering_fine[i]]);
-            }
+            if (cell_fine.active_fe_index() == active_fe_index_fine)
+              {
+                cell_fine.get_dof_indices(local_dof_indices);
+                for (unsigned int i = 0;
+                     i < transfer.schemes[0].n_dofs_per_cell_coarse;
+                     i++)
+                  level_dof_indices_fine_0[i] =
+                    transfer.partitioner_fine->global_to_local(
+                      local_dof_indices[lexicographic_numbering_fine[i]]);
+              }
+            else
+              {
+                for (unsigned int i = 0;
+                     i < transfer.schemes[0].n_dofs_per_cell_coarse;
+                     i++)
+                  level_dof_indices_fine_0[i] = numbers::invalid_dof_index;
+              }
 
             // move pointers
             {
@@ -1388,29 +1413,49 @@ namespace internal
             // parent (only once at the beginning)
             if (c == 0)
               {
-                if (mg_level_coarse == numbers::invalid_unsigned_int)
-                  cell_coarse->get_dof_indices(local_dof_indices);
-                else
-                  cell_coarse->get_mg_dof_indices(local_dof_indices);
+                if (cell_coarse->active_fe_index() == active_fe_index_coarse)
+                  {
+                    if (mg_level_coarse == numbers::invalid_unsigned_int)
+                      cell_coarse->get_dof_indices(local_dof_indices);
+                    else
+                      cell_coarse->get_mg_dof_indices(local_dof_indices);
 
-                for (unsigned int i = 0;
-                     i < transfer.schemes[1].n_dofs_per_cell_coarse;
-                     i++)
-                  level_dof_indices_coarse_1[i] =
-                    transfer.partitioner_coarse->global_to_local(
-                      local_dof_indices[lexicographic_numbering_coarse[i]]);
+                    for (unsigned int i = 0;
+                         i < transfer.schemes[1].n_dofs_per_cell_coarse;
+                         i++)
+                      level_dof_indices_coarse_1[i] =
+                        transfer.partitioner_coarse->global_to_local(
+                          local_dof_indices[lexicographic_numbering_coarse[i]]);
+                  }
+                else
+                  {
+                    for (unsigned int i = 0;
+                         i < transfer.schemes[1].n_dofs_per_cell_coarse;
+                         i++)
+                      level_dof_indices_coarse_1[i] =
+                        numbers::invalid_dof_index;
+                  }
               }
 
             // child
-            {
-              cell_fine.get_dof_indices(local_dof_indices);
-              for (unsigned int i = 0;
-                   i < transfer.schemes[1].n_dofs_per_cell_coarse;
-                   i++)
-                level_dof_indices_fine_1[cell_local_chilren_indices[c][i]] =
-                  transfer.partitioner_fine->global_to_local(
-                    local_dof_indices[lexicographic_numbering_fine[i]]);
-            }
+            if (cell_fine.active_fe_index() == active_fe_index_fine)
+              {
+                cell_fine.get_dof_indices(local_dof_indices);
+                for (unsigned int i = 0;
+                     i < transfer.schemes[1].n_dofs_per_cell_coarse;
+                     i++)
+                  level_dof_indices_fine_1[cell_local_chilren_indices[c][i]] =
+                    transfer.partitioner_fine->global_to_local(
+                      local_dof_indices[lexicographic_numbering_fine[i]]);
+              }
+            else
+              {
+                for (unsigned int i = 0;
+                     i < transfer.schemes[1].n_dofs_per_cell_coarse;
+                     i++)
+                  level_dof_indices_fine_1[cell_local_chilren_indices[c][i]] =
+                    numbers::invalid_dof_index;
+              }
 
             // move pointers (only once at the end)
             if (c + 1 == GeometryInfo<dim>::max_children_per_cell)
@@ -2663,8 +2708,10 @@ MGTwoLevelTransfer<dim, LinearAlgebra::distributed::Vector<Number>>::
               if ((scheme.n_dofs_per_cell_fine != 0) &&
                   (scheme.n_dofs_per_cell_coarse != 0))
                 for (unsigned int i = 0; i < scheme.n_dofs_per_cell_fine; ++i)
-                  this->vec_coarse.local_element(indices_coarse[i]) =
-                    vec_fine_ptr->local_element(indices_fine[i]);
+                  if (indices_coarse[i] != numbers::invalid_unsigned_int &&
+                      indices_fine[i] != numbers::invalid_unsigned_int)
+                    this->vec_coarse.local_element(indices_coarse[i]) =
+                      vec_fine_ptr->local_element(indices_fine[i]);
 
               indices_fine += scheme.n_dofs_per_cell_fine;
               indices_coarse += scheme.n_dofs_per_cell_coarse;
