39c7c82d0ed4eb11b89d9fe4a070ae1fa2bcd2bc
David Wells <drwells@email.unc.edu>
2022-05-16 14:00:19 -0400

    Avoid doing extra TLS lookups in FunctionParser.
    While we are at it, get rid of the calls to begin_raw() and end_raw() (they are
    deprecated).

--- source/base/tensor_function_parser.cc
+++ source/base/tensor_function_parser.cc
@@ -14,6 +14,7 @@
 // ---------------------------------------------------------------------
 
 
+#include <deal.II/base/array_view.h>
 #include <deal.II/base/mu_parser_internal.h>
 #include <deal.II/base/patterns.h>
 #include <deal.II/base/tensor.h>
@@ -87,7 +88,7 @@ TensorFunctionParser<rank, dim, Number>::initialize(
   const std::map<std::string, double> &constants,
   const bool                           time_dependent)
 {
-  this->tfp.clear(); // this will reset all thread-local objects
+  this->parser_data.clear(); // this will reset all thread-local objects
 
   this->constants   = constants;
   this->var_names   = Utilities::split_string_list(variables, ',');
@@ -172,28 +173,26 @@ TensorFunctionParser<rank, dim, Number>::init_muparser() const
   // check that we have not already initialized the parser on the
   // current thread, i.e., that the current function is only called
   // once per thread
-  Assert(tfp.get().size() == 0, ExcInternalError());
+  ParserData &data = parser_data.get();
+  Assert(data.parsers.size() == 0 && data.vars.size() == 0, ExcInternalError());
 
-  // initialize the objects for the current thread (tfp.get() and
-  // vars.get())
-  tfp.get().reserve(this->n_components);
-  vars.get().resize(var_names.size());
+  // initialize the objects for the current thread
+  data.parsers.reserve(this->n_components);
+  data.vars.resize(var_names.size());
   for (unsigned int component = 0; component < this->n_components; ++component)
     {
-      tfp.get().emplace_back(
+      data.parsers.emplace_back(
         std::make_unique<
           internal::TensorFunctionParserImplementation::Parser>());
       mu::Parser &parser =
         dynamic_cast<internal::TensorFunctionParserImplementation::Parser &>(
-          *tfp.get().back());
+          *data.parsers.back());
 
       for (const auto &constant : constants)
-        {
-          parser.DefineConst(constant.first, constant.second);
-        }
+        parser.DefineConst(constant.first, constant.second);
 
       for (unsigned int iv = 0; iv < var_names.size(); ++iv)
-        parser.DefineVar(var_names[iv], &vars.get()[iv]);
+        parser.DefineVar(var_names[iv], &data.vars[iv]);
 
       // define some compatibility functions:
       parser.DefineFun("if", internal::FunctionParser::mu_if, true);
@@ -219,7 +218,7 @@ TensorFunctionParser<rank, dim, Number>::init_muparser() const
           // space between the name of the function and the opening
           // parenthesis. this is awkward because it is not backward
           // compatible to the library we used to use before muparser
-          // (the tfparser library) but also makes no real sense.
+          // (the fparser library) but also makes no real sense.
           // consequently, in the expressions we set, remove any space
           // we may find after function names
           std::string transformed_expression = expressions[component];
@@ -295,32 +294,30 @@ TensorFunctionParser<rank, dim, Number>::value(const Point<dim> &p) const
   Assert(initialized == true, ExcNotInitialized());
 
   // initialize the parser if that hasn't happened yet on the current thread
-  if (tfp.get().size() == 0)
+  ParserData &data = parser_data.get();
+  if (data.vars.size() == 0)
     init_muparser();
 
   for (unsigned int i = 0; i < dim; ++i)
-    vars.get()[i] = p(i);
+    data.vars[i] = p(i);
   if (dim != n_vars)
-    vars.get()[dim] = this->get_time();
+    data.vars[dim] = this->get_time();
 
-  // initialize tensor with zeros
-  Tensor<rank, dim, Number> value;
+  std::array<Number, Tensor<rank, dim, Number>::n_independent_components>
+    values;
 
   try
     {
-      unsigned int component = 0;
-      for (Number *value_ptr = value.begin_raw(); value_ptr != value.end_raw();
-           ++value_ptr)
+      for (unsigned int component = 0; component < values.size(); ++component)
         {
           Assert(dynamic_cast<
                    internal::TensorFunctionParserImplementation::Parser *>(
-                   tfp.get()[component].get()),
+                   data.parsers[component].get()),
                  ExcInternalError());
           mu::Parser &parser = static_cast< // NOLINT
             internal::TensorFunctionParserImplementation::Parser &>(
-            *tfp.get()[component]);
-          *value_ptr = parser.Eval();
-          ++component;
+            *data.parsers[component]);
+          values[component] = parser.Eval();
         } // for
     }     // try
   catch (mu::ParserError &e)
@@ -335,7 +332,8 @@ TensorFunctionParser<rank, dim, Number>::value(const Point<dim> &p) const
       return Tensor<rank, dim, Number>();
     } // catch
 
-  return value;
+  return Tensor<rank, dim, Number>(
+    make_array_view(values.begin(), values.end()));
 }
 
 
