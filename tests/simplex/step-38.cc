fde6997f22ad714380c2133647e3031d2aac6b3f
Wolfgang Bangerth <bangerth@colostate.edu>
2022-05-21 21:23:38 -0600

    Update copyright years.

--- tests/simplex/step-38.cc
+++ tests/simplex/step-38.cc
@@ -1,6 +1,6 @@
 /* ---------------------------------------------------------------------
  *
- * Copyright (C) 2010 - 2021 by the deal.II authors
+ * Copyright (C) 2010 - 2022 by the deal.II authors
  *
  * This file is part of the deal.II library.
  *
