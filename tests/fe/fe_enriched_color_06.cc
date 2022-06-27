7f05289a4ecce6f0d558557937ea1d683b746bd0
Daniel Arndt <arndtd@ornl.gov>
2022-06-27 07:51:23 -0400

    Fix the reamaining occurrences

--- tests/fe/fe_enriched_color_06.cc
+++ tests/fe/fe_enriched_color_06.cc
@@ -115,7 +115,8 @@ main(int argc, char **argv)
     {
       // constant function.
       Functions::ConstantFunction<dim> func(10 + i); // constant function
-      vec_enrichments.push_back(std::make_shared<ConstantFunction<dim>>(func));
+      vec_enrichments.push_back(
+        std::make_shared<Functions::ConstantFunction<dim>>(func));
     }
 
   // Construct helper class to construct FE collection
