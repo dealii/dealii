9f50087fd2cf3ad3947ae850e01520f483e9648c
Nicolas Barnafi <nabw91@gmail.com>
2022-09-11 15:11:55 +0200

    missing parenthesis

--- source/lac/petsc_precondition.cc
+++ source/lac/petsc_precondition.cc
@@ -955,9 +955,7 @@ namespace PETScWrappers
     ierr = PCSetFromOptions(pc);
     AssertThrow(ierr == 0, ExcPETScError(ierr));
 #  else
-    {
-        AssertThrow(false, ExcMessage("BDDC preconditioner requires PETSc 3.10.0 or newer");
-    }
+    AssertThrow(false, ExcMessage("BDDC preconditioner requires PETSc 3.10.0 or newer"));
 #  endif
   }
 
