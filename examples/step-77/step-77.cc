e09310ce5ceeefbe5608168baa27fe2f3a05520c
Stefano Zampini <stefano.zampini@gmail.com>
2022-11-26 21:42:31 +0300

    STEP-77: fix bug

--- examples/step-77/step-77.cc
+++ examples/step-77/step-77.cc
@@ -364,7 +364,7 @@ namespace Step77
                                     evaluation_point_gradients[q]);
 
             for (unsigned int i = 0; i < dofs_per_cell; ++i)
-              cell_residual(i) =
+              cell_residual(i) +=
                 (fe_values.shape_grad(i, q)      // \nabla \phi_i
                  * coeff                         // * a_n
                  * evaluation_point_gradients[q] // * \nabla u_n
