633e97dcd37cf4cd7cc8d472d80f6b0132a5878e
Matthias Maier <tamiko@43-1.org>
2022-12-10 00:38:49 -0600

    tests: add a test target that runs the quick tests

--- tests/run_quick_tests.cmake
+++ tests/run_quick_tests.cmake
@@ -13,40 +13,48 @@
 ##
 ## ---------------------------------------------------------------------
 
+#
 # This file is run when "make test" is executed by the user and is
-# responsible for running the tests and printing some helpful
-# error messages.
+# responsible for running the tests and printing some helpful error
+# messages.
+#
+
 include(ProcessorCount)
 PROCESSORCOUNT(_n_processors)
+
 if(_n_processors EQUAL 0)
   set(_n_processors "1")
 endif()
 
-
-# Windows quick tests have a race condition, so disable compiling/running
-# tests in parallel. This avoid errors like:
-#
-# error MSB3491: Could not write lines to file
-# "obj_boost_system_debug.dir\Debug\obj_boos.4A356C5C.tlog\obj_boost_system_debug.lastbuildstate". The
-# process cannot access the file '...' because it is being used by another
-# process.
+# Avoid race conditions with native Windows build tools:
 if(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
   set(_n_processors "1")
 endif()
 
-separate_arguments(ALL_TESTS)
+if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+  set(CMAKE_BUILD_TYPE "Debug")
+endif()
+message(STATUS "Running quick_tests in ${CMAKE_BUILD_TYPE} mode with -j${_n_processors}:")
 
-execute_process(COMMAND ${CMAKE_CTEST_COMMAND} -j${_n_processors}
-  -C ${CMAKE_BUILD_TYPE}
-  --force-new-ctest-process
-  --output-on-failure
-  -O quicktests.log
-  RESULT_VARIABLE res_var)
+#
+# Always restrict quick tests with specified build type, but run the step
+# and affinity quick tests in all available configuration:
+#
 
-if(NOT "${res_var}" STREQUAL "0")
+string(TOLOWER "${CMAKE_BUILD_TYPE}" _build_type)
+execute_process(COMMAND ${CMAKE_CTEST_COMMAND}
+  -j${_n_processors} -C ${CMAKE_BUILD_TYPE} --force-new-ctest-process
+  -R "quick_tests/(step.debug|step.release|affinity|.*.${_build_type})"
+  OUTPUT_VARIABLE _output ERROR_VARIABLE _output RESULT_VARIABLE _return_value
+  )
+message(${_output})
+
+if(NOT "${_return_value}" STREQUAL "0")
   message("
 ***************************************************************************
+**                                                                       **
 **                 Error: Some of the quick tests failed.                **
+**                                                                       **
 ***************************************************************************
 
 Please scroll up or check the file tests/quick_tests/quicktests.log for the
@@ -54,39 +62,31 @@ error messages. If you are unable to fix the problems, see the FAQ or write
 to the mailing list linked at http://www.dealii.org\n"
     )
 
-  foreach(test ${ALL_TESTS})  
-    if (${test} MATCHES "^affinity" AND NOT EXISTS ${test}-OK)
+  string(REPLACE "\n" ";" _output "${_output}")
+  foreach(_line ${_output})
+    if ("${_line}" MATCHES "affinity.*FAILED")
       message("
 The affinity test can fail when you are linking in a library like BLAS
 which uses OpenMP. Even without calling any BLAS functions, OpenMP messes
 with the thread affinity which causes TBB to run single-threaded only. You
-can fix this by exporting OMP_NUM_THREADS=1. Also see GOMP_CPU_AFFINITY 
+can fix this by exporting OMP_NUM_THREADS=1. Also see GOMP_CPU_AFFINITY
 and OMP_PROC_BIND.\n"
         )
     endif()
 
-    if (${test} MATCHES "^step-petsc" AND NOT EXISTS ${test}-OK)
+    if (${_line} MATCHES "step-petsc.*FAILED")
       message("
 Additional information about PETSc issues is available
 at:\nhttp://www.dealii.org/developer/external-libs/petsc.html\n"
         )
     endif()
 
-    if (${test} MATCHES "^p4est" AND NOT EXISTS ${test}-OK)
+    if (${test} MATCHES "p4est.*FAILED" AND NOT EXISTS ${test}-OK)
       message("
 The p4est test can fail if you are running an OpenMPI version before 1.5.
 This is a known problem and the only work around is to update to a more
 recent version or use a different MPI library like MPICH.\n"
         )
     endif()
-
   endforeach()
-
-  # The CMake command message(SEND_ERROR ...) is, to the best of the authors'
-  # knowledge, the only way to set the exit status of CMake to a nonzero value.
-  # If we used message(SEND_ERROR ...) at the top (with the actual error
-  # message) then subsequent messages (i.e., the test specific help) would not
-  # be printed. Hence, do it down here.
-  message(SEND_ERROR "")
-
 endif()
