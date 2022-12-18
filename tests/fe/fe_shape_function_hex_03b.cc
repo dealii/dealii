71fc4da58dd64182d47d8f92c3be4429a4cd3e73
Jean-Paul Pelteret <jppelteret@gmail.com>
2022-12-18 21:21:29 +0100

    FE tests: Plot shape functions on Hexahedra

---
+++ tests/fe/fe_shape_function_hex_03b.cc
@@ -0,0 +1,53 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2022 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+// Plot shape functions for scalar and vector shape functions
+// - Hexahedra
+// - FE_NedelecSZ
+
+#include <deal.II/fe/fe_nedelec_sz.h>
+
+#include "../tests.h"
+
+#include "common/fe_shape_function_plotter.h"
+
+
+template <int dim>
+void
+plot_all(const unsigned int n_divs = 4)
+{
+  std::cout << "Plotting in dim " << dim << "." << std::endl;
+
+  Assert(dim == 2 || dim == 3, ExcDimensionMismatch2(dim, 2, 3));
+  const ReferenceCell &reference_cell = ReferenceCells::get_hypercube<dim>();
+
+  plot_one<FE_NedelecSZ<dim>>("FE_NedelecSZ", 0, 1, reference_cell, n_divs);
+}
+
+
+int
+main()
+{
+  initlog();
+
+  // These are very expensive elements to evaluate
+  const unsigned int n_divs = 2;
+  plot_all<3>(n_divs);
+
+  deallog << "OK" << std::endl;
+
+  return 0;
+}
