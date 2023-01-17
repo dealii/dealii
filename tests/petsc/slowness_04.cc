522444094337adc3dc1fa41bc51f131b29b0640e
Stefano Zampini <stefano.zampini@gmail.com>
2023-01-17 11:59:13 +0300

    Add missing compress in tests

--- tests/petsc/slowness_04.cc
+++ tests/petsc/slowness_04.cc
@@ -145,6 +145,8 @@ test()
   PETScWrappers::MPI::Vector v2(PETSC_COMM_WORLD, N * N, N * N);
   for (unsigned int i = 0; i < N * N; ++i)
     v1(i) = i;
+  v1.compress(VectorOperation::insert);
+
   matrix.vmult(v2, v1);
 
   deallog << v1 * v2 << std::endl;
