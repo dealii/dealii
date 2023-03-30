29a7ba33b74a9d53c8ec9307ddc06d9c081658f3
Matthias Maier <tamiko@43-1.org>
2023-03-30 11:21:17 -0500

    CMake: refactor repetitive option handling into two macros

--- cmake/macros/macro_populate_target_properties.cmake
+++ cmake/macros/macro_populate_target_properties.cmake
@@ -104,19 +104,15 @@ function(populate_target_properties _target _build)
   # interface:
   #
 
-  separate_arguments(_compile_options UNIX_COMMAND
+  target_compile_flags(${_target} PRIVATE
     "${DEAL_II_WARNING_FLAGS} ${DEAL_II_CXX_FLAGS} ${DEAL_II_CXX_FLAGS_${_build}}"
     )
-  shell_escape_option_groups(_compile_options)
-  target_compile_options(${_target} PRIVATE ${_compile_options})
 
   get_property(_type TARGET ${_target} PROPERTY TYPE)
   if(NOT "${_type}" STREQUAL "OBJECT_LIBRARY")
-    separate_arguments(_link_options UNIX_COMMAND
+    target_link_flags(${_target} PRIVATE
       "${DEAL_II_LINKER_FLAGS} ${DEAL_II_LINKER_FLAGS_${_build}}"
       )
-    shell_escape_option_groups(_link_options)
-    target_link_options(${_target} PRIVATE ${_link_options})
   endif()
 
   target_link_libraries(${_target} ${_visibility}
