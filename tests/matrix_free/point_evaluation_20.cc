f121f893f95c8171ba463284da7f5a02faace640
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2023-03-30 08:58:32 -0400

    Add test

---
+++ tests/matrix_free/point_evaluation_20.cc
@@ -0,0 +1,146 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2021 - 2022 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+// Check FEPointEvaluation::evaluate()/integrate() with precomputed mapping
+// with different location of unit points provided my NonMatching::MappingInfo.
+
+#include <deal.II/base/function_lib.h>
+
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/mapping_cartesian.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/vector.h>
+
+#include <deal.II/matrix_free/fe_point_evaluation.h>
+
+#include <deal.II/numerics/vector_tools.h>
+
+#include <iostream>
+
+#include "../tests.h"
+
+
+
+template <int dim>
+class MyFunction : public Function<dim>
+{
+public:
+  MyFunction()
+    : Function<dim>(dim)
+  {}
+
+  double
+  value(const Point<dim> &p, const unsigned int component) const override
+  {
+    return component + p[component];
+  }
+};
+
+
+
+template <int dim>
+void
+test()
+{
+  using namespace dealii;
+  Triangulation<dim> tria;
+
+  GridGenerator::subdivided_hyper_cube(tria, 2, 0, 1);
+
+  MappingCartesian<dim> mapping;
+  deallog << "Cartesian linear mapping" << std::endl;
+
+  std::vector<Point<dim>> unit_points;
+  for (unsigned int i = 0; i < 13; ++i)
+    {
+      Point<dim> p;
+      for (unsigned int d = 0; d < dim; ++d)
+        p[d] = static_cast<double>(i) / 17. + 0.015625 * d;
+      unit_points.push_back(p);
+    }
+
+  std::vector<Point<dim>> unit_points2;
+  for (unsigned int i = 0; i < 13; ++i)
+    {
+      Point<dim> p;
+      for (unsigned int d = 0; d < dim; ++d)
+        p[d] = static_cast<double>(i) / 19. + 0.015625 * d;
+      unit_points2.push_back(p);
+    }
+
+  FESystem<dim>   fe(FE_Q<dim>(2), dim);
+  DoFHandler<dim> dof_handler(tria);
+  dof_handler.distribute_dofs(fe);
+  Vector<double> vector(dof_handler.n_dofs());
+
+  // mapping info object for precomputed mapping
+  NonMatching::MappingInfo<dim, dim> mapping_info(mapping,
+                                                  update_values |
+                                                    update_gradients);
+  // First evaluator to use precomputed mapping
+  FEPointEvaluation<dim, dim> evaluator1(mapping_info, fe);
+
+  VectorTools::interpolate(mapping, dof_handler, MyFunction<dim>(), vector);
+
+  std::vector<double> solution_values(fe.dofs_per_cell);
+
+  for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+      cell->get_dof_values(vector,
+                           solution_values.begin(),
+                           solution_values.end());
+
+      mapping_info.reinit(cell, unit_points);
+
+      evaluator1.evaluate(solution_values,
+                          EvaluationFlags::values | EvaluationFlags::gradients);
+
+      deallog << "Cell with center " << cell->center(true) << std::endl;
+      for (unsigned int i = 0; i < unit_points.size(); ++i)
+        deallog << mapping.transform_unit_to_real_cell(cell, unit_points[i])
+                << ": " << evaluator1.get_value(i) << std::endl;
+      deallog << std::endl;
+
+      mapping_info.reinit(cell, unit_points2);
+
+      evaluator1.evaluate(solution_values,
+                          EvaluationFlags::values | EvaluationFlags::gradients);
+
+      deallog << "Cell with center " << cell->center(true) << std::endl;
+      for (unsigned int i = 0; i < unit_points2.size(); ++i)
+        deallog << mapping.transform_unit_to_real_cell(cell, unit_points2[i])
+                << ": " << evaluator1.get_value(i) << std::endl;
+      deallog << std::endl;
+    }
+}
+
+
+
+int
+main()
+{
+  initlog();
+  deallog << std::setprecision(10);
+
+  test<2>();
+  test<3>();
+}
