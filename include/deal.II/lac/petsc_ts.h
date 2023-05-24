ad83973bbd73fb5bf922a797b8f6d4248b22b5c3
Wolfgang Bangerth <bangerth@colostate.edu>
2023-05-24 14:55:53 -0600

    Also check PETSc TS and SNES requirements on matrices.

--- include/deal.II/lac/petsc_ts.h
+++ include/deal.II/lac/petsc_ts.h
@@ -295,8 +295,15 @@ namespace PETScWrappers
   template <typename VectorType  = PETScWrappers::VectorBase,
             typename PMatrixType = PETScWrappers::MatrixBase,
             typename AMatrixType = PMatrixType>
-  DEAL_II_CXX20_REQUIRES((concepts::is_dealii_petsc_vector_type<VectorType> ||
-                          std::constructible_from<VectorType, Vec>))
+  DEAL_II_CXX20_REQUIRES(
+    (concepts::is_dealii_petsc_vector_type<VectorType> ||
+     std::constructible_from<
+       VectorType,
+       Vec>)&&(concepts::is_dealii_petsc_matrix_type<PMatrixType> ||
+               std::constructible_from<
+                 PMatrixType,
+                 Mat>)&&(concepts::is_dealii_petsc_matrix_type<AMatrixType> ||
+                         std::constructible_from<AMatrixType, Mat>))
   class TimeStepper
   {
   public:
