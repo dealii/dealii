d6405cb90cbf5ff76e3415bd4d13eb8d48188448
Wolfgang Bangerth <bangerth@colostate.edu>
2023-06-01 15:36:37 -0600

    Unify multiple copies of the same function.

--- source/sundials/kinsol.cc
+++ source/sundials/kinsol.cc
@@ -37,6 +37,7 @@
 #  endif
 
 #  include <deal.II/sundials/n_vector.h>
+#  include <deal.II/sundials/utilities.h>
 
 // Make sure we #include the SUNDIALS config file...
 #  include <sundials/sundials_config.h>
@@ -52,77 +53,6 @@ DEAL_II_NAMESPACE_OPEN
 
 namespace SUNDIALS
 {
-  namespace
-  {
-    /**
-     * A function that calls the function object given by its first argument
-     * with the set of arguments following at the end. If the call returns
-     * regularly, the current function returns zero to indicate success. If the
-     * call fails with an exception of type RecoverableUserCallbackError, then
-     * the current function returns 1 to indicate that the called function
-     * object thought the error it encountered is recoverable. If the call fails
-     * with any other exception, then the current function returns with an error
-     * code of -1. In each of the last two cases, the exception thrown by `f`
-     * is captured and `eptr` is set to the exception. In case of success,
-     * `eptr` is set to `nullptr`.
-     */
-    template <typename F, typename... Args>
-    int
-    call_and_possibly_capture_exception(const F &           f,
-                                        std::exception_ptr &eptr,
-                                        Args &&...args)
-    {
-      // See whether there is already something in the exception pointer
-      // variable. This can only happen if we had previously had
-      // a recoverable exception, and the underlying library actually
-      // did recover successfully. In that case, we can abandon the
-      // exception previously thrown. If eptr contains anything other,
-      // then we really don't know how that could have happened, and
-      // should probably bail out:
-      if (eptr)
-        {
-          try
-            {
-              std::rethrow_exception(eptr);
-            }
-          catch (const RecoverableUserCallbackError &)
-            {
-              // ok, ignore, but reset the pointer
-              eptr = nullptr;
-            }
-          catch (...)
-            {
-              // uh oh:
-              AssertThrow(false, ExcInternalError());
-            }
-        }
-
-      // Call the function and if that succeeds, return zero:
-      try
-        {
-          f(std::forward<Args>(args)...);
-          eptr = nullptr;
-          return 0;
-        }
-      // If the call failed with a recoverable error, then
-      // ignore the exception for now (but store a pointer to it)
-      // and return a positive return value (+1). If the underlying
-      // implementation manages to recover
-      catch (const RecoverableUserCallbackError &)
-        {
-          eptr = std::current_exception();
-          return 1;
-        }
-      // For any other exception, capture the exception and
-      // return -1:
-      catch (...)
-        {
-          eptr = std::current_exception();
-          return -1;
-        }
-    }
-  } // namespace
-
   template <typename VectorType>
   KINSOL<VectorType>::AdditionalData::AdditionalData(
     const SolutionStrategy &strategy,
@@ -344,11 +274,11 @@ namespace SUNDIALS
 
           Assert(solver.iteration_function, ExcInternalError());
 
-          const int err =
-            call_and_possibly_capture_exception(solver.iteration_function,
-                                                solver.pending_exception,
-                                                *src_yy,
-                                                *dst_FF);
+          const int err = Utilities::call_and_possibly_capture_exception(
+            solver.iteration_function,
+            solver.pending_exception,
+            *src_yy,
+            *dst_FF);
 
           return err;
         },
@@ -366,7 +296,7 @@ namespace SUNDIALS
 
           Assert(solver.residual, ExcInternalError());
 
-          const int err = call_and_possibly_capture_exception(
+          const int err = Utilities::call_and_possibly_capture_exception(
             solver.residual, solver.pending_exception, *src_yy, *dst_FF);
 
           return err;
@@ -452,12 +382,12 @@ namespace SUNDIALS
               auto src_b = internal::unwrap_nvector_const<VectorType>(b);
               auto dst_x = internal::unwrap_nvector<VectorType>(x);
 
-              const int err =
-                call_and_possibly_capture_exception(solver.solve_with_jacobian,
-                                                    solver.pending_exception,
-                                                    *src_b,
-                                                    *dst_x,
-                                                    tol);
+              const int err = Utilities::call_and_possibly_capture_exception(
+                solver.solve_with_jacobian,
+                solver.pending_exception,
+                *src_b,
+                *dst_x,
+                tol);
 
               return err;
             }
@@ -481,7 +411,7 @@ namespace SUNDIALS
               // src_ycur and src_fcur, and so we simply pass dummy vector in.
               // These vectors will have zero lengths because we don't reinit
               // them above.
-              const int err = call_and_possibly_capture_exception(
+              const int err = Utilities::call_and_possibly_capture_exception(
                 solver.solve_jacobian_system,
                 solver.pending_exception,
                 *src_ycur,
@@ -552,10 +482,8 @@ namespace SUNDIALS
             auto fcur = internal::unwrap_nvector<VectorType>(f);
 
             // Call the user-provided setup function with these arguments:
-            return call_and_possibly_capture_exception(solver.setup_jacobian,
-                                                       solver.pending_exception,
-                                                       *ycur,
-                                                       *fcur);
+            return Utilities::call_and_possibly_capture_exception(
+              solver.setup_jacobian, solver.pending_exception, *ycur, *fcur);
           });
         AssertKINSOL(status);
       }
