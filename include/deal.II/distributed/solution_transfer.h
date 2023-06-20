67fbf0ccf2bbeb8fc77740ee3da3689def309ba8
Matthias Maier <tamiko@43-1.org>
2023-06-19 22:16:46 -0500

    update copyright years: include, source

--- include/deal.II/distributed/solution_transfer.h
+++ include/deal.II/distributed/solution_transfer.h
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (C) 2009 - 2021 by the deal.II authors
+// Copyright (C) 2009 - 2022 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
