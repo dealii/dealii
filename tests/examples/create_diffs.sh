4e072de184f35c03fb7fb8d5b53c78249aa68e6c
Timo Heister <timo.heister@gmail.com>
2023-07-01 07:04:37 -0400

    test examples

---
+++ tests/examples/create_diffs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# create .diff files from current .cc files
+
+# Find all *.diff files in the current directory
+diff_files=$(find . -maxdepth 1 -type f -name "*.diff")
+
+for file in $diff_files; do
+  # Extract the filename without the extension
+  filename=$(basename "$file" .diff)
+  src="../../examples/$filename/$filename.cc"
+
+  # Check if the corresponding .cc file exists
+  if [[ -f "$src" ]]; then
+      echo "creating $file from $src and $filename.cc"
+    diff "$src" "$filename.cc" > $file
+  else
+      echo "No matching .cc file found for $diff_file"
+      exit -1
+  fi
+done
