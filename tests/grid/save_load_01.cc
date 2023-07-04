ea9f882618d1207fc8c8fd56270d25e659c972c0
Pasquale Africa <pafrica@sissa.it>
2023-07-04 19:41:32 +0000

    Add tests for serializing serial triangulation

---
+++ tests/grid/save_load_01.cc
@@ -0,0 +1,85 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2008 - 2023 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+
+// Test Triangulation::load()/save().
+// Create a triangulation, save it and load it.
+// The initial and the loaded triangulations must be the same.
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_description.h>
+
+#include <deal.II/lac/vector.h>
+
+#include "./tests.h"
+
+using namespace dealii;
+
+template <int dim, typename TriangulationType>
+void
+test(TriangulationType &triangulation)
+{
+  const std::string filename = "save_load_" + std::to_string(dim) + "d_out";
+
+  triangulation.save(filename);
+  deallog.push("save");
+  print_statistics(triangulation, false);
+  deallog.pop();
+
+  triangulation.clear();
+
+  triangulation.load(filename);
+  deallog.push("load");
+  print_statistics(triangulation, false);
+  deallog.pop();
+}
+
+
+int
+main(int argc, char **argv)
+{
+  initlog();
+
+  deallog.push("2d");
+  {
+    constexpr int dim = 2;
+
+    Triangulation<dim> triangulation;
+    GridGenerator::hyper_cube(triangulation);
+    triangulation.refine_global(3);
+
+    test<dim>(triangulation);
+  }
+  deallog.pop();
+
+  deallog.push("3d");
+  {
+    constexpr int dim = 3;
+
+    Triangulation<dim> triangulation;
+    GridGenerator::hyper_cube(triangulation);
+    triangulation.refine_global(3);
+
+    test<dim>(triangulation);
+  }
+  deallog.pop();
+}
