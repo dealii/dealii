6083fdfe197b4fc1d0107c9d838842bbde12ca92
Wolfgang Bangerth <bangerth@colostate.edu>
2023-08-17 14:25:18 -0600

    Be explicit about types.

--- source/grid/tria_description.cc
+++ source/grid/tria_description.cc
@@ -268,21 +268,24 @@ namespace TriangulationDescription
           {
             std::sort(this->coarse_cell_vertices.begin(),
                       this->coarse_cell_vertices.end(),
-                      [](const auto &a, const auto &b) {
+                      [](const std::pair<unsigned int, Point<spacedim>> &a,
+                         const std::pair<unsigned int, Point<spacedim>> &b) {
                         return a.first < b.first;
                       });
             this->coarse_cell_vertices.erase(
-              std::unique(this->coarse_cell_vertices.begin(),
-                          this->coarse_cell_vertices.end(),
-                          [](const auto &a, const auto &b) {
-                            if (a.first == b.first)
-                              {
-                                Assert(a.second.distance(b.second) < 10e-8,
-                                       ExcInternalError());
-                                return true;
-                              }
-                            return false;
-                          }),
+              std::unique(
+                this->coarse_cell_vertices.begin(),
+                this->coarse_cell_vertices.end(),
+                [](const std::pair<unsigned int, Point<spacedim>> &a,
+                   const std::pair<unsigned int, Point<spacedim>> &b) {
+                  if (a.first == b.first)
+                    {
+                      Assert(a.second.distance(b.second) < 10e-8,
+                             ExcInternalError());
+                      return true;
+                    }
+                  return false;
+                }),
               this->coarse_cell_vertices.end());
           }
 
