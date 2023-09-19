e68ba6dcc62e4d2cf6080353b940ec8c1055d938
Wolfgang Bangerth <bangerth@colostate.edu>
2023-09-19 16:50:03 -0600

    Remove unused variables.

--- tests/lac/linear_operator_12.cc
+++ tests/lac/linear_operator_12.cc
@@ -188,7 +188,6 @@ Step4<dim>::setup_system()
   constraints.clear();
   constraints.reinit(dof_handler.locally_owned_dofs(),
                      DoFTools::extract_locally_relevant_dofs(dof_handler));
-  std::map<unsigned int, double> boundary_values;
   VectorTools::interpolate_boundary_values(dof_handler,
                                            0,
                                            BoundaryValues<dim>(),
