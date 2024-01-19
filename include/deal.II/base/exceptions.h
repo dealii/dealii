4caa9cd13cbb5fddb8ab62c67fbf0405de6a85b9
Matthias Maier <tamiko@43-1.org>
2024-01-19 11:22:11 -0600

    config.h: for consistency rename DEAL_II_ASSUME to DEAL_II_CXX23_ASSUME

--- include/deal.II/base/exceptions.h
+++ include/deal.II/base/exceptions.h
@@ -1637,7 +1637,7 @@ namespace deal_II_exceptions
 #    endif /*ifdef KOKKOS_ACTIVE_EXECUTION_MEMORY_SPACE_HOST*/
 #  endif   /*KOKKOS_ACTIVE_EXECUTION_MEMORY_SPACE_HOST*/
 #else      /*ifdef DEBUG*/
-#  define Assert(cond, exc) DEAL_II_ASSUME(cond)
+#  define Assert(cond, exc) DEAL_II_CXX23_ASSUME(cond)
 #endif /*ifdef DEBUG*/
 
 
@@ -1689,7 +1689,7 @@ namespace deal_II_exceptions
       while (false)
 #  endif /*ifdef DEAL_II_HAVE_BUILTIN_EXPECT*/
 #else
-#  define AssertNothrow(cond, exc) DEAL_II_ASSUME(cond)
+#  define AssertNothrow(cond, exc) DEAL_II_CXX23_ASSUME(cond)
 #endif
 
 /**
