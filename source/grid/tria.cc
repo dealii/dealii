77382b0692f7d6dc7c111ef72596fe75e7b2f511
David Wells <drwells@email.unc.edu>
2024-04-06 10:37:37 -0400

    Split a very long string.

--- source/grid/tria.cc
+++ source/grid/tria.cc
@@ -5142,7 +5142,9 @@ namespace internal
               Assert(
                 next_unused_vertex < triangulation.vertices.size(),
                 ExcMessage(
-                  "Internal error: During refinement, the triangulation wants to access an element of the 'vertices' array but it turns out that the array is not large enough."));
+                  "Internal error: During refinement, the triangulation wants "
+                  "to access an element of the 'vertices' array but it turns "
+                  "out that the array is not large enough."));
               triangulation.vertices_used[next_unused_vertex] = true;
 
               new_vertices[8] = next_unused_vertex;
