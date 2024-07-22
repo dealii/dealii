e0033aba243fb8531b4f9d405d8d6045f315bc62
Matthias Maier <tamiko@43-1.org>
2024-07-22 06:55:47 -0500

    Test sundials/kinsol_08: remove calls to deprecated and removed functions

--- tests/sundials/kinsol_08.cc
+++ tests/sundials/kinsol_08.cc
@@ -112,10 +112,14 @@ main()
       J_inverse.vmult(dst, rhs);
     };
 
-  kinsol.custom_setup = [](void *kinsol_mem) {
-    // test custom_setup callback by querying some information from KINSOL
+  kinsol.custom_setup = [](void *kinsol_mem [[maybe_unused]]) {
+  // test custom_setup callback by querying some information from KINSOL
+#if DEAL_II_SUNDIALS_VERSION_LT(7, 0, 0)
+    // TODO: KINSetInfoHandlerFn and KINSetPrintLevel have been removed in
+    // version 7.0, so ideally do something else here. MM
     KINSetInfoHandlerFn(kinsol_mem, kinsol_info_callback, nullptr);
     KINSetPrintLevel(kinsol_mem, 1);
+#endif
   };
 
   VectorType v(N);
