e71ff27e7a7b118c8302915349c525d933899844
Timo Heister <timo.heister@gmail.com>
2024-09-04 17:53:07 -0400

    NVCC: work around compiler bug

--- include/deal.II/lac/petsc_matrix_base.h
+++ include/deal.II/lac/petsc_matrix_base.h
@@ -68,7 +68,15 @@ namespace PETScWrappers
      */
     class const_iterator
     {
+#  ifdef __CUDA_ARCH__
+      // NVCC, at least until 12.6, fails to compile the
+      // implementations of the nested Accessor class if
+      // it is declared as private. Work around this by
+      // making it public.
+    public:
+#  else
     private:
+#  endif
       /**
        * Accessor class for iterators
        */
