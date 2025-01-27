2b16c209ad51ce086f551d70f1264b04f1e7b059
Wolfgang Bangerth <bangerth@colostate.edu>
2025-01-27 13:39:13 -0700

    Explicitly re-enable output for some tests.

--- tests/lac/gmres_reorthogonalize_02.cc
+++ tests/lac/gmres_reorthogonalize_02.cc
@@ -41,7 +41,10 @@ test()
   for (unsigned int i = 0; i < n; ++i)
     matrix.diag_element(i) = (i + 1);
 
-  SolverControl control(1000, 1e3 * std::numeric_limits<number>::epsilon());
+  SolverControl                                        control(1000,
+                        1e3 * std::numeric_limits<number>::epsilon(),
+                        false,
+                        true);
   typename SolverGMRES<Vector<number>>::AdditionalData data;
   data.max_basis_size = 200;
   data.orthogonalization_strategy =
