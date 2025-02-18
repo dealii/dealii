f637d44744f84d51ad9184c22e358c560de42b54
Sam Scheuerman <sam.scheuerman.051@gmail.com>
2025-02-18 11:35:54 -0700

    Rearrange included headers to better match other tutorial programs

--- examples/step-93/step-93.cc
+++ examples/step-93/step-93.cc
@@ -18,39 +18,37 @@
 
 // @sect3{Include files and other top matter}
 
-#include <deal.II/grid/tria.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/timer.h>
+
 #include <deal.II/dofs/dof_handler.h>
-#include <deal.II/grid/grid_generator.h>
-
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/fe_system.h>
-
 #include <deal.II/dofs/dof_tools.h>
 
+#include <deal.II/fe/fe_enriched.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
 #include <deal.II/fe/fe_values.h>
-#include <deal.II/base/quadrature_lib.h>
 
-#include <deal.II/base/function.h>
-#include <deal.II/numerics/vector_tools.h>
-
-#include <deal.II/lac/vector.h>
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/sparse_matrix.h>
-#include <deal.II/lac/dynamic_sparsity_pattern.h>
-#include <deal.II/lac/solver_minres.h>
-#include <deal.II/lac/precondition.h>
-
-#include <deal.II/numerics/data_out.h>
-#include <fstream>
-#include <iostream>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
 
 #include <deal.II/hp/fe_collection.h>
 #include <deal.II/hp/fe_values.h>
 
-#include <deal.II/fe/fe_enriched.h>
-
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/solver_minres.h>
 #include <deal.II/lac/sparse_direct.h>
-#include <deal.II/base/timer.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <fstream>
+#include <iostream>
 
 
 namespace Step93
