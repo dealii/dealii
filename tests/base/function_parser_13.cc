58e03f9d4efb3a95128c74d491776f36fd20b2c6
Olivier Gaboriault <oliviergabo@gmail.com>
2025-02-24 15:44:58 -0500

    Indent

--- tests/base/function_parser_13.cc
+++ tests/base/function_parser_13.cc
@@ -14,77 +14,77 @@
 
 
 // This program tests the functionality of imposing a default expression
-// when declaring a ParsedFunction. 
+// when declaring a ParsedFunction.
 
-#include <deal.II/base/parsed_function.h>
 #include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/parsed_function.h>
+
+#include "deal.II/matrix_free/matrix_free.h"
 
 #include "../tests.h"
-#include "deal.II/matrix_free/matrix_free.h"
 
 
 void
-test() {
-    // set up problem:
-    ParameterHandler prm;
-    Functions::ParsedFunction<3> fct_1(1);
-    Functions::ParsedFunction<3> fct_2(2);
-    Functions::ParsedFunction<3> fct_3(3);
+test()
+{
+  // set up problem:
+  ParameterHandler             prm;
+  Functions::ParsedFunction<3> fct_1(1);
+  Functions::ParsedFunction<3> fct_2(2);
+  Functions::ParsedFunction<3> fct_3(3);
 
-    std::string expression_1 = "1000";
-    std::string expression_3 = "10;10;10";
+  std::string expression_1 = "1000";
+  std::string expression_3 = "10;10;10";
 
-    // Declare
-    prm.enter_subsection("Function 1");
-    fct_1.declare_parameters(prm, 1, expression_1);
-    prm.leave_subsection();
+  // Declare
+  prm.enter_subsection("Function 1");
+  fct_1.declare_parameters(prm, 1, expression_1);
+  prm.leave_subsection();
 
-    prm.enter_subsection("Function 2");
-    fct_2.declare_parameters(prm, 2);
-    prm.leave_subsection();
+  prm.enter_subsection("Function 2");
+  fct_2.declare_parameters(prm, 2);
+  prm.leave_subsection();
 
-    prm.enter_subsection("Function 3");
-    fct_3.declare_parameters(prm, 3, expression_3);
-    prm.leave_subsection();
-
-
-    // Parse
-    prm.enter_subsection("Function 1");
-    fct_1.parse_parameters(prm);
-    prm.leave_subsection();
-
-    prm.enter_subsection("Function 2");
-    fct_2.parse_parameters(prm);
-    prm.leave_subsection();
-
-    prm.enter_subsection("Function 3");
-    fct_3.parse_parameters(prm);
-    prm.leave_subsection();
+  prm.enter_subsection("Function 3");
+  fct_3.declare_parameters(prm, 3, expression_3);
+  prm.leave_subsection();
 
 
-    // Dummy point
-    const Point<3> p;
+  // Parse
+  prm.enter_subsection("Function 1");
+  fct_1.parse_parameters(prm);
+  prm.leave_subsection();
 
-    const double result_1 = fct_1.value(p);
-    Vector<double> result_2(2);
-    Vector<double> result_3(3);
+  prm.enter_subsection("Function 2");
+  fct_2.parse_parameters(prm);
+  prm.leave_subsection();
 
-    fct_2.vector_value(p,result_2);
-    fct_3.vector_value(p,result_3);
+  prm.enter_subsection("Function 3");
+  fct_3.parse_parameters(prm);
+  prm.leave_subsection();
 
-    deallog << "Function 1 '" << expression_1 << "' is "
-        << result_1 << std::endl;
 
-    deallog << "Function 2 '' is "
-    << result_2 << std::endl;
+  // Dummy point
+  const Point<3> p;
 
-    deallog << "Function 3 '" << expression_3 << "' is "
-    << result_3 << std::endl;
+  const double   result_1 = fct_1.value(p);
+  Vector<double> result_2(2);
+  Vector<double> result_3(3);
+
+  fct_2.vector_value(p, result_2);
+  fct_3.vector_value(p, result_3);
+
+  deallog << "Function 1 '" << expression_1 << "' is " << result_1 << std::endl;
+
+  deallog << "Function 2 '' is " << result_2 << std::endl;
+
+  deallog << "Function 3 '" << expression_3 << "' is " << result_3 << std::endl;
 }
 
 int
-main() {
-    initlog();
+main()
+{
+  initlog();
 
-    test();
+  test();
 }
