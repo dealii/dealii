ea5804b462c351257e230cfbf13c9e6c82d945d3
Wolfgang Bangerth <bangerth@colostate.edu>
2025-03-28 14:34:19 -0600

    Ensure a symbol in a header file has external linkage.

--- include/deal.II/matrix_free/portable_matrix_free.h
+++ include/deal.II/matrix_free/portable_matrix_free.h
@@ -73,7 +73,7 @@ namespace Portable
    * of blocks in a BlockVector and the number of FEEvaluation objects that can
    * be active in a single cell_loop().
    */
-  static constexpr const unsigned int n_max_dof_handlers = 5;
+  inline constexpr const unsigned int n_max_dof_handlers = 5;
 
   /**
    * Type for source and destination vectors in device functions like
