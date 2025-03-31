34ac8ed8f7bdd2440d726546bc3c2eef3969b2b3
[Yann Jobic] <yjobic@gmail.com>
2025-03-31 09:20:02 +0200

    add the test to the new collect_periodic_faces

---
+++ tests/grid/grid_tools_collect_periodic_faces_parallel_tolerance.cc
@@ -0,0 +1,95 @@
+// ------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2023 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Part of the source code is dual licensed under Apache-2.0 WITH
+// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+// governing the source code and code contributions can be found in
+// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+//
+// ------------------------------------------------------------------------
+
+#include <deal.II/base/exceptions.h>
+#include <deal.II/base/mpi.h>
+#include <deal.II/base/point.h>
+
+#include <deal.II/distributed/tria.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/tria.h>
+
+#include <vector>
+
+#include "../tests.h"
+
+template <int dim>
+void
+test(MPI_Comm comm)
+{
+  deallog << "dim = " << dim << std::endl;
+  parallel::distributed::Triangulation<dim> tria(comm);
+
+  std::vector<typename GridTools::PeriodicFacePair<
+    typename parallel::distributed::Triangulation<dim>::cell_iterator>>
+    periodicity_vector;
+
+  unsigned int              num_refinements = 1 << 4;
+  Point<dim>                p1;
+  Point<dim>                p2;
+  std::vector<unsigned int> repetitions(dim);
+
+  for (unsigned int i = 0; i < dim; ++i)
+    {
+      p1[i]          = 0.;
+      p2[i]          = 1.;
+      repetitions[i] = num_refinements;
+    }
+  GridGenerator::subdivided_hyper_rectangle(tria, repetitions, p1, p2, true);
+  
+  //We have to match periodic faces by looking at equal position componants 
+  //other than the periodic direction, in which the nodes lie in the same
+  //boundary Id.
+  //Thus, here we put the faces in the high xi-direction in the same boundary 
+  //Id than the low x-direction.
+  typename Triangulation<dim>::face_iterator face = tria.begin_face()
+  ,endface = tria.end_face();
+  for (; face != endface; ++face)
+    if (face->at_boundary())
+      if (face->boundary_id() == 1)
+        face->set_boundary_id(0);
+
+  //Here we are only interested in using the tolerance parameter of the 
+  //collect_periodic_faces function, the other default parameter stay as
+  //default one.
+  const Tensor<1, dim> defaultOffset=dealii::Tensor<1, dim>();
+  const FullMatrix<double> matrix = FullMatrix<double>();
+  const double tolerance=1e-7;
+  const unsigned int direction = 0; //x-direction
+
+  // Collect periodic faces in the x-direction
+  GridTools::collect_periodic_faces(tria, 
+                                    0, //boundary Id
+                                    direction, 
+                                    periodicity_vector,
+                                    defaultOffset,
+                                    matrix,
+                                    tolerance);
+  // Check the size of the matched_pairs vector
+  deallog << periodicity_vector.size() << std::endl;
+}
+
+int
+main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+  mpi_initlog();
+  MPI_Comm comm = MPI_COMM_WORLD;
+  test<2>(comm);
+  test<3>(comm);
+
+  return 0;
+}
