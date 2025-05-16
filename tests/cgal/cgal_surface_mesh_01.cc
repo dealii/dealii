14011ba524705735ec8e572d17a12390b5039fbb
Sascha Hofstetter <hofssasc@gmail.com>
2025-05-16 10:17:46 +0200

    changes to CGAL functions
    Fix the problem of not closed and oriented meshes
    
    clean up
    
    fix indent
    
    changes in test files
    
    clean up as proposed by bergbauer
    
    indent
    
    give correct output for test case
    
    patch credit added
    
    forgot indent

--- tests/cgal/cgal_surface_mesh_01.cc
+++ tests/cgal/cgal_surface_mesh_01.cc
@@ -26,6 +26,7 @@
 #include <deal.II/grid/tria.h>
 
 #include <CGAL/IO/io.h>
+#include <CGAL/Polygon_mesh_processing/orientation.h>
 
 #include "../tests.h"
 
@@ -56,7 +57,24 @@ test()
       const auto cell = tria.begin_active();
       dealii_cell_to_cgal_surface_mesh(cell, *mapping, mesh);
 
-      Assert(mesh.is_valid(), dealii::ExcMessage("The CGAL mesh is not valid"));
+      Assert(mesh.is_valid(), ExcMessage("The CGAL mesh is not valid"));
+
+      if (dim == 3)
+        {
+          // is closed/oriented only for 3 dimensional objects important
+          Assert(CGAL::is_closed(mesh),
+                 ExcMessage("The CGAL mesh is not closed"));
+
+          // orientation not supported for wedges, this needs special treatment
+          if (r_cell != ref_cells[3][2])
+            {
+              Assert(
+                CGAL::Polygon_mesh_processing::is_outward_oriented(mesh),
+                ExcMessage(
+                  "The normal vectors of the CGAL mesh are not oriented outwards"));
+            }
+        }
+
       deallog << "deal vertices: " << cell->n_vertices() << ", cgal vertices "
               << mesh.num_vertices() << std::endl;
       deallog << "deal faces: " << cell->n_faces() << ", cgal faces "
