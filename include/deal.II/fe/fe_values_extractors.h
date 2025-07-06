26f8b8fd602ba433a1846b0852a25ef56cdc8441
Gordan Segon <gsegon@finitemagnetics.com>
2025-07-06 12:15:29 +0200

    Implement `component_wise` renumbering supporting FEValuesExtractors for component grouping. Applied review suggestions.

--- include/deal.II/fe/fe_values_extractors.h
+++ include/deal.II/fe/fe_values_extractors.h
@@ -21,6 +21,7 @@
 #include <deal.II/base/types.h>
 
 #include <string>
+#include <variant>
 
 
 DEAL_II_NAMESPACE_OPEN
@@ -309,6 +310,16 @@ namespace FEValuesExtractors
      */
     const Extractor extractor;
   };
+
+  /**
+   * Convenience alias for variant type of supported FEValuesExtractors structs.
+   * In other words, an object of this type can be initialized with a scalar,
+   * vector, tensor, or any other of the `FEValuesExtractors` data types.
+   */
+  using ExtractorVariant = std::variant<FEValuesExtractors::Scalar,
+                                        FEValuesExtractors::Vector,
+                                        FEValuesExtractors::Tensor<2>,
+                                        FEValuesExtractors::SymmetricTensor<2>>;
 } // namespace FEValuesExtractors
 
 
