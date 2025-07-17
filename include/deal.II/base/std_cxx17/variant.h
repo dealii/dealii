d6b4acdca603f634579107d62774240a7d588a89
Daniel Arndt <arndtd@ornl.gov>
2025-07-16 22:55:09 -0400

    Also take care of DEAL_II_EARLY_DEPRECATIONS guards

--- include/deal.II/base/std_cxx17/variant.h
+++ include/deal.II/base/std_cxx17/variant.h
@@ -19,10 +19,8 @@
 
 #include <variant>
 
-#ifdef DEAL_II_EARLY_DEPRECATIONS
 DEAL_II_WARNING("This file is deprecated."
                 "Use the corresponding C++17 header variant instead.")
-#endif
 
 DEAL_II_NAMESPACE_OPEN
 namespace std_cxx17
