9acf56532b98b8732303f97d4d76fa0df939ed8b
David Wells <drwells@email.unc.edu>
2025-10-19 22:11:39 -0400

    Add Utilities::MPI::logical_and().
    Like Utilities::MPI::logical_or().

---
+++ tests/mpi/collective_logical_and.cc
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2025 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Part of the source code is dual licensed under Apache-2.0 WITH
+// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+// governing the source code and code contributions can be found in
+// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+//
+// ------------------------------------------------------------------------
+
+
+
+// check Utilities::MPI::logical_and()
+
+#include <deal.II/base/utilities.h>
+
+#include "../tests.h"
+
+void
+test()
+{
+  const unsigned int myid = Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
+
+  bool collective_false = Utilities::MPI::logical_and(false, MPI_COMM_WORLD);
+  bool collective_alternating =
+    Utilities::MPI::logical_and(((myid % 2) == 0) ? true : false,
+                                MPI_COMM_WORLD);
+  bool collective_true = Utilities::MPI::logical_and(true, MPI_COMM_WORLD);
+
+  if (myid == 0)
+    deallog << std::boolalpha << collective_false << ' '
+            << collective_alternating << ' ' << collective_true << std::endl;
+}
+
+
+
+int
+main(int argc, char *argv[])
+{
+#ifdef DEAL_II_WITH_MPI
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(
+    argc, argv, testing_max_num_threads());
+#else
+  (void)argc;
+  (void)argv;
+  compile_time_error;
+
+#endif
+
+  if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+    {
+      initlog();
+
+      deallog.push("mpi");
+      test();
+      deallog.pop();
+    }
+  else
+    test();
+}
