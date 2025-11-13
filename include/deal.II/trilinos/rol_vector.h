0f4610ee0e6824525b45ae269ba19b219dd43fc8
Marc Fehling <mafehling.git@gmail.com>
2025-11-13 12:26:21 +0100

    Added mask to ROL Wrapper.

--- include/deal.II/trilinos/rol_vector.h
+++ include/deal.II/trilinos/rol_vector.h
@@ -19,13 +19,16 @@
 
 #ifdef DEAL_II_TRILINOS_WITH_ROL
 #  include <deal.II/base/exceptions.h>
+#  include <deal.II/base/index_set.h>
 #  include <deal.II/base/mpi.h>
+#  include <deal.II/base/types.h>
 
 #  include <deal.II/lac/vector.h>
 
 #  include <ROL_Vector.hpp>
 
 #  include <limits>
+#  include <tuple>
 #  include <type_traits>
 
 
@@ -62,48 +65,52 @@ namespace TrilinosWrappers
    * // Reinitialize the current vector using a given vector's
    * // size (and the parallel distribution) without copying
    * // the elements.
-   * VectorType::reinit(const VectorType &, ...);
-   *
-   * // Globally add a given vector to the current.
-   * VectorType::operator+=(const VectorType &);
-   *
-   * // Scale all elements by a given scalar.
-   * VectorType::operator*=(const VectorType::value_type &);
-   *
-   * // Perform dot product with a given vector.
-   * VectorType::operator*=(const VectorType &);
-   *
-   * // Scale all elements of the current vector and globally
-   * // add a given vector to it.
-   * VectorType::add(const VectorType::value_type, const VectorType &);
+   * void VectorType::reinit(const VectorType &, ...);
    *
    * // Copies the data of a given vector to the current.
    * // Resize the current vector if necessary (MPI safe).
-   * VectorType::operation=(const VectorType &);
+   * VectorType& VectorType::operation=(const VectorType &);
    *
-   * // Return the global size of the current vector.
-   * VectorType::size();
+   * // Access the value of the ith component.
+   * VectorType::value_type&
+   * VectorType::operator[](const VectorType::size_type i);
    *
-   * // Return L^2 norm of the current vector
-   * VectorType::l2_norm();
+   * // Return whether the vector currently is in a state where
+   * // ghost values can be read or not.
+   * bool VectorType::has_ghost_elements();
    *
-   * // Iterator to the start of the (locally owned) element
-   * // of the current vector.
-   * VectorType::begin();
-   *
-   * // Iterator to the one past the last (locally owned)
-   * // element of the current vector.
-   * VectorType::end();
+   * // Fills the data field for ghost indices with the values stored
+   * // in the respective positions of the owning processor.
+   * void VectorType::update_ghost_values();
    *
    * // Compress the vector i.e., flush the buffers of the
    * // vector object if it has any.
-   * VectorType::compress(VectorOperation::insert);
+   * void VectorType::compress(VectorOperation::insert);
    * @endcode
    *
    * @note The current implementation in ROL doesn't support vector sizes above
-   * the largest value of int type. Some of the vectors in deal.II (see
-   * @ref Vector)
-   * may not satisfy the above requirements.
+   *   the largest value of int type. Some of the vectors in deal.II (see
+   *   @ref Vector) may not satisfy the above requirements.
+   *
+   *
+   * <h3>Optimization and finite elements</h3>
+   *
+   * Numerical optimization can be a useful tool in the context of finite
+   * element methods. However, not all degrees of freedom of a finite element
+   * discretization should really be part of the optimization space -- typically
+   * those that are constrained through boundary conditions and hanging nodes
+   * should be excluded from the optimization process.
+   *
+   * This can be achieved in two different ways: either through constrained
+   * optimization; or by reducing the optimization space which allows
+   * unconstrained optimization. For the latter, one way to do it is masking
+   * those vector elements that correspond to the optimization space. All
+   * operations necessary for the optimization process would only \e see these
+   * masked entries, which are effectively only a subspace of the finite element
+   * discretization.
+   *
+   * This wrapper class provides a means for masking using an IndexSet upon
+   * construction.
    *
    *
    * <h3>Ghost elements on distributed vectors</h3>
@@ -150,68 +157,116 @@ namespace TrilinosWrappers
      */
     ROL::Ptr<VectorType> vector_ptr;
 
+    /**
+     * IndexSet that represents the locally owned optimization space.
+     */
+    IndexSet optimization_space;
+
+    /**
+     * Global dimension of the optimization space.
+     */
+    IndexSet::size_type global_dimension;
+
+    /**
+     * Prefix sum of the number of elements in the optimization space of lower
+     * rank MPI processes.
+     */
+    IndexSet::size_type prefix_sum;
+
   public:
     /**
      * Constructor.
+     *
+     * The optimization space covers all locally owned degrees of freedom.
      */
     ROLVector(const ROL::Ptr<VectorType> &vector_ptr);
 
     /**
-     * Return the ROL pointer to the wrapper vector, #vector_ptr.
+     * Constructor.
+     *
+     * The IndexSet @p optimization_space masks entries in the wrapped vector
+     * on which the optimization process will be applied only. It needs to
+     * contain a subset of locally owned indices of the wrapped vector.
+     */
+    ROLVector(const ROL::Ptr<VectorType> &vector_ptr,
+              const IndexSet             &optimization_space);
+
+    /**
+     * Return the ROL pointer to the wrapped vector.
      */
     ROL::Ptr<VectorType>
     getVector();
 
     /**
-     * Return the ROL pointer to const vector.
+     * Return the ROL pointer to the wrapped vector as read-only.
      */
     ROL::Ptr<const VectorType>
     getVector() const;
 
     /**
-     * Return the dimension (global vector size) of the wrapped vector.
+     * Return the global dimension of the optimization space.
      */
     int
     dimension() const;
 
     /**
-     * Set the wrapper vector to a given ROL::Vector @p rol_vector by
-     * overwriting its contents.
+     * Set the wrapped vector to a given @p rol_vector by overwriting its
+     * contents.
      *
-     * If the current wrapper vector has ghost elements,
+     * If the current wrapped vector has ghost elements,
      * then <code> VectorType::operator=(const VectorType&) </code> should still
      * be allowed on it.
+     *
+     * @note @p rol_vector has to be of type ROLVector.
      */
     void
     set(const ROL::Vector<value_type> &rol_vector);
 
     /**
-     * Perform addition.
+     * Add @p rol_vector to the wrapped vector.
+     *
+     * The operation will only be applied to masked elements. Both vectors need
+     * to correspond to the same optimization space.
+     *
+     * @note @p rol_vector has to be of type ROLVector.
      */
     void
     plus(const ROL::Vector<value_type> &rol_vector);
 
     /**
-     * Scale the wrapper vector by @p alpha and add ROL::Vector @p rol_vector
-     * to it.
+     * Scale the wrapped vector by @p alpha and add @p rol_vector to it.
+     *
+     * The operation will only be applied to masked elements. Both vectors need
+     * to correspond to the same optimization space.
+     *
+     * @note @p rol_vector has to be of type ROLVector.
      */
     void
     axpy(const value_type alpha, const ROL::Vector<value_type> &rol_vector);
 
     /**
-     * Scale the wrapper vector.
+     * Scale the wrapped vector by @p alpha.
+     *
+     * The operation will only be applied to masked elements.
      */
     void
     scale(const value_type alpha);
 
     /**
-     * Return the dot product with a given ROL::Vector @p rol_vector.
+     * Return the dot product with a given @p rol_vector.
+     *
+     * The operation will only be applied to masked elements. Both vectors need
+     * to correspond to the same optimization space.
+     *
+     * @note @p rol_vector has to be of type ROLVector.
      */
     value_type
     dot(const ROL::Vector<value_type> &rol_vector) const;
 
     /**
      * Return the $L^{2}$ norm of the wrapped vector.
+     *
+     * The operation will only be applied to masked elements.
      *
      * The returned type is of ROLVector::value_type so as to maintain
      * consistency with ROL::Vector<ROLVector::value_type> and
@@ -223,20 +278,22 @@ namespace TrilinosWrappers
     norm() const;
 
     /**
-     * Return a clone of the wrapped vector.
+     * Return a clone of the wrapped vector as a pointer to the parent class.
      */
     ROL::Ptr<ROL::Vector<value_type>>
     clone() const;
 
     /**
      * Create and return a ROL pointer to the basis vector corresponding to the
-     * @p i ${}^{th}$ element of the wrapper vector.
+     * @p i ${}^{th}$ element of the global optimization space.
      */
     ROL::Ptr<ROL::Vector<value_type>>
     basis(const int i) const;
 
     /**
      * Apply unary function @p f to all the elements of the wrapped vector.
+     *
+     * The operation will only be applied to masked elements.
      */
     void
     applyUnary(const ROL::Elementwise::UnaryFunction<value_type> &f);
@@ -244,6 +301,11 @@ namespace TrilinosWrappers
     /**
      * Apply binary function @p f along with ROL::Vector @p rol_vector to all
      * the elements of the wrapped vector.
+     *
+     * The operation will only be applied to masked elements. Both vectors need
+     * to correspond to the same optimization space.
+     *
+     * @note @p rol_vector has to be of type ROLVector.
      */
     void
     applyBinary(const ROL::Elementwise::BinaryFunction<value_type> &f,
@@ -252,12 +314,17 @@ namespace TrilinosWrappers
     /**
      * Return the accumulated value on applying reduction operation @p r on
      * all the elements of the wrapped vector.
+     *
+     * The operation will only be applied to masked elements.
      */
     value_type
     reduce(const ROL::Elementwise::ReductionOp<value_type> &r) const;
 
     /**
      * Print the wrapped vector to the output stream @p outStream.
+     *
+     * This function will print the entire vector, and not just the masked
+     * elements.
      */
     void
     print(std::ostream &outStream) const;
@@ -270,8 +337,32 @@ namespace TrilinosWrappers
 
   template <typename VectorType>
   ROLVector<VectorType>::ROLVector(const ROL::Ptr<VectorType> &vector_ptr)
-    : vector_ptr(vector_ptr)
+    : ROLVector(vector_ptr, vector_ptr->locally_owned_elements())
   {}
+
+
+
+  template <typename VectorType>
+  ROLVector<VectorType>::ROLVector(const ROL::Ptr<VectorType> &vector_ptr,
+                                   const IndexSet &optimization_space)
+    : vector_ptr(vector_ptr)
+    , optimization_space(optimization_space)
+  {
+    Assert(
+      optimization_space.is_subset_of(vector_ptr->locally_owned_elements()),
+      ExcMessage(
+        "Provided IndexSet needs to be a subset of locally owned indices."));
+
+    std::tie(prefix_sum, global_dimension) =
+      Utilities::MPI::partial_and_total_sum(optimization_space.n_elements(),
+                                            vector_ptr->get_mpi_communicator());
+
+    // The return type of ROL::Vector::dimension() has to be int,
+    // so we check this requirement of ROL here.
+    Assert(global_dimension < std::numeric_limits<int>::max(),
+           ExcMessage("The number of elements to optimize is greater than the "
+                      "largest value of type int."));
+  }
 
 
 
@@ -295,28 +386,32 @@ namespace TrilinosWrappers
 
   template <typename VectorType>
   void
-  ROLVector<VectorType>::set(const ROL::Vector<value_type> &rol_vector)
+  ROLVector<VectorType>::set(const ROL::Vector<value_type> &other_)
   {
-    const ROLVector &vector_adaptor =
-      dynamic_cast<const ROLVector &>(rol_vector);
+    const ROLVector &other = dynamic_cast<const ROLVector &>(other_);
 
-    (*vector_ptr) = *(vector_adaptor.getVector());
+    (*vector_ptr)      = *(other.getVector());
+    optimization_space = other.optimization_space;
+    global_dimension   = other.global_dimension;
+    prefix_sum         = other.prefix_sum;
   }
 
 
 
   template <typename VectorType>
   void
-  ROLVector<VectorType>::plus(const ROL::Vector<value_type> &rol_vector)
+  ROLVector<VectorType>::plus(const ROL::Vector<value_type> &other_)
   {
+    const ROLVector &other = dynamic_cast<const ROLVector &>(other_);
+
     Assert(vector_ptr->has_ghost_elements() == false, ExcGhostsPresent());
-    Assert(this->dimension() == rol_vector.dimension(),
-           ExcDimensionMismatch(this->dimension(), rol_vector.dimension()));
+    Assert(optimization_space == other.optimization_space,
+           ExcMessage("Optimization spaces of vectors do not match."));
 
-    const ROLVector &vector_adaptor =
-      dynamic_cast<const ROLVector &>(rol_vector);
+    for (const auto i : optimization_space)
+      (*vector_ptr)[i] += (*other.getVector())[i];
 
-    *vector_ptr += *(vector_adaptor.getVector());
+    vector_ptr->compress(VectorOperation::add);
   }
 
 
@@ -324,16 +419,18 @@ namespace TrilinosWrappers
   template <typename VectorType>
   void
   ROLVector<VectorType>::axpy(const value_type               alpha,
-                              const ROL::Vector<value_type> &rol_vector)
+                              const ROL::Vector<value_type> &other_)
   {
+    const ROLVector &other = dynamic_cast<const ROLVector &>(other_);
+
     Assert(vector_ptr->has_ghost_elements() == false, ExcGhostsPresent());
-    Assert(this->dimension() == rol_vector.dimension(),
-           ExcDimensionMismatch(this->dimension(), rol_vector.dimension()));
+    Assert(optimization_space == other.optimization_space,
+           ExcMessage("Optimization spaces of vectors do not match."));
 
-    const ROLVector &vector_adaptor =
-      dynamic_cast<const ROLVector &>(rol_vector);
+    for (const auto i : optimization_space)
+      (*vector_ptr)[i] += alpha * (*other.getVector())[i];
 
-    vector_ptr->add(alpha, *(vector_adaptor.getVector()));
+    vector_ptr->compress(VectorOperation::add);
   }
 
 
@@ -342,10 +439,7 @@ namespace TrilinosWrappers
   int
   ROLVector<VectorType>::dimension() const
   {
-    Assert(vector_ptr->size() < std::numeric_limits<int>::max(),
-           ExcMessage("The size of the vector being used is greater than "
-                      "largest value of type int."));
-    return static_cast<int>(vector_ptr->size());
+    return static_cast<int>(global_dimension);
   }
 
 
@@ -356,22 +450,29 @@ namespace TrilinosWrappers
   {
     Assert(vector_ptr->has_ghost_elements() == false, ExcGhostsPresent());
 
-    (*vector_ptr) *= alpha;
+    for (const auto i : optimization_space)
+      (*vector_ptr)[i] *= alpha;
+
+    vector_ptr->compress(VectorOperation::insert);
   }
 
 
 
   template <typename VectorType>
   typename VectorType::value_type
-  ROLVector<VectorType>::dot(const ROL::Vector<value_type> &rol_vector) const
+  ROLVector<VectorType>::dot(const ROL::Vector<value_type> &other_) const
   {
-    Assert(this->dimension() == rol_vector.dimension(),
-           ExcDimensionMismatch(this->dimension(), rol_vector.dimension()));
+    const ROLVector &other = dynamic_cast<const ROLVector &>(other_);
 
-    const ROLVector &vector_adaptor =
-      dynamic_cast<const ROLVector &>(rol_vector);
+    Assert(optimization_space == other.optimization_space,
+           ExcMessage("Optimization spaces of vectors do not match."));
 
-    return (*vector_ptr) * (*vector_adaptor.getVector());
+    value_type dot(0);
+    for (const auto i : optimization_space)
+      dot += (*vector_ptr)[i] * (*other.getVector())[i];
+
+    return Utilities::MPI::sum<value_type>(dot,
+                                           vector_ptr->get_mpi_communicator());
   }
 
 
@@ -380,7 +481,7 @@ namespace TrilinosWrappers
   typename VectorType::value_type
   ROLVector<VectorType>::norm() const
   {
-    return vector_ptr->l2_norm();
+    return std::sqrt(this->dot(*this));
   }
 
 
@@ -391,7 +492,8 @@ namespace TrilinosWrappers
   {
     ROL::Ptr<VectorType> vec_ptr = ROL::makePtr<VectorType>(*vector_ptr);
 
-    return ROL::makePtr<ROLVector>(vec_ptr);
+    return ROL::makePtr<ROLVector>(vec_ptr, optimization_space);
+    // TODO: also somehow pass the partial sums etc?
   }
 
 
@@ -400,17 +502,28 @@ namespace TrilinosWrappers
   ROL::Ptr<ROL::Vector<typename VectorType::value_type>>
   ROLVector<VectorType>::basis(const int i) const
   {
-    ROL::Ptr<VectorType> vec_ptr = ROL::makePtr<VectorType>();
+    AssertIndexRange(i, global_dimension);
 
-    // Zero all the entries in dealii vector.
-    vec_ptr->reinit(*vector_ptr, false);
+    // clone the internal vector as basis
+    ROL::Ptr<VectorType> basis_ptr = ROL::makePtr<VectorType>();
+    basis_ptr->reinit(*vector_ptr, false);
 
-    if (vector_ptr->locally_owned_elements().is_element(i))
-      vec_ptr->operator[](i) = 1.;
+    // check whether the i-th basis belongs to us
+    if ((i >= prefix_sum) && (i < prefix_sum + optimization_space.n_elements()))
+      {
+        const IndexSet::size_type local_index = i - prefix_sum;
 
-    vec_ptr->compress(VectorOperation::insert);
+        // global dof index corresponding to i-th basis in optimization
+        const dealii::types::global_dof_index global_dof_index =
+          optimization_space.nth_index_in_set(local_index);
 
-    return ROL::makePtr<ROLVector>(vec_ptr);
+        (*basis_ptr)[global_dof_index] = 1.;
+      }
+
+    basis_ptr->compress(VectorOperation::insert);
+
+    return ROL::makePtr<ROLVector>(basis_ptr, optimization_space);
+    // TODO: also somehow pass the partial sums etc?
   }
 
 
@@ -422,12 +535,8 @@ namespace TrilinosWrappers
   {
     Assert(vector_ptr->has_ghost_elements() == false, ExcGhostsPresent());
 
-    const typename VectorType::iterator vend = vector_ptr->end();
-
-    for (typename VectorType::iterator iterator = vector_ptr->begin();
-         iterator != vend;
-         iterator++)
-      *iterator = f.apply(*iterator);
+    for (const auto i : optimization_space)
+      (*vector_ptr)[i] = f.apply((*vector_ptr)[i]);
 
     vector_ptr->compress(VectorOperation::insert);
   }
@@ -438,25 +547,16 @@ namespace TrilinosWrappers
   void
   ROLVector<VectorType>::applyBinary(
     const ROL::Elementwise::BinaryFunction<value_type> &f,
-    const ROL::Vector<value_type>                      &rol_vector)
+    const ROL::Vector<value_type>                      &other_)
   {
+    const ROLVector &other = dynamic_cast<const ROLVector &>(other_);
+
     Assert(vector_ptr->has_ghost_elements() == false, ExcGhostsPresent());
-    Assert(this->dimension() == rol_vector.dimension(),
-           ExcDimensionMismatch(this->dimension(), rol_vector.dimension()));
+    Assert(optimization_space == other.optimization_space,
+           ExcMessage("Optimization spaces of vectors do not match."));
 
-    const ROLVector &vector_adaptor =
-      dynamic_cast<const ROLVector &>(rol_vector);
-
-    const VectorType &given_rol_vector = *(vector_adaptor.getVector());
-
-    const typename VectorType::iterator       vend   = vector_ptr->end();
-    const typename VectorType::const_iterator rolend = given_rol_vector.end();
-
-    typename VectorType::const_iterator r_iterator = given_rol_vector.begin();
-    for (typename VectorType::iterator l_iterator = vector_ptr->begin();
-         l_iterator != vend && r_iterator != rolend;
-         l_iterator++, r_iterator++)
-      *l_iterator = f.apply(*l_iterator, *r_iterator);
+    for (const auto i : optimization_space)
+      (*vector_ptr)[i] = f.apply((*vector_ptr)[i], (*other.getVector())[i]);
 
     vector_ptr->compress(VectorOperation::insert);
   }
@@ -471,12 +571,8 @@ namespace TrilinosWrappers
     value_type result = r.initialValue();
 
     // local reduction
-    const typename VectorType::iterator vend = vector_ptr->end();
-
-    for (typename VectorType::iterator iterator = vector_ptr->begin();
-         iterator != vend;
-         iterator++)
-      r.reduce(*iterator, result);
+    for (const auto i : optimization_space)
+      r.reduce((*vector_ptr)[i], result);
 
     // global reduction
     const auto combiner = [&r](const value_type a,
---
+++ tests/rol/rolvector_basis_01.cc
@@ -0,0 +1,62 @@
+// ------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2025 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Part of the source code is dual licensed under Apache-2.0 WITH
+// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+// governing the source code and code contributions can be found in
+// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+//
+// ------------------------------------------------------------------------
+
+// Check ROLVector::basis() on a distributed vector.
+
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/signaling_nan.h>
+
+#include <deal.II/lac/la_parallel_vector.h>
+#include <deal.II/lac/trilinos_vector.h>
+
+#include <deal.II/trilinos/rol_vector.h>
+
+#include "../tests.h"
+
+
+template <typename VectorType>
+void
+test()
+{
+  // set up distributed vector
+  const unsigned int nprocs = Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD);
+  const unsigned int myid   = Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
+
+  IndexSet owned(nprocs);
+  owned.add_index(myid);
+
+  VectorType v(owned, MPI_COMM_WORLD);
+  v[myid] = myid;
+
+  // wrap for ROL
+  TrilinosWrappers::ROLVector<VectorType> rol_v(ROL::makePtrFromRef(v));
+
+  // get all basis vectors
+  for (unsigned int b = 0; b < nprocs; ++b)
+    {
+      const auto basis = rol_v.basis(b);
+      basis->print(deallog.get_file_stream());
+    }
+}
+
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+  MPILogInitAll                    log;
+
+  test<TrilinosWrappers::MPI::Vector>();
+  test<LinearAlgebra::distributed::Vector<double>>();
+}
