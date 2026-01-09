9c41de9b86f20f7c75c656aa50246f2278cabd21
Daniel Arndt <arndtd@ornl.gov>
2026-01-09 08:57:56 -0500

    Clean up doxygen
    Co-authored-by: Jan Philipp Thiele <thiele@wias-berlin.de>

--- include/deal.II/lac/trilinos_tpetra_precondition.h
+++ include/deal.II/lac/trilinos_tpetra_precondition.h
@@ -57,7 +57,6 @@ namespace LinearAlgebra
       /**
        * @brief Standardized data struct to pipe additional flags to the
        * preconditioner.
-       *
        */
       struct AdditionalData
       {};
@@ -66,7 +65,6 @@ namespace LinearAlgebra
        * @brief Constructor. Does not do anything. The <tt>initialize</tt> function of
        * the derived classes will have to create the preconditioner from a given
        * sparse matrix.
-       *
        */
       PreconditionBase() = default;
 
@@ -197,7 +195,6 @@ namespace LinearAlgebra
        * based on an AdditionalData object that exposes and defaults the most
        * sensible parameters. But some classes will also offer full
        * customization for experienced Trilinos users.
-       *
        */
       Teuchos::ParameterList parameter_list;
     };
@@ -220,7 +217,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief Construct identity preconditioner.
-       *
        */
       PreconditionIdentity() = default;
 
@@ -263,7 +259,6 @@ namespace LinearAlgebra
       /**
        * Initializes the preconditioner for the matrix <tt>A</tt> based on
        * the <tt>parameter_set</tt>.
-       *
        */
       void
       initialize(const SparseMatrix<Number, MemorySpace> &A);
@@ -348,7 +343,6 @@ namespace LinearAlgebra
      *
      * @ingroup TpetraWrappers
      * @ingroup Preconditioners
-     *
      */
     template <typename Number, typename MemorySpace = dealii::MemorySpace::Host>
     class PreconditionJacobi
@@ -357,7 +351,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -368,7 +361,7 @@ namespace LinearAlgebra
          * The defaults yield a textbook Jacobi preconditioner.
          *
          * @param omega The damping factor for the relaxation.
-         * @param fix_diagonal Fix small diagonal entries for the inversion?
+         * @param fix_diagonal Fix small diagonal entries for the inversion.
          * @param min_diagonal Threshold for fix_diagonal.
          * @param n_sweeps Number of relaxation sweeps per call to vmult or Tvmult.
          */
@@ -379,7 +372,6 @@ namespace LinearAlgebra
 
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
@@ -396,18 +388,15 @@ namespace LinearAlgebra
          *
          * If the threshold is zero (default) only entries which are exactly
          * zero will be replaced will small nonzero values.
-         *
          */
         double min_diagonal;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
       /**
        * @brief Constructor.
-       *
        */
       PreconditionJacobi();
 
@@ -441,7 +430,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -453,7 +441,7 @@ namespace LinearAlgebra
          * original publication.
          *
          * @param omega The damping factor for the relaxation.
-         * @param fix_diagonal Fix small diagonal entries for the inversion?
+         * @param fix_diagonal Fix small diagonal entries for the inversion.
          * @param min_diagonal Threshold for fix_diagonal.
          * @param n_sweeps Number of relaxation sweeps per call to vmult or Tvmult.
          */
@@ -464,7 +452,6 @@ namespace LinearAlgebra
 
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
@@ -481,19 +468,16 @@ namespace LinearAlgebra
          *
          * If the threshold is zero (default) only entries which are exactly
          * zero will be replaced will small nonzero values.
-         *
          */
         double min_diagonal;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionL1Jacobi();
 
@@ -528,7 +512,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -541,7 +524,7 @@ namespace LinearAlgebra
          *
          * @param omega The damping factor for the relaxation.
          * @param eta Threshold parameter for diagonal correction.
-         * @param fix_diagonal Fix small diagonal entries for the inversion?
+         * @param fix_diagonal Fix small diagonal entries for the inversion.
          * @param min_diagonal Threshold for fix_diagonal.
          * @param n_sweeps Number of relaxation sweeps per call to vmult or Tvmult.
          */
@@ -553,7 +536,6 @@ namespace LinearAlgebra
 
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
 
@@ -562,7 +544,6 @@ namespace LinearAlgebra
          *
          * The l1 correction for a row i will only be added if it is more than
          * eta times larger than the original diagonal entry.
-         *
          */
         double eta;
         /**
@@ -579,19 +560,16 @@ namespace LinearAlgebra
          *
          * If the threshold is zero (default) only entries which are exactly
          * zero will be replaced will small nonzero values.
-         *
          */
         double min_diagonal;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionL1GaussSeidel();
 
@@ -623,7 +601,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -635,7 +612,7 @@ namespace LinearAlgebra
          *
          * @param omega The damping factor for the relaxation.
          * @param overlap Overlap between processor local matrices.
-         * @param fix_diagonal Fix small diagonal entries for the inversion?
+         * @param fix_diagonal Fix small diagonal entries for the inversion.
          * @param min_diagonal Threshold for fix_diagonal.
          * @param n_sweeps Number of relaxation sweeps per call to vmult or Tvmult.
          */
@@ -647,7 +624,6 @@ namespace LinearAlgebra
 
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
@@ -674,19 +650,16 @@ namespace LinearAlgebra
          *
          * If the threshold is zero (default) only entries which are exactly
          * zero will be replaced will small nonzero values.
-         *
          */
         double min_diagonal;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionSOR();
 
@@ -715,7 +688,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -727,7 +699,7 @@ namespace LinearAlgebra
          *
          * @param omega The damping factor for the relaxation.
          * @param overlap Overlap between processor local matrices.
-         * @param fix_diagonal Fix small diagonal entries for the inversion?
+         * @param fix_diagonal Fix small diagonal entries for the inversion.
          * @param min_diagonal Threshold for fix_diagonal.
          * @param n_sweeps Number of relaxation sweeps per call to vmult or Tvmult.
          */
@@ -739,7 +711,6 @@ namespace LinearAlgebra
 
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
@@ -766,12 +737,10 @@ namespace LinearAlgebra
          *
          * If the threshold is zero (default) only entries which are exactly
          * zero will be replaced will small nonzero values.
-         *
          */
         double min_diagonal;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
@@ -779,7 +748,6 @@ namespace LinearAlgebra
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionSSOR();
 
@@ -809,7 +777,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -835,7 +802,6 @@ namespace LinearAlgebra
          * The degree directly corresponds to the number of matrix-vector
          * products that have to be performed during a single application of the
          * vmult() and Tvmult() operations.
-         *
          */
         int degree;
         /*
@@ -847,12 +813,10 @@ namespace LinearAlgebra
         double max_eigenvalue;
         /*
          * @brief Lower bound for the minimum eigenvalue of the matrix.
-         *
          */
         double min_eigenvalue;
         /**
          * @brief Estimated ratio between maximum and minimum eigenvalue.
-         *
          */
         double eigenvalue_ratio;
         /**
@@ -860,7 +824,6 @@ namespace LinearAlgebra
          *
          * If the threshold is zero (default) only entries which are exactly
          * zero will be replaced will small nonzero values.
-         *
          */
         double min_diagonal;
         /**
@@ -873,14 +836,12 @@ namespace LinearAlgebra
          * can be useful to append previous data to the Chebyshev corrections.
          * The user should really know what they are doing when setting this
          * flag to true.
-         *
          */
         bool nonzero_starting;
       };
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionChebyshev();
 
@@ -920,7 +881,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -947,17 +907,14 @@ namespace LinearAlgebra
          *
          * Level-of-fill to increase the sparsity pattern of the preconditioner.
          * A large enough value will result in a complete LU factorization.
-         *
          */
         int ilu_fill;
         /**
          * @brief Constant to be added to each diagonal entry before factorization.
-         *
          */
         double ilu_atol;
         /**
          * @brief Factor to scale all diagonal entries by before factorization.
-         *
          */
         double ilu_rtol;
         /**
@@ -974,7 +931,6 @@ namespace LinearAlgebra
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionILU();
 
@@ -1004,7 +960,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -1042,17 +997,14 @@ namespace LinearAlgebra
          * A large enough value will result in a complete LU factorization.
          * Note, however, that this will drastically increase the memory
          * requirement, especially for 3d simulations.
-         *
          */
         double ilut_fill;
         /**
          * @brief Constant to be added to each diagonal entry before factorization.
-         *
          */
         double ilut_atol;
         /**
          * @brief Factor to scale all diagonal entries by before factorization.
-         *
          */
         double ilut_rtol;
         /**
@@ -1071,7 +1023,6 @@ namespace LinearAlgebra
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionILUT();
 
@@ -1108,7 +1059,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -1137,17 +1087,14 @@ namespace LinearAlgebra
         int n_local_parts;
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
          * @brief Amount of overlap between blocks
-         *
          */
         int block_overlap;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
@@ -1155,7 +1102,6 @@ namespace LinearAlgebra
 
       /**
        * @brief Constructor.
-       *
        */
       PreconditionBlockJacobi();
 
@@ -1188,7 +1134,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -1216,7 +1161,6 @@ namespace LinearAlgebra
         int n_local_parts;
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
@@ -1231,7 +1175,6 @@ namespace LinearAlgebra
         int overlap;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
@@ -1261,7 +1204,6 @@ namespace LinearAlgebra
     public:
       /**
        * @brief The set of additional parameters to tune the preconditioner.
-       *
        */
       struct AdditionalData
       {
@@ -1288,7 +1230,6 @@ namespace LinearAlgebra
         int n_local_parts;
         /**
          * @brief Relaxation damping factor.
-         *
          */
         double omega;
         /**
@@ -1303,7 +1244,6 @@ namespace LinearAlgebra
         int overlap;
         /**
          * @brief Set how often the preconditioner should be applied during vmult() or Tvmult().
-         *
          */
         int n_sweeps;
       };
