d93626e04574f4372bb7bf5a97ed6629bdbc85fe
lucia-agullo-marti <luciaagullo@outlook.com>
2026-01-23 12:39:23 +0000

    Remove unused header includes from step-101

--- examples/step-101/step-101.cc
+++ examples/step-101/step-101.cc
@@ -18,34 +18,12 @@
 
 // First we include header files that we have already used in previous
 // example programs:
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/function.h>
-#include <deal.II/base/tensor.h>
-#include <deal.II/base/utilities.h> //TODO: CHECK
-#include <deal.II/base/exceptions.h>
-
-#include <deal.II/lac/vector.h>
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/sparse_matrix.h>
-#include <deal.II/lac/dynamic_sparsity_pattern.h>
-#include <deal.II/lac/solver_cg.h> //TODO: DELETE?
-#include <deal.II/lac/precondition.h> //TODO: DELETE?
-#include <deal.II/lac/affine_constraints.h>
-
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_description.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_refinement.h> //TODO: DELETE?
 
 #include <deal.II/dofs/dof_handler.h>
-#include <deal.II/dofs/dof_tools.h>
 
-#include <deal.II/fe/fe_values.h>
-
-#include <deal.II/numerics/vector_tools.h>
 #include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/error_estimator.h> //TODO: DELETE?
-#include <deal.II/numerics/matrix_creator.h>
 
 #include <deal.II/fe/fe_system.h>
 #include <deal.II/fe/fe_q.h>
@@ -53,10 +31,9 @@
 // This again is C++:
 #include <fstream>
 #include <iostream>
-//TODO: CHECK, these are possible libraries that I might need
 #include <algorithm>
 #include <cmath>
-#include <numeric>
+#include <limits>
 
 // The last step is as in all previous programs:
 namespace Step101
