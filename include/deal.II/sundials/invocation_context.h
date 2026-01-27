6e58da7b87f5729480099c1d9f520d1ff2b264cf
Vladimir Ivannikov <vladimir.ivannikov@hereon.de>
2026-01-27 10:46:09 +0100

    Move Arkode exception to a separate file
    Introduce InvocationContext
    
    Introduce ARKode stepper interface
    
    Move ARKStep related functionality to the ARKStepper class
    
    Add ARKStepper instantiations
    
    Use FunctionProxy to replicate original interface
    
    Change documentation and modify ctors
    
    Format code and improve documentation
    
    Make FunctionProxy and ARKodeStepper::reinit() private
    
    Pass ARKodeStepper by reference
    
    Improve documentation for ARKodeStepper
    
    Remove default values from 2nd ctor of ARKode::AdditionalData
    
    Move accuracy order parameter to ARKStepper
    
    Remove obsolete comments and fix compilation
    
    Review: fix module guards, simplify CallbackContext, etc

---
+++ include/deal.II/sundials/invocation_context.h
@@ -0,0 +1,58 @@
+// ------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2026 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Part of the source code is dual licensed under Apache-2.0 WITH
+// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+// governing the source code and code contributions can be found in
+// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+//
+// ------------------------------------------------------------------------
+
+
+#ifndef dealii_sundials_invocation_context_h
+#define dealii_sundials_invocation_context_h
+
+#include <deal.II/base/config.h>
+
+
+#ifdef DEAL_II_WITH_SUNDIALS
+
+#  include <sundials/sundials_types.h>
+
+#  include <exception>
+
+#endif // DEAL_II_WITH_SUNDIALS
+
+
+DEAL_II_NAMESPACE_OPEN
+
+#ifdef DEAL_II_WITH_SUNDIALS
+namespace SUNDIALS
+{
+  namespace internal
+  {
+    /**
+     * This struct contains references to the std::exception_ptr @p
+     * pending_exception and SUNContext @p arkode_ctx for SUNDIALS 6.0 and
+     * newer. This is required for situations when an object needs to pass
+     * the context to the objects it uses.
+     */
+    struct InvocationContext
+    {
+      std::exception_ptr &pending_exception;
+#  if DEAL_II_SUNDIALS_VERSION_GTE(6, 0, 0)
+      SUNContext &arkode_ctx;
+#  endif
+    };
+  } // namespace internal
+} // namespace SUNDIALS
+
+#endif // DEAL_II_WITH_SUNDIALS
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
