d46f5c199f8290f57f2f3d1ffe4612157a0dc20e
Wolfgang Bangerth <bangerth@colostate.edu>
2026-01-30 12:34:41 -0800

    Better document how we save parent indices.
    While there, also fix a small mistake.

--- source/grid/tria.cc
+++ source/grid/tria.cc
@@ -4937,6 +4937,11 @@ namespace internal
             subcells[i]->set_manifold_id(cell->manifold_id());
             subcells[i]->set_subdomain_id(subdomainid);
 
+            // We only store the parent for every second cell. That's because
+            // cells are created during refinement in multiples of two, and so
+            // two successive cells always share the same parent. As a
+            // consequence, we can save a bit of work by skipping setting parent
+            // indices for the odd children.
             if (i % 2 == 0)
               subcells[i]->set_parent(cell->index());
           }
@@ -5362,6 +5367,11 @@ namespace internal
                 subcells[i]->set_combined_face_orientation(
                   face_no, inherited_orientations[child_lines[i][face_no]]);
 
+              // We only store the parent for every second cell. That's because
+              // cells are created during refinement in multiples of two, and so
+              // two successive cells always share the same parent. As a
+              // consequence, we can save a bit of work by skipping setting
+              // parent indices for the odd children.
               if (i % 2 == 0)
                 subcells[i]->set_parent(cell->index());
             }
@@ -5561,6 +5571,11 @@ namespace internal
                   if (dim == spacedim - 1)
                     first_child->set_direction_flag(cell->direction_flag());
 
+                  // We only store the parent for every second cell. That's
+                  // because cells are created during refinement in multiples of
+                  // two, and so two successive cells always share the same
+                  // parent. As a consequence, we can save a bit of work by
+                  // skipping setting parent indices for the second child.
                   first_child->set_parent(cell->index());
 
                   // Set manifold id of the right face. Only do this
@@ -6726,9 +6741,13 @@ namespace internal
                       new_cell->set_manifold_id(cell->manifold_id());
                       new_cell->set_subdomain_id(cell->subdomain_id());
 
-                      // Why every other? See
-                      // TriangulationImplementation::TriaLevel::parents
-                      if (i % 2)
+                      // We only store the parent for every second cell. That's
+                      // because cells are created during refinement in
+                      // multiples of two, and so two successive cells always
+                      // share the same parent. As a consequence, we can save a
+                      // bit of work by skipping setting parent indices for the
+                      // odd children.
+                      if (i % 2 == 0)
                         new_cell->set_parent(cell->index());
 
                       // set the orientation flag to its default state for all
@@ -8944,8 +8963,13 @@ namespace internal
                       new_hexes[i]->set_manifold_id(hex->manifold_id());
                       new_hexes[i]->set_subdomain_id(subdomainid);
 
-                      if (i % 2)
+                      // We only store the parent for every second cell. That's
+                      // because cells are created during refinement in
+                      // multiples of two, and so two successive cells always
+                      // share the same parent.
+                      if (i % 2 == 0)
                         new_hexes[i]->set_parent(hex->index());
+
                       // set the face_orientation flag to true for all
                       // faces initially, as this is the default value
                       // which is true for all faces interior to the
