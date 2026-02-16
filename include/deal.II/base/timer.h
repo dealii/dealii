54b0ee3c9b166fe15780f3868f24d68a214bf596
Matthias Maier <tamiko@43-1.org>
2026-02-16 11:04:48 -0600

    include: update license headers

--- include/deal.II/base/timer.h
+++ include/deal.II/base/timer.h
@@ -1,16 +1,14 @@
-// ------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 //
-// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 // Copyright (C) 1998 - 2025 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
-// Part of the source code is dual licensed under Apache-2.0 WITH
-// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
-// governing the source code and code contributions can be found in
-// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+// Detailed license information governing the source code and contributions
+// can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
 //
-// ------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #ifndef dealii_timer_h
 #define dealii_timer_h
