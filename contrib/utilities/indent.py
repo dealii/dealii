6b470a8e5dedf2b1764997ce2aa377d480622ccd
Matthias Maier <tamiko@43-1.org>
2026-02-16 11:06:31 -0600

    contrib: update license headers

--- contrib/utilities/indent.py
+++ contrib/utilities/indent.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ##
-## SPDX-License-Identifier: LGPL-2.1-or-later
+## SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 ## Copyright (C) 2019 - 2025 by the deal.II authors
 ##
 ## This file is part of the deal.II library.
 ##
-## Part of the source code is dual licensed under Apache-2.0 WITH
-## LLVM-exception OR LGPL-2.1-or-later. Detailed license information
-## governing the source code and code contributions can be found in
-## LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+## Detailed license information governing the source code and contributions
+## can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
 ##
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 """This script can be used to format code in external projects
 (that use deal.II) by adding .clang_format files.
 
--- contrib/utilities/prm_tools.py
+++ contrib/utilities/prm_tools.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ##
-## SPDX-License-Identifier: LGPL-2.1-or-later
+## SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 ## Copyright (C) 2016 - 2025 by the deal.II authors
 ##
 ## This file is part of the deal.II library.
 ##
-## Part of the source code is dual licensed under Apache-2.0 WITH
-## LLVM-exception OR LGPL-2.1-or-later. Detailed license information
-## governing the source code and code contributions can be found in
-## LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+## Detailed license information governing the source code and contributions
+## can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
 ##
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 
 """Routines to read, format and write deal.II parameter input files (.prm files)"""
 
