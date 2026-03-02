7efd85fb3f6c4b60da5c6d6376ae46e1875d7f79
Lukas Dreyer <dreyer@ifam.uni-hannover.de>
2026-03-02 18:22:30 +0100

    AMR: remove t8code reference

--- include/deal.II/distributed/amr.h
+++ include/deal.II/distributed/amr.h
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 //
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
-// Copyright (C) 2016 - 2025 by the deal.II authors
+// Copyright (C) 2016 - 2026 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
@@ -16,21 +16,15 @@
 #ifdef DEAL_II_WITH_P4EST
 #  include <deal.II/distributed/p4est_wrappers.h>
 #endif // DEAL_II_WITH_P4EST
-#ifdef DEAL_II_WITH_T8CODE
-#  include <deal.II/distributed/t8code_wrappers.h>
-#endif // DEAL_II_WITH_T8CODE
-
 
 namespace amr
 {
-#if defined(DEAL_II_WITH_T8CODE)
-  using namespace dealii::internal::t8code;
-#elif defined(DEAL_II_WITH_P4EST)
+#if defined(DEAL_II_WITH_P4EST)
   using namespace dealii::internal::p4est;
 #else
-#  error DEAL_II_WITH_P4EST or DEAL_II_WITH_T8CODE required
+#  error DEAL_II_WITH_P4EST required
 #endif
 } // namespace amr
 
 
-#endif // dealii_p4est_wrappers_h
+#endif // amr_h
