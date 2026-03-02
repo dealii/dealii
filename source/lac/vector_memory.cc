8d7baee15504ce2ca2ddd651fe0e0474f9a6b825
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2026-03-02 16:12:43 +0100

    Add missing vector memory instantiations

--- source/lac/vector_memory.cc
+++ source/lac/vector_memory.cc
@@ -35,6 +35,14 @@ template class GrowingVectorMemory<
   LinearAlgebra::distributed::Vector<float, MemorySpace::Default>>;
 template class GrowingVectorMemory<
   LinearAlgebra::distributed::Vector<double, MemorySpace::Default>>;
+template class VectorMemory<
+  LinearAlgebra::distributed::BlockVector<float, MemorySpace::Default>>;
+template class VectorMemory<
+  LinearAlgebra::distributed::BlockVector<double, MemorySpace::Default>>;
+template class GrowingVectorMemory<
+  LinearAlgebra::distributed::BlockVector<float, MemorySpace::Default>>;
+template class GrowingVectorMemory<
+  LinearAlgebra::distributed::BlockVector<double, MemorySpace::Default>>;
 
 namespace internal
 {
@@ -50,6 +58,13 @@ namespace internal
       dealii::GrowingVectorMemory<dealii::LinearAlgebra::distributed::Vector<
         double,
         MemorySpace::Default>>::release_unused_memory();
+
+      dealii::GrowingVectorMemory<
+        dealii::LinearAlgebra::distributed::
+          BlockVector<float, MemorySpace::Default>>::release_unused_memory();
+      dealii::GrowingVectorMemory<
+        dealii::LinearAlgebra::distributed::
+          BlockVector<double, MemorySpace::Default>>::release_unused_memory();
     }
   } // namespace GrowingVectorMemoryImplementation
 } // namespace internal
