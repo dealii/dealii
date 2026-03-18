8c0ee03763ac4e20800c8a41709f6e1d8e78b76f
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2026-03-18 14:57:57 +0100

    Make empty matrix work

--- tests/trilinos_tpetra/n_nonzeros_03.cc
+++ tests/trilinos_tpetra/n_nonzeros_03.cc
@@ -41,10 +41,19 @@ test()
   Table<2, DoFTools::Coupling> coupling(1, 1);
   coupling.fill(DoFTools::none);
 
+  const std::vector<types::global_dof_index> dofs_per_block = DoFTools::count_dofs_per_fe_block (dof_handler,
+                                                                            {{0}});
+
+  const IndexSet &locally_owned_dofs = dof_handler.locally_owned_dofs();
+  const IndexSet  locally_relevant_dofs =
+    DoFTools::extract_locally_relevant_dofs(dof_handler);
+
+  const std::vector<IndexSet> system_partitioning = locally_owned_dofs.split_by_block(dofs_per_block);
+  const std::vector<IndexSet> system_relevant_partitioning = locally_relevant_dofs.split_by_block(dofs_per_block);
+
   // create an empty sparsity pattern
-  dealii::LinearAlgebra::TpetraWrappers::BlockSparsityPattern sparsity;
-  sparsity.reinit(std::vector<unsigned int>(1, dof_handler.n_dofs()),
-                  std::vector<unsigned int>(1, dof_handler.n_dofs()));
+  dealii::LinearAlgebra::TpetraWrappers::BlockSparsityPattern sparsity(system_relevant_partitioning, MPI_COMM_WORLD);
+
   DoFTools::make_sparsity_pattern(dof_handler,
                                   coupling,
                                   sparsity,
