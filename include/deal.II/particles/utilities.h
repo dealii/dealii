307f54b792800e1b2686e07b201ac72e7326ad83
Bruno Blais <blais.bruno@gmail.com>
2026-04-14 11:49:44 +0200

    Add a fast version of thet interpolate_on_particle

--- include/deal.II/particles/utilities.h
+++ include/deal.II/particles/utilities.h
@@ -31,6 +31,7 @@
 
 #include <deal.II/particles/particle_handler.h>
 
+#include <deal.II/matrix_free/fe_point_evaluation.h>
 
 DEAL_II_NAMESPACE_OPEN
 
@@ -243,6 +244,138 @@ namespace Particles
             }
         }
       interpolated_field.compress(VectorOperation::add);
+    }
+
+
+    /**
+     * Given a DoFHandler and a ParticleHandler, interpolate a vector field
+     * at the position of the particles. The result is stored in an output
+     * vector whose size corresponds to the number of locally owned particles *
+     * number of active components. This version relies on FePointEvaluator
+     * to interpolate the field at the particle location efficiently. The drawback
+     * is that this requires knowledge of the number of components at compile time.
+     *
+     * @param[in] field_dh The DOF Handler which was used to generate the
+     * field vector that is to be interpolated.
+     *
+     * @param[in] particle_handler The particle handler whose particle serve as
+     * the interpolation points.
+     *
+     * @param[in] field_vector The vector of the field to be interpolated. This
+     * vector must be coherent with the dof_handler provided
+     *
+     * @param[in,out] interpolated_field The interpolated value of the field at
+     * the position of the particles. The size of the vector must be
+     * n_locally_owned_particles times the n_components
+     *
+     * @param[in] field_comps An optional component mask that decides which
+     * subset of the vector fields are interpolated
+     */
+
+    template <int n_components,
+                int dim,
+                  int spacedim,
+                  typename InputVectorType,
+                  typename OutputVectorType>
+    void
+    interpolate_field_on_particles_fast(
+      const DoFHandler<dim, spacedim>                 &field_dh,
+      const Particles::ParticleHandler<dim, spacedim> &particle_handler,
+      const InputVectorType                           &field_vector,
+      OutputVectorType                                &interpolated_field,
+      const ComponentMask                             &field_comps = {},
+      const Mapping<dim, spacedim> &mapping =
+  (ReferenceCells::get_hypercube<dim>()
+#ifndef _MSC_VER
+     .template get_default_linear_mapping<spacedim>()
+#else
+     .ReferenceCell<dim>::get_default_linear_mapping<spacedim>()
+#endif
+     )
+     )
+    {
+      if (particle_handler.n_locally_owned_particles() == 0)
+        {
+          interpolated_field.compress(VectorOperation::add);
+          return; // nothing else to do here
+        }
+
+      const auto &fe       = field_dh.get_fe();
+      auto        particle = particle_handler.begin();
+
+      // Take care of components
+      const ComponentMask comps =
+        (field_comps.size() == 0 ? ComponentMask(n_components, true) :
+                                   field_comps);
+      AssertDimension(comps.size(), n_components);
+      const auto n_selected_comps = comps.n_selected_components();
+
+      AssertDimension(field_vector.size(), field_dh.n_dofs());
+      AssertDimension(interpolated_field.size(),
+                      particle_handler.get_next_free_particle_index() *
+                        n_selected_comps);
+
+      // Global to local indices
+      std::vector<unsigned int> space_gtl(fe.n_components(),
+                                          numbers::invalid_unsigned_int);
+      for (unsigned int i = 0, j = 0; i < space_gtl.size(); ++i)
+        if (comps[i])
+          space_gtl[i] = j++;
+
+      std::vector<types::global_dof_index> dof_indices(fe.n_dofs_per_cell());
+
+      // Generate an evaluator that will be used to interpolate the fields at the particle location.
+      FEPointEvaluation<n_components, dim, spacedim> evaluator(mapping, fe, update_values);
+      std::vector<Point<dim>>     particle_reference_locations;
+      std::vector<types::particle_index> particle_indices;
+      Vector<double> local_dof_values(fe.dofs_per_cell);
+
+      while (particle != particle_handler.end())
+        {
+          const auto &cell = particle->get_surrounding_cell();
+          const auto &dh_cell =
+            typename DoFHandler<dim, spacedim>::cell_iterator(*cell, &field_dh);
+          dh_cell->get_dof_indices(dof_indices);
+          const auto pic = particle_handler.particles_in_cell(cell);
+          dh_cell->get_dof_values(field_vector, local_dof_values);
+
+          Assert(pic.begin() == particle, ExcInternalError());
+
+          // Gather the reference location  and ids of all particles
+          particle_reference_locations.clear();
+          particle_indices.clear();
+          for (auto &p : pic)
+            {
+              particle_reference_locations.emplace_back(p.get_reference_location());
+              particle_indices.emplace_back(p.get_id());
+            }
+
+          evaluator.reinit(cell, particle_reference_locations);
+          evaluator.evaluate(make_array_view(local_dof_values),
+                             EvaluationFlags::values);
+          for (unsigned int particle_index = 0; particle != pic.end();
+               ++particle, ++particle_index)
+            {
+              const unsigned id = particle_indices[particle_index];
+
+              if constexpr (n_components == 1)
+                {
+                  interpolated_field[id] = evaluator.get_value(particle_index);
+                }
+              else
+                {
+                  unsigned int j_comp =0;
+                  for (unsigned int j = 0; j  < n_components;  ++j)
+                    {
+                      if (field_comps[j])
+                        {
+                          interpolated_field[id * n_selected_comps + j_comp] = evaluator.get_value(particle_index)[j];
+                          j_comp++;
+                        }
+                    }
+                }
+            }
+        }
     }
 
   } // namespace Utilities
