7a7682cdb19e747a25488de0b25b6e54e17c3005
Luca Heltai <luca.heltai@unipi.it>
2026-04-15 17:40:28 +0200

    Fix header.

--- tests/non_matching/coupling_interpolation_02.cc
+++ tests/non_matching/coupling_interpolation_02.cc
@@ -1,17 +1,14 @@
-// ---------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 //
-// Copyright (C) 2019 by the deal.II authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+// Copyright (C) 2018 - 2025 by the deal.II authors
 //
 // This file is part of the deal.II library.
 //
-// The deal.II library is free software; you can use it, redistribute
-// it, and/or modify it under the terms of the GNU Lesser General
-// Public License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-// The full text of the license can be found in the file LICENSE.md at
-// the top level directory of deal.II.
+// Detailed license information governing the source code and contributions
+// can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
 //
-// ---------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 // Construct a sparsity pattern using NonMatching::DoFHandlerCoupling
 
