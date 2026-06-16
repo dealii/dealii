6cdf7d2703ec6fb37c6566d05a21bd0b89336ada
Dominik Still <dominik.still@tum.de>
2026-06-16 15:31:22 +0200

    Use set of pairs for hp_quad_dof_identities

--- source/hp/fe_collection.cc
+++ source/hp/fe_collection.cc
@@ -560,16 +560,29 @@ namespace hp
   template <int dim, int spacedim>
   std::vector<std::map<unsigned int, unsigned int>>
   FECollection<dim, spacedim>::hp_quad_dof_identities(
-    const std::set<unsigned int> &fes,
-    const unsigned int            face_no) const
+    const std::set<std::pair<unsigned int, unsigned int>> &fes_and_faces) const
   {
-    auto query_quad_dof_identities = [this,
-                                      face_no](const unsigned int fe_index_1,
-                                               const unsigned int fe_index_2) {
-      return (*this)[fe_index_1].hp_quad_dof_identities((*this)[fe_index_2],
-                                                        face_no);
-    };
-    return compute_hp_dof_identities(fes, query_quad_dof_identities);
+    // first entry in the pair is the fe_index, the second entry is the face_no
+    std::pair<unsigned int, unsigned int> fe_and_face_1 =
+      *fes_and_faces.begin();
+    std::pair<unsigned int, unsigned int> fe_and_face_2 =
+      *(++fes_and_faces.begin());
+
+    auto query_quad_dof_identities =
+      [this, &fe_and_face_1, &fe_and_face_2](const unsigned int fe_index_1,
+                                             const unsigned int fe_index_2) {
+        // check if fe_index_1 is the same fe index as in fe_and_face_1
+        // then use the corresponding face
+        const unsigned int face_no = fe_index_1 == fe_and_face_1.first ?
+                                       fe_and_face_1.second :
+                                       fe_and_face_2.second;
+        return (*this)[fe_index_1].hp_quad_dof_identities((*this)[fe_index_2],
+                                                          face_no);
+      };
+
+    return compute_hp_dof_identities(
+      std::set<unsigned int>{fe_and_face_1.first, fe_and_face_2.first},
+      query_quad_dof_identities);
   }
 
 
