ad327ca5a9f25f0927c3b1ab671c0224c6bbbae1
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2026-06-24 16:48:21 +0200

    Fix missing instantiation and add missing function.

---
+++ tests/trilinos_tpetra/72.cc
@@ -0,0 +1,106 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2004 - 2026 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+
+// like 38, but check
+// LinearAlgebra::TpetraWrappers::Vector<double>::add(std::vector<types::global_dof_index>,
+// dealii::Vector)
+
+#include <deal.II/base/utilities.h>
+
+#include <deal.II/lac/trilinos_tpetra_vector.h>
+#include <deal.II/lac/vector.h>
+
+#include <iostream>
+#include <vector>
+
+#include "../tests.h"
+
+
+void
+test(LinearAlgebra::TpetraWrappers::Vector<double> &v,
+     ::dealii::Vector<double>                      &w)
+{
+  std::vector<dealii::types::global_dof_index> indices(v.size());
+  for (unsigned int i = 0; i < v.size(); ++i)
+    {
+      indices[i] = i;
+      v(i)       = i;
+      w(i)       = i + 1.;
+    }
+
+  v.compress(VectorOperation::insert);
+  w.compress(VectorOperation::insert);
+
+  v.add(indices, w);
+
+  // make sure we get the expected result
+  for (unsigned int i = 0; i < v.size(); ++i)
+    {
+      AssertThrow(w(i) == i + 1., ExcInternalError());
+      AssertThrow(v(i) == i + (i + 1.), ExcInternalError());
+    }
+
+  deallog << "OK" << std::endl;
+}
+
+
+
+int
+main(int argc, char **argv)
+{
+  initlog();
+
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(
+    argc, argv, testing_max_num_threads());
+
+
+  try
+    {
+      {
+        LinearAlgebra::TpetraWrappers::Vector<double> v;
+        v.reinit(complete_index_set(100), MPI_COMM_WORLD);
+        dealii::Vector<double> w(100);
+        test(v, w);
+      }
+    }
+  catch (const std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    };
+}
