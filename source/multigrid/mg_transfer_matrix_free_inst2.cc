f50aa971b5c3e0354e2ef132120caf804a3f7251
Timo Heister <timo.heister@gmail.com>
2026-06-25 20:42:29 -0400

    split instantiations of mg_tranfer_matrix_free

---
+++ source/multigrid/mg_transfer_matrix_free_inst2.cc
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+// Copyright (C) 2016 - 2025 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Detailed license information governing the source code and contributions
+// can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
+//
+// -----------------------------------------------------------------------------
+
+#include <deal.II/multigrid/mg_transfer_matrix_free.templates.h>
+
+DEAL_II_NAMESPACE_OPEN
+
+#define SPLIT_INSTANTIATIONS_COUNT 3
+#define SPLIT_INSTANTIATIONS_INDEX 1
+
+#include "multigrid/mg_transfer_matrix_free.inst"
+
+DEAL_II_NAMESPACE_CLOSE
