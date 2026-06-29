83f1d479309c57bf8dc95f80c729bffd70873c0e
Musawer Ahmad Saqif <saqif@umich.edu>
2026-06-29 15:54:58 -0600

    Check Boost.Process availability for Gmsh

--- cmake/configure/configure_50_gmsh.cmake
+++ cmake/configure/configure_50_gmsh.cmake
@@ -14,6 +14,63 @@
 # Configuration for the gmsh executable:
 #
 
+set(FEATURE_GMSH_AFTER BOOST)
+
+macro(feature_gmsh_find_external var)
+  feature_find_external(GMSH ${var})
+
+  if(${var})
+    reset_cmake_required()
+    if(DEAL_II_FEATURE_BOOST_BUNDLED_CONFIGURED)
+      list(APPEND CMAKE_REQUIRED_INCLUDES ${BOOST_FOLDER}/include)
+    else()
+      list(APPEND CMAKE_REQUIRED_INCLUDES
+        ${BOOST_INCLUDE_DIRS} ${Boost_INCLUDE_DIRS}
+        )
+      list(APPEND CMAKE_REQUIRED_LIBRARIES
+        ${BOOST_TARGETS} ${BOOST_LIBRARIES} ${Boost_LIBRARIES}
+        )
+    endif()
+
+    CHECK_CXX_SOURCE_COMPILES(
+      "
+      #include <boost/process/args.hpp>
+      #include <boost/process/exe.hpp>
+      #include <boost/process/io.hpp>
+      #include <boost/process/system.hpp>
+
+      #include <string>
+      #include <vector>
+
+      int main()
+      {
+        namespace bp = boost::process;
+
+        const std::vector<std::string> arguments{\"--version\"};
+        return bp::system(bp::exe = \"gmsh\",
+                          bp::args = arguments,
+                          bp::std_out > \"gmsh.log\",
+                          bp::std_err > \"gmsh_warn.log\");
+      }
+      "
+      DEAL_II_GMSH_WITH_BOOST_PROCESS
+      )
+
+    if(NOT DEAL_II_GMSH_WITH_BOOST_PROCESS)
+      message(STATUS
+        "DEAL_II_WITH_GMSH requires a usable Boost.Process setup, "
+        "but a simple compile test failed."
+        )
+      set(GMSH_ADDITIONAL_ERROR_STRING
+        "DEAL_II_WITH_GMSH requires a usable Boost.Process setup, "
+        "but a simple compile test failed.\n"
+        )
+      set(${var} FALSE)
+    endif()
+    reset_cmake_required()
+  endif()
+endmacro()
+
 macro(feature_gmsh_configure_external)
   set(DEAL_II_GMSH_WITH_API ${GMSH_WITH_API})
 endmacro()
