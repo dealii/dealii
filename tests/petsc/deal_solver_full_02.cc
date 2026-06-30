c40e48e1f070d8eab8e3cdc9d4c1c2ed6dde5f79
Martin Kronbichler <martin.kronbichler@rub.de>
2026-06-30 08:37:28 +0200

    Make a test more robust

--- tests/petsc/deal_solver_full_02.cc
+++ tests/petsc/deal_solver_full_02.cc
@@ -69,7 +69,7 @@ main(int argc, char **argv)
     check_solver_within_range(solver.solve(A, u, f, preconditioner),
                               control.last_step(),
                               48,
-                              51);
+                              54);
   }
   GrowingVectorMemory<PETScWrappers::MPI::Vector>::release_unused_memory();
 }
