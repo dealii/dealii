b4dd62ddfacea0d757e9b0e008521e4d74fb5c3c
Jonas Plank <Jonas.jp.plank@gmail.com>
2026-07-07 12:31:20 +0200

    Add ITK for generating level sets from images

---
+++ cmake/configure/configure_50_itk.cmake
@@ -0,0 +1,17 @@
+## -----------------------------------------------------------------------------
+##
+## SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+## Copyright (C) 2012 - 2023 by the deal.II authors
+##
+## This file is part of the deal.II library.
+##
+## Detailed license information governing the source code and contributions
+## can be found in LICENSE.md and CONTRIBUTING.md at the top level directory.
+##
+## -----------------------------------------------------------------------------
+
+#
+# Configuration for the ITK library:
+#
+
+configure_feature(ITK)
